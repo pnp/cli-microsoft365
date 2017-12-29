1cd1e97a44eaa4762afb7b3a431104db0ea74e05
Waldek Mastykarz <waldek@mastykarz.nl>
2017-12-29 11:53:14 +0100

    Added detecting dev version of the CLI to the telemetry

--- src/appInsights.ts
+++ src/appInsights.ts
@@ -1,11 +1,17 @@
 const packageJSON = require('../package.json');
+import * as fs from 'fs';
+import * as path from 'path';
 
 import * as appInsights from 'applicationinsights';
 const config = appInsights.setup('6b908c80-d09f-4cf6-8274-e54349a0149a');
 config.setInternalLogging(false, false);
 appInsights.start();
+// append -dev to the version number when ran locally
+// to distinguish production and dev version of the CLI
+// in the telemetry
+const version: string = `${packageJSON.version}${fs.existsSync(path.join(__dirname, `..${path.sep}src`)) ? '-dev' : ''}`;
 appInsights.defaultClient.commonProperties = {
-  version: packageJSON.version
+  version: version
 };
 
 export default appInsights.defaultClient;
