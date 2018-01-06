bbdc96182ec769e507c3f5024b9341ad67fc173b
Waldek Mastykarz <waldek@mastykarz.nl>
2018-01-06 14:21:03 +0100

    Added the 'spo sitescript add' command solving #65

---
+++ docs/manual/docs/cmd/spo/sitescript/sitescript-add.md
@@ -0,0 +1,42 @@
+# spo sitescript add
+
+Adds site script for use with site designs
+
+## Usage
+
+```sh
+spo sitescript add [options]
+```
+
+## Options
+
+Option|Description
+------|-----------
+`--help`|output usage information
+`-t, --title <title>`|Site script title
+`-c, --content <content>`|JSON string containing the site script
+`-d, --description [description]`|Site script description
+`-o, --output [output]`|Output type. `json|text`. Default `text`
+`--verbose`|Runs command with verbose logging
+`--debug`|Runs command with debug logging
+
+!!! important
+    Before using this command, connect to a SharePoint Online site, using the [spo connect](../connect.md) command.
+
+## Remarks
+
+To add a site script, you have to first connect to a SharePoint site using the [spo connect](../connect.md) command, eg. `spo connect https://contoso.sharepoint.com`.
+
+Each time you execute the `spo sitescript add` command, it will create a new site script with a unique ID. Before creating a site script, be sure that another script with the same name doesn't already exist.
+
+## Examples
+
+Create new site script for use with site designs. Script contents are stored in the `$script` variable
+
+```sh
+spo sitescript add --title "Contoso" --description "Contoso theme script" --content $script
+```
+
+## More information
+
+- SharePoint site design and site script overview: [https://docs.microsoft.com/en-us/sharepoint/dev/declarative-customization/site-design-overview](https://docs.microsoft.com/en-us/sharepoint/dev/declarative-customization/site-design-overview)
---
+++ src/o365/spo/commands/sitescript/sitescript-add.ts
@@ -0,0 +1,168 @@
+import auth from '../../SpoAuth';
+import config from '../../../../config';
+import * as request from 'request-promise-native';
+import commands from '../../commands';
+import {
+  CommandOption, CommandValidate
+} from '../../../../Command';
+import SpoCommand from '../../SpoCommand';
+import Utils from '../../../../Utils';
+import { ContextInfo } from '../../spo';
+import GlobalOptions from '../../../../GlobalOptions';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  title: string;
+  description?: string;
+  content: string;
+}
+
+class SpoSiteScriptAddCommand extends SpoCommand {
+  public get name(): string {
+    return `${commands.SITESCRIPT_ADD}`;
+  }
+
+  public get description(): string {
+    return 'Adds site script for use with site designs';
+  }
+
+  public getTelemetryProperties(args: CommandArgs): any {
+    const telemetryProps: any = super.getTelemetryProperties(args);
+    telemetryProps.description = (!(!args.options.description)).toString();
+    return telemetryProps;
+  }
+
+  public commandAction(cmd: CommandInstance, args: CommandArgs, cb: () => void): void {
+    auth
+      .ensureAccessToken(auth.service.resource, cmd, this.debug)
+      .then((accessToken: string): Promise<ContextInfo> => {
+        if (this.debug) {
+          cmd.log(`Retrieved access token ${accessToken}. Retrieving request digest...`);
+        }
+
+        if (this.verbose) {
+          cmd.log(`Retrieving request digest...`);
+        }
+
+        return this.getRequestDigest(cmd, this.debug);
+      })
+      .then((res: ContextInfo): Promise<string> => {
+        if (this.debug) {
+          cmd.log('Response:')
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        const requestOptions: any = {
+          url: `${auth.site.url}/_api/Microsoft.Sharepoint.Utilities.WebTemplateExtensions.SiteScriptUtility.CreateSiteScript(Title=@title, Description=@description)?@title='${encodeURIComponent(args.options.title)}'&@description='${encodeURIComponent(args.options.description || '')}'`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            'X-RequestDigest': res.FormDigestValue,
+            'content-type': 'application/json;charset=utf-8',
+            accept: 'application/json;odata=nometadata'
+          }),
+          body: JSON.parse(args.options.content),
+          json: true
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.post(requestOptions);
+      })
+      .then((res: any): void => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        cmd.log(res);
+
+        if (this.verbose) {
+          cmd.log(vorpal.chalk.green('DONE'));
+        }
+
+        cb();
+      }, (err: any): void => this.handleRejectedODataJsonPromise(err, cmd, cb));
+  }
+
+  public options(): CommandOption[] {
+    const options: CommandOption[] = [
+      {
+        option: '-t, --title <title>',
+        description: 'Site script title'
+      },
+      {
+        option: '-c, --content <content>',
+        description: 'JSON string containing the site script'
+      },
+      {
+        option: '-d, --description [description]',
+        description: 'Site script description'
+      }
+    ];
+
+    const parentOptions: CommandOption[] = super.options();
+    return options.concat(parentOptions);
+  }
+
+  public validate(): CommandValidate {
+    return (args: CommandArgs): boolean | string => {
+      if (!args.options.title) {
+        return 'Required parameter title missing';
+      }
+
+      if (!args.options.content) {
+        return 'Required parameter content missing';
+      }
+
+      try {
+        JSON.parse(args.options.content);
+      }
+      catch (e) {
+        return `Specified content value is not a valid JSON string. Error: ${e}`;
+      }
+
+      return true;
+    };
+  }
+
+  public commandHelp(args: {}, log: (help: string) => void): void {
+    const chalk = vorpal.chalk;
+    log(vorpal.find(this.name).helpInformation());
+    log(
+      `  ${chalk.yellow('Important:')} before using this command, connect to a SharePoint Online site using the
+      ${chalk.blue(commands.CONNECT)} command.
+        
+  Remarks:
+
+    To add a site script, you have to first connect to a SharePoint site using the ${chalk.blue(commands.CONNECT)} command,
+    eg. ${chalk.grey(`${config.delimiter} ${commands.CONNECT} https://contoso.sharepoint.com`)}.
+
+    Each time you execute the ${chalk.blue(this.name)} command, it will create a new site script
+    with a unique ID. Before creating a site script, be sure that another script with the same name
+    doesn't already exist.
+
+  Examples:
+  
+    Create new site script for use with site designs. Script contents are stored in the ${chalk.grey('$script')} variable
+      ${chalk.grey(config.delimiter)} ${this.name} --title "Contoso" --description "Contoso theme script" --content $script
+
+  More information:
+
+    SharePoint site design and site script overview
+      https://docs.microsoft.com/en-us/sharepoint/dev/declarative-customization/site-design-overview
+`);
+  }
+}
+
+module.exports = new SpoSiteScriptAddCommand();
