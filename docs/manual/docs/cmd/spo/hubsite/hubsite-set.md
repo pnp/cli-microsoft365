39ddc79c4744c574a2f612b4e9b746bffc4de2dd
Waldek Mastykarz <waldek@mastykarz.nl>
2018-01-19 21:48:32 +0100

    Added the 'spo hubsite set' command solving #96

---
+++ docs/manual/docs/cmd/spo/hubsite/hubsite-set.md
@@ -0,0 +1,49 @@
+# spo hubsite set
+
+Updates properties of the specified hub site
+
+## Usage
+
+```sh
+spo hubsite set [options]
+```
+
+## Options
+
+Option|Description
+------|-----------
+`--help`|output usage information
+`-i, --id <id>`|ID of the hub site to update
+`-t, --title [title]`|The new title for the hub site
+`-d, --description [description]`|The new description for the hub site
+`-l, --logoUrl [logoUrl]`|The URL of the new logo for the hub site
+`-o, --output [output]`|Output type. `json|text`. Default `text`
+`--verbose`|Runs command with verbose logging
+`--debug`|Runs command with debug logging
+
+!!! important
+    Before using this command, connect to a SharePoint Online tenant admin site, using the [spo connect](../connect.md) command.
+
+## Remarks
+
+To update hub site's properties, you have to first connect to a tenant admin site using the [spo connect](../connect.md) command, eg. `spo connect https://contoso-admin.sharepoint.com`.
+
+If the specified `id` doesn't refer to an existing hub site, you will get an `Unknown Error` error.
+
+## Examples
+
+Update hub site's title
+
+```sh
+spo hubsite set --id 255a50b2-527f-4413-8485-57f4c17a24d1 --title Sales
+```
+
+Update hub site's title and description
+
+```sh
+spo hubsite set --id 255a50b2-527f-4413-8485-57f4c17a24d1 --title Sales --description "All things sales"
+```
+
+## More information
+
+- SharePoint hub sites new in Office 365: [https://techcommunity.microsoft.com/t5/SharePoint-Blog/SharePoint-hub-sites-new-in-Office-365/ba-p/109547](https://techcommunity.microsoft.com/t5/SharePoint-Blog/SharePoint-hub-sites-new-in-Office-365/ba-p/109547)
---
+++ src/o365/spo/commands/hubsite/hubsite-set.ts
@@ -0,0 +1,211 @@
+import auth from '../../SpoAuth';
+import { ContextInfo, ClientSvcResponse, ClientSvcResponseContents } from '../../spo';
+import config from '../../../../config';
+import * as request from 'request-promise-native';
+import commands from '../../commands';
+import GlobalOptions from '../../../../GlobalOptions';
+import {
+  CommandOption,
+  CommandValidate,
+  CommandError,
+  CommandTypes
+} from '../../../../Command';
+import SpoCommand from '../../SpoCommand';
+import Utils from '../../../../Utils';
+import { HubSiteProperties } from './HubSiteProperties';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  id: string;
+  title?: string;
+  description?: string;
+  logoUrl?: string;
+}
+
+class SpoHubSiteSetCommand extends SpoCommand {
+  public get name(): string {
+    return commands.HUBSITE_SET;
+  }
+
+  public get description(): string {
+    return 'Updates properties of the specified hub site';
+  }
+
+  public getTelemetryProperties(args: CommandArgs): any {
+    const telemetryProps: any = super.getTelemetryProperties(args);
+    telemetryProps.title = typeof args.options.title === 'string';
+    telemetryProps.description = typeof args.options.description === 'string'
+    telemetryProps.logoUrl = typeof args.options.logoUrl === 'string'
+    return telemetryProps;
+  }
+
+  protected requiresTenantAdmin(): boolean {
+    return true;
+  }
+
+  public commandAction(cmd: CommandInstance, args: CommandArgs, cb: () => void): void {
+    if (this.debug) {
+      cmd.log(`Retrieving access token for ${auth.service.resource}...`);
+    }
+
+    auth
+      .ensureAccessToken(auth.service.resource, cmd, this.debug)
+      .then((accessToken: string): Promise<ContextInfo> => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(accessToken);
+          cmd.log('');
+        }
+
+        return this.getRequestDigest(cmd, this.debug);
+      })
+      .then((res: ContextInfo): Promise<string> => {
+        if (this.debug) {
+          cmd.log('Response:')
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        if (this.verbose) {
+          cmd.log(`Updating hub site ${args.options.id}...`);
+        }
+
+        const title: string = typeof args.options.title === 'string' ? `<SetProperty Id="13" ObjectPathId="10" Name="Title"><Parameter Type="String">${Utils.escapeXml(args.options.title)}</Parameter></SetProperty>` : '';
+        const description: string = typeof args.options.description === 'string' ? `<SetProperty Id="15" ObjectPathId="10" Name="Description"><Parameter Type="String">${Utils.escapeXml(args.options.description)}</Parameter></SetProperty>` : '';
+        const logoUrl: string = typeof args.options.logoUrl === 'string' ? `<SetProperty Id="14" ObjectPathId="10" Name="LogoUrl"><Parameter Type="String">${Utils.escapeXml(args.options.logoUrl)}</Parameter></SetProperty>` : '';
+
+        const requestOptions: any = {
+          url: `${auth.site.url}/_vti_bin/client.svc/ProcessQuery`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            'X-RequestDigest': res.FormDigestValue
+          }),
+          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="9" ObjectPathId="8" /><ObjectPath Id="11" ObjectPathId="10" /><Query Id="12" ObjectPathId="10"><Query SelectAllProperties="true"><Properties /></Query></Query>${title}${logoUrl}${description}<Method Name="Update" Id="16" ObjectPathId="10" /></Actions><ObjectPaths><Constructor Id="8" TypeId="{268004ae-ef6b-4e9b-8425-127220d84719}" /><Method Id="10" ParentId="8" Name="GetHubSitePropertiesById"><Parameters><Parameter Type="Guid">${Utils.escapeXml(args.options.id)}</Parameter></Parameters></Method></ObjectPaths></Request>`
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.post(requestOptions);
+      })
+      .then((res: string): void => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        const json: ClientSvcResponse = JSON.parse(res);
+        const response: ClientSvcResponseContents = json[0];
+        if (response.ErrorInfo) {
+          cmd.log(new CommandError(response.ErrorInfo.ErrorMessage));
+        }
+        else {
+          const hubSite: HubSiteProperties = json.pop();
+          delete hubSite._ObjectType_;
+
+          hubSite.ID = hubSite.ID.replace('/Guid(','').replace(')/','');
+          hubSite.SiteId = hubSite.SiteId.replace('/Guid(','').replace(')/','');
+
+          cmd.log(hubSite);
+
+          if (this.verbose) {
+            cmd.log(vorpal.chalk.green('DONE'));
+          }
+        }
+        cb();
+      }, (err: any): void => this.handleRejectedPromise(err, cmd, cb));
+  }
+
+  public options(): CommandOption[] {
+    const options: CommandOption[] = [
+      {
+        option: '-i, --id <id>',
+        description: 'ID of the hub site to update'
+      },
+      {
+        option: '-t, --title [title]',
+        description: 'The new title for the hub site'
+      },
+      {
+        option: '-d, --description [description]',
+        description: 'The new description for the hub site'
+      },
+      {
+        option: '-l, --logoUrl [logoUrl]',
+        description: 'The URL of the new logo for the hub site'
+      }
+    ];
+
+    const parentOptions: CommandOption[] = super.options();
+    return options.concat(parentOptions);
+  }
+
+  public validate(): CommandValidate {
+    return (args: CommandArgs): boolean | string => {
+      if (!args.options.id) {
+        return 'Required option id missing';
+      }
+
+      if (!Utils.isValidGuid(args.options.id)) {
+        return `${args.options.id} is not a valid GUID`;
+      }
+
+      if (!args.options.title &&
+        !args.options.description &&
+        !args.options.logoUrl) {
+        return 'Specify title, description or logoUrl to update';
+      }
+
+      return true;
+    };
+  }
+
+  public types(): CommandTypes {
+    // required to support passing empty strings as valid values
+    return {
+      string: ['t', 'title', 'd', 'description', 'l', 'logoUrl']
+    }
+  }
+
+  public commandHelp(args: CommandArgs, log: (help: string) => void): void {
+    const chalk = vorpal.chalk;
+    log(vorpal.find(commands.HUBSITE_SET).helpInformation());
+    log(
+      `  ${chalk.yellow('Important:')} before using this command, connect to a SharePoint Online tenant
+    admin site, using the ${chalk.blue(commands.CONNECT)} command.
+        
+  Remarks:
+
+    To update hub site's properties, you have to first connect to a tenant admin
+    site using the ${chalk.blue(commands.CONNECT)} command, eg.
+    ${chalk.grey(`${config.delimiter} ${commands.CONNECT} https://contoso-admin.sharepoint.com`)}.
+
+    If the specified ${chalk.grey('id')} doesn't refer to an existing hub site, you will get
+    an ${chalk.grey('Unknown Error')} error.
+
+  Examples:
+  
+    Update hub site's title
+      ${chalk.grey(config.delimiter)} ${this.name} --id 255a50b2-527f-4413-8485-57f4c17a24d1 --title Sales
+
+    Update hub site's title and description
+      ${chalk.grey(config.delimiter)} ${this.name} --id 255a50b2-527f-4413-8485-57f4c17a24d1 --title Sales --description "All things sales"
+
+  More information:
+
+    SharePoint hub sites new in Office 365
+      https://techcommunity.microsoft.com/t5/SharePoint-Blog/SharePoint-hub-sites-new-in-Office-365/ba-p/109547
+`);
+  }
+}
+
+module.exports = new SpoHubSiteSetCommand();
