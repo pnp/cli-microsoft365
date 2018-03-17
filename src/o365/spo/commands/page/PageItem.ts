fa01c8bcac62550e968f12a8099e10f2f6d84d27
Waldek Mastykarz <waldek@mastykarz.nl>
2018-03-17 14:32:07 +0100

    Added the 'spo page get' command solving #360

---
+++ src/o365/spo/commands/page/PageItem.ts
@@ -0,0 +1,15 @@
+export interface PageItem {
+  ListItemAllFields: {
+    _ModerationStatus: string;
+    BannerImageUrl: string;
+    ClientSideApplicationId: string;
+    CommentsDisabled: boolean;
+    CanvasContent1: string;
+    ContentTypeId: string;
+    FileLeafRef: string;
+    FirstPublishedDate: string;
+    PageLayoutType: string;
+    PromotedState: string;
+    Title: string;
+  }
+}
