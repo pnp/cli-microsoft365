797f749802f1197826e502c6531328dc8a80c96f
Robert Jaakke <robert.jaakke@mavention.nl>
2018-04-04 18:20:27 +0200

    Implemented hubsite data get command

---
+++ src/o365/spo/commands/hubsite/hubsite-data-get.ts
@@ -0,0 +1,147 @@
+import auth from '../../SpoAuth';
+import config from '../../../../config';
+import * as request from 'request-promise-native';
+import commands from '../../commands';
+import {
+  CommandOption, CommandValidate
+} from '../../../../Command';
+import SpoCommand from '../../SpoCommand';
+import Utils from '../../../../Utils';
+import GlobalOptions from '../../../../GlobalOptions';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  webUrl: string;
+  forceRefresh?: boolean;
+}
+
+class SpoHubSiteDataGetCommand extends SpoCommand {
+  public get name(): string {
+    return `${commands.HUBSITE_DATA_GET}`;
+  }
+
+  public get description(): string {
+    return 'Get hub site data for the specified site';
+  }
+
+  public commandAction(cmd: CommandInstance, args: CommandArgs, cb: () => void): void {
+    auth
+      .ensureAccessToken(auth.service.resource, cmd, this.debug)
+      .then((accessToken: string): request.RequestPromise => {
+        if (this.debug) {
+          cmd.log(`Retrieved access token ${accessToken}. Retrieving request digest...`);
+        }
+
+        let forceRefresh = false;
+
+        if (args.options.forceRefresh) {
+          forceRefresh = true;
+        }
+
+        const requestOptions: any = {
+          url: `${args.options.webUrl}/_api/web/HubSiteData(${forceRefresh})`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            accept: 'application/json;odata=nometadata'
+            //accept: 'application/json;odata=verbose'
+          }),
+          json: true
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.get(requestOptions);
+      })
+      .then((res: any): void => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(JSON.stringify(res, null, 4));
+          cmd.log('');
+        }
+
+        cmd.log(res);
+
+        if (this.verbose) {
+          cmd.log(vorpal.chalk.green('DONE'));
+        }
+
+        cb();
+      }, (err: any): void => this.handleRejectedODataJsonPromise(err, cmd, cb));
+  }
+
+  public options(): CommandOption[] {
+    const options: CommandOption[] = [
+      {
+        option: '-u, --webUrl <webUrl>',
+        description: 'URL of the site for which to retrieve hub site data'
+      },
+      {
+        option: '-f, --forceRefresh',
+        description: `Default value is false. If false, data is returned from the server's cache. If true,
+                       the cache is refreshed with latest updates. Use this if you just made changes and
+                        need to see the changes right away.`
+      }
+    ];
+
+    const parentOptions: CommandOption[] = super.options();
+    return options.concat(parentOptions);
+  }
+
+  public validate(): CommandValidate {
+    return (args: CommandArgs): boolean | string => {
+      if (!args.options.webUrl) {
+        return 'Required parameter webUrl missing';
+      }
+
+      const isValidSharePointUrl: boolean | string = SpoCommand.isValidSharePointUrl(args.options.webUrl);
+      if (isValidSharePointUrl !== true) {
+        return isValidSharePointUrl;
+      }
+
+      return true;
+    };
+  }
+
+  public commandHelp(args: {}, log: (help: string) => void): void {
+    const chalk = vorpal.chalk;
+    log(vorpal.find(this.name).helpInformation());
+    log(
+      `  ${chalk.yellow('Important:')} before using this command, connect to a SharePoint Online site using
+    the ${chalk.blue(commands.CONNECT)} command.
+        
+  Remarks:
+
+    ${chalk.yellow('Attention:')} This command is based on a SharePoint API that is currently
+    in preview and is subject to change once the API reached general
+    availability.
+
+    To get hub site data for a web, you have to first connect to
+    a SharePoint site using the ${chalk.blue(commands.CONNECT)} command,
+    eg. ${chalk.grey(`${config.delimiter} ${commands.CONNECT} https://contoso.sharepoint.com`)}.
+
+    If the specified ${chalk.grey('webUrl')} doesn't refer to an existing hub site, you will get
+    a ${chalk.grey('odata.null: true')} message.
+
+  Examples:
+  
+    Get information about the hub site data for a web with Url ${chalk.grey('https://contoso.sharepoint.com/sites/project-x')}
+    ${chalk.grey(config.delimiter)} ${this.name} --webUrl https://contoso.sharepoint.com/sites/project-x
+
+  More information:
+
+    SharePoint hub sites new in Office 365
+      https://techcommunity.microsoft.com/t5/SharePoint-Blog/SharePoint-hub-sites-new-in-Office-365/ba-p/109547
+`);
+  }
+}
+
+module.exports = new SpoHubSiteDataGetCommand();
