5e5b1204556d6b8a9a57d29a8fa8e104e8d8e73a
Waldek Mastykarz <waldek@mastykarz.nl>
2018-04-26 21:00:58 +0200

    Extended telemetry for tracking users and sessions

--- src/appInsights.spec.ts
+++ src/appInsights.spec.ts
@@ -2,10 +2,15 @@ import * as sinon from 'sinon';
 import * as assert from 'assert';
 import Utils from './Utils';
 import * as fs from 'fs';
+import { getUserId } from './appInsights';
 
 describe('appInsights', () => {
   afterEach(() => {
-    Utils.restore(fs.existsSync);
+    Utils.restore([
+      fs.existsSync,
+      fs.readFileSync,
+      fs.writeFileSync
+    ]);
     delete require.cache[require.resolve('./appInsights')];
   });
 
@@ -19,5 +24,57 @@ describe('appInsights', () => {
     sinon.stub(fs, 'existsSync').callsFake(() => false);
     const i: any = require('./appInsights');
     assert(i.default.commonProperties.version.indexOf('-dev') === -1);
+  });
+
+  it('reads the previously stored user id', () => {
+    sinon.stub(fs, 'existsSync').callsFake(() => true);
+    sinon.stub(fs, 'readFileSync').callsFake((path, encoding) => 'abc');
+    assert.equal(getUserId(), 'abc');
+  });
+
+  it('generates new user id if the user file is empty', () => {
+    sinon.stub(fs, 'existsSync').callsFake(() => true);
+    sinon.stub(fs, 'readFileSync').callsFake(() => '');
+    assert.notEqual(getUserId(), '');
+  });
+
+  it('generates new user id if the user file doesn\'t exist', () => {
+    sinon.stub(fs, 'existsSync').callsFake(() => false);
+    assert.notEqual(getUserId(), '');
+  });
+
+  it('writes newly generated user id, when the user file doesn\'t exist', () => {
+    sinon.stub(fs, 'existsSync').callsFake(() => false);
+    const writeFileSyncStub = sinon.stub(fs, 'writeFileSync').callsFake(() => { });
+    getUserId();
+    assert(writeFileSyncStub.called);
+  });
+
+  it('writes newly generated user id, when the user file is empty', () => {
+    sinon.stub(fs, 'existsSync').callsFake(() => true);
+    sinon.stub(fs, 'readFileSync').callsFake(() => '');
+    const writeFileSyncStub = sinon.stub(fs, 'writeFileSync').callsFake(() => { });
+    getUserId();
+    assert(writeFileSyncStub.called);
+  });
+
+  it('doesn\'t write user id, when one is already stored in the user file', () => {
+    sinon.stub(fs, 'existsSync').callsFake(() => true);
+    sinon.stub(fs, 'readFileSync').callsFake(() => 'abc');
+    const writeFileSyncStub = sinon.stub(fs, 'writeFileSync').callsFake(() => { });
+    getUserId();
+    assert(writeFileSyncStub.notCalled);
+  });
+
+  it('doesn\'t fail when reading the user file throws an exception', () => {
+    sinon.stub(fs, 'existsSync').callsFake(() => true);
+    sinon.stub(fs, 'readFileSync').callsFake(() => { throw new Error; });
+    assert.notEqual(getUserId(), '');
+  });
+
+  it('doesn\'t fail when writing the user file throws an exception', () => {
+    sinon.stub(fs, 'existsSync').callsFake(() => false);
+    sinon.stub(fs, 'writeFileSync').callsFake(() => { throw new Error; });
+    assert.notEqual(getUserId(), '');
   });
 })
--- src/appInsights.ts
+++ src/appInsights.ts
@@ -1,6 +1,7 @@
 const packageJSON = require('../package.json');
 import * as fs from 'fs';
 import * as path from 'path';
+import * as crypto from 'crypto';
 
 import * as appInsights from 'applicationinsights';
 const config = appInsights.setup('6b908c80-d09f-4cf6-8274-e54349a0149a');
@@ -13,5 +14,29 @@ const version: string = `${packageJSON.version}${fs.existsSync(path.join(__dirna
 appInsights.defaultClient.commonProperties = {
   version: version
 };
+appInsights.defaultClient.context.tags['ai.session.id'] = crypto.randomBytes(24).toString('base64');
+appInsights.defaultClient.context.tags['ai.user.id'] = getUserId();
+
+export function getUserId(): string {
+  const filePath: string = path.join(__dirname, `..${path.sep}.user`);
+  let userId: string = '';
+
+  try {
+    if (fs.existsSync(filePath)) {
+      userId = fs.readFileSync(filePath, 'utf-8');
+    }
+  }
+  catch { }
+
+  if (!userId) {
+    userId = crypto.randomBytes(24).toString('base64');
+    try {
+      fs.writeFileSync(filePath, userId);
+    }
+    catch { }
+  }
+
+  return userId;
+}
 
 export default appInsights.defaultClient;
