eac8d40736017e6c7d549b9465b220723f3a8a8b
Waldek Mastykarz <waldek@mastykarz.nl>
2018-06-18 21:29:38 +0200

    Added support for upgrading SPFx@1.3.0 projects solving #483

---
+++ src/o365/spfx/commands/project/project-upgrade/test-projects/spfx-130-webpart-react/src/webparts/helloWorld/components/HelloWorld.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import styles from './HelloWorld.module.scss';
+import { IHelloWorldProps } from './IHelloWorldProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
+  public render(): React.ReactElement<IHelloWorldProps> {
+    return (
+      <div className={styles.helloWorld}>
+        <div className={styles.container}>
+          <div className={`ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}`}>
+            <div className="ms-Grid-col ms-lg10 ms-xl8 ms-xlPush2 ms-lgPush1">
+              <span className="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
+              <p className="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
+              <p className="ms-font-l ms-fontColor-white">{escape(this.props.description)}</p>
+              <a href="https://aka.ms/spfx" className={styles.button}>
+                <span className={styles.label}>Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/o365/spfx/commands/project/project-upgrade/upgrade-1.3.1.ts
@@ -0,0 +1,5 @@
+import { FN010001_YORC_version } from "./rules/FN010001_YORC_version";
+
+module.exports = [
+  new FN010001_YORC_version('1.3.1'),
+];
