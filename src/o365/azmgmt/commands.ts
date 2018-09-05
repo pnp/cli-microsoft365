dc551e2061337b398e3b8023813931552e37a52f
Arjen Bloemsma <a.bloemsma@gmail.com>
2018-09-05 21:32:46 +0200

    Added azmgmt flow run list command

--- src/o365/azmgmt/commands.ts
+++ src/o365/azmgmt/commands.ts
@@ -7,5 +7,6 @@ export default {
   FLOW_ENVIRONMENT_LIST: `${prefix} flow environment list`,
   FLOW_GET: `${prefix} flow get`,
   FLOW_LIST: `${prefix} flow list`,
+  FLOW_RUN_LIST: `${prefix} flow run list`,
   STATUS: `${prefix} status`
 };
---
+++ src/o365/azmgmt/commands/flow/flow-run-list.ts
@@ -0,0 +1,142 @@
+import auth from '../../AzmgmtAuth';
+import config from '../../../../config';
+import commands from '../../commands';
+import GlobalOptions from '../../../../GlobalOptions';
+import {
+  CommandOption,
+  CommandValidate
+} from '../../../../Command';
+import * as request from 'request-promise-native';
+import Utils from '../../../../Utils';
+import AzmgmtCommand from '../../AzmgmtCommand';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  environment: string;
+  name: string;
+}
+
+class AzmgmtFlowRunListCommand extends AzmgmtCommand {
+  public get name(): string {
+    return commands.FLOW_RUN_LIST;
+  }
+
+  public get description(): string {
+    return 'Lists runs of the specified Microsoft Flow';
+  }
+
+  public commandAction(cmd: CommandInstance, args: CommandArgs, cb: () => void): void {
+    auth
+      .ensureAccessToken(auth.service.resource, cmd, this.debug)
+      .then((accessToken: string): request.RequestPromise => {
+        if (this.debug) {
+          cmd.log(`Retrieved access token ${accessToken}.`);
+        }
+
+        if (this.verbose) {
+          cmd.log(`Retrieving list of runs for Microsoft Flow ${args.options.name}...`);
+        }
+
+        const requestOptions: any = {
+          url: `${auth.service.resource}providers/Microsoft.ProcessSimple/environments/${encodeURIComponent(args.options.environment)}/flows/${encodeURIComponent(args.options.name)}/runs?api-version=2016-11-01`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${accessToken}`,
+            accept: 'application/json'
+          }),
+          json: true
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.get(requestOptions);
+      })
+      .then((res: { value: [{ name: string, properties: { startTime: string, status: string } }] }): void => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        if (res.value && res.value.length > 0) {
+          if (args.options.output === 'json') {
+            cmd.log(res.value);
+          }
+          else {
+            cmd.log(res.value.map(e => {
+              return {
+                name: e.name,
+                startTime: e.properties.startTime,
+                status: e.properties.status
+              };
+            }));
+          }
+        }
+
+        cb();
+      }, (rawRes: any): void => this.handleRejectedODataJsonPromise(rawRes, cmd, cb));
+  }
+
+  public options(): CommandOption[] {
+    const options: CommandOption[] = [
+      {
+        option: '-n, --name <name>',
+        description: 'The name of the Microsoft Flow to retrieve the runs for'
+      },
+      {
+        option: '-e, --environment <environment>',
+        description: 'The name of the environment for which to retrieve available Flows'
+      }
+    ];
+
+    const parentOptions: CommandOption[] = super.options();
+    return options.concat(parentOptions);
+  }
+
+  public validate(): CommandValidate {
+    return (args: CommandArgs): boolean | string => {
+      if (!args.options.name) {
+        return 'Required option name missing';
+      }
+
+      if (!args.options.environment) {
+        return 'Required option environment missing';
+      }
+
+      return true;
+    };
+  }
+
+  public commandHelp(args: {}, log: (help: string) => void): void {
+    const chalk = vorpal.chalk;
+    log(vorpal.find(commands.FLOW_RUN_LIST).helpInformation());
+    log(
+      `  ${chalk.yellow('Important:')} before using this command, connect to the Azure Management Service,
+    using the ${chalk.blue(commands.CONNECT)} command.
+
+  Remarks:
+
+    ${chalk.yellow('Attention:')} This command is based on an API that is currently
+    in preview and is subject to change once the API reached general
+    availability.
+  
+    To get information about the runs of the specified Microsoft Flow, you have to first
+    connect to the Azure Management Service using the ${chalk.blue(commands.CONNECT)} command.
+   
+  Examples:
+  
+    List runs of the specified Microsoft Flow
+      ${chalk.grey(config.delimiter)} ${this.getCommandName()} --environment Default-d87a7535-dd31-4437-bfe1-95340acd55c5 --name 5923cb07-ce1a-4a5c-ab81-257ce820109a
+`);
+  }
+}
+
+module.exports = new AzmgmtFlowRunListCommand();
