299ed40dd1e12ea358a64e7c9ddf55e89941835b
Arjen Bloemsma <a.bloemsma@gmail.com>
2018-09-11 19:29:52 +0200

    Added some more tests

---
+++ src/o365/spo/commands/list/list-webhook-list.spec.ts
@@ -0,0 +1,420 @@
+import commands from '../../commands';
+import Command, { CommandValidate, CommandOption, CommandError } from '../../../../Command';
+import * as sinon from 'sinon';
+import appInsights from '../../../../appInsights';
+import auth, { Site } from '../../SpoAuth';
+const command: Command = require('./list-webhook-list');
+import * as assert from 'assert';
+import * as request from 'request-promise-native';
+import Utils from '../../../../Utils';
+
+describe(commands.LIST_WEBHOOK_LIST, () => {
+  let vorpal: Vorpal;
+  let log: any[];
+  let cmdInstance: any;
+  let cmdInstanceLogSpy: sinon.SinonSpy;
+  let trackEvent: any;
+  let telemetry: any;
+  let stubAuth: any = () => {
+    sinon.stub(request, 'post').callsFake((opts) => {
+      if (opts.url.indexOf('/common/oauth2/token') > -1) {
+        return Promise.resolve('abc');
+      }
+
+      if (opts.url.indexOf('/_api/contextinfo') > -1) {
+        return Promise.resolve({
+          FormDigestValue: 'abc'
+        });
+      }
+
+      return Promise.reject('Invalid request');
+    });
+  }
+
+  before(() => {
+    sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
+    sinon.stub(auth, 'getAccessToken').callsFake(() => { return Promise.resolve('ABC'); });
+    sinon.stub(command as any, 'getRequestDigest').callsFake(() => { return { FormDigestValue: 'abc' }; });
+    trackEvent = sinon.stub(appInsights, 'trackEvent').callsFake((t) => {
+      telemetry = t;
+    });
+  });
+
+  beforeEach(() => {
+    vorpal = require('../../../../vorpal-init');
+    log = [];
+    cmdInstance = {
+      log: (msg: string) => {
+        log.push(msg);
+      }
+    };
+    cmdInstanceLogSpy = sinon.spy(cmdInstance, 'log');
+    auth.site = new Site();
+    telemetry = null;
+  });
+
+  afterEach(() => {
+    Utils.restore([
+      vorpal.find,
+      request.get
+    ]);
+  });
+
+  after(() => {
+    Utils.restore([
+      appInsights.trackEvent,
+      auth.getAccessToken,
+      auth.restoreAuth,
+      request.get
+    ]);
+  });
+
+  it('has correct name', () => {
+    assert.equal(command.name.startsWith(commands.LIST_WEBHOOK_LIST), true);
+  });
+
+  it('has a description', () => {
+    assert.notEqual(command.description, null);
+  });
+
+  it('calls telemetry', (done) => {
+    cmdInstance.action = command.action();
+    cmdInstance.action({ options: {} }, () => {
+      try {
+        assert(trackEvent.called);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('logs correct telemetry event', (done) => {
+    cmdInstance.action = command.action();
+    cmdInstance.action({ options: {} }, () => {
+      try {
+        assert.equal(telemetry.name, commands.LIST_WEBHOOK_LIST);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('aborts when not connected to a SharePoint site', (done) => {
+    auth.site = new Site();
+    auth.site.connected = false;
+    cmdInstance.action = command.action();
+    cmdInstance.action({ options: { debug: false, webUrl: 'https://contoso.sharepoint.com', title: 'Documents' } }, (err?: any) => {
+      try {
+        assert.equal(JSON.stringify(err), JSON.stringify(new CommandError('Connect to a SharePoint Online site first')));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('retrieves all webhooks of the specific list if title option is passed (debug)', (done) => {
+    stubAuth();
+
+    sinon.stub(request, 'get').callsFake((opts) => {
+      return Promise.resolve(
+        {
+          "d": {
+            "results": [{
+              "__metadata": {
+                "id": "https://contoso.sharepoint.com/_api/Microsoft.SharePoint.Webhooks.Subscriptionf2569c33-7b3c-4d6b-a925-77e96f6a334d",
+                "uri": "https://contoso.sharepoint.com/_api/Microsoft.SharePoint.Webhooks.Subscriptionf2569c33-7b3c-4d6b-a925-77e96f6a334d",
+                "type": "Microsoft.SharePoint.Webhooks.Subscription"
+              },
+              "clientState": "pnp-js-core-subscription",
+              "expirationDateTime": "2018-12-09T18:01:55.097Z",
+              "id": "cfda40f2-6ca2-4424-9be0-33e9785b0e67",
+              "notificationUrl": "https://webhookfunction.azurewebsites.net/api/FakeWebhookEndpoint?code=QlM2zaeJRti4WFGQUEqSo1ZmKMtRdB2JQ3mc2kzPj2aX6pNBAWVU4w==",
+              "resource": "dfddade1-4729-428d-881e-7fedf3cae50d",
+              "resourceData": null
+            }, {
+              "__metadata": {
+                "id": "https://contoso.sharepoint.com/_api/Microsoft.SharePoint.Webhooks.Subscriptione304cdd9-51ee-4c7a-84a2-730001cccb33",
+                "uri": "https://contoso.sharepoint.com/_api/Microsoft.SharePoint.Webhooks.Subscriptione304cdd9-51ee-4c7a-84a2-730001cccb33",
+                "type": "Microsoft.SharePoint.Webhooks.Subscription"
+              },
+              "clientState": null,
+              "expirationDateTime": "2019-01-27T16:32:05.4610008Z",
+              "id": "cc27a922-8224-4296-90a5-ebbc54da2e85",
+              "notificationUrl": "https://webhookfunction.azurewebsites.net/api/HandleWebHookNotification?code=jZyDfmBffPn7x0xYCQtZuxfqapu7cJzJo6puvruJiMUOxUl6XkxXAA==",
+              "resource": "dfddade1-4729-428d-881e-7fedf3cae50d",
+              "resourceData": null
+            }]
+          }
+        }
+      );
+    });
+
+    auth.site = new Site();
+    auth.site.connected = true;
+    auth.site.url = 'https://contoso-admin.sharepoint.com';
+    auth.site.tenantId = 'abc';
+    cmdInstance.action = command.action();
+    cmdInstance.action({
+      options: {
+        debug: true,
+        title: 'Documents',
+        webUrl: 'https://contoso.sharepoint.com'
+      }
+    }, () => {
+      try {
+        assert(cmdInstanceLogSpy.calledWith([
+          {
+            id: 'cfda40f2-6ca2-4424-9be0-33e9785b0e67',
+            clientState: 'pnp-js-core-subscription',
+            expirationDateTime: '2018-12-09T18:01:55.097Z',
+            resource: 'dfddade1-4729-428d-881e-7fedf3cae50d'
+          },
+          {
+            id: 'cc27a922-8224-4296-90a5-ebbc54da2e85',
+            clientState: '',
+            expirationDateTime: '2019-01-27T16:32:05.4610008Z',
+            resource: 'dfddade1-4729-428d-881e-7fedf3cae50d'
+          }
+        ]));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+      finally {
+        Utils.restore(request.get);
+        Utils.restore(request.post);
+      }
+    });
+  });
+
+  it('retrieves all webhooks of the specific list if title option is passed (debug)', (done) => {
+    assert.fail('Not yet implemented');
+  });
+
+  it('retrieves all webhooks of the specific list if title option is passed (debug)', (done) => {
+    assert.fail('Not yet implemented');
+  });
+
+  it('retrieves all webhooks of the specific list if id option is passed', (done) => {
+    assert.fail('Not yet implemented');
+  });
+
+  it('renders empty string for clientState, if no value for clientState was specified in the webhook', (done) => {
+    assert.fail('Not yet implemented');
+  });
+
+  it('command correctly handles list get reject request', (done) => {
+    sinon.stub(request, 'post').callsFake((opts) => {
+      if (opts.url.indexOf('/_api/contextinfo') > -1) {
+        return Promise.resolve({
+          FormDigestValue: 'abc'
+        });
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    const err = 'Invalid request';
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url.indexOf('/_api/web/lists/GetByTitle(') > -1) {
+        return Promise.reject(err);
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    auth.site = new Site();
+    auth.site.connected = true;
+    auth.site.url = 'https://contoso.sharepoint.com';
+    cmdInstance.action = command.action();
+
+    const actionTitle: string = 'Documents';
+
+    cmdInstance.action({
+      options: {
+        debug: true,
+        title: actionTitle,
+        webUrl: 'https://contoso.sharepoint.com',
+      }
+    }, (error?: any) => {
+      try {
+        assert.equal(JSON.stringify(error), JSON.stringify(new CommandError(err)));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+      finally {
+        Utils.restore([
+          request.post,
+          request.get
+        ]);
+      }
+    });
+  });
+
+  it('uses correct API url when id option is passed', (done) => {
+    stubAuth();
+
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url.indexOf('/_api/web/lists(guid') > -1) {
+        return Promise.resolve('Correct Url')
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    auth.site = new Site();
+    auth.site.connected = true;
+    auth.site.url = 'https://contoso.sharepoint.com';
+    cmdInstance.action = command.action();
+
+    const actionId: string = '0CD891EF-AFCE-4E55-B836-FCE03286CCCF';
+
+    cmdInstance.action({
+      options: {
+        debug: false,
+        id: actionId,
+        webUrl: 'https://contoso.sharepoint.com',
+      }
+    }, () => {
+
+      try {
+        assert(1 === 1);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+      finally {
+        Utils.restore([
+          request.post,
+          request.get
+        ]);
+      }
+    });
+
+  });
+
+  it('supports debug mode', () => {
+    const options = (command.options() as CommandOption[]);
+    let containsDebugOption = false;
+    options.forEach(o => {
+      if (o.option === '--debug') {
+        containsDebugOption = true;
+      }
+    });
+    assert(containsDebugOption);
+  });
+
+  it('supports specifying URL', () => {
+    const options = (command.options() as CommandOption[]);
+    let containsTypeOption = false;
+    options.forEach(o => {
+      if (o.option.indexOf('<webUrl>') > -1) {
+        containsTypeOption = true;
+      }
+    });
+    assert(containsTypeOption);
+  });
+
+  it('fails validation if the url option not specified', () => {
+    const actual = (command.validate() as CommandValidate)({ options: {} });
+    assert.notEqual(actual, true);
+  });
+
+  it('fails validation if both id and title options are not passed', () => {
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com' } });
+    assert.notEqual(actual, true);
+  });
+
+  it('fails validation if the url option is not a valid SharePoint site URL', () => {
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'foo' } });
+    assert.notEqual(actual, true);
+  });
+
+  it('passes validation if the url option is a valid SharePoint site URL', () => {
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', id: '0CD891EF-AFCE-4E55-B836-FCE03286CCCF' } });
+    assert(actual);
+  });
+
+  it('fails validation if the id option is not a valid GUID', () => {
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', id: '12345' } });
+    assert.notEqual(actual, true);
+  });
+
+  it('passes validation if the id option is a valid GUID', () => {
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', id: '0CD891EF-AFCE-4E55-B836-FCE03286CCCF' } });
+    assert(actual);
+  });
+
+  it('fails validation if both id and title options are passed', () => {
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', id: '0CD891EF-AFCE-4E55-B836-FCE03286CCCF', title: 'Documents' } });
+    assert.notEqual(actual, true);
+  });
+
+  it('has help referring to the right command', () => {
+    const cmd: any = {
+      log: (msg: string) => { },
+      prompt: () => { },
+      helpInformation: () => { }
+    };
+    const find = sinon.stub(vorpal, 'find').callsFake(() => cmd);
+    cmd.help = command.help();
+    cmd.help({}, () => { });
+    assert(find.calledWith(commands.LIST_WEBHOOK_LIST));
+  });
+
+  it('has help with examples', () => {
+    const _log: string[] = [];
+    const cmd: any = {
+      log: (msg: string) => {
+        _log.push(msg);
+      },
+      prompt: () => { },
+      helpInformation: () => { }
+    };
+    sinon.stub(vorpal, 'find').callsFake(() => cmd);
+    cmd.help = command.help();
+    cmd.help({}, () => { });
+    let containsExamples: boolean = false;
+    _log.forEach(l => {
+      if (l && l.indexOf('Examples:') > -1) {
+        containsExamples = true;
+      }
+    });
+    Utils.restore(vorpal.find);
+    assert(containsExamples);
+  });
+
+  it('correctly handles lack of valid access token', (done) => {
+    Utils.restore(auth.getAccessToken);
+    sinon.stub(auth, 'getAccessToken').callsFake(() => { return Promise.reject(new Error('Error getting access token')); });
+    auth.site = new Site();
+    auth.site.connected = true;
+    auth.site.url = 'https://contoso.sharepoint.com';
+    cmdInstance.action = command.action();
+    cmdInstance.action({
+      options: {
+        id: "BC448D63-484F-49C5-AB8C-96B14AA68D50",
+        webUrl: "https://contoso.sharepoint.com",
+        debug: false
+      }
+    }, (err?: any) => {
+      try {
+        assert.equal(JSON.stringify(err), JSON.stringify(new CommandError('Error getting access token')));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+});
