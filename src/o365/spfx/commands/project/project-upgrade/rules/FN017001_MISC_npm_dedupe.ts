c1d9e1f72c184abb30a6c62a560b708ffb5b5e2a
Waldek Mastykarz <waldek@mastykarz.nl>
2018-09-21 22:25:19 +0200

    Added 'npm dedupe' SPFx project upgrade rule solving #612

---
+++ src/o365/spfx/commands/project/project-upgrade/rules/FN017001_MISC_npm_dedupe.ts
@@ -0,0 +1,45 @@
+import { Finding } from "../";
+import { Project } from "../model";
+import { Rule } from "./Rule";
+
+export class FN017001_MISC_npm_dedupe extends Rule {
+  constructor() {
+    /* istanbul ignore next */
+    super();
+  }
+
+  get id(): string {
+    return 'FN017001';
+  }
+
+  get title(): string {
+    return 'Run npm dedupe';
+  }
+
+  get description(): string {
+    return `If, after upgrading npm packages, when building the project you have errors similar to: "error TS2345: Argument of type 'SPHttpClientConfiguration' is not assignable to parameter of type 'SPHttpClientConfiguration'", try running 'npm dedupe' to cleanup npm packages.`;
+  }
+
+  get resolution(): string {
+    return 'npm dedupe';
+  };
+
+  get resolutionType(): string {
+    return 'cmd';
+  }
+
+  get file(): string {
+    return './package.json';
+  };
+
+  get severity(): string {
+    return 'Optional';
+  }
+
+  visit(project: Project, findings: Finding[]): void {
+    const npmFinding: Finding | undefined = findings.find(f => typeof f.occurrences.find(o => o.resolution.indexOf('npm i ') === 0 || o.resolution.indexOf('npm un ') === 0) !== 'undefined');
+    if (npmFinding) {
+      this.addFinding(findings);
+    }
+  }
+}
