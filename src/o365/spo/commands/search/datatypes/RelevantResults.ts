ce930300fcc08bde190d3c545092522eb5e6b031
Stijn Brouwers <stijnbrouwers@gmail.com>
2018-09-29 18:08:02 +0200

    Add rowLimit arg + unit tests

--- src/o365/spo/commands/search/datatypes/RelevantResults.ts
+++ src/o365/spo/commands/search/datatypes/RelevantResults.ts
@@ -2,11 +2,11 @@ import { SearchResultProperty } from "./SearchResultProperty";
 import { ResultTable } from "./ResultTable";
 
 export interface RelevantResults {
-  RelevantResults:string;
-  ItemTemplateId:string;
+  GroupTemplateId:string | null;
+  ItemTemplateId:string | null;
   Properties:SearchResultProperty[];
-  ResultTitle:string;
-  ResultTitleUrl:string;
+  ResultTitle:string | null;
+  ResultTitleUrl:string | null;
   RowCount:number;
   Table:ResultTable;
   TotalRows:number;
