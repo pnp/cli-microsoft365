329de308be5aad5aee2d1ece3db1866666cf60f4
Arjen Bloemsma <a.bloemsma@gmail.com>
2018-11-01 14:50:09 +0100

    Inital version of command

--- src/o365/spo/commands.ts
+++ src/o365/spo/commands.ts
@@ -166,6 +166,7 @@ export default {
   STORAGEENTITY_SET: `${prefix} storageentity set`,
   STORAGEENTITY_REMOVE: `${prefix} storageentity remove`,
   TENANT_APPCATALOGURL_GET: `${prefix} tenant appcatalogurl get`,
+  TENANT_DELETEDSITE_LIST: `${prefix} tenant deletedsite list`,
   TENANT_SETTINGS_LIST: `${prefix} tenant settings list`,
   TENANT_SETTINGS_SET: `${prefix} tenant settings set`,
   TERM_ADD: `${prefix} term add`,
