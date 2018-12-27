6a1bce0bc38aa7f06149fc3ca261fd9a03c8358a
Waldek Mastykarz <waldek@mastykarz.nl>
2018-12-27 15:09:15 +0100

    Disabled telemetry auto start

--- src/appInsights.ts
+++ src/appInsights.ts
@@ -6,7 +6,6 @@ import * as crypto from 'crypto';
 import * as appInsights from 'applicationinsights';
 const config = appInsights.setup('6b908c80-d09f-4cf6-8274-e54349a0149a');
 config.setInternalLogging(false, false);
-appInsights.start();
 // append -dev to the version number when ran locally
 // to distinguish production and dev version of the CLI
 // in the telemetry
