1d57959b7c76843baf094e12c316d107a9f094cd
Albert-Jan Schot <albert.jan.schot@mavention.nl>
2019-01-08 11:24:06 +0100

    [wip] implemented team user add command

---
+++ src/o365/graph/commands/teams/teams-user-add.ts
@@ -0,0 +1,171 @@
+import auth from '../../GraphAuth';
+import config from '../../../../config';
+import commands from '../../commands';
+import GlobalOptions from '../../../../GlobalOptions';
+import {
+  CommandOption, CommandValidate
+} from '../../../../Command';
+import { GraphItemsListCommand } from '../GraphItemsListCommand';
+import Utils from '../../../../Utils';
+import { GroupUser } from '../o365group/GroupUser';
+import * as request from 'request-promise-native';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  role?: string;
+  teamId: string;
+  userName: string;
+}
+
+class GraphTeamsUserAddCommand extends GraphItemsListCommand<GroupUser> {
+  public get name(): string {
+    return `${commands.TEAMS_USER_ADD}`;
+  }
+
+  public get description(): string {
+    return 'Adds an owner or member to the specified Microsoft Teams team';
+  }
+
+  public getTelemetryProperties(args: CommandArgs): any {
+    const telemetryProps: any = super.getTelemetryProperties(args);
+    telemetryProps.role = args.options.role;
+    return telemetryProps;
+  }
+
+  // Todo implement
+  public commandAction(cmd: CommandInstance, args: CommandArgs, cb: () => void): void {
+    auth
+      .ensureAccessToken(auth.service.resource, cmd, this.debug)
+      .then((): request.RequestPromise => {
+        const requestOptions: any = {
+          url: `${auth.service.resource}/v1.0/users/${encodeURIComponent(args.options.userName)}/id`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            accept: 'application/json;odata.metadata=none'
+          }),
+          json: true
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.get(requestOptions);
+      })
+      .then((res: any): request.RequestPromise => {
+        if (this.debug) {
+          cmd.log('Response:')
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        let endpoint: string = `${auth.service.resource}/v1.0/groups/${args.options.teamId}/members/$ref`;
+
+        if (args.options.role === "Owner") {
+          endpoint = `${auth.service.resource}/v1.0/groups/${args.options.teamId}/owners/$ref`;
+        }
+
+        const requestOptions: any = {
+          url: endpoint,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            'accept': 'application/json;odata.metadata=none'
+          }),
+          json: true,
+          body: { "@odata.id": "https://graph.microsoft.com/v1.0/directoryObjects/" + res.value }
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.post(requestOptions);
+      })
+      .then((): void => {
+        if (this.verbose) {
+          cmd.log(vorpal.chalk.green('DONE'));
+        }
+
+        cb();
+      }, (err: any) => this.handleRejectedODataJsonPromise(err, cmd, cb));
+  }
+
+  public options(): CommandOption[] {
+    const options: CommandOption[] = [
+      {
+        option: '-i, --teamId <teamId>',
+        description: 'The ID of the team for which to add a new owner or member'
+      },
+      {
+        option: '-n, --userName [userName]',
+        description: 'User\'s UPN (user principal name - e.g. johndoe@example.com)'
+      },
+      {
+        option: '-r, --role [type]',
+        description: 'The role to be assigned to the new user: Owner|Member',
+        autocomplete: ['Owner', 'Member']
+      }
+    ];
+
+    const parentOptions: CommandOption[] = super.options();
+    return options.concat(parentOptions);
+  }
+
+  public validate(): CommandValidate {
+    return (args: CommandArgs): boolean | string => {
+      if (!args.options.teamId) {
+        return 'Required parameter teamId missing';
+      }
+
+      if (!Utils.isValidGuid(args.options.teamId as string)) {
+        return `${args.options.teamId} is not a valid GUID`;
+      }
+
+      if (!args.options.userName) {
+        return 'Required parameter userName missing';
+      }
+
+      if (args.options.role) {
+        if (['Owner', 'Member'].indexOf(args.options.role) === -1) {
+          return `${args.options.role} is not a valid role value. Allowed values Owner|Member`;
+        }
+      }
+
+      return true;
+    };
+  }
+
+  public commandHelp(args: {}, log: (help: string) => void): void {
+    const chalk = vorpal.chalk;
+    log(vorpal.find(this.name).helpInformation());
+    log(
+      `  ${chalk.yellow('Important:')} before using this command, log in to the Microsoft Graph
+    using the ${chalk.blue(commands.LOGIN)} command.
+        
+  Remarks:
+
+    To add users in the specified Microsoft Teams team, you have to first
+    log in to the Microsoft Graph using the ${chalk.blue(commands.LOGIN)} command,
+    eg. ${chalk.grey(`${config.delimiter} ${commands.LOGIN}`)}.
+
+  Examples:
+  
+    Add a new member to the specified team 
+      ${chalk.grey(config.delimiter)} ${this.name} --teamId '00000000-0000-0000-0000-000000000000' --userName anne.matthews@contoso.onmicrosoft.com
+
+    Add a new owner to the specified team 
+      ${chalk.grey(config.delimiter)} ${this.name} --teamId '00000000-0000-0000-0000-000000000000' --userName anne.matthews@contoso.onmicrosoft.com --role Owner 
+`);
+  }
+}
+
+module.exports = new GraphTeamsUserAddCommand();
