5456e148872eba11f793260cb9c300a807ffed2b
Laurent Sittler <github@lsonline.fr>
2019-02-09 13:17:37 +0100

    Added the 'spo list view field add' command solving #735

--- src/o365/spo/commands.ts
+++ src/o365/spo/commands.ts
@@ -74,6 +74,7 @@ export default {
   LIST_VIEW_LIST: `${prefix} list view list`,
   LIST_VIEW_REMOVE: `${prefix} list view remove`,
   LIST_VIEW_SET: `${prefix} list view set`,
+  LIST_VIEW_FIELD_ADD: `${prefix} list view field add`,
   LIST_VIEW_FIELD_REMOVE: `${prefix} list view field remove`,
   LIST_WEBHOOK_ADD: `${prefix} list webhook add`,
   LIST_WEBHOOK_GET: `${prefix} list webhook get`,
