813299d6b58fa96ab949fb0ab5bacea2e71cae36
Arnie Raju <arnie.raju@thesolutioncollective.com.au>
2019-03-25 22:34:55 +1000

    Updated listitem record declare with additional tests

--- src/o365/spo/commands/listitem/listitem-record-declare.spec.ts
+++ src/o365/spo/commands/listitem/listitem-record-declare.spec.ts
@@ -1,5 +1,6 @@
 import commands from '../../commands';
 import Command from '../../../../Command';
+
 import { CommandValidate, CommandOption, CommandError, CommandTypes } from '../../../../Command';
 import * as sinon from 'sinon';
 import appInsights from '../../../../appInsights';
@@ -16,6 +17,94 @@ describe(commands.LISTITEM_RECORD_DECLARE, () => {
   let cmdInstance: any;
   let trackEvent: any;
   let telemetry: any;
+  let declareItemAsRecordFakeCalled = false;
+
+  let postFakes = (opts: any) => {
+    if (opts.url.indexOf('/common/oauth2/token') > -1) {
+      return Promise.resolve('abc');
+    }
+
+    if (opts.url.indexOf('/_api/contextinfo') > -1) {
+      return Promise.resolve({
+        FormDigestValue: 'abc'
+      });
+    }
+
+    if (opts.url.indexOf('_vti_bin/client.svc/ProcessQuery') > -1) {
+
+      // requestObjectIdentity mock
+      if (opts.body.indexOf('Name="Current"') > -1) {
+
+        if (opts.url.indexOf('rejectme.sharepoint.com') > -1) {
+          return Promise.reject('Failed request')
+        }
+
+        if (opts.url.indexOf('returnerror.sharepoint.com') > -1) {
+          return Promise.reject(JSON.stringify(
+            [{ "ErrorInfo": "error occurred" }]
+          ))
+        }
+
+        return Promise.resolve(JSON.stringify(
+          [
+            {
+              "SchemaVersion": "15.0.0.0",
+              "LibraryVersion": "16.0.7618.1204",
+              "ErrorInfo": null,
+              "TraceCorrelationId": "3e3e629e-30cc-5000-9f31-cf83b8e70021"
+            },
+            {
+              "_ObjectType_": "SP.Web",
+              "_ObjectIdentity_": "d704ae73-d5ed-459e-80b0-b8103c5fb6e0|8f2be65d-f195-4699-b0de-24aca3384ba9:site:0ead8b78-89e5-427f-b1bc-6e5a77ac191c:web:4c076c07-e3f1-49a8-ad01-dbb70b263cd7",
+              "ServerRelativeUrl": "\\u002fsites\\u002fprojectx"
+            }
+          ])
+        )
+      }
+
+      if (opts.body.indexOf('Name="DeclareItemAsRecord') > -1
+        || opts.body.indexOf('Name="DeclareItemAsRecordWithDeclarationDate') > -1) {
+
+        if (opts.url.indexOf('alreadydeclared') > -1) {
+          return Promise.resolve(JSON.stringify([
+            {
+              "SchemaVersion": "15.0.0.0", "LibraryVersion": "16.0.8713.1223", "ErrorInfo": {
+                "ErrorMessage": "This item has already been declared a record.", "ErrorValue": null, "TraceCorrelationId": "9d66cc9e-e0fa-8000-1225-3a9b7ff9284d", "ErrorCode": -2146232832, "ErrorTypeName": "Microsoft.SharePoint.SPException"
+              }, "TraceCorrelationId": "9d66cc9e-e0fa-8000-1225-3a9b7ff9284d"
+            }
+          ]));
+        }
+
+        declareItemAsRecordFakeCalled = true;
+        return Promise.resolve(JSON.stringify(
+          [
+            {
+              "SchemaVersion": "15.0.0.0",
+              "LibraryVersion": "16.0.8713.1221",
+              "ErrorInfo": null,
+              "TraceCorrelationId": "9d20cc9e-7077-8000-1225-32482bc95341"
+            }
+          ])
+        );
+
+      }
+    }
+    return Promise.reject('Invalid request');
+  }
+
+  let getFakes = (opts: any) => {
+    if (opts.url.indexOf('/_api/web/lists') > -1 &&
+      opts.url.indexOf('$select=Id') > -1) {
+      cmdInstance.log('faked!');
+      return Promise.resolve({
+        Id: '81f0ecee-75a8-46f0-b384-c8f4f9f31d99'
+      });
+    }
+    if (opts.url.indexOf('/id') > -1) {
+      return Promise.resolve({ value: "f64041f2-9818-4b67-92ff-3bc5dbbef27e" });
+    }
+    return Promise.reject('Invalid request');
+  }
 
   before(() => {
     sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
@@ -53,6 +142,7 @@ describe(commands.LISTITEM_RECORD_DECLARE, () => {
     ]);
   });
 
+
   it('has correct name', () => {
     assert.equal(command.name.startsWith(commands.LISTITEM_RECORD_DECLARE), true);
   });
@@ -87,17 +177,226 @@ describe(commands.LISTITEM_RECORD_DECLARE, () => {
     });
   });
 
+  it('declares a record using list title is specified', (done) => {
+    sinon.stub(request, 'get').callsFake(getFakes);
+    sinon.stub(request, 'post').callsFake(postFakes);
+
+    auth.site = new Site();
+    auth.site.connected = true;
+    auth.site.url = 'https://contoso.sharepoint.com';
+    cmdInstance.action = command.action();
+
+    const options: any = {
+      debug: true,
+      listTitle: 'Test List',
+      id: 147,
+      webUrl: `https://contoso.sharepoint.com/sites/project-y/`,
+    };
+
+    declareItemAsRecordFakeCalled = false;
+    cmdInstance.action({ options: options }, () => {
+      try {
+        assert(declareItemAsRecordFakeCalled);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+      finally {
+        Utils.restore(request.get);
+        Utils.restore(request.post);
+      }
+    });
+  });
+
+  it('declares a record using list id is passed as an option', (done) => {
+    sinon.stub(request, 'get').callsFake(getFakes);
+    sinon.stub(request, 'post').callsFake(postFakes);
+
+    auth.site = new Site();
+    auth.site.connected = true;
+    auth.site.url = 'https://contoso.sharepoint.com';
+    cmdInstance.action = command.action();
+
+    const options: any = {
+      debug: true,
+      listId: '99a14fe8-781c-3ce1-a1d5-c6e6a14561da',
+      id: 147,
+      webUrl: `https://contoso.sharepoint.com/sites/project-y/`,
+    };
+
+    declareItemAsRecordFakeCalled = false;
+    cmdInstance.action({ options: options }, () => {
+      try {
+        assert(declareItemAsRecordFakeCalled);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+      finally {
+        Utils.restore(request.get);
+        Utils.restore(request.post);
+      }
+    });
+  });
+
+  it('declares a record when specifying a date', (done) => {
+    sinon.stub(request, 'get').callsFake(getFakes);
+    sinon.stub(request, 'post').callsFake(postFakes);
+
+    auth.site = new Site();
+    auth.site.connected = true;
+    auth.site.url = 'https://contoso.sharepoint.com';
+    cmdInstance.action = command.action();
+
+    const options: any = {
+      debug: true,
+      listId: '99a14fe8-781c-3ce1-a1d5-c6e6a14561da',
+      id: 147,
+      date: '2019-03-14',
+      webUrl: `https://contoso.sharepoint.com/sites/project-y/`,
+    };
+
+    declareItemAsRecordFakeCalled = false;
+    cmdInstance.action({ options: options }, () => {
+      try {
+        assert(declareItemAsRecordFakeCalled);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+      finally {
+        Utils.restore(request.get);
+        Utils.restore(request.post);
+      }
+    });
+  });
+
+  it('it reports an error correctly when an item is already declared', (done) => {
+    sinon.stub(request, 'get').callsFake(getFakes);
+    sinon.stub(request, 'post').callsFake(postFakes);
+
+    auth.site = new Site();
+    auth.site.connected = true;
+    auth.site.url = 'https://contoso.sharepoint.com';
+    cmdInstance.action = command.action();
+
+    const options: any = {
+      debug: true,
+      listId: '99a14fe8-781c-3ce1-a1d5-c6e6a14561da',
+      id: 147,
+      date: '2019-03-14',
+      webUrl: `https://alreadydeclared.sharepoint.com/sites/project-y/`,
+    };
+
+    cmdInstance.action({ options: options }, () => {
+
+      let correctErrorReported = false;
+      log.forEach(l => {
+        if (l && typeof l === 'string' &&
+          l.indexOf('This item has already been declared a record') > -1) {
+          correctErrorReported = true;
+        }
+      });
+
+      try {
+        assert(correctErrorReported);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+
+      finally {
+        Utils.restore(request.get);
+        Utils.restore(request.post);
+      }
+    });
+  });
+
+
   it('aborts when not logged in to a SharePoint site', (done) => {
     auth.site = new Site();
     auth.site.connected = false;
     cmdInstance.action = command.action();
-    cmdInstance.action({ options: { debug: false, webUrl: 'https://contoso.sharepoint.com', listTitle: 'Demo List' } }, (err?: any) => {
+    cmdInstance.action({ options: { debug: false, webUrl: 'https://contoso.sharepoint.com', listTitle: 'Test List' } }, (err?: any) => {
       try {
         assert.equal(JSON.stringify(err), JSON.stringify(new CommandError('Log in to a SharePoint Online site first')));
         done();
       }
       catch (e) {
         done(e);
+      }
+    });
+  });
+
+  it('fails to get _ObjecttIdentity_ when an error is returned by the _ObjectIdentity_ CSOM request', (done) => {
+
+    sinon.stub(request, 'get').callsFake(getFakes);
+    sinon.stub(request, 'post').callsFake(postFakes);
+
+    auth.site = new Site();
+    auth.site.connected = true;
+    auth.site.url = 'https://returnerror.sharepoint.com';
+    cmdInstance.action = command.action();
+    let options: any = {
+      debug: true,
+      listId: '99a14fe8-781c-3ce1-a1d5-c6e6a14561da',
+      id: 147,
+      date: '2019-03-14',
+      webUrl: `https://returnerror.sharepoint.com/sites/project-y/`
+    }
+
+    declareItemAsRecordFakeCalled = false;
+    cmdInstance.action({ options: options }, () => {
+
+      try {
+        assert.notEqual(declareItemAsRecordFakeCalled, true);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+      finally {
+        Utils.restore(request.get);
+        Utils.restore(request.post);
+      }
+    });
+
+  });
+
+  it('fails to declare a list item as a record when an error is returned', (done) => {
+
+    sinon.stub(request, 'get').callsFake(getFakes);
+    sinon.stub(request, 'post').callsFake(postFakes);
+
+    auth.site = new Site();
+    auth.site.connected = true;
+    auth.site.url = 'https://rejectme.sharepoint.com';
+    cmdInstance.action = command.action();
+
+    let options: any = {
+      debug: false,
+      listTitle: 'Test List',
+      id: 147,
+      webUrl: 'https://rejectme.sharepoint.com/sites/project-y',
+    }
+
+    declareItemAsRecordFakeCalled = false;
+    cmdInstance.action({ options: options }, () => {
+
+      try {
+        assert.notEqual(declareItemAsRecordFakeCalled, true);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+      finally {
+        Utils.restore(request.get);
+        Utils.restore(request.post);
       }
     });
   });
@@ -135,32 +434,32 @@ describe(commands.LISTITEM_RECORD_DECLARE, () => {
   });
 
   it('fails validation if listTitle and listId are specified together', () => {
-    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', listTitle: 'Demo List', listId: '0CD891EF-AFCE-4E55-B836-FCE03286CCCF' } });
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', listTitle: 'Test List', listId: '0CD891EF-AFCE-4E55-B836-FCE03286CCCF' } });
     assert.notEqual(actual, true);
   });
 
   it('fails validation if the webUrl option not specified', () => {
-    const actual = (command.validate() as CommandValidate)({ options: { title: 'Demo List' } });
+    const actual = (command.validate() as CommandValidate)({ options: { title: 'Test List' } });
     assert.notEqual(actual, true);
   });
 
   it('fails validation if the webUrl option is not a valid SharePoint site URL', () => {
-    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'foo', listTitle: 'Demo List', id: '1' } });
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'foo', listTitle: 'Test List', id: '1' } });
     assert.notEqual(actual, true);
   });
 
   it('fails validation if the item ID is not specified', () => {
-    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', listTitle: 'Demo List' } });
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', listTitle: 'Test List' } });
     assert.notEqual(actual, true);
   });
 
   it('fails validation if the item ID is not a number', () => {
-    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', listTitle: 'Demo List', id: 'foo' } });
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', listTitle: 'Test List', id: 'foo' } });
     assert.notEqual(actual, true);
   });
 
   it('passes validation if the webUrl option is a valid SharePoint site URL and numerical ID specified', () => {
-    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', listTitle: 'Demo List', id: '1' } });
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', listTitle: 'Test List', id: '1' } });
     assert(actual);
   });
 
@@ -170,7 +469,17 @@ describe(commands.LISTITEM_RECORD_DECLARE, () => {
   });
 
   it('passes validation if the listId option is a valid GUID', () => {
-    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', listId: '0CD891EF-AFCE-4E55-B836-FCE03286CCCF', id: '1' } });
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', listId: '0CD891EF-AFCE-4E55-B836-FCE03286CCCF', id: '1', debug: true } });
+    assert(actual);
+  });
+
+  it('fails validation if the date passed in is not in ISO format', () => {
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', listId: '0CD891EF-AFCE-4E55-B836-FCE03286CCCF', id: '1', date: 'foo', debug: true } });
+    assert.notEqual(actual, true);
+  });
+
+  it('passes validation if the date passed in is in ISO format', () => {
+    const actual = (command.validate() as CommandValidate)({ options: { webUrl: 'https://contoso.sharepoint.com', listId: '0CD891EF-AFCE-4E55-B836-FCE03286CCCF', id: '1', date: 'foo', debug: true } });
     assert(actual);
   });
 
