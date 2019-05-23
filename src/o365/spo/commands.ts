78685b07371e9903c5d8548a4b2d2d4ea6f627b5
User <davej.calabro@gmail.com>
2019-05-23 01:29:44 -0400

    Added the 'spo orgnewssite remove' command solving #976

--- src/o365/spo/commands.ts
+++ src/o365/spo/commands.ts
@@ -100,6 +100,7 @@ export default {
   NAVIGATION_NODE_LIST: `${prefix} navigation node list`,
   NAVIGATION_NODE_REMOVE: `${prefix} navigation node remove`,
   ORGNEWSSITE_LIST:  `${prefix} orgnewssite list`,
+  ORGNEWSSITE_REMOVE: `${prefix} orgnewssite remove`,
   ORGNEWSSITE_SET: `${prefix} orgnewssite set`,
   PAGE_ADD: `${prefix} page add`,
   PAGE_GET: `${prefix} page get`,
