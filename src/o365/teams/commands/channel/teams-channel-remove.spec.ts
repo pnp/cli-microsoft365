b89d0f509407049eab1bdd50cd6565cddcdd6c0f
Garry Trinder <garry.trinder@live.com>
2019-08-20 00:01:03 +0100

    Added basic command validation tests

---
+++ src/o365/teams/commands/channel/teams-channel-remove.spec.ts
@@ -0,0 +1,243 @@
+import commands from '../../commands';
+import Command, { CommandValidate, CommandOption } from '../../../../Command';
+import * as sinon from 'sinon';
+import appInsights from '../../../../appInsights';
+import auth from '../../../../Auth';
+const command: Command = require('./teams-channel-remove');
+import * as assert from 'assert';
+import request from '../../../../request';
+import Utils from '../../../../Utils';
+
+describe(commands.TEAMS_CHANNEL_REMOVE, () => {
+  let vorpal: Vorpal;
+  let log: string[];
+  let cmdInstance: any;
+  let promptOptions: any;
+
+  before(() => {
+    sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
+    sinon.stub(appInsights, 'trackEvent').callsFake(() => { });
+    auth.service.connected = true;
+  });
+
+  beforeEach(() => {
+    vorpal = require('../../../../vorpal-init');
+    log = [];
+    cmdInstance = {
+      commandWrapper: {
+        command: command.name
+      },
+      action: command.action(),
+      log: (msg: string) => {
+        log.push(msg);
+      },
+      prompt: (options: any, cb: (result: { continue: boolean }) => void) => {
+        promptOptions = options;
+        cb({ continue: false });
+      }
+    };
+    promptOptions = undefined;
+  });
+
+  afterEach(() => {
+    Utils.restore([
+      vorpal.find,
+      request.get,
+      request.delete
+    ]);
+  });
+
+  after(() => {
+    Utils.restore([
+      auth.restoreAuth,
+      appInsights.trackEvent
+    ]);
+    auth.service.connected = false;
+  });
+
+  it('has correct name', () => {
+    assert.equal(command.name.startsWith(commands.TEAMS_CHANNEL_REMOVE), true);
+  });
+
+  it('has a description', () => {
+    assert.notEqual(command.description, null);
+  });
+
+  it('passes validation when valid channelId & teamId is specified', () => {
+    const actual = (command.validate() as CommandValidate)({
+      options: {
+        channelId: '19:f3dcbb1674574677abcae89cb626f1e6@thread.skype',
+        teamId: 'd66b8110-fcad-49e8-8159-0d488ddb7656',
+      }
+    });
+    assert.equal(actual, true);
+  });
+
+  it('fails validation if the teamId & channelId are not provided', (done) => {
+    const actual = (command.validate() as CommandValidate)({
+      options: {
+
+      }
+    });
+    assert.notEqual(actual, true);
+    done();
+  });
+
+  it('fails validation if the teamId is not provided', (done) => {
+    const actual = (command.validate() as CommandValidate)({
+      options: {
+        channelId: '19:f3dcbb1674574677abcae89cb626f1e6@thread.skype'
+      }
+    });
+    assert.notEqual(actual, true);
+    done();
+  });
+
+  it('fails validation if the channelId is not provided', (done) => {
+    const actual = (command.validate() as CommandValidate)({
+      options: {
+        teamId: 'd66b8110-fcad-49e8-8159-0d488ddb7656',
+      }
+    });
+    assert.notEqual(actual, true);
+    done();
+  });
+
+  it('prompts before removing the specified channel when confirm option not passed', (done) => {
+    cmdInstance.action({
+      options: {
+        debug: false,
+        channelId: '19:f3dcbb1674574677abcae89cb626f1e6@thread.skype',
+        teamId: 'd66b8110-fcad-49e8-8159-0d488ddb7656',
+      }
+    }, () => {
+      let promptIssued = false;
+
+      if (promptOptions && promptOptions.type === 'confirm') {
+        promptIssued = true;
+      }
+
+      try {
+        assert(promptIssued);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('prompts before removing the specified channel when confirm option not passed (debug)', (done) => {
+    cmdInstance.action({
+      options: {
+        debug: true,
+        channelId: '19:f3dcbb1674574677abcae89cb626f1e6@thread.skype',
+        teamId: 'd66b8110-fcad-49e8-8159-0d488ddb7656',
+      }
+    }, () => {
+      let promptIssued = false;
+
+      if (promptOptions && promptOptions.type === 'confirm') {
+        promptIssued = true;
+      }
+
+      try {
+        assert(promptIssued);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('aborts removing the specified channel when confirm option not passed and prompt not confirmed', (done) => {
+    const postSpy = sinon.spy(request, 'delete');
+    cmdInstance.prompt = (options: any, cb: (result: { continue: boolean }) => void) => {
+      cb({ continue: false });
+    };
+    cmdInstance.action({
+      options: {
+        debug: true,
+        channelId: '19:f3dcbb1674574677abcae89cb626f1e6@thread.skype',
+        teamId: 'd66b8110-fcad-49e8-8159-0d488ddb7656',
+      }
+    }, () => {
+      try {
+        assert(postSpy.notCalled);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('aborts removing the specified channel when confirm option not passed and prompt not confirmed (debug)', (done) => {
+    const postSpy = sinon.spy(request, 'delete');
+    cmdInstance.prompt = (options: any, cb: (result: { continue: boolean }) => void) => {
+      cb({ continue: false });
+    };
+    cmdInstance.action({
+      options: {
+        debug: true,
+        channelId: '19:f3dcbb1674574677abcae89cb626f1e6@thread.skype',
+        teamId: 'd66b8110-fcad-49e8-8159-0d488ddb7656',
+      }
+    }, () => {
+      try {
+        assert(postSpy.notCalled);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('supports debug mode', () => {
+    const options = (command.options() as CommandOption[]);
+    let containsOption = false;
+    options.forEach(o => {
+      if (o.option === '--debug') {
+        containsOption = true;
+      }
+    });
+    assert(containsOption);
+  });
+
+  it('has help referring to the right command', () => {
+    const cmd: any = {
+      log: (msg: string) => { },
+      prompt: () => { },
+      helpInformation: () => { }
+    };
+    const find = sinon.stub(vorpal, 'find').callsFake(() => cmd);
+    cmd.help = command.help();
+    cmd.help({}, () => { });
+    assert(find.calledWith(commands.TEAMS_CHANNEL_REMOVE));
+  });
+
+  it('has help with examples', () => {
+    const _log: string[] = [];
+    const cmd: any = {
+      log: (msg: string) => {
+        _log.push(msg);
+      },
+      prompt: () => { },
+      helpInformation: () => { }
+    };
+    sinon.stub(vorpal, 'find').callsFake(() => cmd);
+    cmd.help = command.help();
+    cmd.help({}, () => { });
+    let containsExamples: boolean = false;
+    _log.forEach(l => {
+      if (l && l.indexOf('Examples:') > -1) {
+        containsExamples = true;
+      }
+    });
+    Utils.restore(vorpal.find);
+    assert(containsExamples);
+  });
+
+});
