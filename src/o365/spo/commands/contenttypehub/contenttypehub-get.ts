6c2dee78032444fdfd87ad2faabf6bd78af69331
Stijn Brouwers <stijnbrouwers@gmail.com>
2019-08-30 19:33:49 +0200

    Add unit tests and require logged in status when running the command

--- src/o365/spo/commands/contenttypehub/contenttypehub-get.ts
+++ src/o365/spo/commands/contenttypehub/contenttypehub-get.ts
@@ -2,6 +2,7 @@ import config from '../../../../config';
 import commands from '../../commands';
 import GlobalOptions from '../../../../GlobalOptions';
 import request from '../../../../request';
+import auth from '../../../../Auth';
 import {
   CommandOption,
   CommandValidate,
@@ -30,6 +31,11 @@ class SpoContentTypeHubGetCommand extends SpoCommand {
   }
 
   public commandAction(cmd: CommandInstance, args: CommandArgs, cb: (err?: any) => void): void {
+    if (!auth.service.connected) {
+      cb(new CommandError('Login to Office 365 first'));
+      return;
+    }
+
     let spoUrl: string = '';
 
     this
@@ -49,24 +55,24 @@ class SpoContentTypeHubGetCommand extends SpoCommand {
             'X-RequestDigest': res.FormDigestValue
           },
           body: `<Request xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009" AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}">
-          <Actions>
-             <ObjectPath Id="2" ObjectPathId="1" />
-             <ObjectIdentityQuery Id="3" ObjectPathId="1" />
-             <ObjectPath Id="5" ObjectPathId="4" />
-             <ObjectIdentityQuery Id="6" ObjectPathId="4" />
-             <Query Id="7" ObjectPathId="4">
-                <Query SelectAllProperties="false">
-                   <Properties>
-                      <Property Name="ContentTypePublishingHub" ScalarProperty="true" />
-                   </Properties>
-                </Query>
-             </Query>
-          </Actions>
-          <ObjectPaths>
-             <StaticMethod Id="1" Name="GetTaxonomySession" TypeId="{981cbc68-9edc-4f8d-872f-71146fcbb84f}" />
-             <Method Id="4" ParentId="1" Name="GetDefaultSiteCollectionTermStore" />
-          </ObjectPaths>
-       </Request>`
+  <Actions>
+    <ObjectPath Id="2" ObjectPathId="1" />
+    <ObjectIdentityQuery Id="3" ObjectPathId="1" />
+    <ObjectPath Id="5" ObjectPathId="4" />
+    <ObjectIdentityQuery Id="6" ObjectPathId="4" />
+    <Query Id="7" ObjectPathId="4">
+      <Query SelectAllProperties="false">
+        <Properties>
+          <Property Name="ContentTypePublishingHub" ScalarProperty="true" />
+        </Properties>
+      </Query>
+    </Query>
+  </Actions>
+  <ObjectPaths>
+    <StaticMethod Id="1" Name="GetTaxonomySession" TypeId="{981cbc68-9edc-4f8d-872f-71146fcbb84f}" />
+    <Method Id="4" ParentId="1" Name="GetDefaultSiteCollectionTermStore" />
+  </ObjectPaths>
+</Request>`
         };
 
         return request.post(requestOptions);
