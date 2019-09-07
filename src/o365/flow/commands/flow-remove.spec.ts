c88156d470489d6585aa8f0d2e9cc8d2955e44cb
Garry Trinder <garry.trinder@live.com>
2019-09-07 01:34:04 +0100

    Added 100% test coverage

--- src/o365/flow/commands/flow-remove.spec.ts
+++ src/o365/flow/commands/flow-remove.spec.ts
@@ -73,6 +73,16 @@ describe(commands.FLOW_REMOVE, () => {
     assert.notEqual(actual, true);
   });
 
+  it('fails validation if the name is not valid GUID', () => {
+    const actual = (command.validate() as CommandValidate)({
+      options: {
+        environment: 'Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c',
+        name: 'invalid'
+      }
+    });
+    assert.notEqual(actual, true);
+  });
+
   it('fails validation if the environment is not specified', () => {
     const actual = (command.validate() as CommandValidate)({
       options: {
@@ -81,6 +91,7 @@ describe(commands.FLOW_REMOVE, () => {
     });
     assert.notEqual(actual, true);
   });
+
 
   it('passes validation when the name and environment specified', () => {
     const actual = (command.validate() as CommandValidate)({
@@ -92,7 +103,7 @@ describe(commands.FLOW_REMOVE, () => {
     assert.equal(actual, true);
   });
 
-  it('prompts before removing the specified Microsoft Flow when confirm option not passed', (done) => {
+  it('prompts before removing the specified Microsoft Flow owned by the currently signed-in user when confirm option not passed', (done) => {
     cmdInstance.action({
       options: {
         debug: false,
@@ -116,7 +127,7 @@ describe(commands.FLOW_REMOVE, () => {
     });
   });
 
-  it('aborts removing the specified Microsoft Flow when confirm option not passed and prompt not confirmed', (done) => {
+  it('aborts removing the specified Microsoft Flow owned by the currently signed-in user when confirm option not passed and prompt not confirmed', (done) => {
     const postSpy = sinon.spy(request, 'delete');
     cmdInstance.prompt = (options: any, cb: (result: { continue: boolean }) => void) => {
       cb({ continue: false });
@@ -138,10 +149,10 @@ describe(commands.FLOW_REMOVE, () => {
     });
   });
 
-  it('removes the specified Microsoft Flow when prompt confirmed', (done) => {
+  it('removes the specified Microsoft Flow owned by the currently signed-in user when prompt confirmed', (done) => {
     sinon.stub(request, 'delete').callsFake((opts) => {
       if (opts.url === `https://management.azure.com/providers/Microsoft.ProcessSimple/environments/Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c/flows/0f64d9dd-01bb-4c1b-95b3-cb4a1a08ac72?api-version=2016-11-01`) {
-        return Promise.resolve();
+        return Promise.resolve({ statusCode: 200 });
       }
 
       return Promise.reject('Invalid request');
@@ -167,10 +178,58 @@ describe(commands.FLOW_REMOVE, () => {
     });
   });
 
-  it('removes the specified Microsoft Flow as Admin when prompt confirmed', (done) => {
+  it('prompts before removing the specified Microsoft Flow owned by another user when confirm option not passed', (done) => {
+    cmdInstance.action({
+      options: {
+        debug: false,
+        environment: 'Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c',
+        name: '0f64d9dd-01bb-4c1b-95b3-cb4a1a08ac72',
+        asAdmin: true
+      }
+    }, () => {
+      let promptIssued = false;
+
+      if (promptOptions && promptOptions.type === 'confirm') {
+        promptIssued = true;
+      }
+
+      try {
+        assert(promptIssued);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('aborts removing the specified Microsoft Flow owned by another user when confirm option not passed and prompt not confirmed', (done) => {
+    const postSpy = sinon.spy(request, 'delete');
+    cmdInstance.prompt = (options: any, cb: (result: { continue: boolean }) => void) => {
+      cb({ continue: false });
+    };
+    cmdInstance.action({
+      options: {
+        debug: false,
+        environment: 'Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c',
+        name: '0f64d9dd-01bb-4c1b-95b3-cb4a1a08ac72',
+        asAdmin: true
+      }
+    }, () => {
+      try {
+        assert(postSpy.notCalled);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('removes the specified Microsoft Flow owned by another user when prompt confirmed', (done) => {
     sinon.stub(request, 'delete').callsFake((opts) => {
       if (opts.url === `https://management.azure.com/providers/Microsoft.ProcessSimple/scopes/admin/environments/Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c/flows/0f64d9dd-01bb-4c1b-95b3-cb4a1a08ac72?api-version=2016-11-01`) {
-        return Promise.resolve();
+        return Promise.resolve({ statusCode: 200 });
       }
 
       return Promise.reject('Invalid request');
@@ -200,7 +259,7 @@ describe(commands.FLOW_REMOVE, () => {
   it('removes the specified Microsoft Flow without prompting when confirm specified', (done) => {
     sinon.stub(request, 'delete').callsFake((opts) => {
       if (opts.url === `https://management.azure.com/providers/Microsoft.ProcessSimple/environments/Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c/flows/0f64d9dd-01bb-4c1b-95b3-cb4a1a08ac72?api-version=2016-11-01`) {
-        return Promise.resolve();
+        return Promise.resolve({ statusCode: 200 });
       }
 
       return Promise.reject('Invalid request');
@@ -221,7 +280,7 @@ describe(commands.FLOW_REMOVE, () => {
   it('removes the specified Microsoft Flow without prompting when confirm specified', (done) => {
     sinon.stub(request, 'delete').callsFake((opts) => {
       if (opts.url === `https://management.azure.com/providers/Microsoft.ProcessSimple/environments/Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c/flows/0f64d9dd-01bb-4c1b-95b3-cb4a1a08ac72?api-version=2016-11-01`) {
-        return Promise.resolve();
+        return Promise.resolve({ statusCode: 200 });
       }
 
       return Promise.reject('Invalid request');
@@ -242,7 +301,7 @@ describe(commands.FLOW_REMOVE, () => {
   it('removes the specified Microsoft Flow as Admin without prompting when confirm specified', (done) => {
     sinon.stub(request, 'delete').callsFake((opts) => {
       if (opts.url === `https://management.azure.com/providers/Microsoft.ProcessSimple/scopes/admin/environments/Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c/flows/0f64d9dd-01bb-4c1b-95b3-cb4a1a08ac72?api-version=2016-11-01`) {
-        return Promise.resolve();
+        return Promise.resolve({ statusCode: 200 });
       }
 
       return Promise.reject('Invalid request');
@@ -290,36 +349,6 @@ describe(commands.FLOW_REMOVE, () => {
     });
   });
 
-  it('correctly handles no environment found as Admin without prompting when confirm specified', (done) => {
-    sinon.stub(request, 'delete').callsFake((opts) => {
-      return Promise.reject({
-        "error": {
-          "code": "EnvironmentAccessDenied",
-          "message": "Access to the environment 'Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c' is denied."
-        }
-      });
-    });
-
-    cmdInstance.action({
-      options:
-      {
-        debug: false,
-        environment: 'Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c',
-        name: '0f64d9dd-01bb-4c1b-95b3-cb4a1a08ac72',
-        confirm: true,
-        asAdmin: true
-      }
-    }, (err?: any) => {
-      try {
-        assert.equal(JSON.stringify(err), JSON.stringify(new CommandError(`Access to the environment 'Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c' is denied.`)));
-        done();
-      }
-      catch (e) {
-        done(e);
-      }
-    });
-  });
-
   it('correctly handles no environment found when prompt confirmed', (done) => {
     sinon.stub(request, 'delete').callsFake((opts) => {
       return Promise.reject({
@@ -352,14 +381,9 @@ describe(commands.FLOW_REMOVE, () => {
     });
   });
 
-  it('correctly handles no environment found as Admin when prompt confirmed', (done) => {
+  it('correctly handles no Microsoft Flow found when prompt confirmed', (done) => {
     sinon.stub(request, 'delete').callsFake((opts) => {
-      return Promise.reject({
-        "error": {
-          "code": "EnvironmentAccessDenied",
-          "message": "Access to the environment 'Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c' is denied."
-        }
-      });
+      return Promise.resolve({ statusCode: 204 });
     });
 
     cmdInstance.prompt = (options: any, cb: (result: { continue: boolean }) => void) => {
@@ -371,12 +395,35 @@ describe(commands.FLOW_REMOVE, () => {
       {
         debug: false,
         environment: 'Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c',
-        name: '0f64d9dd-01bb-4c1b-95b3-cb4a1a08ac72',
-        asAdmin: true
+        name: '0f64d9dd-01bb-4c1b-95b3-cb4a1a08ac72'
       }
     }, (err?: any) => {
       try {
-        assert.equal(JSON.stringify(err), JSON.stringify(new CommandError(`Access to the environment 'Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c' is denied.`)));
+        assert(cmdInstanceLogSpy.called);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('correctly handles no Microsoft Flow found when confirm specified', (done) => {
+    sinon.stub(request, 'delete').callsFake((opts) => {
+      return Promise.resolve({ statusCode: 204 });
+    });
+
+    cmdInstance.action({
+      options:
+      {
+        debug: false,
+        environment: 'Default-eff8592e-e14a-4ae8-8771-d96d5c549e1c',
+        name: '0f64d9dd-01bb-4c1b-95b3-cb4a1a08ac72',
+        confirm: true
+      }
+    }, (err?: any) => {
+      try {
+        assert(cmdInstanceLogSpy.called);
         done();
       }
       catch (e) {
