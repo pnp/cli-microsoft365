785c149b5d70c089613b2abc30ba157439e67bf3
Waldek Mastykarz <waldek@mastykarz.nl>
2019-09-15 16:56:09 +0200

    Fixed handling Flow nextLink solving #1114

--- src/o365/base/AzmgmtItemsListCommand.ts
+++ src/o365/base/AzmgmtItemsListCommand.ts
@@ -1,6 +1,7 @@
 import AzmgmtCommand from "./AzmgmtCommand";
 import request from '../../request';
 import { AzmgmtResponse } from './AzmgmtResponse';
+import * as url from 'url';
 
 export abstract class AzmgmtItemsListCommand<T> extends AzmgmtCommand {
   protected items: T[];
@@ -11,10 +12,10 @@ export abstract class AzmgmtItemsListCommand<T> extends AzmgmtCommand {
     this.items = [];
   }
 
-  protected getAllItems(url: string, cmd: CommandInstance, firstRun: boolean): Promise<void> {
+  protected getAllItems(_url: string, cmd: CommandInstance, firstRun: boolean): Promise<void> {
     return new Promise<void>((resolve: () => void, reject: (error: any) => void): void => {
       const requestOptions: any = {
-        url: url,
+        url: _url,
         headers: {
           accept: 'application/json;odata.metadata=none'
         },
@@ -31,8 +32,20 @@ export abstract class AzmgmtItemsListCommand<T> extends AzmgmtCommand {
           this.items = this.items.concat(res.value);
 
           if (res.nextLink) {
+            // when retrieving Flows as admin, the API returns nextLink
+            // pointing to https://emea.api.flow.microsoft.com:11777
+            // which leads to authentication exceptions because it's not an AAD
+            // resource for which we can retrieve an access token, so we need to
+            // rewrite it back to the API management URL
+            const nextLinkUrl: url.URL = new url.URL(res.nextLink);
+            if (nextLinkUrl.host !== 'management.azure.com') {
+              nextLinkUrl.host = 'management.azure.com';
+              nextLinkUrl.port = '';
+            }
+            const nextLink: string = nextLinkUrl.href;
+
             this
-              .getAllItems(res.nextLink as string, cmd, false)
+              .getAllItems(nextLink, cmd, false)
               .then((): void => {
                 resolve();
               }, (err: any): void => {
