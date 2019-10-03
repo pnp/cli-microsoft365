ff956a308d0df91cf80c62a0b963d25aa8859022
Rabia Williams <rabiawilliams@gmail.com>
2019-10-03 23:53:25 +1000

    Fixed bug for failing test in command `list webhook add` resolving #1131

--- src/o365/spo/commands/list/list-webhook-add.spec.ts
+++ src/o365/spo/commands/list/list-webhook-add.spec.ts
@@ -476,7 +476,8 @@ describe(commands.LIST_WEBHOOK_ADD, () => {
 
   it('passes validation if the expirationDateTime is in the future but no more than six months from now', () => {
     const currentDate: Date = new Date();
-    const dateString: string = currentDate.getFullYear() + "-" + (currentDate.getMonth() + 4) + "-01";
+    currentDate.setMonth(currentDate.getMonth() + 4);
+    const dateString: string = currentDate.getFullYear() + "-" + currentDate.getMonth() + "-01";
 
     const actual = (command.validate() as CommandValidate)({
       options:
