7f74541c73b672a69db419169b8945668a22bac9
Vincent Biret <vincentbiret@hotmail.com>
2019-10-10 10:30:06 -0400

    externalize: added filename auto detection

--- src/o365/spfx/commands/project/project-externalize/rules/DynamicRule.ts
+++ src/o365/spfx/commands/project/project-externalize/rules/DynamicRule.ts
@@ -1,6 +1,7 @@
 import { BasicDependencyRule } from "./BasicDependencyRule";
 import { Project } from "../../project-upgrade/model";
 import { ExternalizeEntry } from "../model/ExternalizeEntry";
+import * as fs from 'fs';
 
 export class DynamicRule extends BasicDependencyRule {
   public get ModuleName () {
@@ -16,17 +17,36 @@ export class DynamicRule extends BasicDependencyRule {
 
       validPackageNames.forEach((packageName) => {
         const version = project.packageJson && project.packageJson.dependencies[packageName];
-        if(version) {
+        const filePath = this.cleanFilePath(this.getFilePath(packageName));
+        if(version && filePath) {
           findings.push({
             key: packageName,
-            path: `https://unpkg.com/${packageName}@${version}/dist/pnpjs.es5.umd.bundle.min.js`,
+            path: `https://unpkg.com/${packageName}@${version}/${filePath}`,
           });
         }
       });
     }
-  }//TODO get filename from package.json first module then main
+  }
   //TODO try injecting min in name
   //TODO head test on url
+  private getFilePath = (packageName: string): string | undefined => {
+    const packageJsonFilePath = `node_modules/${packageName}/package.json`;
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonFilePath, 'utf8'));
+    if(packageJson.module) {
+      return packageJson.module;
+    } else if (packageJson.main) {
+      return packageJson.main;
+    } else {
+      return undefined;
+    }
+  }
+  private cleanFilePath = (filePath: string | undefined): string | undefined => {
+    if(filePath) {
+      return filePath.replace('./', '');
+    } else {
+      return undefined;
+    }
+  }
   private restrictedModules = ['react', 'react-dom', '@microsoft/decorators', '@microsoft/sp-application-base', '@microsoft/sp-core-library', '@microsoft/sp-lodash-subset', '@microsoft/sp-office-ui-fabric-core', '@microsoft/sp-page-context', '@microsoft/sp-webpart-base'];
-  private typesPrefix = '@types/'
+  private typesPrefix = '@types/';
 }
