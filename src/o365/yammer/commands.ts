d05ecb772cd0db528e99a355a04232ea8fc38b6c
Patrick Lamber <patrick@nubo.eu>
2019-10-19 00:23:18 +0200

    Added the 'yammer user list' command solving #1113

--- src/o365/yammer/commands.ts
+++ src/o365/yammer/commands.ts
@@ -2,5 +2,6 @@ const prefix: string = 'yammer';
 
 export default {
   YAMMER_NETWORK_LIST: `${prefix} network list`,
-  YAMMER_USER_GET: `${prefix} user get`
+  YAMMER_USER_GET: `${prefix} user get`,
+  YAMMER_USER_LIST: `${prefix} user list`
 };
