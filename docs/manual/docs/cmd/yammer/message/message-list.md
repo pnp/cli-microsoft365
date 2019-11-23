51b8f39b01adfed1f7ce9a94cc04c6ba121bf15c
Patrick Lamber <patrick@nubo.eu>
2019-11-23 08:08:18 +0100

    Extended the 'yammer message list' command solving #1184

--- docs/manual/docs/cmd/yammer/message/message-list.md
+++ docs/manual/docs/cmd/yammer/message/message-list.md
@@ -15,6 +15,9 @@ Option|Description
 `--help`|output usage information
 `--olderThanId [olderThanId]`|Returns messages older than the message ID specified as a numeric string
 `--threaded`|Will only return the thread starter (first message) for each thread. This parameter is intended for apps which need to display message threads collapsed
+`-f, --feedType [feedType]`|Returns messages from a specific feed. Available options: `All|Top|My|Following|Sent|Private|Received`. Default `All`
+`--groupId [groupId]`|Returns the messages from a specific group
+`--threadId [threadId]`|Returns the messages from a specific thread
 `--limit [limit]`|Limits the messages returned
 `-o, --output [output]`|Output type. `json|text`. Default `text`
 `--verbose`|Runs command with verbose logging
@@ -24,6 +27,16 @@ Option|Description
 
 !!! attention
     In order to use this command, you need to grant the Azure AD application used by the Office 365 CLI the permission to the Yammer API. To do this, execute the `consent --service yammer` command.
+
+Feed types
+
+- All: Corresponds to “All” conversations in the Yammer web interface
+- Top: The algorithmic feed for the user that corresponds to "Top" conversations. The Top conversations feed is the feed currently shown in the Yammer mobile apps
+- My: The user’s feed, based on the selection they have made between "Following" and "Top" conversations
+- Following: The "Following" feed which is conversations involving people and topics that the user is following
+- Sent: All messages sent by the user
+- Private: Private messages received by the user
+- Received: All messages received by the user
 
 ## Examples
 
@@ -49,4 +62,22 @@ Returns the first 10 Yammer network messages
 
 ```sh
 yammer message list --limit 10
+```
+
+Returns the first 10 Yammer network messages from the Yammer group 312891231
+
+```sh
+yammer message list --groupId 312891231  --limit 10
+```
+
+Returns the first 10 Yammer network messages from thread 5611239081
+
+```sh
+yammer message list --threadId 5611239081  --limit 10
+```
+
+Returns the first 20 Yammer message from the sent feed of the user
+
+```sh
+yammer message list --feedType Sent  --limit 20
 ```
--- src/o365/yammer/commands/message/message-list.ts
+++ src/o365/yammer/commands/message/message-list.ts
@@ -16,10 +16,14 @@ interface Options extends GlobalOptions {
   olderThanId?: number;
   threaded?: boolean;
   limit?: number;
+  feedType?: string;
+  groupId?: number;
+  threadId?: number;
 }
 
 class YammerMessageListCommand extends YammerCommand {
-  protected items: any[];
+  private items: any[];
+  private static readonly feedTypes: string[] = ['All', 'Top', 'My', 'Following', 'Sent', 'Private', 'Received'];
 
   /* istanbul ignore next */
   constructor() {
@@ -40,32 +44,71 @@ class YammerMessageListCommand extends YammerCommand {
     telemetryProps.olderThanId = args.options.olderThanId !== undefined;
     telemetryProps.threaded = args.options.threaded;
     telemetryProps.limit = args.options.limit !== undefined;
+    telemetryProps.feedType = args.options.feedType !== undefined;
+    telemetryProps.threadId = args.options.threadId !== undefined;
+    telemetryProps.groupId = args.options.groupId !== undefined;
     return telemetryProps;
   }
 
   private getAllItems(cmd: CommandInstance, args: CommandArgs, messageId: number): Promise<void> {
     return new Promise<void>((resolve: () => void, reject: (error: any) => void): void => {
-      let endPoint = `${this.resource}/v1/messages.json`;
+      let endpoint = `${this.resource}/v1`;
+
+      if (args.options.threadId) {
+        endpoint += `/messages/in_thread/${args.options.threadId}.json`
+      }
+      else if (args.options.groupId) {
+        endpoint += `/messages/in_group/${args.options.groupId}.json`
+      }
+      else {
+        if (!args.options.feedType) {
+          args.options.feedType = "All";
+        }
+
+        switch (args.options.feedType) {
+          case 'Top':
+            endpoint += `/messages/algo.json`;
+            break;
+          case 'My':
+            endpoint += `/messages/my_feed.json`;
+            break;
+          case 'Following':
+            endpoint += `/messages/following.json`;
+            break;
+          case 'Sent':
+            endpoint += `/messages/sent.json`;
+            break;
+          case 'Private':
+            endpoint += `/messages/private.json`;
+            break;
+          case 'Received':
+            endpoint += `/messages/received.json`;
+            break;
+          default:
+            endpoint += `/messages.json`;
+        }
+      }
 
       if (messageId !== -1) {
-        endPoint += `?older_than=${messageId}`;
+        endpoint += `?older_than=${messageId}`;
       }
       else if (args.options.olderThanId) {
-        endPoint += `?older_than=${args.options.olderThanId}`;
+        endpoint += `?older_than=${args.options.olderThanId}`;
       }
 
       if (args.options.threaded) {
-        if (endPoint.indexOf("?") > -1) {
-          endPoint += "&";
-        } else {
-          endPoint += "?";
+        if (endpoint.indexOf("?") > -1) {
+          endpoint += "&";
+        }
+        else {
+          endpoint += "?";
         }
 
-        endPoint += `threaded=true`;
+        endpoint += `threaded=true`;
       }
 
       const requestOptions: any = {
-        url: endPoint,
+        url: endpoint,
         headers: {
           accept: 'application/json;odata.metadata=none',
           'content-type': 'application/json;odata=nometadata'
@@ -84,7 +127,8 @@ class YammerMessageListCommand extends YammerCommand {
           }
           else {
             if (res.meta.older_available === true) {
-              this.getAllItems(cmd, args, this.items[this.items.length - 1].id)
+              this
+                .getAllItems(cmd, args, this.items[this.items.length - 1].id)
                 .then((): void => {
                   resolve();
                 }, (err: any): void => {
@@ -148,6 +192,19 @@ class YammerMessageListCommand extends YammerCommand {
         description: 'Returns messages older than the message ID specified as a numeric string'
       },
       {
+        option: '-f, --feedType [feedType]',
+        description: 'Returns messages from a specific feed. Available options: All|Top|My|Following|Sent|Private|Received. Default All',
+        autocomplete: YammerMessageListCommand.feedTypes
+      },
+      {
+        option: '--groupId [groupId]',
+        description: 'Returns the messages from a specific group'
+      },
+      {
+        option: '--threadId [threadId]',
+        description: 'Returns the messages from a specific thread'
+      },
+      {
         option: '--threaded',
         description: 'Will only return the thread starter (first message) for each thread. This parameter is intended for apps which need to display message threads collapsed'
       },
@@ -163,8 +220,28 @@ class YammerMessageListCommand extends YammerCommand {
 
   public validate(): CommandValidate {
     return (args: CommandArgs): boolean | string => {
+      if (args.options.groupId && args.options.threadId) {
+        return `You cannot specify groupId and threadId at the same time`;
+      }
+
+      if (args.options.feedType && (args.options.groupId || args.options.threadId)) {
+        return `You cannot specify the feedType with groupId or threadId at the same time`;
+      }
+
+      if (args.options.feedType && YammerMessageListCommand.feedTypes.indexOf(args.options.feedType) < 0) {
+        return `${args.options.feedType} is not a valid value for the feedType option. Allowed values are ${YammerMessageListCommand.feedTypes.join('|')}`;
+      }
+
       if (args.options.olderThanId && typeof args.options.olderThanId !== 'number') {
         return `${args.options.olderThanId} is not a number`;
+      }
+
+      if (args.options.groupId && typeof args.options.groupId !== 'number') {
+        return `${args.options.groupId} is not a number`;
+      }
+
+      if (args.options.threadId && typeof args.options.threadId !== 'number') {
+        return `${args.options.threadId} is not a number`;
       }
 
       if (args.options.limit && typeof args.options.limit !== 'number') {
@@ -184,6 +261,20 @@ class YammerMessageListCommand extends YammerCommand {
     ${chalk.yellow('Attention:')} In order to use this command, you need to grant the Azure AD
     application used by the Office 365 CLI the permission to the Yammer API.
     To do this, execute the ${chalk.blue('consent --service yammer')} command.
+
+    Feed types:
+  
+      - All: Corresponds to “All” conversations in the Yammer web interface
+      - Top: The algorithmic feed for the user that corresponds to "Top"
+        conversations. The Top conversations feed is the feed currently shown in
+        the Yammer mobile apps
+      - My: The user’s feed, based on the selection they have made between
+        "Following" and "Top" conversations
+      - Following: The "Following" feed which is conversations involving people
+        and topics that the user is following
+      - Sent: All messages sent by the user
+      - Private: Private messages received by the user
+      - Received: All messages received by the user
     
   Examples:
     
@@ -198,7 +289,16 @@ class YammerMessageListCommand extends YammerCommand {
 
     Returns the first 10 Yammer network messages
       ${this.name} --limit 10
-    `);
+
+    Returns the first 10 Yammer network messages from the Yammer group 312891231
+      ${this.name} --groupId 312891231 --limit 10
+
+    Returns the first 10 Yammer network messages from thread 5611239081
+      ${this.name} --threadId 5611239081 --limit 10
+    
+    Returns the first 20 Yammer message from the sent feed of the user
+      ${this.name} --feedType Sent --limit 20
+`);
   }
 }
 
