f51cf37bbb6e06a6c2ee0c4e63a6bb1ebf9295e8
Patrick Lamber <patrick@nubo.eu>
2019-11-29 12:56:35 +0100

    changed command name

--- src/o365/outlook/commands.ts
+++ src/o365/outlook/commands.ts
@@ -3,5 +3,5 @@ const prefix: string = 'outlook';
 export default {
   OUTLOOK_SENDMAIL: `${prefix} sendmail`,
   OUTLOOK_MAIL_SEND: `${prefix} mail send`,
-  OUTLOOK_REPORT_MAILBOXUSAGEQUOTASTATUSMAILBOXCOUNTS: `${prefix} usage mailboxusagequotastatusmailboxcounts`
+  OUTLOOK_REPORT_MAILBOXUSAGEQUOTASTATUSMAILBOXCOUNTS: `${prefix} report mailboxusagequotastatusmailboxcounts`
 };
