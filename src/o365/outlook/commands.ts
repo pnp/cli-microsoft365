632b0dbe22ff70107ee1326b31a3ec5ffe6a8355
Patrick Lamber <patrick@nubo.eu>
2019-12-01 15:16:17 +0100

    Added 'outlook report mailappusageusercounts' command solving #1214

--- src/o365/outlook/commands.ts
+++ src/o365/outlook/commands.ts
@@ -6,6 +6,7 @@ export default {
   OUTLOOK_REPORT_MAILACTIVITYUSERCOUNTS: `${prefix} report mailactivityusercounts`,
   OUTLOOK_REPORT_MAILACTIVITYUSERDETAIL: `${prefix} report mailactivityuserdetail`,
   OUTLOOK_REPORT_MAILAPPUSAGEAPPSUSERCOUNTS: `${prefix} report mailappusageappsusercounts`,
+  OUTLOOK_REPORT_MAILAPPUSAGEUSERCOUNTS: `${prefix} report mailappusageusercounts`,
   OUTLOOK_REPORT_MAILAPPUSAGEUSERDETAIL: `${prefix} report mailappusageuserdetail`,
   OUTLOOK_REPORT_MAILAPPUSAGEVERSIONSUSERCOUNTS: `${prefix} report mailappusageversionsusercounts`,
   OUTLOOK_REPORT_MAILBOXUSAGEDETAIL: `${prefix} report mailboxusagedetail`,
