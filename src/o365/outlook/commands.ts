6aa910bca399a80ae7d8cec1f1876a5288e99e48
Patrick Lamber <patrick@nubo.eu>
2019-12-06 07:51:37 +0100

    implements #1209

--- src/o365/outlook/commands.ts
+++ src/o365/outlook/commands.ts
@@ -1,7 +1,8 @@
 const prefix: string = 'outlook';
 
 export default {
-  OUTLOOK_MAIL_SEND: `${prefix} mail send`,
+  OUTLOOK_MAIL_SEND: `${prefix} mail send`,  
+  OUTLOOK_REPORT_MAILACTIVITYUSERDETAIL: `${prefix} report mailactivityuserdetail`,
   OUTLOOK_REPORT_MAILBOXUSAGEQUOTASTATUSMAILBOXCOUNTS: `${prefix} report mailboxusagequotastatusmailboxcounts`,
   OUTLOOK_REPORT_MAILBOXUSAGESTORAGE: `${prefix} report mailboxusagestorage`,
   OUTLOOK_SENDMAIL: `${prefix} sendmail`
