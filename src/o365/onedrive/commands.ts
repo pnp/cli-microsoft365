cf7d540ea0638b9b7de06490ba29099c0c701de1
Patrick Lamber <patrick@nubo.eu>
2019-12-07 16:20:57 +0100

    Added the 'onedrive report usagestorage' command solving #1254

--- src/o365/onedrive/commands.ts
+++ src/o365/onedrive/commands.ts
@@ -3,4 +3,5 @@ const prefix: string = 'onedrive';
 export default {
   REPORT_ACTIVITYFILECOUNTS: `${prefix} report activityfilecounts`,
   REPORT_ACTIVITYUSERCOUNTS: `${prefix} report activityusercounts`,
+  REPORT_USAGESTORAGE: `${prefix} report usagestorage`
 };
