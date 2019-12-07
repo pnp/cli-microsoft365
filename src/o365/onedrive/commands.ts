e693060e1a8884f854e87f638234f209de8174f6
Patrick Lamber <patrick@nubo.eu>
2019-12-07 15:46:03 +0100

    Added the 'onedrive report activityusercounts' command solving #1256

--- src/o365/onedrive/commands.ts
+++ src/o365/onedrive/commands.ts
@@ -1,5 +1,6 @@
 const prefix: string = 'onedrive';
 
 export default {
-  REPORT_ACTIVITYFILECOUNTS: `${prefix} report activityfilecounts`
+  REPORT_ACTIVITYFILECOUNTS: `${prefix} report activityfilecounts`,
+  REPORT_ACTIVITYUSERCOUNTS: `${prefix} report activityusercounts`,
 };
