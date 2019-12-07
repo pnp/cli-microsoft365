08e92d39b6141c378506b1bc28794d1fde5a06dd
Patrick Lamber <patrick@nubo.eu>
2019-12-07 20:21:51 +0100

    implements #1253

---
+++ src/o365/spo/commands/report/spo-report-onedriveusagefilecounts.ts
@@ -0,0 +1,42 @@
+import commands from '../../commands';
+import PeriodBasedReport from '../../../base/PeriodBasedReport';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+class SpoReportOneDriveUsageFileCountsCommand extends PeriodBasedReport {
+  public get name(): string {
+    return commands.REPORT_ONEDRIVEUSAGEFILECOUNTS;
+  }
+
+  public get usageEndpoint(): string {
+    return 'getOneDriveUsageFileCounts';
+  }
+
+  public get description(): string {
+    return 'Gets the total number of files across all sites and how many are active files';
+  }
+
+  public commandHelp(args: {}, log: (help: string) => void): void {
+    log(vorpal.find(this.name).helpInformation());
+    log(
+      `  Remarks:
+      
+    A file is considered active if it has been saved, synced, modified, or shared within the specified time period.     
+        
+         Examples:
+      
+    Gets the total number of files across all sites and how many are active files for the last week
+      ${commands.REPORT_ONEDRIVEUSAGEFILECOUNTS} --period D7
+
+    Gets the total number of files across all sites and how many are active files for the last week
+    and exports the report data in the specified path in text format
+      ${commands.REPORT_ONEDRIVEUSAGEFILECOUNTS} --period D7 --output text --outputFile 'C:/report.txt'
+
+    Gets the total number of files across all sites and how many are active files for the last week
+    and exports the report data in the specified path in json format
+      ${commands.REPORT_ONEDRIVEUSAGEFILECOUNTS} --period D7 --output json --outputFile 'C:/report.json'
+`);
+  }
+}
+
+module.exports = new SpoReportOneDriveUsageFileCountsCommand();
