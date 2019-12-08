67eb2000d11f79ab853059002759728b00d322b1
Patrick Lamber <patrick@nubo.eu>
2019-12-08 07:16:17 +0100

    implements #1252

--- src/o365/spo/commands.ts
+++ src/o365/spo/commands.ts
@@ -135,6 +135,7 @@ export default {
   SERVICEPRINCIPAL_SET: `${prefix} serviceprincipal set`,
   SEARCH: `${prefix} search`,
   GET: `${prefix} get`,
+  REPORT_ONEDRIVEUSAGEACCOUNTCOUNTS: `${prefix} report onedriveusageaccountcounts`,
   SET: `${prefix} set`,
   SITE_ADD: `${prefix} site add`,
   SITE_APPCATALOG_ADD: `${prefix} site appcatalog add`,
---
+++ src/o365/spo/commands/report/spo-report-onedriveusageaccountcounts.ts
@@ -0,0 +1,38 @@
+import commands from '../../commands';
+import PeriodBasedReport from '../../../base/PeriodBasedReport';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+class SpoReportOneDriveUsageAccountCountsCommand extends PeriodBasedReport {
+  public get name(): string {
+    return commands.REPORT_ONEDRIVEUSAGEACCOUNTCOUNTS;
+  }
+
+  public get usageEndpoint(): string {
+    return 'getOneDriveUsageAccountCounts';
+  }
+
+  public get description(): string {
+    return 'Gets the trend in the number of active OneDrive for Business sites';
+  }
+
+  public commandHelp(args: {}, log: (help: string) => void): void {
+    log(vorpal.find(this.name).helpInformation());
+    log(
+      `  Examples:
+      
+    Gets the trend in the number of active OneDrive for Business sites for the last week
+      ${commands.REPORT_ONEDRIVEUSAGEACCOUNTCOUNTS} --period D7
+
+    Gets the trend in the number of active OneDrive for Business sites for the last week
+    and exports the report data in the specified path in text format
+      ${commands.REPORT_ONEDRIVEUSAGEACCOUNTCOUNTS} --period D7 --output text --outputFile 'C:/report.txt'
+
+    Gets the trend in the number of active OneDrive for Business sites for the last week
+    and exports the report data in the specified path in json format
+      ${commands.REPORT_ONEDRIVEUSAGEACCOUNTCOUNTS} --period D7 --output json --outputFile 'C:/report.json'
+`);
+  }
+}
+
+module.exports = new SpoReportOneDriveUsageAccountCountsCommand();
