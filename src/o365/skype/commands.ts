bbedd0b2b6acedbfeb003c1e6216b1d8d2007f24
Patrick Lamber <patrick@nubo.eu>
2020-03-07 05:04:28 +0100

    Adds 'skype report activityusercounts' command. Closes #1303

--- src/o365/skype/commands.ts
+++ src/o365/skype/commands.ts
@@ -1,5 +1,6 @@
 const prefix: string = 'skype';
 
 export default {
-  SKYPE_REPORT_ACTIVITYCOUNTS: `${prefix} report activitycounts`
+  SKYPE_REPORT_ACTIVITYCOUNTS: `${prefix} report activitycounts`,
+  SKYPE_REPORT_ACTIVITYUSERCOUNTS: `${prefix} report activityusercounts`
 };
---
+++ src/o365/skype/commands/report/report-activityusercounts.ts
@@ -0,0 +1,45 @@
+import commands from '../../commands';
+import PeriodBasedReport from '../../../base/PeriodBasedReport';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+class SkypeReportActivityUserCountsCommand extends PeriodBasedReport {
+  public get name(): string {
+    return commands.SKYPE_REPORT_ACTIVITYUSERCOUNTS;
+  }
+
+  public get usageEndpoint(): string {
+    return 'getSkypeForBusinessActivityUserCounts';
+  }
+
+  public get description(): string {
+    return 'Gets the trends on how many unique users organized and participated in conference sessions held in your organization through Skype for Business. The report also includes the number of peer-to-peer sessions';
+  }
+
+  public commandHelp(args: {}, log: (help: string) => void): void {
+    log(vorpal.find(this.name).helpInformation());
+    log(
+      `  Examples:
+      
+    Gets the trends on how many unique users organized and participated in
+    conference  sessions held in your organization through Skype for Business.
+    The report also includes the number of peer-to-peer sessions for the last
+    week
+      ${commands.SKYPE_REPORT_ACTIVITYUSERCOUNTS} --period D7
+
+    Gets the trends on how many unique users organized and participated in
+    conference sessions held in your organization through Skype for Business.
+    The report also includes the number of peer-to-peer sessions for the last
+    week and exports the report data in the specified path in text format
+      ${commands.SKYPE_REPORT_ACTIVITYUSERCOUNTS} --period D7 --output text --outputFile "activityusercounts.txt"
+
+    Gets the trends on how many unique users organized and participated in
+    conference sessions held in your organization through Skype for Business.
+    The report also includes the number of peer-to-peer sessions for the last
+    week and exports the report data in the specified path in json format
+      ${commands.SKYPE_REPORT_ACTIVITYUSERCOUNTS} --period D7 --output json --outputFile "activityusercounts.json"
+`);
+  }
+}
+
+module.exports = new SkypeReportActivityUserCountsCommand();
