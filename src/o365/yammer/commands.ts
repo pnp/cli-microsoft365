5313cdd8adbbc0a020744c224497b6d6f23e4d91
Patrick Lamber <patrick@nubo.eu>
2020-03-07 04:27:59 +0100

    Adds 'yammer report groupsactivitycounts' command. Closes #1390

--- src/o365/yammer/commands.ts
+++ src/o365/yammer/commands.ts
@@ -7,6 +7,7 @@ export default {
   YAMMER_MESSAGE_LIST: `${prefix} message list`,
   YAMMER_MESSAGE_REMOVE: `${prefix} message remove`,
   YAMMER_NETWORK_LIST: `${prefix} network list`,
+  YAMMER_REPORT_GROUPSACTIVITYCOUNTS: `${prefix} report groupsactivitycounts`,
   YAMMER_USER_GET: `${prefix} user get`,
   YAMMER_USER_LIST: `${prefix} user list`
 };
---
+++ src/o365/yammer/commands/report/report-groupsactivitycounts.ts
@@ -0,0 +1,41 @@
+import commands from '../../commands';
+import PeriodBasedReport from '../../../base/PeriodBasedReport';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+class YammerReportGroupsActivityCountsCommand extends PeriodBasedReport {
+  public get name(): string {
+    return commands.YAMMER_REPORT_GROUPSACTIVITYCOUNTS;
+  }
+
+  public get usageEndpoint(): string {
+    return 'getYammerGroupsActivityCounts';
+  }
+
+  public get description(): string {
+    return 'Gets the number of Yammer messages posted, read, and liked in groups';
+  }
+
+  public commandHelp(args: {}, log: (help: string) => void): void {
+    log(vorpal.find(this.name).helpInformation());
+    log(
+      `  Examples:
+      
+    Gets the number of Yammer messages posted, read, and liked in groups for
+    the last week
+      ${commands.YAMMER_REPORT_GROUPSACTIVITYCOUNTS} --period D7
+
+    Gets the number of Yammer messages posted, read, and liked in groups for
+    the last week and exports the report data in the specified path in text
+    format
+      ${commands.YAMMER_REPORT_GROUPSACTIVITYCOUNTS} --period D7 --output text --outputFile "groupsactivitycounts.txt"
+
+    Gets the number of Yammer messages posted, read, and liked in groups for
+    the last week and exports the report data in the specified path in json
+    format
+      ${commands.YAMMER_REPORT_GROUPSACTIVITYCOUNTS} --period D7 --output json --outputFile "groupsactivitycounts.json"
+`);
+  }
+}
+
+module.exports = new YammerReportGroupsActivityCountsCommand();
