2c7174aec8defe1e1fb4c7ef46667bd9c720abfb
Patrick Lamber <patrick@nubo.eu>
2020-03-07 04:27:59 +0100

    new command: yammer report groupsactivitycounts

---
+++ src/o365/yammer/commands/report/report-groupactivityfilecounts.ts
@@ -0,0 +1,38 @@
+import commands from '../../commands';
+import PeriodBasedReport from '../../../base/PeriodBasedReport';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+class YammerGroupActivityCountsCommand extends PeriodBasedReport {
+  public get name(): string {
+    return commands.YAMMER_REPORT_GROUPACTIVITYCOUNTS;
+  }
+
+  public get usageEndpoint(): string {
+    return 'getYammerGroupsActivityCounts';
+  }
+
+  public get description(): string {
+    return 'Gets the number of Yammer messages posted, read, and liked in groups';
+  }
+
+  public commandHelp(args: {}, log: (help: string) => void): void {
+    log(vorpal.find(this.name).helpInformation());
+    log(
+      `  Examples:
+      
+    Gets the number of Yammer messages posted, read, and liked in groups for the last week
+      ${commands.YAMMER_REPORT_GROUPACTIVITYCOUNTS} --period D7
+
+    Gets the number of Yammer messages posted, read, and liked in groups for the last week and exports the report data
+    in the specified path in text format
+      ${commands.YAMMER_REPORT_GROUPACTIVITYCOUNTS} --period D7 --output text --outputFile groupactivityfilecounts.txt
+
+    Gets the number of Yammer messages posted, read, and liked in groups for the last week and exports the report data
+    in the specified path in json format
+      ${commands.YAMMER_REPORT_GROUPACTIVITYCOUNTS} --period D7 --output json --outputFile groupactivityfilecounts.json
+`);
+  }
+}
+
+module.exports = new YammerGroupActivityCountsCommand();
