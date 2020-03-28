14ee72a737d641389fa2860902a2ca50184f3dba
Patrick Lamber <patrick@nubo.eu>
2020-03-28 16:26:08 +0100

    new-command: yammer report groupsactivitygroupcoun

--- src/o365/yammer/commands.ts
+++ src/o365/yammer/commands.ts
@@ -8,6 +8,7 @@ export default {
   YAMMER_MESSAGE_REMOVE: `${prefix} message remove`,
   YAMMER_NETWORK_LIST: `${prefix} network list`,
   YAMMER_REPORT_GROUPSACTIVITYCOUNTS: `${prefix} report groupsactivitycounts`,
+  YAMMER_REPORT_GROUPSACTIVITYGROUPCOUNTS: `${prefix} report groupsactivitygroupcounts`,
   YAMMER_USER_GET: `${prefix} user get`,
   YAMMER_USER_LIST: `${prefix} user list`
 };
