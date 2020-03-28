f898f8694f2fd673b63f4ec600dab76fdec18212
Patrick Lamber <patrick@nubo.eu>
2020-03-28 16:23:48 +0100

    Adds 'yammer report groupsactivitydetail' command. Closes #1388

--- src/o365/yammer/commands.ts
+++ src/o365/yammer/commands.ts
@@ -14,6 +14,7 @@ export default {
   YAMMER_REPORT_DEVICEUSAGEUSERCOUNTS: `${prefix} report deviceusageusercounts`,
   YAMMER_REPORT_DEVICEUSAGEUSERDETAIL: `${prefix} report deviceusageuserdetail`,
   YAMMER_REPORT_GROUPSACTIVITYCOUNTS: `${prefix} report groupsactivitycounts`,
+  YAMMER_REPORT_GROUPSACTIVITYDETAIL: `${prefix} report groupsactivitydetail`,
   YAMMER_USER_GET: `${prefix} user get`,
   YAMMER_USER_LIST: `${prefix} user list`
 };
---
+++ src/o365/yammer/commands/report/report-groupsactivitydetail.ts
@@ -0,0 +1,46 @@
+import commands from '../../commands';
+import DateAndPeriodBasedReport from '../../../base/DateAndPeriodBasedReport';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+class YammerReportGroupsActivityDetailCommand extends DateAndPeriodBasedReport {
+  public get name(): string {
+    return commands.YAMMER_REPORT_GROUPSACTIVITYDETAIL;
+  }
+
+  public get usageEndpoint(): string {
+    return 'getYammerGroupsActivityDetail';
+  }
+
+  public get description(): string {
+    return 'Gets details about Yammer groups activity by group';
+  }
+
+  public commandHelp(args: {}, log: (help: string) => void): void {
+    log(vorpal.find(this.name).helpInformation());
+    log(
+      `  Remarks:
+    
+    As this report is only available for the past 28 days, date parameter value
+    should be a date from that range.
+
+  Examples:
+      
+    Gets details about Yammer groups activity by group for the last week
+      ${commands.YAMMER_REPORT_GROUPSACTIVITYDETAIL} --period D7
+
+    Gets details about Yammer groups activity by group for July 1, 2019
+      ${commands.YAMMER_REPORT_GROUPSACTIVITYDETAIL} --date 2019-07-01
+
+    Gets details about Yammer groups activity by group for the last week
+    and exports the report data in the specified path in text format
+      ${commands.YAMMER_REPORT_GROUPSACTIVITYDETAIL} --period D7 --output text --outputFile "groupsactivitydetail.txt"
+
+    Gets details about Yammer groups activity by group for the last week
+    and exports the report data in the specified path in json format
+      ${commands.YAMMER_REPORT_GROUPSACTIVITYDETAIL} --period D7 --output json --outputFile "groupsactivitydetail.json"
+`);
+  }
+}
+
+module.exports = new YammerReportGroupsActivityDetailCommand();
