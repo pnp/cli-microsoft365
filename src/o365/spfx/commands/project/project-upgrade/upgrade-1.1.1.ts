8c67d98e7947f978529222f5d9195b820e95c3f8
Waldek Mastykarz <waldek@mastykarz.nl>
2020-05-16 19:28:41 +0200

    Adds suggesting upgrading dependency @microsoft/sp-http. Closes #1516

--- src/o365/spfx/commands/project/project-upgrade/upgrade-1.1.1.ts
+++ src/o365/spfx/commands/project/project-upgrade/upgrade-1.1.1.ts
@@ -2,10 +2,12 @@ import { FN001004_DEP_microsoft_sp_webpart_base } from "./rules/FN001004_DEP_mic
 import { FN001012_DEP_microsoft_sp_application_base } from "./rules/FN001012_DEP_microsoft_sp_application_base";
 import { FN001014_DEP_microsoft_sp_listview_extensibility } from "./rules/FN001014_DEP_microsoft_sp_listview_extensibility";
 import { FN010001_YORC_version } from "./rules/FN010001_YORC_version";
+import { FN001027_DEP_microsoft_sp_http } from "./rules/FN001027_DEP_microsoft_sp_http";
 
 module.exports = [
   new FN001004_DEP_microsoft_sp_webpart_base('1.1.1'),
   new FN001012_DEP_microsoft_sp_application_base('1.1.1'),
   new FN001014_DEP_microsoft_sp_listview_extensibility('0.1.1'),
+  new FN001027_DEP_microsoft_sp_http('1.1.1'),
   new FN010001_YORC_version('1.1.1'),
 ];
--- src/o365/spfx/commands/project/project-upgrade/upgrade-1.2.0.ts
+++ src/o365/spfx/commands/project/project-upgrade/upgrade-1.2.0.ts
@@ -27,6 +27,7 @@ import { FN011007_MAN_listViewCommandSet_removeCommands } from "./rules/FN011007
 import { FN014004_CODE_settings_jsonSchemas_configJson_url } from "./rules/FN014004_CODE_settings_jsonSchemas_configJson_url";
 import { FN003005_CFG_localizedResource_pathLib } from "./rules/FN003005_CFG_localizedResource_pathLib";
 import { FN001023_DEP_microsoft_sp_component_base } from "./rules/FN001023_DEP_microsoft_sp_component_base";
+import { FN001027_DEP_microsoft_sp_http } from "./rules/FN001027_DEP_microsoft_sp_http";
 
 module.exports = [
   new FN001001_DEP_microsoft_sp_core_library('1.2.0'),
@@ -35,6 +36,7 @@ module.exports = [
   new FN001012_DEP_microsoft_sp_application_base('1.2.0'),
   new FN001014_DEP_microsoft_sp_listview_extensibility('1.2.0'),
   new FN001023_DEP_microsoft_sp_component_base('1.2.0'),
+  new FN001027_DEP_microsoft_sp_http('1.2.0'),
   new FN002001_DEVDEP_microsoft_sp_build_web('1.2.0'),
   new FN002002_DEVDEP_microsoft_sp_module_interfaces('1.2.0'),
   new FN002003_DEVDEP_microsoft_sp_webpart_workbench('1.2.0'),
