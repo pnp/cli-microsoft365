f76993dcebb2afd2caa0026f32eb62b2135cb8bb
Waldek Mastykarz <waldek@mastykarz.nl>
2020-05-27 20:25:37 +0200

    Adds suggesting upgrading dependency @microsoft/sp-module-interfaces. Closes #1519

--- src/o365/spfx/commands/project/project-upgrade/upgrade-1.1.1.ts
+++ src/o365/spfx/commands/project/project-upgrade/upgrade-1.1.1.ts
@@ -3,11 +3,13 @@ import { FN001012_DEP_microsoft_sp_application_base } from "./rules/FN001012_DEP
 import { FN001014_DEP_microsoft_sp_listview_extensibility } from "./rules/FN001014_DEP_microsoft_sp_listview_extensibility";
 import { FN010001_YORC_version } from "./rules/FN010001_YORC_version";
 import { FN001027_DEP_microsoft_sp_http } from "./rules/FN001027_DEP_microsoft_sp_http";
+import { FN001030_DEP_microsoft_sp_module_interfaces } from "./rules/FN001030_DEP_microsoft_sp_module_interfaces";
 
 module.exports = [
   new FN001004_DEP_microsoft_sp_webpart_base('1.1.1'),
   new FN001012_DEP_microsoft_sp_application_base('1.1.1'),
   new FN001014_DEP_microsoft_sp_listview_extensibility('0.1.1'),
   new FN001027_DEP_microsoft_sp_http('1.1.1'),
+  new FN001030_DEP_microsoft_sp_module_interfaces('1.1.1'),
   new FN010001_YORC_version('1.1.1'),
 ];
