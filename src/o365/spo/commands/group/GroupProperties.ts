12669d64b0c8aafdfa9133897f19b61f1d135d2f
Dips365 <Dips365@hotmail.com>
2020-07-15 23:47:02 +0530

    Added mew commend group-list

---
+++ src/o365/spo/commands/group/GroupProperties.ts
@@ -0,0 +1,11 @@
+export interface GroupProperties {
+  Id:number,
+  IsHiddenInUI:boolean,
+  LoginName:string,
+  Title:string,
+  PrincipalType:number,
+  AllowMembersEditMembership:boolean,
+  AllowRequestToJoinLeave:boolean,
+  AutoAcceptRequestToJoinLeave:boolean,
+  Description:string
+}
