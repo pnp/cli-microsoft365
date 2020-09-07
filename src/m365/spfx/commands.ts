98f89d368a7808fdcc508a3739b3168f21a14b29
Roman Rozinov <rroman81@gmail.com>
2020-09-07 15:33:41 -0700

    added new command to cmd list

--- src/m365/spfx/commands.ts
+++ src/m365/spfx/commands.ts
@@ -2,6 +2,7 @@ const prefix: string = 'spfx';
 
 export default {
   DOCTOR: `${prefix} doctor`,
+  DOCTOR_VERSION: `${prefix} doctor version`,
   PROJECT_UPGRADE: `${prefix} project upgrade`,
   PROJECT_EXTERNALIZE: `${prefix} project externalize`,
   PROJECT_RENAME: `${prefix} project rename`
