e9faf21a8b99ad2a52c4d9bda22b51c62d10775b
Nanddeep Nachan <nanddeepnachan@gmail.com>
2020-10-29 11:12:48 +0000

    extends channel list with teamName fixes #1914

--- src/m365/teams/commands/channel/channel-list.spec.ts
+++ src/m365/teams/commands/channel/channel-list.spec.ts
@@ -16,7 +16,7 @@ describe(commands.TEAMS_CHANNEL_LIST, () => {
 
   before(() => {
     sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
-    sinon.stub(appInsights, 'trackEvent').callsFake(() => {});
+    sinon.stub(appInsights, 'trackEvent').callsFake(() => { });
     auth.service.connected = true;
   });
 
@@ -48,6 +48,34 @@ describe(commands.TEAMS_CHANNEL_LIST, () => {
     assert.strictEqual(command.name.startsWith(commands.TEAMS_CHANNEL_LIST), true);
   });
 
+  it('has a description', () => {
+    assert.notStrictEqual(command.description, null);
+  });
+
+  it('fails validation if both teamId and teamName options are passed', (done) => {
+    const actual = command.validate({
+      options: {
+        teamId: '00000000-0000-0000-0000-000000000000',
+        teamName: 'Team Name',
+        name: 'Architecture Discussion',
+        description: 'Architecture'
+      }
+    });
+    assert.notStrictEqual(actual, true);
+    done();
+  });
+
+  it('fails validation if both channelId and channelName options are not passed', (done) => {
+    const actual = command.validate({
+      options: {
+        name: 'Architecture Discussion',
+        description: 'Architecture'
+      }
+    });
+    assert.notStrictEqual(actual, true);
+    done();
+  });
+
   it('fails validation if the teamId is not a valid guid.', (done) => {
     const actual = command.validate({
       options: {
@@ -56,10 +84,6 @@ describe(commands.TEAMS_CHANNEL_LIST, () => {
     });
     assert.notStrictEqual(actual, true);
     done();
-  });
-
-  it('has a description', () => {
-    assert.notStrictEqual(command.description, null);
   });
 
   it('validates for a correct input.', (done) => {
@@ -72,7 +96,101 @@ describe(commands.TEAMS_CHANNEL_LIST, () => {
     done();
   });
 
-  it('correctly lists all channels in a Microsoft teams team', (done) => {
+  it('fails to get team when team does not exists', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if ((opts.url as string).indexOf(`/beta/groups?$filter=resourceProvisioningOptions/Any(x:x eq 'Team') and displayName eq '`) > -1) {
+        return Promise.resolve({ value: [] });
+      }
+      return Promise.reject('The specified team does not exist in the Microsoft Teams');
+    });
+
+    command.action(logger, {
+      options: {
+        debug: true,
+        teamName: 'Team Name',
+        name: 'Architecture Discussion',
+        description: 'Architecture'
+      }
+    }, (err?: any) => {
+      try {
+        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError(`The specified team does not exist in the Microsoft Teams`)));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('fails when multiple teams with same name exists', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if ((opts.url as string).indexOf(`/beta/groups?$filter=resourceProvisioningOptions/Any(x:x eq 'Team') and displayName eq '`) > -1) {
+        return Promise.resolve({
+          "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#teams",
+          "@odata.count": 2,
+          "value": [
+            {
+              "id": "00000000-0000-0000-0000-000000000000",
+              "createdDateTime": null,
+              "displayName": "Team Name",
+              "description": "Team Description",
+              "internalId": null,
+              "classification": null,
+              "specialization": null,
+              "visibility": null,
+              "webUrl": null,
+              "isArchived": false,
+              "isMembershipLimitedToOwners": null,
+              "memberSettings": null,
+              "guestSettings": null,
+              "messagingSettings": null,
+              "funSettings": null,
+              "discoverySettings": null
+            },
+            {
+              "id": "00000000-0000-0000-0000-000000000000",
+              "createdDateTime": null,
+              "displayName": "Team Name",
+              "description": "Team Description",
+              "internalId": null,
+              "classification": null,
+              "specialization": null,
+              "visibility": null,
+              "webUrl": null,
+              "isArchived": false,
+              "isMembershipLimitedToOwners": null,
+              "memberSettings": null,
+              "guestSettings": null,
+              "messagingSettings": null,
+              "funSettings": null,
+              "discoverySettings": null
+            }
+          ]
+        });
+      }
+
+      return Promise.reject('Multiple Microsoft Teams teams with name Team Name found: 00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000');
+    });
+
+    command.action(logger, {
+      options: {
+        debug: true,
+        teamName: 'Team Name',
+        name: 'Architecture Discussion',
+        description: 'Architecture'
+      }
+    }, (err?: any) => {
+      try {
+        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError(`Multiple Microsoft Teams teams with name Team Name found: 00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000`)));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('correctly lists all channels in a Microsoft teams team by team id', (done) => {
     sinon.stub(request, 'get').callsFake((opts) => {
       if (opts.url === `https://graph.microsoft.com/v1.0/teams/00000000-0000-0000-0000-000000000000/channels`) {
         return Promise.resolve({
@@ -127,7 +245,98 @@ describe(commands.TEAMS_CHANNEL_LIST, () => {
             {
               "id": "19:12ff25ec5325468dba1f73522cd08248@thread.skype",
               "displayName": "Social"
+            }
+          ]
+        ));
 
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('correctly lists all channels in a Microsoft teams team by team name', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if ((opts.url as string).indexOf(`/beta/groups?$filter=resourceProvisioningOptions/Any(x:x eq 'Team') and displayName eq '`) > -1) {
+        return Promise.resolve({
+          "value": [
+            {
+              "id": "00000000-0000-0000-0000-000000000000",
+              "createdDateTime": null,
+              "displayName": "Team Name",
+              "description": "Team Description",
+              "internalId": null,
+              "classification": null,
+              "specialization": null,
+              "visibility": null,
+              "webUrl": null,
+              "isArchived": false,
+              "isMembershipLimitedToOwners": null,
+              "memberSettings": null,
+              "guestSettings": null,
+              "messagingSettings": null,
+              "funSettings": null,
+              "discoverySettings": null
+            }
+          ]
+        });
+      }
+
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams/00000000-0000-0000-0000-000000000000/channels`) {
+        return Promise.resolve({
+          value: [
+            {
+              "id": "19:17de660d16844149ab3f0240405f9316@thread.skype",
+              "displayName": "General",
+              "description": "Test group for office cli commands",
+              "isFavoriteByDefault": null,
+              "email": "",
+              "webUrl": "https://teams.microsoft.com/l/channel/19%3a17de660d16844149ab3f0240405f9316%40thread.skype/General?teamId=290a87a4-38f4-4f6c-a664-9dddf09bdbcc&tenantId=3a7a651b-2620-433b-a1a3-42de27ae94e8"
+            },
+            {
+              "id": "19:e14b10cd0b684901b53d14e89aa4221f@thread.skype",
+              "displayName": "Development",
+              "description": null,
+              "isFavoriteByDefault": null,
+              "email": "",
+              "webUrl": "https://teams.microsoft.com/l/channel/19%3ae14b10cd0b684901b53d14e89aa4221f%40thread.skype/Development?teamId=290a87a4-38f4-4f6c-a664-9dddf09bdbcc&tenantId=3a7a651b-2620-433b-a1a3-42de27ae94e8"
+            },
+            {
+              "id": "19:12ff25ec5325468dba1f73522cd08248@thread.skype",
+              "displayName": "Social",
+              "description": null,
+              "isFavoriteByDefault": null,
+              "email": "",
+              "webUrl": "https://teams.microsoft.com/l/channel/19%3a12ff25ec5325468dba1f73522cd08248%40thread.skype/Social?teamId=290a87a4-38f4-4f6c-a664-9dddf09bdbcc&tenantId=3a7a651b-2620-433b-a1a3-42de27ae94e8"
+            }
+          ]
+        });
+      }
+      return Promise.reject('Invalid request');
+    });
+
+    command.action(logger, {
+      options: {
+        debug: false,
+        teamName: 'Team Name'
+      }
+    }, () => {
+      try {
+        assert(loggerSpy.calledWith(
+          [
+            {
+              "id": "19:17de660d16844149ab3f0240405f9316@thread.skype",
+              "displayName": "General"
+            },
+            {
+              "id": "19:e14b10cd0b684901b53d14e89aa4221f@thread.skype",
+              "displayName": "Development"
+            },
+            {
+              "id": "19:12ff25ec5325468dba1f73522cd08248@thread.skype",
+              "displayName": "Social"
             }
           ]
         ));
--- src/m365/teams/commands/channel/channel-list.ts
+++ src/m365/teams/commands/channel/channel-list.ts
@@ -1,9 +1,8 @@
 import * as chalk from 'chalk';
 import { Logger } from '../../../../cli';
-import {
-  CommandOption
-} from '../../../../Command';
+import { CommandOption } from '../../../../Command';
 import GlobalOptions from '../../../../GlobalOptions';
+import request from '../../../../request';
 import Utils from '../../../../Utils';
 import { GraphItemsListCommand } from '../../../base/GraphItemsListCommand';
 import { Channel } from '../../Channel';
@@ -14,7 +13,8 @@ interface CommandArgs {
 }
 
 interface Options extends GlobalOptions {
-  teamId: string;
+  teamId?: string;
+  teamName?: string;
 }
 
 class TeamsChannelListCommand extends GraphItemsListCommand<Channel>{
@@ -26,11 +26,52 @@ class TeamsChannelListCommand extends GraphItemsListCommand<Channel>{
     return 'Lists channels in the specified Microsoft Teams team';
   }
 
-  public commandAction(logger: Logger, args: CommandArgs, cb: () => void): void {
-    const endpoint: string = `${this.resource}/v1.0/teams/${args.options.teamId}/channels`;
+  public getTelemetryProperties(args: CommandArgs): any {
+    const telemetryProps: any = super.getTelemetryProperties(args);
+    telemetryProps.teamId = typeof args.options.teamId !== 'undefined';
+    telemetryProps.teamName = typeof args.options.teamName !== 'undefined';
+    return telemetryProps;
+  }
 
+  private getTeamId(args: CommandArgs): Promise<string> {
+    if (args.options.teamId) {
+      return Promise.resolve(args.options.teamId);
+    }
+
+    const requestOptions: any = {      
+      url: `${this.resource}/beta/groups?$filter=resourceProvisioningOptions/Any(x:x eq 'Team') and displayName eq '${encodeURIComponent(args.options.teamName as string)}'`,
+      headers: {
+        accept: 'application/json;odata.metadata=none'
+      },
+      responseType: 'json'
+    };
+
+    return request
+      .get<{ value: [{ id: string }] }>(requestOptions)
+      .then(response => {
+        const groupItem: { id: string } | undefined = response.value[0];
+
+        if (!groupItem) {
+          return Promise.reject(`The specified team does not exist in the Microsoft Teams`);
+        }
+
+        if (response.value.length > 1) {
+          return Promise.reject(`Multiple Microsoft Teams teams with name ${args.options.teamName} found: ${response.value.map(x => x.id)}`);
+        }
+
+        return Promise.resolve(groupItem.id);
+      });
+  }
+
+  public commandAction(logger: Logger, args: CommandArgs, cb: () => void): void {
     this
-      .getAllItems(endpoint, logger, true)
+      .getTeamId(args)
+      .then((teamId: string): Promise<void> => {
+        const endpoint: string = `${this.resource}/v1.0/teams/${teamId}/channels`;
+
+        return this
+          .getAllItems(endpoint, logger, true)
+      })
       .then((): void => {
         if (args.options.output === 'json') {
           logger.log(this.items);
@@ -50,11 +91,16 @@ class TeamsChannelListCommand extends GraphItemsListCommand<Channel>{
         cb();
       }, (err: any): void => this.handleRejectedODataJsonPromise(err, logger, cb));
   }
+
   public options(): CommandOption[] {
     const options: CommandOption[] = [
       {
-        option: '-i, --teamId <teamId>',
-        description: 'The ID of the team to list the channels of'
+        option: '-i, --teamId [teamId]',
+        description: 'The ID of the team to list the channels of. Specify either teamId or teamName but not both'
+      },
+      {
+        option: '--teamName [teamName]',
+        description: 'The display name of the team to list the channels of. Specify either teamId or teamName but not both'
       }
     ];
 
@@ -63,7 +109,15 @@ class TeamsChannelListCommand extends GraphItemsListCommand<Channel>{
   }
 
   public validate(args: CommandArgs): boolean | string {
-    if (!Utils.isValidGuid(args.options.teamId as string)) {
+    if (args.options.teamId && args.options.teamName) {
+      return 'Specify either teamId or teamName, but not both';
+    }
+
+    if (!args.options.teamId && !args.options.teamName) {
+      return 'Specify teamId or teamName, one is required';
+    }
+
+    if (args.options.teamId && !Utils.isValidGuid(args.options.teamId)) {
       return `${args.options.teamId} is not a valid GUID`;
     }
 
