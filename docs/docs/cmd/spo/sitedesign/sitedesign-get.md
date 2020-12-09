ba30799ce423922d60bff3e6178546aaf66b20bb
Nanddeep Nachan <nanddeepnachan@gmail.com>
2020-12-09 15:14:55 +0000

    Extend 'spo sitedesign get' to include title

--- docs/docs/cmd/spo/sitedesign/sitedesign-get.md
+++ docs/docs/cmd/spo/sitedesign/sitedesign-get.md
@@ -10,14 +10,17 @@ m365 spo sitedesign get [options]
 
 ## Options
 
-`-i, --id <id>`
-: Site design ID
+`-i, --id [id]`
+: Site design ID. Specify either id or title but not both
+
+`--title [title]`
+: Site design title. Specify either id or title but not both
 
 --8<-- "docs/cmd/_global.md"
 
 ## Remarks
 
-If the specified `id` doesn't refer to an existing site design, you will get a `File not found` error.
+If the specified `id` or `title` doesn't refer to an existing site design, you will get a `File not found` error.
 
 ## Examples
 
@@ -25,6 +28,12 @@ Get information about the site design with ID _2c1ba4c4-cd9b-4417-832f-92a34bc34
 
 ```sh
 m365 spo sitedesign get --id 2c1ba4c4-cd9b-4417-832f-92a34bc34b2a
+```
+
+Get information about the site design with title _Contoso Site Design_
+
+```sh
+m365 spo sitedesign get --title "Contoso Site Design"
 ```
 
 ## More information
--- src/m365/spo/commands/sitedesign/sitedesign-get.ts
+++ src/m365/spo/commands/sitedesign/sitedesign-get.ts
@@ -9,6 +9,7 @@ import Utils from '../../../../Utils';
 import SpoCommand from '../../../base/SpoCommand';
 import commands from '../../commands';
 import { ContextInfo } from '../../spo';
+import { SiteDesign } from './SiteDesign';
 
 interface CommandArgs {
   options: Options;
@@ -19,6 +20,9 @@ interface Options extends GlobalOptions {
 }
 
 class SpoSiteDesignGetCommand extends SpoCommand {
+  private spoUrl: string = "";
+  private formDigestValue: string = '';
+
   public get name(): string {
     return `${commands.SITEDESIGN_GET}`;
   }
@@ -27,24 +31,57 @@ class SpoSiteDesignGetCommand extends SpoCommand {
     return 'Gets information about the specified site design';
   }
 
-  public commandAction(logger: Logger, args: CommandArgs, cb: () => void): void {
-    let spoUrl: string = '';
+  private getSiteDesignId(args: CommandArgs): Promise<string> {
+    if (args.options.id) {
+      return Promise.resolve(args.options.id);
+    }
 
+    const requestOptions: any = {
+      url: `${this.spoUrl}/_api/Microsoft.Sharepoint.Utilities.WebTemplateExtensions.SiteScriptUtility.GetSiteDesigns`,
+      headers: {
+        'X-RequestDigest': this.formDigestValue,
+        accept: 'application/json;odata=nometadata'
+      },
+      responseType: 'json'
+    };
+
+    return request
+      .post<{ value: SiteDesign[] }>(requestOptions)
+      .then(response => {
+        const siteDesignItem: SiteDesign | undefined = response.value.filter(x => x.Title == args.options.title)[0];
+
+        if (!siteDesignItem) {
+          return Promise.reject(`The specified site design does not exist`);
+        }
+
+        if (response.value.filter(x => x.Title == args.options.title).length > 1) {
+          return Promise.reject(`Multiple site designs with title ${args.options.title} found: ${response.value.filter(x => x.Title == args.options.title).map(x => x.Id)}`);
+        }
+
+        return Promise.resolve(siteDesignItem.Id);
+      });
+  }
+
+  public commandAction(logger: Logger, args: CommandArgs, cb: () => void): void {
     this
       .getSpoUrl(logger, this.debug)
       .then((_spoUrl: string): Promise<ContextInfo> => {
-        spoUrl = _spoUrl;
-        return this.getRequestDigest(spoUrl);
+        this.spoUrl = _spoUrl
+        return this.getRequestDigest(this.spoUrl);
       })
       .then((res: ContextInfo): Promise<string> => {
+        this.formDigestValue = res.FormDigestValue;
+        return this.getSiteDesignId(args);
+      })
+      .then((siteDesignId: string): Promise<string> => {
         const requestOptions: any = {
-          url: `${spoUrl}/_api/Microsoft.Sharepoint.Utilities.WebTemplateExtensions.SiteScriptUtility.GetSiteDesignMetadata`,
+          url: `${this.spoUrl}/_api/Microsoft.Sharepoint.Utilities.WebTemplateExtensions.SiteScriptUtility.GetSiteDesignMetadata`,
           headers: {
-            'X-RequestDigest': res.FormDigestValue,
+            'X-RequestDigest': this.formDigestValue,
             'content-type': 'application/json;charset=utf-8',
             accept: 'application/json;odata=nometadata'
           },
-          data: { id: args.options.id },
+          data: { id: siteDesignId },
           responseType: 'json'
         };
 
@@ -64,8 +101,12 @@ class SpoSiteDesignGetCommand extends SpoCommand {
   public options(): CommandOption[] {
     const options: CommandOption[] = [
       {
-        option: '-i, --id <id>',
-        description: 'Site design ID'
+        option: '-i, --id [id]',
+        description: 'Site design ID. Specify either id or title but not both'
+      },
+      {
+        option: '--title [title]',
+        description: 'Site design title. Specify either id or title but not both'
       }
     ];
 
@@ -74,7 +115,15 @@ class SpoSiteDesignGetCommand extends SpoCommand {
   }
 
   public validate(args: CommandArgs): boolean | string {
-    if (!Utils.isValidGuid(args.options.id)) {
+    if (args.options.id && args.options.title) {
+      return 'Specify either id or title, but not both.';
+    }
+
+    if (!args.options.id && !args.options.title) {
+      return 'Specify id or title, one is required';
+    }
+
+    if (args.options.id && !Utils.isValidGuid(args.options.id as string)) {
       return `${args.options.id} is not a valid GUID`;
     }
 
