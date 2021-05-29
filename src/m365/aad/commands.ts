909b490d6930a7582f80baf9812a9961ee924b5a
Vipul Kelkar <vipul.kelkar@gmail.com>
2021-05-29 13:43:44 +0530

    Adds the 'aad o365group recyclebinitem clear' command. Closes #2033

--- src/m365/aad/commands.ts
+++ src/m365/aad/commands.ts
@@ -18,6 +18,7 @@ export default {
   O365GROUP_ADD: `${prefix} o365group add`,
   O365GROUP_GET: `${prefix} o365group get`,
   O365GROUP_LIST: `${prefix} o365group list`,
+  O365GROUP_RECYCLEBINITEM_CLEAR: `${prefix} o365group recyclebinitem clear`,
   O365GROUP_RECYCLEBINITEM_LIST: `${prefix} o365group recyclebinitem list`,
   O365GROUP_SET: `${prefix} o365group set`,
   O365GROUP_TEAMIFY: `${prefix} o365group teamify`,
