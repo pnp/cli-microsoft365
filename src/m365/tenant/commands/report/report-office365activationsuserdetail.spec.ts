539be5b7ff50f2008523194dc3897c732af95f13
Abderahman88 <rags_place@hotmail.com>
2021-06-04 17:07:37 +0200

    fix issues

--- src/m365/tenant/commands/report/report-office365activationsuserdetail.spec.ts
+++ src/m365/tenant/commands/report/report-office365activationsuserdetail.spec.ts
@@ -97,7 +97,7 @@ describe(commands.REPORT_OFFICE365ACTIVATIONSUSERDETAIL, () => {
       try {
         assert.strictEqual(requestStub.lastCall.args[0].url, "https://graph.microsoft.com/v1.0/reports/getOffice365ActivationsUserDetail");
         assert.strictEqual(requestStub.lastCall.args[0].headers["accept"], 'application/json;odata.metadata=none');
-        assert(loggerLogSpy.calledWith('[{"Report Refresh Date":"2021-05-25","User Principal Name":"user1@contoso.onmicrosoft.com","Display Name":"User1","Product Type":"MICROSOFT 365 APPS FOR ENTERPRISE","Last Activated Date":"","Windows":"0","Mac":"0","Windows 10 Mobile":"0","iOS":"0","Android":"0","Activated On Shared Computer":"False"},{"Report Refresh Date":"2021-05-25","User Principal Name":"user1@contoso.onmicrosoft.com","Display Name":"User1","Product Type":"MICROSOFT EXCEL ADVANCED ANALYTICS","Last Activated Date":"","Windows":"0","Mac":"0","Windows 10 Mobile":"0","iOS":"0","Android":"0","Activated On Shared Computer":"False"}]'));
+        assert(loggerLogSpy.calledWith([{"Report Refresh Date":"2021-05-25","User Principal Name":"user1@contoso.onmicrosoft.com","Display Name":"User1","Product Type":"MICROSOFT 365 APPS FOR ENTERPRISE","Last Activated Date":"","Windows":"0","Mac":"0","Windows 10 Mobile":"0","iOS":"0","Android":"0","Activated On Shared Computer":"False"},{"Report Refresh Date":"2021-05-25","User Principal Name":"user1@contoso.onmicrosoft.com","Display Name":"User1","Product Type":"MICROSOFT EXCEL ADVANCED ANALYTICS","Last Activated Date":"","Windows":"0","Mac":"0","Windows 10 Mobile":"0","iOS":"0","Android":"0","Activated On Shared Computer":"False"}]));
         done();
       }
       catch (e) {
