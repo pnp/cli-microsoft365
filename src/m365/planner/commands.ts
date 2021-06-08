1ff5ae39c8bd6154d4b9292e4c2cba14f97c07ce
Aakash Bhardwaj <aabhardwaj@microsoft.com>
2021-06-08 20:00:18 +0530

    Adds the 'planner bucket add' command. Closes #2254

--- src/m365/planner/commands.ts
+++ src/m365/planner/commands.ts
@@ -1,6 +1,7 @@
 const prefix: string = 'planner';
 
 export default {
+  BUCKET_ADD: `${prefix} bucket add`,
   PLAN_ADD: `${prefix} plan add`,
   PLAN_LIST: `${prefix} plan list`,
   TASK_LIST: `${prefix} task list`
