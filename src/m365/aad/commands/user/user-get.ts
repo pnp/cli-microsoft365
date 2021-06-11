fddabb04762b93225343179a072e9539a59e679c
Arjun Menon <arjun.umenon@gmail.com>
2021-06-11 21:59:31 +0530

    Add 'spo group user add' command. Closes #2483

--- src/m365/aad/commands/user/user-get.ts
+++ src/m365/aad/commands/user/user-get.ts
@@ -12,7 +12,7 @@ interface CommandArgs {
   options: Options;
 }
 
-interface Options extends GlobalOptions {
+export interface Options extends GlobalOptions {
   id?: string;
   userName?: string;
   properties?: string;
