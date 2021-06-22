8462d5e9ccf723f987fef848406fa23f1f5deebe
Abderahman88 <rags_place@hotmail.com>
2021-06-22 20:06:33 +0200

    add planner get command

--- src/m365/planner/commands.ts
+++ src/m365/planner/commands.ts
@@ -2,5 +2,6 @@ const prefix: string = 'planner';
 
 export default {
   PLAN_ADD: `${prefix} plan add`,
+  PLAN_GET: `${prefix} plan get`,
   TASK_LIST: `${prefix} task list`
 };
---
+++ src/m365/planner/commands/plan/plan-get.ts
@@ -0,0 +1,163 @@
+import { Logger } from '../../../../cli';
+import {
+  CommandOption
+} from '../../../../Command';
+import GlobalOptions from '../../../../GlobalOptions';
+import request from '../../../../request';
+import Utils from '../../../../Utils';
+import GraphCommand from '../../../base/GraphCommand';
+import commands from '../../commands';
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  id?: string;
+  title?: string;
+  ownerGroupId?: string;
+  ownerGroupName?: string;
+}
+
+class PlannerPlanGetCommand extends GraphCommand {
+  public get name(): string {
+    return commands.PLAN_GET;
+  }
+
+  public get description(): string {
+    return 'Get a Microsoft Planner plan';
+  }
+
+  public getTelemetryProperties(args: CommandArgs): any {
+    const telemetryProps: any = super.getTelemetryProperties(args);
+    telemetryProps.id = typeof args.options.id !== 'undefined';
+    telemetryProps.title = typeof args.options.title !== 'undefined';
+    telemetryProps.ownerGroupId = typeof args.options.ownerGroupId !== 'undefined';
+    telemetryProps.ownerGroupName = typeof args.options.ownerGroupName !== 'undefined';
+    return telemetryProps;
+  }
+
+  public defaultProperties(): string[] | undefined {
+    return ['id', 'title', 'createdDateTime', 'owner'];
+  }
+
+  public commandAction(logger: Logger, args: CommandArgs, cb: () => void): void {
+    if (args.options.id) {
+      this
+        .getPlan(args)
+        .then((res: any): void => {
+          logger.log(res);
+          cb();
+        }, (err: any): void => this.handleRejectedODataJsonPromise(err, logger, cb));
+    }
+    else {
+      this
+        .getGroupId(args)
+        .then((groupId: string): Promise<any> => {
+          const requestOptions: any = {
+            url: `${this.resource}/v1.0/groups/${groupId}/planner/plans`,
+            headers: {
+              'accept': 'application/json;odata.metadata=none'
+            },
+            responseType: 'json'
+          };
+
+          return request.get(requestOptions);
+        })
+        .then((res: any): void => {
+          const filteredPlan = res.value.filter((obj: any) => obj.title === args.options.title);
+          logger.log(filteredPlan);
+          cb();
+        }, (err: any): void => this.handleRejectedODataJsonPromise(err, logger, cb));
+    }
+  }
+
+
+  private getGroupId(args: CommandArgs): Promise<string> {
+    if (args.options.ownerGroupId) {
+      return Promise.resolve(args.options.ownerGroupId);
+    }
+
+    const requestOptions: any = {
+      url: `${this.resource}/v1.0/groups?$filter=displayName eq '${encodeURIComponent(args.options.ownerGroupName as string)}'`,
+      headers: {
+        accept: 'application/json;odata.metadata=none'
+      },
+      responseType: 'json'
+    };
+
+    return request
+      .get<{ value: { id: string; }[] }>(requestOptions)
+      .then(response => {
+        const group: { id: string; } | undefined = response.value[0];
+
+        if (!group) {
+          return Promise.reject(`The specified owner group does not exist`);
+        }
+
+        return Promise.resolve(group.id);
+      });
+  }
+
+  private getPlan(args: CommandArgs): Promise<any> {
+    const requestOptions: any = {
+      url: `${this.resource}/v1.0/planner/plans/${args.options.id}`,
+      headers: {
+        'accept': 'application/json;odata.metadata=none'
+      },
+      responseType: 'json'
+    };
+
+    return request.get(requestOptions);
+  }
+
+  public options(): CommandOption[] {
+    const options: CommandOption[] = [
+      {
+        option: '-i, --id [id]'
+      },
+      {
+        option: '-t, --title [title]'
+      },
+      {
+        option: "--ownerGroupId [ownerGroupId]"
+      },
+      {
+        option: "--ownerGroupName [ownerGroupName]"
+      }
+    ];
+
+    const parentOptions: CommandOption[] = super.options();
+    return options.concat(parentOptions);
+  }
+
+  public validate(args: CommandArgs): boolean | string {
+    if (!args.options.id && !args.options.title) {
+      return 'Specify either id or title';
+    }
+
+    if (args.options.id && args.options.title) {
+      return 'Specify either id or title';
+    }
+
+    if (args.options.title && !args.options.ownerGroupId && !args.options.ownerGroupName) {
+      return 'Specify either ownerGroupId or ownerGroupName';
+    }
+
+    if (args.options.title && args.options.ownerGroupId && args.options.ownerGroupName) {
+      return 'Specify either ownerGroupId or ownerGroupName but not both';
+    }
+
+    /*if (args.options.id && !Utils.isValidGuid(args.options.id as string)) {
+      return `${args.options.id} is not a valid GUID`;
+    }*/
+
+    if (args.options.ownerGroupId && !Utils.isValidGuid(args.options.ownerGroupId as string)) {
+      return `${args.options.ownerGroupId} is not a valid GUID`;
+    }
+
+    return true;
+  }
+}
+
+module.exports = new PlannerPlanGetCommand();
