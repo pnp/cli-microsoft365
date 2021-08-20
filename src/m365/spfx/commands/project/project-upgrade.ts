261d02eb49efbf85b2eddf1bb7feb10c01501444
Waldek Mastykarz <waldek@mastykarz.nl>
2021-08-20 14:58:45 +0200

    Adds support for upgrading SPFx projects to 1.13.0-beta.17. Closes #2644

--- src/m365/spfx/commands/project/project-upgrade.ts
+++ src/m365/spfx/commands/project/project-upgrade.ts
@@ -65,7 +65,7 @@ class SpfxProjectUpgradeCommand extends BaseProjectCommand {
     '1.11.0',
     '1.12.0',
     '1.12.1',
-    '1.13.0-beta.15'
+    '1.13.0-beta.17'
   ];
   private static packageCommands = {
     npm: {
--- src/m365/spfx/commands/project/project-upgrade/rules/FN023001_GITIGNORE_release.ts
+++ src/m365/spfx/commands/project/project-upgrade/rules/FN023001_GITIGNORE_release.ts
@@ -3,7 +3,7 @@ import { Project } from "../../model";
 import { Rule } from "./Rule";
 
 export class FN023001_GITIGNORE_release extends Rule {
-  constructor(private add: boolean = true) {
+  constructor() {
     super();
   }
 
@@ -16,12 +16,7 @@ export class FN023001_GITIGNORE_release extends Rule {
   }
 
   get description(): string {
-    if (this.add) {
-      return `To .gitignore add the 'release' folder`;
-    }
-    else {
-      return `From .gitignore remove the 'release' folder`;
-    }
+    return `To .gitignore add the 'release' folder`;
   }
 
   get resolution(): string {
@@ -45,7 +40,7 @@ export class FN023001_GITIGNORE_release extends Rule {
       return;
     }
 
-    if (/^release$/m.test(project.gitignore.source) !== this.add) {
+    if (!/^release$/m.test(project.gitignore.source)) {
       this.addFinding(findings);
     }
   }
