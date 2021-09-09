a6dbe71282d2ec72b38ac4c9904d685bf13349b2
Waldek Mastykarz <waldek@mastykarz.nl>
2021-09-09 19:27:06 +0200

    Adds support for upgrading SPFx projects to v1.13.0-beta.20. Closes #2697

--- src/m365/spfx/commands/project/project-upgrade.ts
+++ src/m365/spfx/commands/project/project-upgrade.ts
@@ -65,7 +65,7 @@ class SpfxProjectUpgradeCommand extends BaseProjectCommand {
     '1.11.0',
     '1.12.0',
     '1.12.1',
-    '1.13.0-beta.17'
+    '1.13.0-beta.20'
   ];
   private static packageCommands = {
     npm: {
