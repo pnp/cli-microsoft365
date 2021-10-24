9f4df0111a965fe6f5683612ec3d38e45c50ec85
Kevin McDonnell <kevin@mcd79.com>
2021-10-24 21:27:51 +0000

    Removed additional commands

--- src/m365/search/commands.ts
+++ src/m365/search/commands.ts
@@ -1,7 +1,5 @@
 const prefix: string = 'search';
 
 export default {
-  EXTERNALCONNECTION_ADD: `${prefix} externalconnection add`,
-  EXTERNALCONNECTION_LIST: `${prefix} externalconnection list`,
-  EXTERNALCONNECTION_REMOVE: `${prefix} externalconnection remove`
+  EXTERNALCONNECTION_ADD: `${prefix} externalconnection add`
 };
