13cbb5b009d15d7d40399bec69e5c65f2daab927
Kevin McDonnell <kevin@mcd79.com>
2021-11-21 21:39:01 +0000

    Working tests

--- src/m365/search/commands/externalconnection/externalconnection-list.spec.ts
+++ src/m365/search/commands/externalconnection/externalconnection-list.spec.ts
@@ -3,7 +3,7 @@ import * as sinon from 'sinon';
 import appInsights from '../../../../appInsights';
 import auth from '../../../../Auth';
 import { Logger } from '../../../../cli';
-import Command, { CommandError } from '../../../../Command';
+import Command from '../../../../Command';
 import request from '../../../../request';
 import Utils from '../../../../Utils';
 import commands from '../../commands';
@@ -15,24 +15,12 @@ describe(commands.EXTERNALCONNECTION_LIST, () => {
   let loggerLogSpy: sinon.SinonSpy;
 
   const externalConnectionListResponse: any = {
-    "@odata.context": "https://graph.microsoft.com/beta/$metadata#connections/$entity",
-    "id": "TestConnectionForCLI",
-    "name": "Twitter Connector",
-    "description": "Connector for showing key tweets",
-    "connectorId": null,
-    "state": null,
-    "ingestedItemsCount": null,
-    "searchSettings": null,
-    "activitySettings": null,
-    "complianceSettings": null,
-    "configuration": {
-      "authorizedApps": [
-        "00000000-0000-0000-0000-000000000000"
-      ],
-      "authorizedAppIds": [
-        "00000000-0000-0000-0000-000000000000"
-      ]
-    }
+    configuration: {
+      authorizedAppIds: []
+    },
+    description: 'Test connection that will not do anything',
+    id: 'TestConnectionForCLI',
+    name: 'Test Connection for CLI'
   };
 
   before(() => {
@@ -54,13 +42,13 @@ describe(commands.EXTERNALCONNECTION_LIST, () => {
         log.push(msg);
       }
     };
-    (command as any).items = [];
     loggerLogSpy = sinon.spy(logger, 'log');
+    (command as any).items = [];
   });
 
   afterEach(() => {
     Utils.restore([
-      request.post
+      request.get
     ]);
   });
 
@@ -72,36 +60,11 @@ describe(commands.EXTERNALCONNECTION_LIST, () => {
     auth.service.connected = false;
   });
 
-  it('has correct name', () => {
-    assert.strictEqual(command.name.startsWith(commands.EXTERNALCONNECTION_LIST), true);
-  });
-
-  it('has a description', () => {
-    assert.notStrictEqual(command.description, null);
-  });
-
-  it('lists an external connection', (done) => {
-      const options: any = {
-        debug: false,
-        externalConnectionId: 'TestConnectionForCLI',
-        externalConnectionName: 'Test Connection for CLI',
-        externalConnectionDescription: 'Test connection that will not do anything',
-      };
   
-      command.action(logger, { options: options } as any, () => {
-        try {
-          assert(loggerLogSpy.calledWith(externalConnectionListResponse));
-          done();
-        }
-        catch (e) {
-          done(e);
-        }
-      });
-    });
-
-
   it('correctly handles error', (done) => {
-    sinon.stub(request, 'post').callsFake(() => {
+    logger.logRaw('testing the error');
+
+    sinon.stub(request, 'get').callsFake(() => {
       return Promise.reject({
         "error": {
           "code": "Error",
@@ -114,20 +77,56 @@ describe(commands.EXTERNALCONNECTION_LIST, () => {
       });
     });
 
-    command.action(logger, { options: { debug: false, subject: 'Lorem ipsum', to: 'mail@domain.com', bodyContents: 'Lorem ipsum' } } as any, (err?: any) => {
+    command.action(logger, {
+      options: {
+        debug: false
+      }
+    }, () => {
       try {
-        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError(`An error has occurred`)));
+        assert(loggerLogSpy.calledWith(externalConnectionListResponse));
         done();
       }
       catch (e) {
         done(e);
       }
+      finally {
+        Utils.restore(request.get);
+      }
     });
   });
 
-  it('fails validation if externalConnectionId is not set', () => {
-    const actual = command.validate({ options: { subject: 'Lorem ipsum', to: 'mail@domain.com', bodyContents: 'Lorem ipsum', bodyContentType: 'Invalid' } });
-    assert.notStrictEqual(actual, true);
+  it('lists an external connection', (done) => {
+    const postStub = sinon.stub(request, 'get').callsFake((opts: any) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/external/connections`) {
+        return Promise.resolve(externalConnectionListResponse);
+      }
+      return Promise.reject('Invalid request');
+    });
+    
+    const options: any = {
+      debug: false,
+      externalConnectionId: 'TestConnectionForCLI',
+      externalConnectionName: 'Test Connection for CLI',
+      externalConnectionDescription: 'Test connection that will not do anything'
+    };
+  
+    command.action(logger, { options: options } as any, () => {
+      try {
+        assert.deepStrictEqual(postStub.getCall(0).args[0].data, externalConnectionListResponse);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+      finally {
+        Utils.restore(request.get);
+      }
+    });
   });
+
+  it('has a description', () => {
+    assert.notStrictEqual(command.description, null);
+  });
+
 
 });
--- src/m365/search/commands/externalconnection/externalconnection-list.ts
+++ src/m365/search/commands/externalconnection/externalconnection-list.ts
@@ -11,19 +11,12 @@ class SearchExternalConnectionListCommand extends GraphItemsListCommand<any> {
     return 'Adds a new External Connection for Microsoft Search';
   }
 
-  public defaultProperties(): string[] | undefined {
-    return ['id', 'name'];
-  }
-
-
   public commandAction(logger: Logger, cb: () => void): void {
     this
       .getAllItems(`${this.resource}/v1.0/external/connections`, logger, true)
       .then((): void => {
         logger.log(this.items);
         cb();
-      }, (err: any): void => {
-        this.handleRejectedODataJsonPromise(err, logger, cb);
       });
   }
 }
