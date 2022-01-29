bc6ee9c8367a1c8b3661da8280fafa2ead306500
Nanddeep Nachan <nanddeepnachan@gmail.com>
2022-01-29 10:09:22 +0000

    tenant-serviceannouncement-healthissue-list

--- src/m365/tenant/commands.ts
+++ src/m365/tenant/commands.ts
@@ -9,8 +9,9 @@ export default {
   REPORT_OFFICE365ACTIVATIONSUSERDETAIL: `${prefix} report office365activationsuserdetail`,
   REPORT_OFFICE365ACTIVATIONSUSERCOUNTS: `${prefix} report office365activationsusercounts`,
   REPORT_SERVICESUSERCOUNTS: `${prefix} report servicesusercounts`,
+  SERVICEANNOUNCEMENT_HEALTHISSUE_LIST: `${prefix} serviceannouncement healthissue list`,
   SERVICE_LIST: `${prefix} service list`,
   SERVICE_MESSAGE_LIST: `${prefix} service message list`,
   SERVICE_REPORT_HISTORICALSERVICESTATUS: `${prefix} service report historicalservicestatus`,
   STATUS_LIST: `${prefix} status list`
-}; 
+};
