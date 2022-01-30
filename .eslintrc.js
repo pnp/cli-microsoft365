3f2134a75a523e45b8513d53f113c8fcea4990ed
Waldek Mastykarz <waldek@mastykarz.nl>
2022-01-30 11:29:39 +0100

    Adds the 'spo app teamspackage download' command. Closes #2934

--- .eslintrc.js
+++ .eslintrc.js
@@ -58,6 +58,7 @@ const dictionary = [
   'site',
   'status',
   'storage',
+  'teams',
   'token',
   'type',
   'user',
--- src/m365/spo/commands.ts
+++ src/m365/spo/commands.ts
@@ -9,6 +9,7 @@ export default {
   APP_LIST: `${prefix} app list`,
   APP_REMOVE: `${prefix} app remove`,
   APP_RETRACT: `${prefix} app retract`,
+  APP_TEAMSPACKAGE_DOWNLOAD: `${prefix} app teamspackage download`,
   APP_UNINSTALL: `${prefix} app uninstall`,
   APP_UPGRADE: `${prefix} app upgrade`,
   APPPAGE_ADD:`${prefix} apppage add`,
