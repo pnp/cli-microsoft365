ebb1b206714f4f8655a05443add7327e97ccd00b
Akash Karda <akashkarda@gmail.com>
2022-02-09 10:17:09 -0500

    Adds 'aad o365group conversation list' command. Closes #3030

---
+++ docs/docs/cmd/aad/o365group/o365group-conversation-list.md
@@ -0,0 +1,24 @@
+# aad o365group conversation list
+
+Lists conversations for the specified Microsoft 365 group
+
+## Usage
+
+```sh
+m365 aad o365group conversation list [options]
+```
+
+## Options
+
+`-i, --groupId <groupId>`
+: The ID of the Microsoft 365 group
+
+--8<-- "docs/cmd/\_global.md"
+
+## Examples
+
+Lists conversations for the specified Microsoft 365 group
+
+```sh
+m365 aad o365group conversation list --groupId '00000000-0000-0000-0000-000000000000'
+```
---
+++ src/m365/aad/commands/o365group/o365group-conversation-list.ts
@@ -0,0 +1,58 @@
+import { Logger } from '../../../../cli';
+import {
+  CommandOption
+} from '../../../../Command';
+import GlobalOptions from '../../../../GlobalOptions';
+import commands from '../../commands';
+import { Conversation } from '@microsoft/microsoft-graph-types';
+import { GraphItemsListCommand } from '../../../base/GraphItemsListCommand';
+import Utils from '../../../../Utils';
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  groupId: string;
+}
+
+class AadO365GroupConversationListCommand extends GraphItemsListCommand<Conversation> {
+  public get name(): string {
+    return commands.O365GROUP_CONVERSATION_LIST;
+  }
+
+  public get description(): string {
+    return 'Lists conversations for the specified Microsoft 365 group';
+  }
+
+  public defaultProperties(): string[] | undefined {
+    return ['topic', 'lastDeliveredDateTime', 'id'];
+  }
+
+  public commandAction(logger: Logger, args: CommandArgs, cb: () => void): void {
+    this.getAllItems(`${this.resource}/v1.0/groups/${args.options.groupId}/conversations`, logger, true)
+      .then((): void => {
+        logger.log(this.items);
+        cb();
+      }, (err: any): void => this.handleRejectedODataJsonPromise(err, logger, cb));
+  }
+
+  public options(): CommandOption[] {
+    const options: CommandOption[] = [
+      {
+        option: '-i, --groupId <groupId>'
+      }
+    ];
+    const parentOptions: CommandOption[] = super.options();
+    return options.concat(parentOptions);
+  }
+
+  public validate(args: CommandArgs): boolean | string {
+    if (!Utils.isValidGuid(args.options.groupId as string)) {
+      return `${args.options.groupId} is not a valid GUID`;
+    }
+    return true;
+  }
+}
+
+module.exports = new AadO365GroupConversationListCommand();
