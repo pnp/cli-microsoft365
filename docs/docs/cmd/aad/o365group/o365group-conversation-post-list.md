2ac81a473dfe679e7d4b8561d1b4966c65723a7c
Akash Karda <akashkarda@gmail.com>
2022-03-08 13:12:31 -0500

    Adds 'm365 aad o365group conversation post list' command

--- docs/docs/cmd/aad/o365group/o365group-conversation-post-list.md
+++ docs/docs/cmd/aad/o365group/o365group-conversation-post-list.md
@@ -10,8 +10,11 @@ m365 aad o365group conversation post list [options]
 
 ## Options
 
-`-i, --groupId <groupId>`
-: The ID of the Microsoft 365 group
+`-i, --groupId [groupId]`
+: The Id of the Office 365 Group. You can specify the groupId or groupDisplayName, but not both.
+
+`-d, --groupDisplayName [groupDisplayName]`
+: The Displayname of the Office 365 Group. You can specify the groupId or groupDisplayName, but not both.
 
 `-t, --threadId <threadId>`
 : The ID of the thread to retrieve details for
@@ -20,8 +23,14 @@ m365 aad o365group conversation post list [options]
 
 ## Examples
 
-Lists the posts of the specific conversation of Microsoft 365 group
+Lists the posts of the specific conversation of Microsoft 365 group by groupId
 
 ```sh
 m365 aad o365group conversation post list --groupId '00000000-0000-0000-0000-000000000000' --threadId 'AAQkADkwN2Q2NDg1LWQ3ZGYtNDViZi1iNGRiLTVhYjJmN2Q5NDkxZQAQAOnRAfDf71lIvrdK85FAn5E='
+```
+
+Lists the posts of the specific conversation of Microsoft 365 group by groupDisplayName
+
+```sh
+m365 aad o365group conversation post list --groupDisplayName 'MyGroup' --threadId 'AAQkADkwN2Q2NDg1LWQ3ZGYtNDViZi1iNGRiLTVhYjJmN2Q5NDkxZQAQAOnRAfDf71lIvrdK85FAn5E='
 ```
--- src/m365/aad/commands/o365group/o365group-conversation-post-list.ts
+++ src/m365/aad/commands/o365group/o365group-conversation-post-list.ts
@@ -4,26 +4,34 @@ import {
   CommandOption
 } from '../../../../Command';
 import GlobalOptions from '../../../../GlobalOptions';
-import Utils from '../../../../Utils';
-import { GraphItemsListCommand } from '../../../base/GraphItemsListCommand';
+import { odata, validation } from '../../../../utils';
+import GraphCommand from '../../../base/GraphCommand';
 import commands from '../../commands';
+import request from '../../../../request';
 
 interface CommandArgs {
   options: Options;
 }
 
 interface Options extends GlobalOptions {
-  groupId: string;
+  groupId?: string;
+  groupDisplayName?: string;
   threadId: string;
 }
 
-class AadO365GroupConversationPostListCommand extends GraphItemsListCommand<Post> {
+class AadO365GroupConversationPostListCommand extends GraphCommand {
   public get name(): string {
     return commands.O365GROUP_CONVERSATION_POST_LIST;
   }
 
   public get description(): string {
     return 'Lists the posts of the specific conversation of Microsoft 365 group';
+  }
+  public getTelemetryProperties(args: CommandArgs): any {
+    const telemetryProps: any = super.getTelemetryProperties(args);
+    telemetryProps.groupId = typeof args.options.groupId !== 'undefined';
+    telemetryProps.groupDisplayName = typeof args.options.groupDisplayName !== 'undefined';
+    return telemetryProps;
   }
 
   public defaultProperties(): string[] | undefined {
@@ -32,17 +40,48 @@ class AadO365GroupConversationPostListCommand extends GraphItemsListCommand<Post
 
   public commandAction(logger: Logger, args: CommandArgs, cb: () => void): void {
     this
-      .getAllItems(`${this.resource}/v1.0/groups/${args.options.groupId}/threads/${args.options.threadId}/posts`, logger, true)
-      .then((): void => {
-        logger.log(this.items);
+      .getGroupId(args)
+      .then((retrievedgroupId: string): Promise<Post[]> => {
+        return odata.getAllItems<Post>(`${this.resource}/v1.0/groups/${retrievedgroupId}/threads/${args.options.threadId}/posts`, logger);
+      })
+      .then((posts): void => {
+        logger.log(posts);
         cb();
       }, (err: any): void => this.handleRejectedODataJsonPromise(err, logger, cb));
+  }
+
+  private getGroupId(args: CommandArgs): Promise<string> {
+    if (args.options.groupId) {
+      return Promise.resolve(encodeURIComponent(args.options.groupId));
+    }
+    const requestOptions: any = {
+      url: `${this.resource}/v1.0/groups?$filter=displayName eq '${encodeURIComponent(args.options.groupDisplayName as string)}'&$select=id`,
+      headers: {
+        accept: 'application/json;odata.metadata=none'
+      },
+      responseType: 'json'
+    };
+
+    return request
+      .get<{ value: { id: string; }[] }>(requestOptions)
+      .then(res => {
+        if (res.value.length === 1) {
+          return Promise.resolve(res.value[0].id);
+        }
+        if (res.value.length === 0) {
+          return Promise.reject(`The specified group does not exist`);
+        }
+        return Promise.reject(`Multiple groups found with name ${args.options.groupDisplayName} found. Please choose between the following IDs: ${res.value.map(a => a.id).join(', ')}`);
+      });
   }
 
   public options(): CommandOption[] {
     const options: CommandOption[] = [
       {
-        option: '-i, --groupId <groupId>'
+        option: '-i, --groupId [groupId]'
+      },
+      {
+        option: '-d, --groupDisplayName [groupDisplayName]'
       },
       {
         option: '-t, --threadId <threadId>'
@@ -53,7 +92,14 @@ class AadO365GroupConversationPostListCommand extends GraphItemsListCommand<Post
   }
 
   public validate(args: CommandArgs): boolean | string {
-    if (!Utils.isValidGuid(args.options.groupId as string)) {
+    if (!args.options.groupId &&
+      !args.options.groupDisplayName) {
+      return 'Specify either groupId or groupDisplayName';
+    }
+    if (args.options.groupId && args.options.groupDisplayName) {
+      return 'Specify either groupId or groupDisplayName, but not both';
+    }
+    if (args.options.groupId && !validation.isValidGuid(args.options.groupId as string)) {
       return `${args.options.groupId} is not a valid GUID`;
     }
 
