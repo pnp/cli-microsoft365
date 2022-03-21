15349ce0c33e53c45260f65f0144b33e63388932
Martin Lingstuyl <martin@i4-you.com>
2022-03-21 20:09:26 +0000

    Add new setting autoOpenLinksInBrowser and implement it in login, reconsent

--- src/m365/cli/commands/config/config-set.spec.ts
+++ src/m365/cli/commands/config/config-set.spec.ts
@@ -76,6 +76,25 @@ describe(commands.CONFIG_SET, () => {
     });
   });
 
+  it(`sets ${settingsNames.autoOpenLinksInBrowser} property`, (done) => {
+    const config = Cli.getInstance().config;
+    let actualKey: string, actualValue: any;
+    sinon.stub(config, 'set').callsFake(((key: string, value: any) => {
+      actualKey = key;
+      actualValue = value;
+    }) as any);
+    command.action(logger, { options: { key: settingsNames.autoOpenLinksInBrowser, value: false } }, () => {
+      try {
+        assert.strictEqual(actualKey, settingsNames.autoOpenLinksInBrowser, 'Invalid key');
+        assert.strictEqual(actualValue, false, 'Invalid value');
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
   it(`sets ${settingsNames.output} property to 'text'`, (done) => {
     const output = "text";
     const config = Cli.getInstance().config;
