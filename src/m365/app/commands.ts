effdc2221fd7911be01b5d6158ffa44b3892cbd8
Nanddeep Nachan <nanddeepnachan@gmail.com>
2022-03-21 07:25:04 +0000

    Adds 'app get' command. Closes #2942

--- src/m365/app/commands.ts
+++ src/m365/app/commands.ts
@@ -1,5 +1,6 @@
 const prefix: string = 'app';
 
 export default {
+  GET: `${prefix} get`,
   PERMISSION_LIST: `${prefix} permission list`
 };
---
+++ src/m365/app/commands/app-get.spec.ts
@@ -0,0 +1,174 @@
+import * as assert from 'assert';
+import * as fs from 'fs';
+import * as sinon from 'sinon';
+import appInsights from '../../../appInsights';
+import auth from '../../../Auth';
+import { Logger } from '../../../cli';
+import Command from '../../../Command';
+import request from '../../../request';
+import { sinonUtil } from '../../../utils';
+import commands from '../commands';
+const command: Command = require('./app-get');
+
+describe(commands.GET, () => {
+  let log: string[];
+  let logger: Logger;
+  let loggerLogSpy: sinon.SinonSpy;
+
+  before(() => {
+    sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
+    sinon.stub(appInsights, 'trackEvent').callsFake(() => { });
+    sinon.stub(fs, 'existsSync').callsFake(() => true);
+    sinon.stub(fs, 'readFileSync').callsFake(() => JSON.stringify({
+      "apps": [
+        {
+          "appId": "9b1b1e42-794b-4c71-93ac-5ed92488b67f",
+          "name": "CLI app1"
+        }
+      ]
+    }));
+    auth.service.connected = true;
+  });
+
+  beforeEach(() => {
+    log = [];
+    logger = {
+      log: (msg: string) => {
+        log.push(msg);
+      },
+      logRaw: (msg: string) => {
+        log.push(msg);
+      },
+      logToStderr: (msg: string) => {
+        log.push(msg);
+      }
+    };
+    loggerLogSpy = sinon.spy(logger, 'log');
+  });
+
+  afterEach(() => {
+    sinonUtil.restore([
+      request.get
+    ]);
+  });
+
+  after(() => {
+    sinonUtil.restore([
+      auth.restoreAuth,
+      appInsights.trackEvent,
+      fs.existsSync,
+      fs.readFileSync
+    ]);
+    auth.service.connected = false;
+  });
+
+  it('has correct name', () => {
+    assert.strictEqual(command.name.startsWith(commands.GET), true);
+  });
+
+  it('has a description', () => {
+    assert.notStrictEqual(command.description, null);
+  });
+
+  it('handles error when the app specified with the appId not found', (done) => {
+    sinon.stub(request, 'get').callsFake(opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/myorganization/applications?$filter=appId eq '9b1b1e42-794b-4c71-93ac-5ed92488b67f'&$select=id`) {
+        return Promise.resolve({ value: [] });
+      }
+
+      return Promise.reject(`Invalid request ${JSON.stringify(opts)}`);
+    });
+
+    command.action(logger, {
+      options: {
+        debug: false,
+        appId: '9b1b1e42-794b-4c71-93ac-5ed92488b67f'
+      }
+    }, (err?: any) => {
+      try {
+        assert.strictEqual(err.message, `No Azure AD application registration with ID 9b1b1e42-794b-4c71-93ac-5ed92488b67f found`);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('handles error when retrieving information about app through appId failed', (done) => {
+    sinon.stub(request, 'get').callsFake(_ => Promise.reject('An error has occurred'));
+
+    command.action(logger, {
+      options: {
+        debug: false,
+        appId: '9b1b1e42-794b-4c71-93ac-5ed92488b67f'
+      }
+    }, (err?: any) => {
+      try {
+        assert.strictEqual(err.message, `An error has occurred`);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it(`should get an Azure AD app registration by its app (client) ID.`, (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/myorganization/applications?$filter=appId eq '9b1b1e42-794b-4c71-93ac-5ed92488b67f'&$select=id`) {
+        return Promise.resolve({
+          value: [
+            {
+              "id": "340a4aa3-1af6-43ac-87d8-189819003952",
+              "appId": "9b1b1e42-794b-4c71-93ac-5ed92488b67f",
+              "createdDateTime": "2019-10-29T17:46:55Z",
+              "displayName": "My App",
+              "description": null
+            }
+          ]
+        });
+      }
+
+      if ((opts.url as string).indexOf('/v1.0/myorganization/applications/') > -1) {
+        return Promise.resolve({
+          "id": "340a4aa3-1af6-43ac-87d8-189819003952",
+          "appId": "9b1b1e42-794b-4c71-93ac-5ed92488b67f",
+          "createdDateTime": "2019-10-29T17:46:55Z",
+          "displayName": "My App",
+          "description": null
+        });
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    command.action(logger, {
+      options: {
+        appId: '9b1b1e42-794b-4c71-93ac-5ed92488b67f'
+      }
+    }, () => {
+      try {
+        const call: sinon.SinonSpyCall = loggerLogSpy.lastCall;
+        assert.strictEqual(call.args[0].id, '340a4aa3-1af6-43ac-87d8-189819003952');
+        assert.strictEqual(call.args[0].appId, '9b1b1e42-794b-4c71-93ac-5ed92488b67f');
+        assert.strictEqual(call.args[0].displayName, 'My App');
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('supports debug mode', () => {
+    const options = command.options();
+    let containsOption = false;
+    options.forEach(o => {
+      if (o.option === '--debug') {
+        containsOption = true;
+      }
+    });
+    assert(containsOption);
+  });
+});
