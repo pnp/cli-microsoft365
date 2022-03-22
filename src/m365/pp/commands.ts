8374a7280ac3dff64c3f5cc5bfa91cdcdfc2a4b5
Reshmee011 <reshmee011@gmail.com>
2022-03-22 06:01:27 +0000

    New cmdlet to list Power Platform environment closes #3016

--- src/m365/pp/commands.ts
+++ src/m365/pp/commands.ts
@@ -1,5 +1,6 @@
 const prefix: string = 'pp';
 
 export default {
-  MANAGEMENTAPP_ADD: `${prefix} managementapp add`
+  MANAGEMENTAPP_ADD: `${prefix} managementapp add`,
+  ENVIRONMENT_LIST: `${prefix} environment list`
 };
