67f2b9b5ea3e701f30091a0c459ad89bbe66a8bb
Jasey Waegebaert <jaseyw@gmigroup.be>
2022-05-11 15:58:53 +0200

    Added task reference add

--- src/m365/planner/commands.ts
+++ src/m365/planner/commands.ts
@@ -6,12 +6,13 @@ export default {
   BUCKET_SET: `${prefix} bucket set`,
   BUCKET_REMOVE: `${prefix} bucket remove`,
   PLAN_ADD: `${prefix} plan add`,
-  PLAN_GET: `${prefix} plan get`,
   PLAN_DETAILS_GET: `${prefix} plan details get`,
+  PLAN_GET: `${prefix} plan get`,
   PLAN_LIST: `${prefix} plan list`,
   TASK_ADD: `${prefix} task add`,
   TASK_DETAILS_GET: `${prefix} task details get`,
   TASK_GET: `${prefix} task get`,
   TASK_LIST: `${prefix} task list`,
+  TASK_REFERENCE_ADD: `${prefix} task reference add`,
   TASK_SET: `${prefix} task set`
 };
