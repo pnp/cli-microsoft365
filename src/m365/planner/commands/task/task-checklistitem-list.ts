7d1cc01c1b22d52a8554ff74a6e5b2eb760c11b2
Akash Karda <akashkarda@gmail.com>
2022-05-17 17:40:52 -0400

    Adds planner task checklistitem list

---
+++ src/m365/planner/commands/task/task-checklistitem-list.ts
@@ -0,0 +1,78 @@
+import { Logger } from "../../../../cli";
+import { CommandOption } from "../../../../Command";
+import { accessToken } from "../../../../utils";
+import GlobalOptions from "../../../../GlobalOptions";
+import Auth from "../../../../Auth";
+import request from "../../../../request";
+import GraphCommand from "../../../base/GraphCommand";
+import commands from "../../commands";
+import { PlannerChecklistItems } from "@microsoft/microsoft-graph-types";
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  taskId: string;
+}
+
+class PlannerTaskChecklistItemListCommand extends GraphCommand {
+  public get name(): string {
+    return commands.TASK_CHECKLISTITEM_LIST;
+  }
+
+  public get description(): string {
+    return "Lists the checklist items of a Planner task.";
+  }
+
+  public commandAction(
+    logger: Logger,
+    args: CommandArgs,
+    cb: () => void
+  ): void {
+    if (
+      accessToken.isAppOnlyAccessToken(
+        Auth.service.accessTokens[this.resource].accessToken
+      )
+    ) {
+      this.handleError(
+        "This command does not support application permissions.",
+        logger,
+        cb
+      );
+      return;
+    }
+
+    const requestOptions: any = {
+      url: `${this.resource}/v1.0/planner/tasks/${encodeURIComponent(
+        args.options.taskId
+      )}/details`,
+      headers: {
+        accept: "application/json;odata.metadata=none"
+      },
+      responseType: "json"
+    };
+
+    request.get(requestOptions).then(
+      (res: any): void => {
+        const checklistitem: PlannerChecklistItems = res.checklist;
+        logger.log(checklistitem);
+        cb();
+      },
+      (err: any): void => this.handleRejectedODataJsonPromise(err, logger, cb)
+    );
+  }
+
+  public options(): CommandOption[] {
+    const options: CommandOption[] = [
+      {
+        option: "-i, --taskId <taskId>"
+      }
+    ];
+
+    const parentOptions: CommandOption[] = super.options();
+    return options.concat(parentOptions);
+  }
+}
+
+module.exports = new PlannerTaskChecklistItemListCommand();
