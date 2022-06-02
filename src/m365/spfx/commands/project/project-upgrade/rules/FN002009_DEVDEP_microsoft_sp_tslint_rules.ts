92c00b610fe763e3623af5a3538106622cd0c9ba
Waldek Mastykarz <waldek@mastykarz.nl>
2022-06-02 20:05:23 +0200

    Adds support for upgrading SPFx projects to v1.15.0-rc.0. Closes #3376

--- src/m365/spfx/commands/project/project-upgrade/rules/FN002009_DEVDEP_microsoft_sp_tslint_rules.ts
+++ src/m365/spfx/commands/project/project-upgrade/rules/FN002009_DEVDEP_microsoft_sp_tslint_rules.ts
@@ -1,8 +1,8 @@
 import { DependencyRule } from "./DependencyRule";
 
 export class FN002009_DEVDEP_microsoft_sp_tslint_rules extends DependencyRule {
-  constructor(packageVersion: string) {
-    super('@microsoft/sp-tslint-rules', packageVersion, true);
+  constructor(packageVersion: string, add: boolean = true) {
+    super('@microsoft/sp-tslint-rules', packageVersion, true, false, add);
   }
 
   get id(): string {
