411cd43982b7ea17af900b6f75d6d743c244a4de
Waldek Mastykarz <waldek@mastykarz.nl>
2022-06-02 20:05:23 +0200

    Adds support for upgrading SPFx projects to v1.15.0-rc.0. Closes #3376

--- src/m365/spfx/commands/project/project-upgrade/rules/FN002018_DEVDEP_microsoft_rush_stack_compiler_3_9.ts
+++ src/m365/spfx/commands/project/project-upgrade/rules/FN002018_DEVDEP_microsoft_rush_stack_compiler_3_9.ts
@@ -1,8 +1,8 @@
 import { DependencyRule } from "./DependencyRule";
 
 export class FN002018_DEVDEP_microsoft_rush_stack_compiler_3_9 extends DependencyRule {
-  constructor(packageVersion: string) {
-    super('@microsoft/rush-stack-compiler-3.9', packageVersion, true);
+  constructor(packageVersion: string, add: boolean = true) {
+    super('@microsoft/rush-stack-compiler-3.9', packageVersion, true, false, add);
   }
 
   get id(): string {
