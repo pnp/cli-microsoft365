e2d2743a60b1422dfbc791f06fd051eae2b1f03f
Milan Holemans <11723921+milanholemans@users.noreply.github.com>
2022-06-21 22:49:45 +0200

    Extends aad o365group recyclebinitem restore command with extra options. Closes #2031

--- docs/docs/cmd/aad/o365group/o365group-recyclebinitem-restore.md
+++ docs/docs/cmd/aad/o365group/o365group-recyclebinitem-restore.md
@@ -16,15 +16,33 @@ m365 aad o365group restore [options]
 
 ## Options
 
-`-i, --id <id>`
-: The ID of the Microsoft 365 Group to restore
+`-i, --id [id]`
+: The ID of the Microsoft 365 Group to restore. Specify either `id`, `displayName` or `mailNickname` but not multiple.
+
+`-d, --displayName [displayName]`
+: Display name for the Microsoft 365 Group to restore. Specify either `id`, `displayName` or `mailNickname` but not multiple.
+
+`-m, --mailNickname [mailNickname]`
+: Name of the group e-mail (part before the @). Specify either `id`, `displayName` or `mailNickname` but not multiple.
 
 --8<-- "docs/cmd/_global.md"
 
 ## Examples
 
-Restores the Microsoft 365 Group with id _28beab62-7540-4db1-a23f-29a6018a3848_
+Restores the Microsoft 365 Group with specific ID
 
 ```sh
 m365 aad o365group recyclebinitem restore --id 28beab62-7540-4db1-a23f-29a6018a3848
+```
+
+Restores the Microsoft 365 Group with specific name
+
+```sh
+m365 aad o365group recyclebinitem restore --displayName "My Group"
+```
+
+Restores the Microsoft 365 Group with specific mail nickname
+
+```sh
+m365 aad o365group recyclebinitem restore --mailNickname "Mygroup"
 ```
--- src/m365/aad/commands/o365group/o365group-recyclebinitem-restore.spec.ts
+++ src/m365/aad/commands/o365group/o365group-recyclebinitem-restore.spec.ts
@@ -5,15 +5,55 @@ import auth from '../../../../Auth';
 import { Logger } from '../../../../cli';
 import Command, { CommandError } from '../../../../Command';
 import request from '../../../../request';
-import { sinonUtil } from '../../../../utils';
+import { formatting, sinonUtil } from '../../../../utils';
 import commands from '../../commands';
 const command: Command = require('./o365group-recyclebinitem-restore');
 
 describe(commands.O365GROUP_RECYCLEBINITEM_RESTORE, () => {
+
+  const validGroupId = '00000000-0000-0000-0000-000000000000';
+  const validGroupDisplayName = 'Dev Team';
+  const validGroupMailNickname = 'Devteam';
+
+  const singleGroupsResponse = {
+    value: [
+      {
+        id: validGroupId,
+        displayName: validGroupDisplayName,
+        mailNickname: validGroupDisplayName,
+        mail: 'Devteam@contoso.com',
+        groupTypes: [
+          "Unified"
+        ]
+      }
+    ]
+  };
+
+  const multipleGroupsResponse = {
+    value: [
+      {
+        id: validGroupId,
+        displayName: validGroupDisplayName,
+        mailNickname: validGroupDisplayName,
+        mail: 'Devteam@contoso.com',
+        groupTypes: [
+          "Unified"
+        ]
+      },
+      {
+        id: validGroupId,
+        displayName: validGroupDisplayName,
+        mailNickname: validGroupDisplayName,
+        mail: 'Devteam@contoso.com',
+        groupTypes: [
+          "Unified"
+        ]
+      }
+    ]
+  };
+
   let log: string[];
   let logger: Logger;
-  let loggerLogSpy: sinon.SinonSpy;
-  let loggerLogToStderrSpy: sinon.SinonSpy;
 
   before(() => {
     sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
@@ -34,12 +74,11 @@ describe(commands.O365GROUP_RECYCLEBINITEM_RESTORE, () => {
         log.push(msg);
       }
     };
-    loggerLogSpy = sinon.spy(logger, 'log');
-    loggerLogToStderrSpy = sinon.spy(logger, 'logToStderr');
   });
 
   afterEach(() => {
     sinonUtil.restore([
+      request.get,
       request.post,
       global.setTimeout
     ]);
@@ -71,18 +110,38 @@ describe(commands.O365GROUP_RECYCLEBINITEM_RESTORE, () => {
     assert.notStrictEqual(command.description, null);
   });
 
-  it('restores the specified group', (done) => {
+  it('defines correct option sets', () => {
+    const optionSets = command.optionSets();
+    assert.deepStrictEqual(optionSets, [['id', 'displayName', 'mailNickname']]);
+  });
+
+  it('fails validation if the id is not a valid GUID', () => {
+    const actual = command.validate({ options: { id: 'abc' } });
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('passes validation when the id is a valid GUID', () => {
+    const actual = command.validate({ options: { id: '2c1ba4c4-cd9b-4417-832f-92a34bc34b2a' } });
+    assert.strictEqual(actual, true);
+  });
+
+  it('restores the specified group by id', (done) => {
     sinon.stub(request, 'post').callsFake((opts) => {
-      if (opts.url === 'https://graph.microsoft.com/v1.0/directory/deleteditems/28beab62-7540-4db1-a23f-29a6018a3848/restore/') {
+      if (opts.url === `https://graph.microsoft.com/v1.0/directory/deleteditems/${validGroupId}/restore`) {
         return Promise.resolve();
       }
 
       return Promise.reject('Invalid request');
     });
 
-    command.action(logger, { options: { debug: false, id: '28beab62-7540-4db1-a23f-29a6018a3848'} }, () => {
+    command.action(logger, {
+      options: {
+        verbose: true,
+        id: validGroupId
+      }
+    }, (err?: any) => {
       try {
-        assert(loggerLogSpy.notCalled);
+        assert.strictEqual(typeof err, 'undefined', err?.message);
         done();
       }
       catch (e) {
@@ -91,18 +150,62 @@ describe(commands.O365GROUP_RECYCLEBINITEM_RESTORE, () => {
     });
   });
 
-  it('restores the specified group (debug)', (done) => {
+  it('correctly restores group by displayName', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/directory/deletedItems/Microsoft.Graph.Group?$filter=displayName eq '${formatting.encodeQueryParameter(validGroupDisplayName)}'`) {
+        return Promise.resolve(singleGroupsResponse);
+      }
+
+      return Promise.reject('Invalid Request');
+    });
     sinon.stub(request, 'post').callsFake((opts) => {
-      if (opts.url === 'https://graph.microsoft.com/v1.0/directory/deleteditems/28beab62-7540-4db1-a23f-29a6018a3848/restore/') {
+      if (opts.url === `https://graph.microsoft.com/v1.0/directory/deleteditems/${validGroupId}/restore`) {
         return Promise.resolve();
       }
 
       return Promise.reject('Invalid request');
     });
 
-    command.action(logger, { options: { debug: true, id: '28beab62-7540-4db1-a23f-29a6018a3848'} } as any, () => {
+    command.action(logger, {
+      options: {
+        verbose: true,
+        displayName: validGroupDisplayName
+      }
+    }, (err?: any) => {
       try {
-        assert(loggerLogToStderrSpy.called);
+        assert.strictEqual(typeof err, 'undefined', err?.message);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('correctly restores group by mailNickname', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/directory/deletedItems/Microsoft.Graph.Group?$filter=mailNickname eq '${formatting.encodeQueryParameter(validGroupMailNickname)}'`) {
+        return Promise.resolve(singleGroupsResponse);
+      }
+
+      return Promise.reject('Invalid Request');
+    });
+    sinon.stub(request, 'post').callsFake((opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/directory/deleteditems/${validGroupId}/restore`) {
+        return Promise.resolve();
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    command.action(logger, {
+      options: {
+        verbose: true,
+        mailNickname: validGroupMailNickname
+      }
+    }, (err?: any) => {
+      try {
+        assert.strictEqual(typeof err, 'undefined', err?.message);
         done();
       }
       catch (e) {
@@ -113,12 +216,62 @@ describe(commands.O365GROUP_RECYCLEBINITEM_RESTORE, () => {
 
   it('correctly handles error when group is not found', (done) => {
     sinon.stub(request, 'post').callsFake(() => {
-      return Promise.reject({ error: { 'odata.error': { message: { value: 'File Not Found.' } } } });
+      return Promise.reject({ error: { 'odata.error': { message: { value: 'Group Not Found.' } } } });
     });
 
     command.action(logger, { options: { debug: false, id: '28beab62-7540-4db1-a23f-29a6018a3848' } } as any, (err?: any) => {
       try {
-        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError('File Not Found.')));
+        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError('Group Not Found.')));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('throws error message when no group was found', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/directory/deletedItems/Microsoft.Graph.Group?$filter=mailNickname eq '${formatting.encodeQueryParameter(validGroupMailNickname)}'`) {
+        return Promise.resolve({ value: [] });
+      }
+
+      return Promise.reject('Invalid Request');
+    });
+
+    command.action(logger, {
+      options: {
+        mailNickname: validGroupMailNickname,
+        confirm: true
+      }
+    }, (err?: any) => {
+      try {
+        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError(`The specified group '${validGroupMailNickname}' does not exist.`)));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('throws error message when multiple groups were found', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/directory/deletedItems/Microsoft.Graph.Group?$filter=mailNickname eq '${formatting.encodeQueryParameter(validGroupMailNickname)}'`) {
+        return Promise.resolve(multipleGroupsResponse);
+      }
+
+      return Promise.reject('Invalid Request');
+    });
+
+    command.action(logger, {
+      options: {
+        mailNickname: validGroupMailNickname,
+        confirm: true
+      }
+    }, (err?: any) => {
+      try {
+        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError(`Multiple groups with name '${validGroupMailNickname}' found: ${multipleGroupsResponse.value.map(x => x.id).join(',')}.`)));
         done();
       }
       catch (e) {
@@ -136,26 +289,5 @@ describe(commands.O365GROUP_RECYCLEBINITEM_RESTORE, () => {
       }
     });
     assert(containsOption);
-  });
-
-  it('supports specifying id', () => {
-    const options = command.options();
-    let containsOption = false;
-    options.forEach(o => {
-      if (o.option.indexOf('--id') > -1) {
-        containsOption = true;
-      }
-    });
-    assert(containsOption);
-  });
-
-  it('fails validation if the id is not a valid GUID', () => {
-    const actual = command.validate({ options: { id: 'abc' } });
-    assert.notStrictEqual(actual, true);
-  });
-
-  it('passes validation when the id is a valid GUID', () => {
-    const actual = command.validate({ options: { id: '2c1ba4c4-cd9b-4417-832f-92a34bc34b2a' } });
-    assert.strictEqual(actual, true);
   });
 });
