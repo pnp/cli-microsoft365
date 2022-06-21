32b9241a1a7817dd4f17cc99e9ef7723d9524c94
Milan Holemans <11723921+milanholemans@users.noreply.github.com>
2022-06-21 22:49:45 +0200

    Adds extra options

--- src/m365/aad/commands/o365group/o365group-recyclebinitem-restore.ts
+++ src/m365/aad/commands/o365group/o365group-recyclebinitem-restore.ts
@@ -1,10 +1,12 @@
+import { Group } from '@microsoft/microsoft-graph-types';
+import { AxiosRequestConfig } from 'axios';
 import { Logger } from '../../../../cli';
 import {
   CommandOption
 } from '../../../../Command';
 import GlobalOptions from '../../../../GlobalOptions';
 import request from '../../../../request';
-import { validation } from '../../../../utils';
+import { formatting, validation } from '../../../../utils';
 import GraphCommand from '../../../base/GraphCommand';
 import commands from '../../commands';
 
@@ -13,7 +15,9 @@ interface CommandArgs {
 }
 
 interface Options extends GlobalOptions {
-  id: string;
+  id?: string;
+  displayName?: string;
+  mailNickname?: string;
 }
 
 class AadO365GroupRecycleBinItemRestoreCommand extends GraphCommand {
@@ -29,29 +33,83 @@ class AadO365GroupRecycleBinItemRestoreCommand extends GraphCommand {
     return [commands.O365GROUP_RESTORE];
   }
 
-  public commandAction(logger: Logger, args: CommandArgs, cb: () => void): void {
+  public async commandAction(logger: Logger, args: CommandArgs, cb: () => void): Promise<void> {
     if (this.verbose) {
-      logger.logToStderr(`Restoring Microsoft 365 Group: ${args.options.id}...`);
+      logger.logToStderr(`Restoring Microsoft 365 Group: ${args.options.id || args.options.displayName || args.options.mailNickname}...`);
     }
 
-    const requestOptions: any = {
-      url: `${this.resource}/v1.0/directory/deleteditems/${args.options.id}/restore/`,
+    try {
+      const groupId = await this.getGroupId(args.options);
+
+      const requestOptions: AxiosRequestConfig = {
+        url: `${this.resource}/v1.0/directory/deleteditems/${groupId}/restore`,
+        headers: {
+          accept: 'application/json;odata.metadata=none',
+          'content-type': 'application/json'
+        },
+        responseType: 'json'
+      };
+
+      await request.post(requestOptions);
+      cb();
+    }
+    catch(err: any) {
+      this.handleRejectedODataJsonPromise(err, logger, cb);
+    }
+  }
+
+  private async getGroupId(options: Options): Promise<string> {
+    const { id, displayName, mailNickname } = options;
+
+    if (id) {
+      return id;
+    }
+
+    let filterValue: string = '';
+    if (displayName) {
+      filterValue = `displayName eq '${formatting.encodeQueryParameter(displayName)}'`;
+    }
+
+    if (mailNickname) {
+      filterValue = `mailNickname eq '${formatting.encodeQueryParameter(mailNickname)}'`;
+    }
+
+    const requestOptions: AxiosRequestConfig = {
+      url: `${this.resource}/v1.0/directory/deletedItems/Microsoft.Graph.Group?$filter=${filterValue}`,
       headers: {
-        'accept': 'application/json;odata.metadata=none',
-        'content-type': 'application/json'
+        accept: 'application/json;odata.metadata=none'
       },
       responseType: 'json'
     };
 
-    request
-      .post(requestOptions)
-      .then(_ => cb(), (rawRes: any): void => this.handleRejectedODataJsonPromise(rawRes, logger, cb));
+    const response = await request.get<{ value: Group[] }>(requestOptions);
+    const groups = response.value;
+
+    if (groups.length === 0) {
+      throw Error(`The specified group '${displayName || mailNickname}' does not exist.`);
+    }
+
+    if (groups.length > 1) {
+      throw Error(`Multiple groups with name '${displayName || mailNickname}' found: ${groups.map(x => x.id).join(',')}.`);
+    }
+
+    return groups[0].id!;
+  }
+
+  public optionSets(): string[][] | undefined {
+    return [['id', 'displayName', 'mailNickname']];
   }
 
   public options(): CommandOption[] {
     const options: CommandOption[] = [
       {
-        option: '-i, --id <id>'
+        option: '-i, --id [id]'
+      },
+      {
+        option: '-d, --displayName [displayName]'
+      },
+      {
+        option: '-m, --mailNickname [mailNickname]'
       }
     ];
 
@@ -60,7 +118,7 @@ class AadO365GroupRecycleBinItemRestoreCommand extends GraphCommand {
   }
 
   public validate(args: CommandArgs): boolean | string {
-    if (!validation.isValidGuid(args.options.id)) {
+    if (args.options.id && !validation.isValidGuid(args.options.id)) {
       return `${args.options.id} is not a valid GUID`;
     }
 
