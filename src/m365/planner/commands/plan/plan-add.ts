15a179b6573ebeab40f5ed19ba6a3df7c08c56fb
Jasey Waegebaert <jwaegebaert@gmail.com>
2022-06-23 16:11:24 +0200

    Extends planner plan add with more options. Closes #3345

--- src/m365/planner/commands/plan/plan-add.ts
+++ src/m365/planner/commands/plan/plan-add.ts
@@ -1,14 +1,14 @@
+import { PlannerPlan, PlannerPlanDetails, User } from '@microsoft/microsoft-graph-types';
+import Auth from '../../../../Auth';
 import { Logger } from '../../../../cli';
 import {
   CommandOption
 } from '../../../../Command';
-import { accessToken, validation } from '../../../../utils';
-import Auth from '../../../../Auth';
 import GlobalOptions from '../../../../GlobalOptions';
 import request from '../../../../request';
+import { aadGroup, accessToken, formatting, validation } from '../../../../utils';
 import GraphCommand from '../../../base/GraphCommand';
 import commands from '../../commands';
-import { aadGroup } from '../../../../utils/aadGroup';
 
 interface CommandArgs {
   options: Options;
@@ -18,6 +18,8 @@ interface Options extends GlobalOptions {
   title: string;
   ownerGroupId?: string;
   ownerGroupName?: string;
+  shareWithUserIds?: string;
+  shareWithUserNames?: string;
 }
 
 class PlannerPlanAddCommand extends GraphCommand {
@@ -33,6 +35,8 @@ class PlannerPlanAddCommand extends GraphCommand {
     const telemetryProps: any = super.getTelemetryProperties(args);
     telemetryProps.ownerGroupId = typeof args.options.ownerGroupId !== 'undefined';
     telemetryProps.ownerGroupName = typeof args.options.ownerGroupName !== 'undefined';
+    telemetryProps.shareWithUserIds = typeof args.options.shareWithUserIds !== 'undefined';
+    telemetryProps.shareWithUserNames = typeof args.options.shareWithUserNames !== 'undefined';
     return telemetryProps;
   }
 
@@ -45,7 +49,7 @@ class PlannerPlanAddCommand extends GraphCommand {
       this.handleError('This command does not support application permissions.', logger, cb);
       return;
     }
-    
+
     this
       .getGroupId(args)
       .then((groupId: string): Promise<any> => {
@@ -63,10 +67,107 @@ class PlannerPlanAddCommand extends GraphCommand {
 
         return request.post(requestOptions);
       })
+      .then(newPlan => this.updatePlanDetails(args.options, newPlan))
       .then((res: any): void => {
         logger.log(res);
         cb();
       }, (err: any): void => this.handleRejectedODataJsonPromise(err, logger, cb));
+  }
+
+  private updatePlanDetails(options: Options, newPlan: PlannerPlan): Promise<PlannerPlan & PlannerPlanDetails> {
+    const planId = newPlan.id as string;
+
+    if (!options.shareWithUserIds && !options.shareWithUserNames) {
+      return Promise.resolve(newPlan);
+    }
+
+    return Promise
+      .all([this.generateSharedWith(options), this.getPlanDetailsEtag(planId)])
+      .then(resArray => {
+        const sharedWith = resArray[0];
+        const etag = resArray[1];
+        const requestOptionsPlanDetails: any = {
+          url: `${this.resource}/v1.0/planner/plans/${planId}/details`,
+          headers: {
+            'accept': 'application/json;odata.metadata=none',
+            'If-Match': etag,
+            'Prefer': 'return=representation'
+          },
+          responseType: 'json',
+          data: {
+            sharedWith: sharedWith
+          }
+        };
+
+        return request.patch(requestOptionsPlanDetails);
+      })
+      .then(planDetails => {
+        return { ...newPlan, ...planDetails as PlannerPlanDetails };
+      });
+  }
+
+  private getPlanDetailsEtag(planId: string): Promise<string> {
+    const requestOptions: any = {
+      url: `${this.resource}/v1.0/planner/plans/${planId}/details`,
+      headers: {
+        accept: 'application/json'
+      },
+      responseType: 'json'
+    };
+
+    return request
+      .get(requestOptions)
+      .then((response: any) => response['@odata.etag']);
+  }
+
+  private generateSharedWith(options: Options): Promise<{ [userId: string]: boolean }> {
+    const sharedWith: { [userId: string]: boolean } = {};
+
+    return this
+      .getUserIds(options)
+      .then((userIds) => {
+        userIds.map(x => sharedWith[x] = true);
+
+        return Promise.resolve(sharedWith);
+      });
+  }
+
+  private getUserIds(options: Options): Promise<string[]> {
+    if (options.shareWithUserIds) {
+      return Promise.resolve(options.shareWithUserIds.split(','));
+    }
+
+    // Hitting this section means assignedToUserNames won't be undefined
+    const userNames = options.shareWithUserNames as string;
+    const userArr: string[] = userNames.split(',').map(o => o.trim());
+
+    const promises: Promise<{ value: User[] }>[] = userArr.map(user => {
+      const requestOptions: any = {
+        url: `${this.resource}/v1.0/users?$filter=userPrincipalName eq '${formatting.encodeQueryParameter(user)}'&$select=id,userPrincipalName`,
+        headers: {
+          'content-type': 'application/json'
+        },
+        responseType: 'json'
+      };
+
+      return request.get(requestOptions);
+    });
+
+    return Promise
+      .all(promises)
+      .then((usersRes: { value: User[] }[]): Promise<string[]> => {
+        const userUpns = usersRes.map(res => res.value[0]?.userPrincipalName as string);
+        const userIds = usersRes.map(res => res.value[0]?.id as string);
+
+        // Find the members where no graph response was found
+        const invalidUsers = userArr.filter(user => !userUpns.some((upn) => upn?.toLowerCase() === user.toLowerCase()));
+
+        if (invalidUsers && invalidUsers.length > 0) {
+          return Promise.reject(`Cannot proceed with planner plan creation. The following users provided are invalid : ${invalidUsers.join(',')}`);
+        }
+
+        return Promise.resolve(userIds);
+      });
   }
 
   private getGroupId(args: CommandArgs): Promise<string> {
@@ -85,10 +186,16 @@ class PlannerPlanAddCommand extends GraphCommand {
         option: '-t, --title <title>'
       },
       {
-        option: "--ownerGroupId [ownerGroupId]"
+        option: '--ownerGroupId [ownerGroupId]'
       },
       {
-        option: "--ownerGroupName [ownerGroupName]"
+        option: '--ownerGroupName [ownerGroupName]'
+      },
+      {
+        option: '--shareWithUserIds [shareWithUserIds]'
+      },
+      {
+        option: '--shareWithUserNames [shareWithUserNames]'
       }
     ];
 
@@ -107,6 +214,14 @@ class PlannerPlanAddCommand extends GraphCommand {
 
     if (args.options.ownerGroupId && !validation.isValidGuid(args.options.ownerGroupId as string)) {
       return `${args.options.ownerGroupId} is not a valid GUID`;
+    }
+
+    if (args.options.shareWithUserIds && args.options.shareWithUserNames) {
+      return 'Specify either shareWithUserIds or shareWithUserNames but not both';
+    }
+
+    if (args.options.shareWithUserIds && !validation.isValidGuidArray(args.options.shareWithUserIds.split(','))) {
+      return 'shareWithUserIds contains invalid GUID';
     }
 
     return true;
