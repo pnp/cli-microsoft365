1d042e113292f80173e8d455ab4719bb6e0e7272
Nanddeep Nachan <nanddeepnachan@gmail.com>
2022-07-10 19:53:57 +0530

    Update hubsite-get.spec.ts

--- src/m365/spo/commands/hubsite/hubsite-get.spec.ts
+++ src/m365/spo/commands/hubsite/hubsite-get.spec.ts
@@ -11,6 +11,17 @@ import * as SpoListItemListCommand from '../listitem/listitem-list';
 const command: Command = require('./hubsite-get');
 
 describe(commands.HUBSITE_GET, () => {
+  const validId = '9ff01368-1183-4cbb-82f2-92e7e9a3f4ce';
+  const validTitle = 'Hub Site';
+  const validUrl = 'https://contoso.sharepoint.com';
+
+  const hubsiteResponse = {
+    "ID": validId,
+    "SiteId": "389d0d83-40bb-40ad-b92a-534b7cb37d0b",
+    "SiteUrl": validUrl,
+    "Title": validTitle
+  };
+
   let log: string[];
   let logger: Logger;
   let loggerLogSpy: sinon.SinonSpy;
@@ -62,36 +73,23 @@ describe(commands.HUBSITE_GET, () => {
     assert.notStrictEqual(command.description, null);
   });
 
+  it('defines correct option sets', () => {
+    const optionSets = command.optionSets();
+    assert.deepStrictEqual(optionSets, [['id', 'title', 'url']]);
+  });
+
   it('gets information about the specified hub site', (done) => {
     sinon.stub(request, 'get').callsFake((opts) => {
-      if ((opts.url as string).indexOf(`/_api/hubsites/getbyid('ee8b42c3-3e6f-4822-87c1-c21ad666046b')`) > -1) {
-        return Promise.resolve({
-          "Description": null,
-          "ID": "389d0d83-40bb-40ad-b92a-534b7cb37d0b",
-          "LogoUrl": "http://contoso.com/__siteIcon__.jpg",
-          "SiteId": "389d0d83-40bb-40ad-b92a-534b7cb37d0b",
-          "SiteUrl": "https://contoso.sharepoint.com/sites/Sales",
-          "Targets": null,
-          "TenantInstanceId": "00000000-0000-0000-0000-000000000000",
-          "Title": "Sales"
-        });
+      if ((opts.url as string).indexOf(`/_api/hubsites/getbyid('${validId}')`) > -1) {
+        return Promise.resolve(hubsiteResponse);
       }
 
       return Promise.reject('Invalid request');
     });
 
-    command.action(logger, { options: { debug: false, id: 'ee8b42c3-3e6f-4822-87c1-c21ad666046b' } }, () => {
+    command.action(logger, { options: { debug: false, id: validId } }, () => {
       try {
-        assert(loggerLogSpy.calledWith({
-          "Description": null,
-          "ID": "389d0d83-40bb-40ad-b92a-534b7cb37d0b",
-          "LogoUrl": "http://contoso.com/__siteIcon__.jpg",
-          "SiteId": "389d0d83-40bb-40ad-b92a-534b7cb37d0b",
-          "SiteUrl": "https://contoso.sharepoint.com/sites/Sales",
-          "Targets": null,
-          "TenantInstanceId": "00000000-0000-0000-0000-000000000000",
-          "Title": "Sales"
-        }));
+        assert(loggerLogSpy.calledWith(hubsiteResponse));
         done();
       }
       catch (e) {
@@ -102,12 +100,152 @@ describe(commands.HUBSITE_GET, () => {
 
   it('gets information about the specified hub site (debug)', (done) => {
     sinon.stub(request, 'get').callsFake((opts) => {
+      if ((opts.url as string).indexOf(`/_api/hubsites/getbyid('${validId}')`) > -1) {
+        return Promise.resolve(hubsiteResponse);
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    command.action(logger, { options: { debug: true, id: validId } }, () => {
+      try {
+        assert(loggerLogSpy.calledWith(hubsiteResponse));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('gets information about the specified hub site by title', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if ((opts.url as string).indexOf(`/_api/hubsites`) > -1) {
+        return Promise.resolve({ value: [hubsiteResponse] });
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    command.action(logger, { options: { debug: false, title: validTitle } }, () => {
+      try {
+        assert(loggerLogSpy.calledWith(hubsiteResponse));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('gets information about the specified hub site by url', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if ((opts.url as string).indexOf(`/_api/hubsites`) > -1) {
+        return Promise.resolve({ value: [hubsiteResponse] });
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    command.action(logger, { options: { debug: false, url: validUrl } }, () => {
+      try {
+        assert(loggerLogSpy.calledWith(hubsiteResponse));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('fails when multiple hubsites found with same title', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if ((opts.url as string).indexOf(`/_api/hubsites`) > -1) {
+        return Promise.resolve({ value: [hubsiteResponse, hubsiteResponse] });
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    command.action(logger, { options: { debug: false, title: validTitle } }, (err?: any) => {
+      try {
+        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError(`Multiple hub sites with ${validTitle} found. Please disambiguate: ${validUrl}, ${validUrl}`)));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('fails when no hubsites found with title', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if ((opts.url as string).indexOf(`/_api/hubsites`) > -1) {
+        return Promise.resolve({ value: [] });
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    command.action(logger, { options: { debug: false, title: validTitle } }, (err?: any) => {
+      try {
+        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError(`The specified hub site ${validTitle} does not exist`)));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('fails when multiple hubsites found with same url', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if ((opts.url as string).indexOf(`/_api/hubsites`) > -1) {
+        return Promise.resolve({ value: [hubsiteResponse, hubsiteResponse] });
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    command.action(logger, { options: { debug: false, url: validUrl } }, (err?: any) => {
+      try {
+        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError(`Multiple hub sites with ${validUrl} found. Please disambiguate: ${validUrl}, ${validUrl}`)));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('fails when no hubsites found with url', (done) => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if ((opts.url as string).indexOf(`/_api/hubsites`) > -1) {
+        return Promise.resolve({ value: [] });
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    command.action(logger, { options: { debug: false, url: validUrl } }, (err?: any) => {
+      try {
+        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError(`The specified hub site ${validUrl} does not exist`)));
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('display error message when includeAssociatedSites option is used with other than json output.', async () => {
+    sinon.stub(request, 'get').callsFake((opts) => {
       if ((opts.url as string).indexOf(`/_api/hubsites/getbyid('ee8b42c3-3e6f-4822-87c1-c21ad666046b')`) > -1) {
         return Promise.resolve({
           "Description": null,
-          "ID": "389d0d83-40bb-40ad-b92a-534b7cb37d0b",
+          "ID": "ee8b42c3-3e6f-4822-87c1-c21ad666046b",
           "LogoUrl": "http://contoso.com/__siteIcon__.jpg",
-          "SiteId": "389d0d83-40bb-40ad-b92a-534b7cb37d0b",
+          "SiteId": "ee8b42c3-3e6f-4822-87c1-c21ad666046b",
           "SiteUrl": "https://contoso.sharepoint.com/sites/Sales",
           "Targets": null,
           "TenantInstanceId": "00000000-0000-0000-0000-000000000000",
@@ -118,22 +256,12 @@ describe(commands.HUBSITE_GET, () => {
       return Promise.reject('Invalid request');
     });
 
-    command.action(logger, { options: { debug: true, id: 'ee8b42c3-3e6f-4822-87c1-c21ad666046b' } }, () => {
+    command.action(logger, { options: { debug: false, id: 'ee8b42c3-3e6f-4822-87c1-c21ad666046b', includeAssociatedSites: true, output: 'text' } }, () => {
       try {
-        assert(loggerLogSpy.calledWith({
-          "Description": null,
-          "ID": "389d0d83-40bb-40ad-b92a-534b7cb37d0b",
-          "LogoUrl": "http://contoso.com/__siteIcon__.jpg",
-          "SiteId": "389d0d83-40bb-40ad-b92a-534b7cb37d0b",
-          "SiteUrl": "https://contoso.sharepoint.com/sites/Sales",
-          "Targets": null,
-          "TenantInstanceId": "00000000-0000-0000-0000-000000000000",
-          "Title": "Sales"
-        }));
-        done();
+        assert.fail('No error message thrown.');
       }
-      catch (e) {
-        done(e);
+      catch (ex) {
+        assert.deepStrictEqual(ex, Error(`includeAssociatedSites option is only allowed with json output mode`));
       }
     });
   });
@@ -232,7 +360,7 @@ describe(commands.HUBSITE_GET, () => {
             "code": "-1, Microsoft.SharePoint.Client.ResourceNotFoundException",
             "message": {
               "lang": "en-US",
-              "value": "Exception of type 'Microsoft.SharePoint.Client.ResourceNotFoundException' was thrown."
+              "value": "The specified hub site with id ee8b42c3-3e6f-4822-87c1-c21ad666046b does not exist"
             }
           }
         }
@@ -241,7 +369,7 @@ describe(commands.HUBSITE_GET, () => {
 
     command.action(logger, { options: { debug: false, id: 'ee8b42c3-3e6f-4822-87c1-c21ad666046b' } } as any, (err?: any) => {
       try {
-        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError("Exception of type 'Microsoft.SharePoint.Client.ResourceNotFoundException' was thrown.")));
+        assert.strictEqual(JSON.stringify(err), JSON.stringify(new CommandError(`The specified hub site with id ee8b42c3-3e6f-4822-87c1-c21ad666046b does not exist`)));
         done();
       }
       catch (e) {
@@ -280,5 +408,14 @@ describe(commands.HUBSITE_GET, () => {
   it('passes validation when the id is a valid GUID', () => {
     const actual = command.validate({ options: { id: '2c1ba4c4-cd9b-4417-832f-92a34bc34b2a' } });
     assert.strictEqual(actual, true);
+  });
+
+  it(`fails validation if the specified url is invalid`, () => {
+    const actual = command.validate({
+      options: {
+        url: '/'
+      }
+    });
+    assert.notStrictEqual(actual, true);
   });
 });
