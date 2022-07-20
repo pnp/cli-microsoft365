d8151c25156fcdcff0839cbe0f9bb657714a151b
Vipul Kelkar <vipul.kelkar@gmail.com>
2022-07-20 11:37:16 +0530

    Fixed status command to show correct connection status. Closes #3260

--- src/m365/commands/status.ts
+++ src/m365/commands/status.ts
@@ -14,19 +14,32 @@ class StatusCommand extends Command {
   }
 
   public commandAction(logger: Logger, args: any, cb: (err?: any) => void): void {
+
     if (auth.service.connected) {
-      if (this.debug) {
-        logger.logToStderr({
-          connectedAs: accessToken.getUserNameFromAccessToken(auth.service.accessTokens[auth.defaultResource].accessToken),
-          authType: AuthType[auth.service.authType],
-          accessTokens: JSON.stringify(auth.service.accessTokens, null, 2)
+      auth
+        .ensureAccessToken(auth.defaultResource, logger, this.debug)
+        .then((): void => {
+          if (this.debug) {
+            logger.logToStderr({
+              connectedAs: accessToken.getUserNameFromAccessToken(auth.service.accessTokens[auth.defaultResource].accessToken),
+              authType: AuthType[auth.service.authType],
+              accessTokens: JSON.stringify(auth.service.accessTokens, null, 2)
+            });
+          }
+          else {
+            logger.log({
+              connectedAs: accessToken.getUserNameFromAccessToken(auth.service.accessTokens[auth.defaultResource].accessToken)
+            });
+          }
+          cb();
+        }, (rej: Error): void => {
+          if (this.debug) {
+            logger.logToStderr(rej);
+          }
+
+          auth.service.logout();
+          cb(new CommandError(`Your login has expired. Sign in again to continue. ${rej.message}`));
         });
-      }
-      else {
-        logger.log({
-          connectedAs: accessToken.getUserNameFromAccessToken(auth.service.accessTokens[auth.defaultResource].accessToken)
-        });
-      }
     }
     else {
       if (this.verbose) {
@@ -35,8 +48,8 @@ class StatusCommand extends Command {
       else {
         logger.log('Logged out');
       }
+      cb();
     }
-    cb();
   }
 
   public action(logger: Logger, args: CommandArgs, cb: (err?: any) => void): void {
