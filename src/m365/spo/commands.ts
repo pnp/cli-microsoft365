cf62e35f13f12a3c27f5d8830c8e2e0f67565f9d
Adam <adam.wojcik.it@gmail.com>
2022-08-07 22:40:00 +0200

    New command: spo roledefinition add. Closes #3548

--- src/m365/spo/commands.ts
+++ src/m365/spo/commands.ts
@@ -267,5 +267,6 @@ export default {
   WEB_LIST: `${prefix} web list`,
   WEB_REINDEX: `${prefix} web reindex`,
   WEB_REMOVE: `${prefix} web remove`,
+  WEB_ROLEINHERITANCE_RESET: `${prefix} web roleinheritance reset`,
   WEB_SET: `${prefix} web set`
 };
---
+++ src/m365/spo/commands/web/web-roleinheritance-reset.ts
@@ -0,0 +1,62 @@
+import { Logger } from '../../../../cli';
+import { CommandOption } from '../../../../Command';
+import GlobalOptions from '../../../../GlobalOptions';
+import request from '../../../../request';
+import { validation } from '../../../../utils';
+import SpoCommand from '../../../base/SpoCommand';
+import commands from '../../commands';
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  webUrl: string;
+}
+
+class SpoWebRoleInheritanceResetCommand extends SpoCommand {
+  public get name(): string {
+    return commands.WEB_ROLEINHERITANCE_RESET;
+  }
+
+  public get description(): string {
+    return 'Restores role inheritance of subsite';
+  }
+
+  public commandAction(logger: Logger, args: CommandArgs, cb: () => void): void {
+    if (this.verbose) {
+      logger.logToStderr(`Restore role inheritance of subsite at ${args.options.webUrl}...`);
+    }
+
+    const requestOptions: any = {
+      url: `${args.options.webUrl}/_api/web/resetroleinheritance`,
+      method: 'POST',
+      headers: {
+        'accept': 'application/json;odata=nometadata',
+        'content-type': 'application/json'
+      },
+      responseType: 'json'
+    };
+
+    request
+      .post(requestOptions)
+      .then(_ => cb(), (err: any): void => this.handleRejectedODataJsonPromise(err, logger, cb));
+  }
+
+  public options(): CommandOption[] {
+    const options: CommandOption[] = [
+      {
+        option: '-u, --webUrl <webUrl>'
+      }
+    ];
+
+    const parentOptions: CommandOption[] = super.options();
+    return options.concat(parentOptions);
+  }
+
+  public validate(args: CommandArgs): boolean | string {
+    return validation.isValidSharePointUrl(args.options.webUrl);
+  }
+}
+
+module.exports = new SpoWebRoleInheritanceResetCommand();
