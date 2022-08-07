d1e4d9cb9ac5ef45ccd85853551e29e470866e8e
Adam <adam.wojcik.it@gmail.com>
2022-08-07 22:40:00 +0200

    Adds 'spo web roleinheritance reset' command. Closes #3548

--- src/m365/spo/commands.ts
+++ src/m365/spo/commands.ts
@@ -269,5 +269,6 @@ export default {
   WEB_LIST: `${prefix} web list`,
   WEB_REINDEX: `${prefix} web reindex`,
   WEB_REMOVE: `${prefix} web remove`,
+  WEB_ROLEINHERITANCE_RESET: `${prefix} web roleinheritance reset`,
   WEB_SET: `${prefix} web set`
 };
