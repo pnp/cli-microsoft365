3bbf39ece7f366a0fdf9714e4b5f9b67de122f45
Adam <adam.wojcik.it@gmail.com>
2022-08-07 23:22:40 +0200

    New command: m365 spo list roleassignment remove. Closes #3542

--- src/m365/spo/commands/user/user-get.ts
+++ src/m365/spo/commands/user/user-get.ts
@@ -12,7 +12,7 @@ interface CommandArgs {
   options: Options;
 }
 
-interface Options extends GlobalOptions {
+export interface Options extends GlobalOptions {
   webUrl: string;
   email?: string;
   id: string | number | undefined;
