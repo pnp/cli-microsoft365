113b5c9f0d2f07e658f0703177e29b2272a3e572
Jasey Waegebaert <jwaegebaert@gmail.com>
2022-08-08 23:30:36 +0200

    Extends docs with a Team page. Closes #2166

---
+++ docs/docs/about/generate-team-cards.js
@@ -0,0 +1,679 @@
+const maintainers = [
+  {
+    name: 'Adam Wojcik',
+    company: '',
+    github: 'Adam-it',
+    twitter: 'Adam25858782'
+  },
+  {
+    name: 'Albert-Jan Schot',
+    company: 'Blis Digital',
+    github: 'appieschot',
+    twitter: 'appieschot'
+  },
+  {
+    name: 'Arjun Menon',
+    company: 'Tata Consultancy Services',
+    github: 'arjunumenon',
+    twitter: 'arjunumenon'
+  },
+  {
+    name: 'Garry Trinder',
+    company: 'Microsoft',
+    github: 'garrytrinder',
+    twitter: 'garrytrinder'
+  },
+  {
+    name: 'Martin Lingstuyl',
+    company: 'I4-YOU',
+    github: 'martinlingstuyl',
+    twitter: 'martinlingstuyl'
+  },
+  {
+    name: 'Patrick Lamber',
+    company: 'EasyLife 365',
+    github: 'plamber',
+    twitter: 'patricklamber'
+  },
+  {
+    name: 'Rabia Williams',
+    company: 'Microsoft',
+    github: 'rabwill',
+    twitter: 'williamsrabia'
+  },
+  {
+    name: 'Waldek Mastykarz',
+    company: 'Microsoft',
+    github: 'waldekmastykarz',
+    twitter: 'waldekm'
+  }
+];
+
+const contributors = [
+  {
+    name: 'Aakash Bhardwaj',
+    company: '',
+    github: 'aakashbhardwaj619',
+    twitter: 'aakash_316'
+  },
+  {
+    name: 'Abderahman Moujahid',
+    company: '',
+    github: 'Abderahman88',
+    twitter: ''
+  },
+  {
+    name: 'Abhishek K M',
+    company: '',
+    github: '',
+    twitter: ''
+  },
+  {
+    name: 'Adam Wojcik',
+    company: '',
+    github: 'Adam-it',
+    twitter: 'Adam25858782'
+  },
+  {
+    name: 'Akash Karda',
+    company: '',
+    github: 'akashkarda',
+    twitter: 'akkikarda'
+  },
+  {
+    name: 'Albert-Jan Schot',
+    company: 'Blis Digital',
+    github: 'appieschot',
+    twitter: 'appieschot'
+  },
+  {
+    name: 'Aleksandar Nikolić',
+    company: '',
+    github: 'alexandair',
+    twitter: 'alexandair'
+  },
+  {
+    name: 'Allan Hvam',
+    company: '',
+    github: '',
+    twitter: ''
+  },
+  {
+    name: 'Andrew Connell',
+    company: 'Voitanos',
+    github: 'andrewconnell',
+    twitter: 'andrewconnell'
+  },
+  {
+    name: 'Anoop Tatti',
+    company: 'Content+Cloud',
+    github: 'anoopt',
+    twitter: 'anooptells'
+  },
+  {
+    name: 'Arjen Bloemsma',
+    company: 'Bloemium',
+    github: 'arjenbloemsma',
+    twitter: ''
+  },
+  {
+    name: 'Arjun Menon',
+    company: 'Tata Consultancy Services',
+    github: 'arjunumenon',
+    twitter: 'arjunumenon'
+  },
+  {
+    name: 'Arnie Raju',
+    company: '',
+    github: '',
+    twitter: ''
+  },
+  {
+    name: 'Atharva Patil',
+    company: '',
+    github: 'Atharva321',
+    twitter: ''
+  },
+  {
+    name: 'Balamurugan Kailasam',
+    company: '',
+    github: 'clientbala',
+    twitter: ''
+  },
+  {
+    name: 'Bassem Khalil',
+    company: '',
+    github: '',
+    twitter: ''
+  },  
+  {
+    name: 'Bruce Albany',
+    company: 'Telstra Purple',
+    github: 'balbany',
+    twitter: 'brucealbany'
+  },
+  {
+    name: 'Cas van Iersel',
+    company: 'Rapid Circle',
+    github: 'casvaniersel',
+    twitter: 'casvaniersel'
+  },
+  {
+    name: 'Christian Bewernitz',
+    company: '',
+    github: 'karfau',
+    twitter: ''
+  },
+  {
+    name: 'Daniel Laskewitz',
+    company: 'Microsoft',
+    github: 'Laskewitz',
+    twitter: 'Laskewitz'
+  },
+  {
+    name: 'Danish Shafi',
+    company: '',
+    github: 'builtbydans',
+    twitter: 'builtbydan'
+  },
+  {
+    name: 'David Calabro',
+    company: '',
+    github: 'DaveCalGH',
+    twitter: 'AllTheCodez'
+  },
+  {
+    name: 'David Ramalho',
+    company: '',
+    github: '',
+    twitter: ''
+  },
+  {
+    name: 'Dipen Shah',
+    company: 'Rapid Circle',
+    github: 'dips365',
+    twitter: 'Dips_365'
+  },
+  {
+    name: 'Douglas Romão',
+    company: '',
+    github: '',
+    twitter: ''
+  },
+  {
+    name: 'Elio Struyf',
+    company: 'Struyf Consulting',
+    github: 'estruyf',
+    twitter: 'eliostruyf'
+  },
+  {
+    name: 'Erwin van Hunen ',
+    company: 'Valo Intranet',
+    github: 'erwinvanhunen',
+    twitter: 'erwinvanhunen'
+  },
+  {
+    name: 'Fredrik Thorild ',
+    company: 'Sogeti Sweden',
+    github: 'fthorild',
+    twitter: 'taxonomythorild'
+  },
+  {
+    name: 'Garry Trinder',
+    company: 'Microsoft',
+    github: 'garrytrinder',
+    twitter: 'garrytrinder'
+  },
+  {
+    name: 'Hugo Bernier',
+    company: 'Microsoft',
+    github: 'hugoabernier',
+    twitter: 'bernierh'
+  },
+  {
+    name: 'Jasey Waegebaert',
+    company: 'GMI group',
+    github: 'Jwaegebaert',
+    twitter: 'jwaegebaert'
+  },
+  {
+    name: 'Jayakumar Balasubramaniam',
+    company: 'Hubfly',
+    github: 'JayakumarB',
+    twitter: 'jayakumrb'
+  },
+  {
+    name: 'Joakim Hagström',
+    company: 'Microsoft',
+    github: 'jhagstrom',
+    twitter: ''
+  },
+  {
+    name: 'Joakim Högberg',
+    company: 'Bravero',
+    github: 'joakimhogberg',
+    twitter: 'joakimhogberg'
+  },
+  {
+    name: 'John Rafael',
+    company: '',
+    github: '',
+    twitter: ''
+  },
+  {
+    name: 'Joseph Velliah',
+    company: '',
+    github: 'sprider',
+    twitter: 'JosephVelliah'
+  },
+  {
+    name: 'Kevin McDonnell',
+    company: 'CPS',
+    github: 'kevmcdonk',
+    twitter: 'kevmcdonk'
+  },
+  {
+    name: 'Kislay Sinha',
+    company: '',
+    github: 'sinhakislay',
+    twitter: ''
+  },
+  {
+    name: 'Laurent Sittler',
+    company: '',
+    github: '',
+    twitter: ''
+  },
+  {
+    name: 'Lee Ford',
+    company: 'Symity Ltd',
+    github: 'leeford',
+    twitter: 'lee_ford'
+  },
+  {
+    name: 'Luise Freese',
+    company: 'M365 Princess',
+    github: 'LuiseFreese',
+    twitter: 'LuiseFreese'
+  },
+  {
+    name: 'Mark Powney',
+    company: 'Valo Intranet',
+    github: 'mpowney',
+    twitter: 'mpowney'
+  },
+  {
+    name: 'Mark Priem',
+    company: '',
+    github: 'mpriem',
+    twitter: ''
+  },
+  {
+    name: 'Mark van Dijk',
+    company: 'Ichicraft',
+    github: 'MarksPoint',
+    twitter: 'MarksPoint'
+  },
+  {
+    name: 'Martin Lingstuyl',
+    company: 'I4-YOU',
+    github: 'martinlingstuyl',
+    twitter: 'martinlingstuyl'
+  },
+  {
+    name: 'Mathijs Verbeeck',
+    company: 'Xylos',
+    github: 'MathijsVerbeeck',
+    twitter: 'mathijsverbeeck'
+  },
+  {
+    name: 'Michaël Maillot',
+    company: 'onepoint',
+    github: 'michaelmaillot',
+    twitter: 'michael_maillot'
+  },
+  {
+    name: 'Mikael Svenson',
+    company: 'Microsoft',
+    github: 'wobba',
+    twitter: 'mikaelsvenson'
+  },
+  {
+    name: 'Milan Holemans',
+    company: 'VanRoey',
+    github: 'milanholemans',
+    twitter: ''
+  },
+  {
+    name: 'Mohamed Ashiq Faleel',
+    company: 'TCS',
+    github: 'ashiqf',
+    twitter: 'ashiqfaleel'
+  },
+  {
+    name: 'Nanddeep Nachan',
+    company: '',
+    github: 'nanddeepn',
+    twitter: 'NanddeepNachan'
+  },
+  {
+    name: 'Nicholas Honen',
+    company: '',
+    github: 'nickhonen',
+    twitter: 'nickhonen4'
+  },
+  {
+    name: 'Patrick Lamber',
+    company: 'Experts Inside',
+    github: 'plamber',
+    twitter: 'patricklamber'
+  },
+  {
+    name: 'Paul Keijzers',
+    company: 'KbWorks',
+    github: 'KbWorks',
+    twitter: 'KbWorks'
+  },
+  {
+    name: 'Paul Schaeflein',
+    company: 'AddIn365',
+    github: 'pschaeflein',
+    twitter: 'paulschaeflein'
+  },
+  {
+    name: 'Paweł Hawrylak',
+    company: 'EG A/S',
+    github: 'phawrylak',
+    twitter: ''
+  },
+  {
+    name: 'Pete Skelly',
+    company: '@transport4',
+    github: 'pkskelly',
+    twitter: 'pskelly'
+  },
+  {
+    name: 'Peter Biro',
+    company: '',
+    github: '',
+    twitter: ''
+  },
+  {
+    name: 'Peter Tane',
+    company: '',
+    github: 'pdtane',
+    twitter: ''
+  },
+  {
+    name: 'Phil Harding',
+    company: '',
+    github: 'phillipharding',
+    twitter: 'phillipharding'
+  },
+  {
+    name: 'Piotr Siatka',
+    company: 'Valo Intranet',
+    github: 'siata13',
+    twitter: 'PiotrSiatka'
+  },
+  {
+    name: 'Pramod Ghuge',
+    company: '',
+    github: 'pramod74',
+    twitter: ''
+  },
+  {
+    name: 'Prasad Kasireddy',
+    company: 'Capgemini Technology Services India',
+    github: 'PrasadKasireddy',
+    twitter: 'Prasad_kasiredd'
+  },
+  {
+    name: 'Rabia Williams',
+    company: 'Microsoft',
+    github: 'rabwill',
+    twitter: 'williamsrabia'
+  },
+  {
+    name: 'Rajesh Sitaraman',
+    company: 'aws',
+    github: 'rjesh-git',
+    twitter: 'rjesh'
+  },
+  {
+    name: 'Reshmee Auckloo',
+    company: '',
+    github: 'reshmee011',
+    twitter: ''
+  },
+  {
+    name: 'Rick van Rousselt',
+    company: 'Advantive',
+    github: 'RickVanRousselt',
+    twitter: 'rickvanrousselt'
+  },
+  {
+    name: 'Robert Dyjas',
+    company: '',
+    github: 'robdy',
+    twitter: 'robdyy'
+  },
+  {
+    name: 'Robert Jaakke',
+    company: 'Rapid Circle',
+    github: 'rjaakke',
+    twitter: ''
+  },
+  {
+    name: 'Robert Schouten',
+    company: 'Rapid Circle',
+    github: 'rfjschouten',
+    twitter: 'rfjschouten'
+  },
+  {
+    name: 'Rodrigo Romano',
+    company: 'Viloc',
+    github: 'RARomano',
+    twitter: 'rodrigo_romano'
+  },
+  {
+    name: 'Roman Rozinov',
+    company: 'CG-Sogeti',
+    github: 'romanatsogeti',
+    twitter: 'romanrozinov'
+  },
+  {
+    name: 'Sebastian Schütze',
+    company: 'DB Systel GmbH',
+    github: 'SebastianSchuetze',
+    twitter: 'RazorSPoint'
+  },
+  {
+    name: 'Sebastien Levert',
+    company: 'Microsoft',
+    github: 'sebastienlevert',
+    twitter: 'sebastienlevert'
+  },
+  {
+    name: 'Sekar Thangavel',
+    company: 'Qatar Foundation',
+    github: 'SekarThangavel',
+    twitter: 'SekThang'
+  },
+  {
+    name: 'Shantha Kumar',
+    company: '',
+    github: 'ktskumar',
+    twitter: 'ktskumar'
+  },
+  {
+    name: 'Siddharth Vaghasia',
+    company: '',
+    github: 'siddharth-vaghasia',
+    twitter: 'siddh_me'
+  },
+  {
+    name: 'Simon Ågren',
+    company: 'Advania - Knowledge Factory',
+    github: 'simonagren',
+    twitter: 'agrenpoint'
+  },
+  {
+    name: 'Smita Nachan',
+    company: '',
+    github: 'SmitaNachan',
+    twitter: 'mitanachan'
+  },
+  {
+    name: 'Stefan Bauer',
+    company: 'N8D',
+    github: 'StfBauer',
+    twitter: 'stfbauer'
+  },
+  {
+    name: 'Steve Mücklisch',
+    company: '',
+    github: 'muecs',
+    twitter: ''
+  },
+  {
+    name: 'Stijn Brouwers',
+    company: 'BD Commit',
+    github: 'stijnbrouwers',
+    twitter: 'stijnbrouwers85'
+  },
+  {
+    name: 'Sudharsan Kesavanarayanan',
+    company: 'NTT Ltd',
+    github: 'sudharsank',
+    twitter: 'sudharsank'
+  },
+  {
+    name: 'Thomas Gölles',
+    company: 'Solvion',
+    github: 'thomyg',
+    twitter: 'thomyg'
+  },
+  {
+    name: 'Vardhaman Deshpande',
+    company: '',
+    github: 'vman',
+    twitter: 'vrdmn'
+  },
+  {
+    name: 'Velin Georgiev',
+    company: '',
+    github: 'VelinGeorgiev',
+    twitter: 'velingeorgiev'
+  },
+  {
+    name: 'Veronique Lengelle',
+    company: 'CPS',
+    github: 'veronicageek',
+    twitter: 'veronicageek'
+  },
+  {
+    name: 'Vincent Biret',
+    company: 'Microsoft',
+    github: 'baywet',
+    twitter: ''
+  },
+  {
+    name: 'Vipul Kelkar',
+    company: 'Rapid Circle',
+    github: 'vipulkelkar',
+    twitter: 'vipulkelkar'
+  },
+  {
+    name: 'Waldek Mastykarz',
+    company: 'Microsoft',
+    github: 'waldekmastykarz',
+    twitter: 'waldekm'
+  },
+  {
+    name: 'Wictor Wilén',
+    company: 'Microsoft',
+    github: 'wictorwilen',
+    twitter: 'wictor'
+  },
+  {
+    name: 'Yannick Plenevaux',
+    company: 'PVX Solutions',
+    github: 'ypcode',
+    twitter: 'yp_code'
+  },
+  {
+    name: 'Yannick Reekmans',
+    company: 'Qubix365',
+    github: 'YannickRe',
+    twitter: 'YannickReekmans'
+  },
+  {
+    name: 'strafe',
+    company: '',
+    github: 'strafe',
+    twitter: ''
+  }
+];
+
+function generateGridCard(profile) {
+  const template = document.getElementById('cli-grid-item-template');
+  const profileCard = template.cloneNode(true);
+
+  // Configure profile image
+  const imgElement = profileCard.getElementsByClassName("cli-grid-item-img")[0];
+  imgElement.setAttribute("alt", profile.name);
+
+  if (profile.github) {
+    imgElement.setAttribute("src", `https://github.com/${profile.github}.png`);
+  } else {
+    imgElement.setAttribute("src", `https://ui-avatars.com/api/?name=${profile.name}`);
+  }
+
+  // Configure profile name
+  const nameElement = profileCard.getElementsByClassName("cli-grid-item-name")[0];
+  nameElement.innerHTML = profile.name;
+
+  // Configure profile company
+  const companyElement = profileCard.getElementsByClassName("cli-grid-item-company")[0];
+  if (profile.company) { 
+    companyElement.innerHTML = profile.company;
+  } else {
+    companyElement.remove();
+  }
+
+  // Configure profile links
+  const githubElement = profileCard.querySelector('[title="GitHub"]');
+  const twitterElement = profileCard.querySelector('[title="Twitter"]');
+  if (profile.github) { 
+    githubElement.setAttribute("href", `https://github.com/${profile.github}`);
+  } else {
+    githubElement.remove();
+  }
+
+  if (profile.twitter) { 
+    twitterElement.setAttribute("href", `https://twitter.com/${profile.twitter}`);
+  } else {
+    twitterElement.remove();
+  }
+
+  // Remove id
+  profileCard.removeAttribute('id');
+
+  return profileCard;
+}
+
+const maintainersGrid = document.getElementById('cli-grid-maintainers');
+const contributorsGrid = document.getElementById('cli-grid-contributors');
+
+maintainers.forEach((profile) => {
+  const profileCard = generateGridCard(profile);
+
+  maintainersGrid.appendChild(profileCard);
+});
+
+contributors.forEach((profile) => {
+  const profileCard = generateGridCard(profile);
+
+  contributorsGrid.append(profileCard);
+});
