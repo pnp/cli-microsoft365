31cbc74111e71eee7b087eaf1a0dc4a169ce26d5
Nanddeep Nachan <nanddeepnachan@gmail.com>
2022-08-17 14:24:07 +0000

    Adds 'search externalConnections remove' command. Closes #3170

--- src/m365/search/commands.ts
+++ src/m365/search/commands.ts
@@ -3,5 +3,6 @@ const prefix: string = 'search';
 export default {
   EXTERNALCONNECTION_ADD: `${prefix} externalconnection add`,
   EXTERNALCONNECTION_GET: `${prefix} externalconnection get`,
-  EXTERNALCONNECTION_LIST: `${prefix} externalconnection list`
+  EXTERNALCONNECTION_LIST: `${prefix} externalconnection list`,
+  EXTERNALCONNECTION_REMOVE: `${prefix} externalconnection remove`
 };
