710939b3bd34d04a5a4cd4deccbe88879d4afc68
Wing Fai Yu <faiyu@quantr.hk>
2022-09-05 22:29:03 +0800

    dev

--- src/m365/spo/commands/web/web-roleinheritance-break.ts
+++ src/m365/spo/commands/web/web-roleinheritance-break.ts
@@ -1,4 +1,4 @@
-import { Logger } from '../../../../cli';
+import { Cli, Logger } from '../../../../cli';
 import GlobalOptions from '../../../../GlobalOptions';
 import SpoCommand from '../../../base/SpoCommand';
 import commands from '../../commands';
@@ -49,20 +49,39 @@ class SpoWebRoleInheritanceBreakCommand extends SpoCommand {
     if (this.verbose) {
       logger.logToStderr(`Break role inheritance of subsite at ${args.options.webUrl}...`);
     }
+    const breakroleInheritance = (): void => {
+      const requestOptions: any = {
+        url: `${args.options.webUrl}/_api/web/breakroleinheritance`,
+        method: 'POST',
+        headers: {
+          'accept': 'application/json;odata=nometadata',
+          'content-type': 'application/json'
+        },
+        responseType: 'json'
+      };
 
-    const requestOptions: any = {
-      url: `${args.options.webUrl}/_api/web/breakroleinheritance`,
-      method: 'POST',
-      headers: {
-        'accept': 'application/json;odata=nometadata',
-        'content-type': 'application/json'
-      },
-      responseType: 'json'
-    };
-
-    request
-      .post(requestOptions)
-      .then(_ => cb(), (err: any): void => this.handleRejectedODataJsonPromise(err, logger, cb));
+      request
+        .post(requestOptions)
+        .then(_ => cb(), (err: any): void => this.handleRejectedODataJsonPromise(err, logger, cb));
+    }
+    if (args.options.confirm) {
+      breakroleInheritance();
+    }
+    else {
+      Cli.prompt({
+        type: 'confirm',
+        name: 'continue',
+        default: false,
+        message: `Are you sure you want to remove the subsite ${args.options.webUrl}`
+      }, (result: { continue: boolean }): void => {
+        if (!result.continue) {
+          cb();
+        }
+        else {
+          breakroleInheritance();
+        }
+      });
+    }
   }
 }
 
