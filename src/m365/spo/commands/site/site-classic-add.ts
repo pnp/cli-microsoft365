9f736fcf8f40867c6909e130070b5c831a344909
jwaegebaert <jwaegebaert@gmail.com>
2022-09-06 22:38:03 +0200

    Classic add fix

--- src/m365/spo/commands/site/site-classic-add.ts
+++ src/m365/spo/commands/site/site-classic-add.ts
@@ -383,6 +383,9 @@ class SpoSiteClassicAddCommand extends SpoCommand {
               });
             }, operation.PollingInterval);
           }
+        })
+        .catch((e) => {
+          reject(e);
         });
     });
   }
