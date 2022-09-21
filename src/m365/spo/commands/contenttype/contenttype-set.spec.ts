7109a4e0a6e91f82365322b060e714ae487b32dd
Milan Holemans <11723921+milanholemans@users.noreply.github.com>
2022-09-21 22:06:38 +0200

    Adds 'spo contenttype set' command. Closes #3613

---
+++ src/m365/spo/commands/contenttype/contenttype-set.spec.ts
@@ -0,0 +1,225 @@
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import appInsights from '../../../../appInsights';
+import auth from '../../../../Auth';
+import { Cli, CommandInfo, Logger } from '../../../../cli';
+import Command, { CommandError } from '../../../../Command';
+import request from '../../../../request';
+import { formatting, sinonUtil } from '../../../../utils';
+import commands from '../../commands';
+const command: Command = require('./contenttype-set');
+
+describe(commands.CONTENTTYPE_SET, () => {
+  const webUrl = 'https://contoso.sharepoint.com';
+  const listId = '00000000-0000-0000-0000-000000000000';
+  const listTitle = 'Assets';
+  const listUrl = '/sites/project-x/Lists/Assets';
+  const id = '0x0101';
+  const name = 'Asset';
+  const newName = 'New asset name';
+
+  const contentTypesResponse = {
+    value: [
+      {
+        Name: name,
+        Group: 'Custom group',
+        Id: {
+          StringValue: id
+        }
+      }
+    ]
+  };
+
+  let log: any[];
+  let logger: Logger;
+  let commandInfo: CommandInfo;
+
+  before(() => {
+    sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
+    sinon.stub(appInsights, 'trackEvent').callsFake(() => { });
+    auth.service.connected = true;
+    commandInfo = Cli.getCommandInfo(command);
+  });
+
+  beforeEach(() => {
+    log = [];
+    logger = {
+      log: (msg: string) => {
+        log.push(msg);
+      },
+      logRaw: (msg: string) => {
+        log.push(msg);
+      },
+      logToStderr: (msg: string) => {
+        log.push(msg);
+      }
+    };
+  });
+
+  afterEach(() => {
+    sinonUtil.restore([
+      request.get,
+      request.patch
+    ]);
+  });
+
+  after(() => {
+    sinonUtil.restore([
+      auth.restoreAuth,
+      appInsights.trackEvent
+    ]);
+    auth.service.connected = false;
+  });
+
+  it('has correct name', () => {
+    assert.strictEqual(command.name.startsWith(commands.CONTENTTYPE_SET), true);
+  });
+
+  it('has a description', () => {
+    assert.notStrictEqual(command.description, null);
+  });
+
+  it('defines correct option sets', () => {
+    const optionSets = command.optionSets;
+    assert.deepStrictEqual(optionSets, [['id', 'name']]);
+  });
+
+  it('fails validation if webUrl is not a valid SharePoint URL', async () => {
+    const actual = await command.validate({ options: { webUrl: 'invalid', id: id } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if id is specified and is not a valid GUID', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, id: id, listId: 'invalid' } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if listId and listTitle is specified', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, id: id, listId: listId, listTitle: listTitle } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if listId and listUrl is specified', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, id: id, listId: listId, listUrl: listUrl } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if listTitle and listUrl is specified', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, id: id, listTitle: listTitle, listUrl: listUrl } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if listId, listUrl and is specified', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, id: id, listId: listId, listUrl: listUrl, listTitle: listTitle } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('passes validation when webUrl, id and listId are specified', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, id: id, listId: listId } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('passes validation when webUrl, name are specified', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, name: name } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('allows unknown options', () => {
+    const allowUnknownOptions = command.allowUnknownOptions();
+    assert.strictEqual(allowUnknownOptions, true);
+  });
+
+  it('correctly updates content type with id', async () => {
+    const patchStub = sinon.stub(request, 'patch').callsFake(() => Promise.resolve());
+
+    await command.action(logger, { options: { webUrl: webUrl, id: id, Name: newName } } as any);
+    assert.strictEqual(patchStub.lastCall.args[0].url, `${webUrl}/_api/Web/ContentTypes/GetById('${id}')`);
+  });
+
+  it('correctly updates content type with name', async () => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url === `${webUrl}/_api/Web/ContentTypes?$filter=Name eq '${name}'&$select=Id`) {
+        return Promise.resolve(contentTypesResponse);
+      }
+
+      return Promise.reject('Invalid request url: ' + opts.url);
+    });
+    const patchStub = sinon.stub(request, 'patch').callsFake(() => Promise.resolve());
+
+    await command.action(logger, { options: { webUrl: webUrl, name: name, Name: newName } } as any);
+    assert.strictEqual(patchStub.lastCall.args[0].url, `${webUrl}/_api/Web/ContentTypes/GetById('${contentTypesResponse.value[0].Id.StringValue}')`);
+  });
+
+  it('correctly updates content type with name and listId', async () => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url === `${webUrl}/_api/Web/Lists/GetById('${formatting.encodeQueryParameter(listId)}')/ContentTypes?$filter=Name eq '${name}'&$select=Id`) {
+        return Promise.resolve(contentTypesResponse);
+      }
+
+      return Promise.reject('Invalid request url: ' + opts.url);
+    });
+    const patchStub = sinon.stub(request, 'patch').callsFake(() => Promise.resolve());
+
+    await command.action(logger, { options: { webUrl: webUrl, name: name, listId: listId, Name: newName } } as any);
+    assert.strictEqual(patchStub.lastCall.args[0].url, `${webUrl}/_api/Web/Lists/GetById('${formatting.encodeQueryParameter(listId)}')/ContentTypes/GetById('${contentTypesResponse.value[0].Id.StringValue}')`);
+  });
+
+  it('correctly updates content type with name and listTitle', async () => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url === `${webUrl}/_api/Web/Lists/GetByTitle('${formatting.encodeQueryParameter(listTitle)}')/ContentTypes?$filter=Name eq '${name}'&$select=Id`) {
+        return Promise.resolve(contentTypesResponse);
+      }
+
+      return Promise.reject('Invalid request url: ' + opts.url);
+    });
+    const patchStub = sinon.stub(request, 'patch').callsFake(() => Promise.resolve());
+
+    await command.action(logger, { options: { webUrl: webUrl, name: name, listTitle: listTitle, Name: newName } } as any);
+    assert.strictEqual(patchStub.lastCall.args[0].url, `${webUrl}/_api/Web/Lists/GetByTitle('${formatting.encodeQueryParameter(listTitle)}')/ContentTypes/GetById('${contentTypesResponse.value[0].Id.StringValue}')`);
+  });
+
+  it('correctly updates content type with name and listUrl', async () => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url === `${webUrl}/_api/Web/GetList('${formatting.encodeQueryParameter(listUrl)}')/ContentTypes?$filter=Name eq '${name}'&$select=Id`) {
+        return Promise.resolve(contentTypesResponse);
+      }
+
+      return Promise.reject('Invalid request url: ' + opts.url);
+    });
+    const patchStub = sinon.stub(request, 'patch').callsFake(() => Promise.resolve());
+
+    await command.action(logger, { options: { webUrl: webUrl, name: name, listUrl: listUrl, Name: newName } } as any);
+    assert.strictEqual(patchStub.lastCall.args[0].url, `${webUrl}/_api/Web/GetList('${formatting.encodeQueryParameter(listUrl)}')/ContentTypes/GetById('${contentTypesResponse.value[0].Id.StringValue}')`);
+  });
+
+  it('fails to update content type with name and listUrl when content type does not exist', async () => {
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url === `${webUrl}/_api/Web/ContentTypes?$filter=Name eq '${name}'&$select=Id`) {
+        return Promise.resolve({ value: [] });
+      }
+
+      return Promise.reject('Invalid request url: ' + opts.url);
+    });
+    const patchStub = sinon.stub(request, 'patch').callsFake(() => Promise.resolve());
+
+    await assert.rejects(command.action(logger, { options: { webUrl: webUrl, name: name, Name: newName } } as any), new CommandError(`The specified content type '${name}' does not exist`));
+    assert(patchStub.notCalled);
+  });
+
+  it('correctly handles random API error', async () => {
+    sinon.stub(request, 'patch').callsFake(() => Promise.reject('An error has occurred'));
+
+    await assert.rejects(command.action(logger, { options: { webUrl: webUrl, id: id, Name: newName } } as any), new CommandError('An error has occurred'));
+  });
+
+  it('supports debug mode', () => {
+    const options = command.options;
+    let containsDebugOption = false;
+    options.forEach(o => {
+      if (o.option === '--debug') {
+        containsDebugOption = true;
+      }
+    });
+    assert(containsDebugOption);
+  });
+});
---
+++ src/m365/spo/commands/contenttype/contenttype-set.ts
@@ -0,0 +1,201 @@
+import { AxiosRequestConfig } from 'axios';
+import { Logger } from '../../../../cli';
+import GlobalOptions from '../../../../GlobalOptions';
+import { formatting, urlUtil, validation } from '../../../../utils';
+import SpoCommand from '../../../base/SpoCommand';
+import commands from '../../commands';
+import request from '../../../../request';
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  webUrl: string;
+  id?: string;
+  name?: string;
+  listTitle?: string;
+  listId: string;
+  listUrl: string;
+}
+
+class SpoContentTypeSetCommand extends SpoCommand {
+  public get name(): string {
+    return commands.CONTENTTYPE_SET;
+  }
+
+  public get description(): string {
+    return 'Updates existing content type';
+  }
+
+  constructor() {
+    super();
+  
+    this.#initTelemetry();
+    this.#initOptions();
+    this.#initTypes();
+    this.#initOptionSets();
+    this.#initValidators();
+  }
+  
+  #initTelemetry(): void {
+    this.telemetry.push((args: CommandArgs) => {
+      Object.assign(this.telemetryProperties, {
+        id: typeof args.options.id !== 'undefined',
+        name: typeof args.options.name !== 'undefined',
+        listTitle: typeof args.options.listTitle !== 'undefined',
+        listId: typeof args.options.listId !== 'undefined',
+        listUrl: typeof args.options.listUrl !== 'undefined'
+      });
+    });
+  }
+  
+  #initOptions(): void {
+    this.options.unshift(
+      {
+        option: '-u, --webUrl <webUrl>'
+      },
+      {
+        option: '-i, --id [id]'
+      },
+      {
+        option: '-n, --name [name]'
+      },
+      {
+        option: '--listTitle [listTitle]'
+      },
+      {
+        option: '--listId [listId]'
+      },
+      {
+        option: '--listUrl [listUrl]'
+      },
+    );
+  }
+  
+  #initValidators(): void {
+    this.validators.push(
+      async (args: CommandArgs) => {
+        const isValidSharePointUrl: boolean | string = validation.isValidSharePointUrl(args.options.webUrl);
+        if (isValidSharePointUrl !== true) {
+          return isValidSharePointUrl;
+        }
+
+        if (args.options.listId && !validation.isValidGuid(args.options.listId)) {
+          return `'${args.options.listId}' is not a valid GUID.`;
+        }
+
+        if ((args.options.listId && (args.options.listTitle || args.options.listUrl)) || (args.options.listTitle && args.options.listUrl)) {
+          return `Specify either listTitle, listId or listUrl.`;
+        }
+    
+        return true;
+      }
+    );
+  }
+
+  #initTypes(): void {
+    this.types.string.push('id', 'i');
+  }
+
+  #initOptionSets(): void {
+  	this.optionSets.push(
+      ['id', 'name']
+    );
+  }
+
+  public allowUnknownOptions(): boolean | undefined {
+    return true;
+  }
+
+  public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
+    const requestOptions: AxiosRequestConfig = {
+      url: `${args.options.webUrl}/_api/Web`,
+      headers: {
+        accept: 'application/json;odata=nometadata'
+      },
+      responseType: 'json',
+      data: this.getRequestPayload(args.options)
+    };
+
+    if (args.options.listId) {
+      requestOptions.url += `/Lists/GetById('${formatting.encodeQueryParameter(args.options.listId)}')`;
+    }
+    else if (args.options.listTitle) {
+      requestOptions.url += `/Lists/GetByTitle('${formatting.encodeQueryParameter(args.options.listTitle)}')`;
+    }
+    else if (args.options.listUrl) {
+      requestOptions.url += `/GetList('${formatting.encodeQueryParameter(urlUtil.getServerRelativePath(args.options.webUrl, args.options.listUrl))}')`;
+    }
+    requestOptions.url += '/ContentTypes';
+
+    try {
+      const contentTypeId = await this.getContentTypeId(args.options);
+      requestOptions.url += `/GetById('${formatting.encodeQueryParameter(contentTypeId)}')`;
+
+      await request.patch(requestOptions);
+    }
+    catch (err: any) {
+      this.handleRejectedPromise(err);
+    }
+  }
+
+  private async getContentTypeId(options: Options): Promise<string> {
+    if (options.id) {
+      return options.id;
+    }
+
+    const requestOptions: AxiosRequestConfig = {
+      url: `${options.webUrl}/_api/Web`,
+      headers: {
+        accept: 'application/json;odata=nometadata'
+      },
+      responseType: 'json'
+    };
+
+    if (options.listId) {
+      requestOptions.url += `/Lists/GetById('${formatting.encodeQueryParameter(options.listId)}')`;
+    }
+    else if (options.listTitle) {
+      requestOptions.url += `/Lists/GetByTitle('${formatting.encodeQueryParameter(options.listTitle)}')`;
+    }
+    else if (options.listUrl) {
+      requestOptions.url += `/GetList('${formatting.encodeQueryParameter(urlUtil.getServerRelativePath(options.webUrl, options.listUrl))}')`;
+    }
+    requestOptions.url += `/ContentTypes?$filter=Name eq '${formatting.encodeQueryParameter(options.name!)}'&$select=Id`;
+
+    const res = await request.get<{ value: { Id: { StringValue: string } }[] }>(requestOptions);
+    
+    if (res.value.length === 0) {
+      throw `The specified content type '${options.name}' does not exist`;
+    }
+
+    return res.value[0].Id.StringValue;
+  }
+
+  private getRequestPayload(options: Options): any {
+    const excludeOptions: string[] = [
+      'webUrl',
+      'id',
+      'name',
+      'listTitle',
+      'listId',
+      'listUrl',
+      'query',
+      'debug',
+      'verbose',
+      'output'
+    ];
+
+    const payload = Object.keys(options)
+      .filter(key => excludeOptions.indexOf(key) === -1)
+      .reduce((object: any, key: string) => {
+        object[key] = options[key];
+        return object;
+      }, {});
+
+    return payload;
+  }
+}
+
+module.exports = new SpoContentTypeSetCommand();
