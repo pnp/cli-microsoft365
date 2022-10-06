7ba50d16200dfac67d7711e74acd12e085b827cd
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2022-10-07 01:22:33 +0200

    Adds 'pp tenant settings list' command. Closes #3657

--- src/m365/pp/commands.ts
+++ src/m365/pp/commands.ts
@@ -4,5 +4,6 @@ export default {
   ENVIRONMENT_LIST: `${prefix} environment list`,
   GATEWAY_LIST: `${prefix} gateway list`,
   MANAGEMENTAPP_ADD: `${prefix} managementapp add`,
-  MANAGEMENTAPP_LIST: `${prefix} managementapp list`
+  MANAGEMENTAPP_LIST: `${prefix} managementapp list`,
+  TENANT_SETTINGS_LIST: `${prefix} tenant settings list`
 };
---
+++ src/m365/pp/commands/tenant/tenant-settings-list.ts
@@ -0,0 +1,39 @@
+import { AxiosRequestConfig } from 'axios';
+import { Logger } from '../../../../cli/Logger';
+import request from '../../../../request';
+import PowerPlatformCommand from '../../../base/PowerPlatformCommand';
+import commands from '../../commands';
+
+class PpTenantSettingsListCommand extends PowerPlatformCommand {
+  public get name(): string {
+    return commands.TENANT_SETTINGS_LIST;
+  }
+
+  public get description(): string {
+    return 'Lists the global Power Platform tenant settings';
+  }
+
+  public defaultProperties(): string[] | undefined {
+    return ['disableCapacityAllocationByEnvironmentAdmins', 'disableEnvironmentCreationByNonAdminUsers', 'disableNPSCommentsReachout', 'disablePortalsCreationByNonAdminUsers', 'disableSupportTicketsVisibleByAllUsers', 'disableSurveyFeedback', 'disableTrialEnvironmentCreationByNonAdminUsers', 'walkMeOptOut'];
+  }
+
+  public async commandAction(logger: Logger): Promise<void> {
+    const requestOptions: AxiosRequestConfig  = {
+      url: `${this.resource}/providers/Microsoft.BusinessAppPlatform/listtenantsettings?api-version=2020-10-01`,
+      headers: {
+        accept: 'application/json'
+      },
+      responseType: 'json'
+    };
+    
+    try {
+      const res = await request.post<any>(requestOptions);
+      logger.log(res);
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+}
+
+module.exports = new PpTenantSettingsListCommand();
