40bf11cca55c12f4dd3c8454d6f7d453e99c2858
Nico De Cleyre <nico.de.cleyre@gmail.com>
2022-10-18 21:07:17 +0200

    card get

---
+++ src/m365/pp/commands/card/card-get.ts
@@ -0,0 +1,87 @@
+import { Logger } from '../../../../cli/Logger';
+import GlobalOptions from '../../../../GlobalOptions';
+import { powerPlatform } from '../../../../utils/powerPlatform';
+import PowerPlatformCommand from '../../../base/PowerPlatformCommand';
+import commands from '../../commands';
+import request from '../../../../request';
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  environment: string;
+  id: string;
+  asAdmin?: boolean;
+}
+
+class PpCardGetCommand extends PowerPlatformCommand {
+
+  public get name(): string {
+    return commands.CARD_GET;
+  }
+
+  public get description(): string {
+    return 'Get specific Microsoft Power Platform card in the specified Power Platform environment.';
+  }
+
+  public defaultProperties(): string[] | undefined {
+    return ['name', 'cardid', 'publishdate', 'createdon', 'modifiedon'];
+  }
+
+  constructor() {
+    super();
+
+    this.#initTelemetry();
+    this.#initOptions();
+  }
+
+  #initTelemetry(): void {
+    this.telemetry.push((args: CommandArgs) => {
+      Object.assign(this.telemetryProperties, {
+        asAdmin: !!args.options.asAdmin
+      });
+    });
+  }
+
+  #initOptions(): void {
+    this.options.unshift(
+      {
+        option: '-e, --environment <environment>'
+      },
+      {
+        option: '-i, --id <id>'
+      },
+      {
+        option: '-a, --asAdmin'
+      }
+    );
+  }
+
+  public async commandAction(logger: Logger, args: any): Promise<void> {
+    if (this.verbose) {
+      logger.logToStderr('');
+    }
+
+    try {
+      const dynamicsApiUrl = await powerPlatform.getDynamicsInstanceApiUrl(args.options.environment, args.options.asAdmin);
+
+      const requestOptions: any = {
+        url: `${dynamicsApiUrl}/api/data/v9.1/cards(${args.options.id})`,
+        headers: {
+          accept: 'application/json;odata.metadata=none'
+        },
+        responseType: 'json'
+      };
+
+      const res = await request.get(requestOptions);
+      logger.log(res);
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+
+}
+
+module.exports = new PpCardGetCommand();
