5a316a323846c821de47f4d5ba740830c4484972
Nico De Cleyre <nico.de.cleyre@gmail.com>
2022-10-22 16:40:46 +0200

    Adds 'pp card get' solution. Closes #3770

--- src/m365/pp/commands.ts
+++ src/m365/pp/commands.ts
@@ -1,6 +1,7 @@
 const prefix: string = 'pp';
 
 export default {
+  CARD_GET: `${prefix} card get`,
   CARD_LIST: `${prefix} card list`,
   DATAVERSE_TABLE_LIST: `${prefix} dataverse table list`,
   ENVIRONMENT_GET: `${prefix} environment get`,
