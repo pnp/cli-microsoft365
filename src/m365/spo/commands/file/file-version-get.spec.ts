fa3596d93fe3e82e5050bd0e79bb801b8cdaf8bc
Nico De Cleyre <nico.de.cleyre@gmail.com>
2022-10-30 21:55:36 +0100

    first commit

---
+++ src/m365/spo/commands/file/file-version-get.spec.ts
@@ -0,0 +1,229 @@
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import appInsights from '../../../../appInsights';
+import auth from '../../../../Auth';
+import { Cli } from '../../../../cli/Cli';
+import { CommandInfo } from '../../../../cli/CommandInfo';
+import { Logger } from '../../../../cli/Logger';
+import Command, { CommandError } from '../../../../Command';
+import request from '../../../../request';
+import { formatting } from '../../../../utils/formatting';
+import { pid } from '../../../../utils/pid';
+import { sinonUtil } from '../../../../utils/sinonUtil';
+import commands from '../../commands';
+const command: Command = require('./file-version-get');
+
+describe(commands.FILE_VERSION_GET, () => {
+  let log: any[];
+  let logger: Logger;
+  let loggerLogSpy: sinon.SinonSpy;
+  let commandInfo: CommandInfo;
+  const validWebUrl = "https://contoso.sharepoint.com";
+  const validFileUrl = "/Shared Documents/Document.docx";
+  const validFileId = "7a9b8bb6-d5c4-4de9-ab76-5210a7879e89";
+  const validLabel = "1.0";
+  const fileVersionResponse = {
+    "value": [
+      {
+        "CheckInComment": "",
+        "Created": "2022-10-30T12:03:06Z",
+        "ID": 512,
+        "IsCurrentVersion": false,
+        "Length": "18898",
+        "Size": 18898,
+        "Url": "_vti_history/512/Shared Documents/Document.docx",
+        "VersionLabel": "1.0"
+      }
+    ]
+  };
+
+  before(() => {
+    sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
+    sinon.stub(appInsights, 'trackEvent').callsFake(() => { });
+    sinon.stub(pid, 'getProcessName').callsFake(() => '');
+    auth.service.connected = true;
+    commandInfo = Cli.getCommandInfo(command);
+  });
+
+  beforeEach(() => {
+    log = [];
+    logger = {
+      log: (msg: string) => {
+        log.push(msg);
+      },
+      logRaw: (msg: string) => {
+        log.push(msg);
+      },
+      logToStderr: (msg: string) => {
+        log.push(msg);
+      }
+    };
+    loggerLogSpy = sinon.spy(logger, 'log');
+  });
+
+  afterEach(() => {
+    sinonUtil.restore([
+      request.get
+    ]);
+  });
+
+  after(() => {
+    sinonUtil.restore([
+      auth.restoreAuth,
+      appInsights.trackEvent,
+      pid.getProcessName
+    ]);
+    auth.service.connected = false;
+  });
+
+  it('has correct name', () => {
+    assert.strictEqual(command.name.startsWith(commands.FILE_VERSION_GET), true);
+  });
+
+  it('has a description', () => {
+    assert.notStrictEqual(command.description, null);
+  });
+
+  it('fails validation if fileId is not a valid guid.', async () => {
+    const actual = await command.validate({
+      options: {
+        webUrl: validWebUrl,
+        label: validLabel,
+        fileId: 'Invalid GUID'
+      }
+    }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if the webUrl option is not a valid SharePoint site URL', async () => {
+    const actual = await command.validate({ options: { webUrl: 'foo', label: validLabel, fileUrl: validFileUrl } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('passes validation if required options specified (fileUrl)', async () => {
+    const actual = await command.validate({ options: { webUrl: validWebUrl, label: validLabel, fileUrl: validFileUrl } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('passes validation if required options specified (fileId)', async () => {
+    const actual = await command.validate({ options: { webUrl: validWebUrl, label: validLabel, fileId: validFileId } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('retrieves version from a file with the fileUrl options', async () => {
+    sinon.stub(request, 'get').callsFake(async (opts) => {
+      if (opts.url === `${validWebUrl}/_api/web/GetFileByServerRelativeUrl('${formatting.encodeQueryParameter(validFileUrl)}')/versions/?$filter=VersionLabel eq '${validLabel}'`) {
+        return fileVersionResponse;
+      }
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        debug: true,
+        webUrl: validWebUrl,
+        label: validLabel,
+        fileUrl: validFileUrl
+      }
+    });
+    assert(loggerLogSpy.calledWith(fileVersionResponse.value[0]));
+  });
+
+  it('retrieves version from a file with the fileId options', async () => {
+    sinon.stub(request, 'get').callsFake(async (opts) => {
+      if (opts.url === `${validWebUrl}/_api/web/GetFileById('${validFileId}')/versions/?$filter=VersionLabel eq '${validLabel}'`) {
+        return fileVersionResponse;
+      }
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        debug: true,
+        webUrl: validWebUrl,
+        label: validLabel,
+        fileId: validFileId
+      }
+    });
+    assert(loggerLogSpy.calledWith(fileVersionResponse.value[0]));
+  });
+
+  it('retrieves version from a file with the fileId options and label in correct format', async () => {
+    sinon.stub(request, 'get').callsFake(async (opts) => {
+      if (opts.url === `${validWebUrl}/_api/web/GetFileById('${validFileId}')/versions/?$filter=VersionLabel eq '${validLabel}'`) {
+        return fileVersionResponse;
+      }
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        debug: true,
+        webUrl: validWebUrl,
+        label: "1",
+        fileId: validFileId
+      }
+    });
+    assert(loggerLogSpy.calledWith(fileVersionResponse.value[0]));
+  });
+
+  it('properly escapes single quotes in fileUrl', async () => {
+    sinon.stub(request, 'get').callsFake(async (opts) => {
+      if (opts.url = `${validWebUrl}/_api/web/GetFileByServerRelativeUrl('Shared%20Documents%2FFo''lde''r')/versions/?$filter=VersionLabel eq '${validLabel}'`) {
+        return fileVersionResponse;
+      }
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        debug: false,
+        webUrl: validWebUrl,
+        label: validLabel,
+        fileUrl: `Shared Documents/Fo'lde'r`
+      }
+    });
+    assert(loggerLogSpy.calledWith(fileVersionResponse.value[0]));
+  });
+
+  it('command correctly handles version list reject request', async () => {
+    const err = 'Invalid version request';
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if ((opts.url as string).indexOf('/_api/web/GetFileById') > -1) {
+        throw err;
+      }
+
+      throw 'Invalid request';
+    });
+
+    await assert.rejects(command.action(logger, {
+      options: {
+        debug: true,
+        webUrl: validWebUrl,
+        label: validLabel
+      }
+    }), new CommandError(err));
+  });
+
+  it('supports debug mode', () => {
+    const options = command.options;
+    let containsDebugOption = false;
+    options.forEach(o => {
+      if (o.option === '--debug') {
+        containsDebugOption = true;
+      }
+    });
+    assert(containsDebugOption);
+  });
+
+  it('supports specifying URL', () => {
+    const options = command.options;
+    let containsTypeOption = false;
+    options.forEach(o => {
+      if (o.option.indexOf('<webUrl>') > -1) {
+        containsTypeOption = true;
+      }
+    });
+    assert(containsTypeOption);
+  });
+});
---
+++ src/m365/spo/commands/file/file-version-get.ts
@@ -0,0 +1,128 @@
+import { Logger } from '../../../../cli/Logger';
+import GlobalOptions from '../../../../GlobalOptions';
+import request from '../../../../request';
+import { formatting } from '../../../../utils/formatting';
+import { validation } from '../../../../utils/validation';
+import SpoCommand from '../../../base/SpoCommand';
+import commands from '../../commands';
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  webUrl: string;
+  label: string;
+  fileUrl?: string;
+  fileId?: string;
+}
+
+class SpoFileVersionGetCommand extends SpoCommand {
+  public get name(): string {
+    return commands.FILE_VERSION_GET;
+  }
+
+  public get description(): string {
+    return 'Get a specific version of a file in a SharePoint Document library';
+  }
+
+  constructor() {
+    super();
+
+    this.#initTelemetry();
+    this.#initOptions();
+    this.#initValidators();
+    this.#initOptionSets();
+  }
+
+  #initTelemetry(): void {
+    this.telemetry.push((args: CommandArgs) => {
+      Object.assign(this.telemetryProperties, {
+        fileUrl: args.options.fileUrl,
+        fileId: args.options.fileId
+      });
+    });
+  }
+
+  #initOptions(): void {
+    this.options.unshift(
+      {
+        option: '-w, --webUrl <webUrl>'
+      },
+      {
+        option: '--label <label>'
+      },
+      {
+        option: '-u, --fileUrl [fileUrl]'
+      },
+      {
+        option: '-i, --fileId [fileId]'
+      }
+    );
+  }
+
+  #initValidators(): void {
+    this.validators.push(
+      async (args: CommandArgs) => {
+        if (args.options.fileId && !validation.isValidGuid(args.options.fileId as string)) {
+          return `${args.options.fileId} is not a valid GUID`;
+        }
+
+        return validation.isValidSharePointUrl(args.options.webUrl);
+      }
+    );
+  }
+
+  #initOptionSets(): void {
+    this.optionSets.push(['fileUrl', 'fileId']);
+  }
+
+  public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
+    args.options.label = this.ensureMajorVersionIsString(args.options.label);
+
+    if (this.verbose) {
+      logger.logToStderr(`Retrieving version ${args.options.label} of the file ${args.options.fileUrl || args.options.fileId} at site ${args.options.webUrl}...`);
+    }
+
+    try {
+      const version = await this.getVersion(args);
+      logger.log(version.value[0]);
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+
+  // Gets files from a folder recursively.
+  private async getVersion(args: CommandArgs): Promise<any> {
+    let requestUrl;
+    if (args.options.fileUrl) {
+      requestUrl = `${args.options.webUrl}/_api/web/GetFileByServerRelativeUrl('${formatting.encodeQueryParameter(args.options.fileUrl)}')/versions/?$filter=VersionLabel eq '${args.options.label}'`;
+    }
+    else {
+      requestUrl = `${args.options.webUrl}/_api/web/GetFileById('${args.options.fileId}')/versions/?$filter=VersionLabel eq '${args.options.label}'`;
+    }
+    const requestOptions: any = {
+      url: requestUrl,
+      method: 'GET',
+      headers: {
+        'accept': 'application/json;odata=nometadata'
+      },
+      responseType: 'json'
+    };
+
+    const response = await request.get<{ value: any[] }>(requestOptions);
+    return response;
+  }
+
+  private ensureMajorVersionIsString(label: string): string {
+    if (label.toString().indexOf('.') > -1) {
+      return label;
+    }
+    else {
+      return `${label}.0`;
+    }
+  }
+}
+
+module.exports = new SpoFileVersionGetCommand();
