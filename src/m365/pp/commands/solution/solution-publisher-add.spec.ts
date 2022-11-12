37163fda86fc667aba6631b8464a0967e7a70092
Nico De Cleyre <nico.de.cleyre@gmail.com>
2022-11-12 09:37:23 +0100

    first commit

---
+++ src/m365/pp/commands/solution/solution-publisher-add.spec.ts
@@ -0,0 +1,152 @@
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import appInsights from '../../../../appInsights';
+import auth from '../../../../Auth';
+import { Cli } from '../../../../cli/Cli';
+import { CommandInfo } from '../../../../cli/CommandInfo';
+import { Logger } from '../../../../cli/Logger';
+import Command, { CommandError } from '../../../../Command';
+import request from '../../../../request';
+import { pid } from '../../../../utils/pid';
+import { powerPlatform } from '../../../../utils/powerPlatform';
+import { sinonUtil } from '../../../../utils/sinonUtil';
+import commands from '../../commands';
+const command: Command = require('./solution-publisher-add');
+
+describe(commands.SOLUTION_PUBLISHER_ADD, () => {
+  let commandInfo: CommandInfo;
+  //#region Mocked Responses
+  const validEnvironment = '4be50206-9576-4237-8b17-38d8aadfaa36';
+  const envUrl = "https://contoso-dev.api.crm4.dynamics.com";
+  const validName = "PublisherName";
+  const validDisplayName = "Publisher Name";
+  const validPrefix = "c6rx";
+  const validChoiceValuePrefix = 10000;
+  //#endregion
+
+  let log: string[];
+  let logger: Logger;
+  let loggerLogToStderrSpy: sinon.SinonSpy;
+
+  before(() => {
+    sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
+    sinon.stub(appInsights, 'trackEvent').callsFake(() => { });
+    sinon.stub(pid, 'getProcessName').callsFake(() => '');
+    auth.service.connected = true;
+    commandInfo = Cli.getCommandInfo(command);
+  });
+
+  beforeEach(() => {
+    log = [];
+    logger = {
+      log: (msg: string) => {
+        log.push(msg);
+      },
+      logRaw: (msg: string) => {
+        log.push(msg);
+      },
+      logToStderr: (msg: string) => {
+        log.push(msg);
+      }
+    };
+    loggerLogToStderrSpy = sinon.spy(logger, 'logToStderr');
+  });
+
+  afterEach(() => {
+    sinonUtil.restore([
+      request.post,
+      powerPlatform.getDynamicsInstanceApiUrl
+    ]);
+  });
+
+  after(() => {
+    sinonUtil.restore([
+      auth.restoreAuth,
+      appInsights.trackEvent,
+      pid.getProcessName
+    ]);
+    auth.service.connected = false;
+  });
+
+  it('has correct name', () => {
+    assert.strictEqual(command.name.startsWith(commands.SOLUTION_PUBLISHER_ADD), true);
+  });
+
+  it('has a description', () => {
+    assert.notStrictEqual(command.description, null);
+  });
+
+  it('defines correct properties for the default output', () => {
+    assert.deepStrictEqual(command.defaultProperties(), ['publisherid', 'uniquename', 'friendlyname']);
+  });
+
+  it('fails validation if choiceValuePrefix contains invalid format.', async () => {
+    const actual = await command.validate({
+      options: {
+        environment: validEnvironment,
+        name: validName,
+        displayName: validDisplayName,
+        prefix: validPrefix,
+        choiceValuePrefix: 'Not A Number'
+      }
+    }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('passes validation if required options specified', async () => {
+    const actual = await command.validate({ options: { environment: validEnvironment, name: validName, displayName: validDisplayName, prefix: validPrefix, choiceValuePrefix: validChoiceValuePrefix } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('correctly adds a specific publisher with the required parameters', async () => {
+    sinon.stub(powerPlatform, 'getDynamicsInstanceApiUrl').callsFake(async () => envUrl);
+
+    sinon.stub(request, 'post').callsFake(async opts => {
+      if ((opts.url === `https://contoso-dev.api.crm4.dynamics.com/api/data/v9.0/publishers`)) {
+        if ((opts.headers?.accept as string)?.indexOf('application/json') === 0) {
+          return;
+        }
+      }
+
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, { options: { verbose: true, environment: validEnvironment, name: validName, displayName: validDisplayName, prefix: validPrefix, choiceValuePrefix: validChoiceValuePrefix } });
+    assert(loggerLogToStderrSpy.called);
+  });
+
+  it('correctly handles API OData error', async () => {
+    sinon.stub(powerPlatform, 'getDynamicsInstanceApiUrl').callsFake(async () => envUrl);
+
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      if ((opts.url === `https://contoso-dev.api.crm4.dynamics.com/api/data/v9.0/publishers`)) {
+        if ((opts.headers?.accept as string)?.indexOf('application/json') === 0) {
+          throw {
+            error: {
+              'odata.error': {
+                code: '-1, InvalidOperationException',
+                message: {
+                  value: `Resource '' does not exist or one of its queried reference-property objects are not present`
+                }
+              }
+            }
+          };
+        }
+      }
+    });
+
+    await assert.rejects(command.action(logger, { options: { debug: false, environment: validEnvironment, name: validName, displayName: validDisplayName, prefix: validPrefix, choiceValuePrefix: validChoiceValuePrefix } } as any),
+      new CommandError(`Resource '' does not exist or one of its queried reference-property objects are not present`));
+  });
+
+  it('supports debug mode', () => {
+    const options = command.options;
+    let containsOption = false;
+    options.forEach(o => {
+      if (o.option === '--debug') {
+        containsOption = true;
+      }
+    });
+    assert(containsOption);
+  });
+});
---
+++ src/m365/pp/commands/solution/solution-publisher-add.ts
@@ -0,0 +1,118 @@
+import { AxiosRequestConfig } from 'axios';
+import { Logger } from '../../../../cli/Logger';
+import GlobalOptions from '../../../../GlobalOptions';
+import request from '../../../../request';
+import { powerPlatform } from '../../../../utils/powerPlatform';
+import PowerPlatformCommand from '../../../base/PowerPlatformCommand';
+import commands from '../../commands';
+
+interface CommandArgs {
+  options: Options;
+}
+
+export interface Options extends GlobalOptions {
+  environment: string;
+  name: string;
+  displayName: string;
+  prefix: string;
+  choiceValuePrefix: number;
+  asAdmin?: boolean;
+}
+
+class PpSolutionPublisherAddCommand extends PowerPlatformCommand {
+  public get name(): string {
+    return commands.SOLUTION_PUBLISHER_ADD;
+  }
+
+  public get description(): string {
+    return 'Adds a specified publisher in a given environment.';
+  }
+
+  public defaultProperties(): string[] | undefined {
+    return ['publisherid', 'uniquename', 'friendlyname'];
+  }
+
+  constructor() {
+    super();
+
+    this.#initTelemetry();
+    this.#initOptions();
+    this.#initValidators();
+  }
+
+  #initTelemetry(): void {
+    this.telemetry.push((args: CommandArgs) => {
+      Object.assign(this.telemetryProperties, {
+        asAdmin: !!args.options.asAdmin
+      });
+    });
+  }
+
+  #initOptions(): void {
+    this.options.unshift(
+      {
+        option: '-e, --environment <environment>'
+      },
+      {
+        option: '-n, --name <name>'
+      },
+      {
+        option: '--displayName <displayName>'
+      },
+      {
+        option: '--prefix <prefix>'
+      },
+      {
+        option: '--choiceValuePrefix <choiceValuePrefix>'
+      },
+      {
+        option: '-a, --asAdmin'
+      }
+    );
+  }
+
+  #initValidators(): void {
+    this.validators.push(
+      async (args: CommandArgs) => {
+        if (args.options.choiceValuePrefix && isNaN(args.options.choiceValuePrefix)) {
+          return `choiceValuePrefix is not a number`;
+        }
+
+        return true;
+      }
+    );
+  }
+
+  public async commandAction(logger: Logger, args: any): Promise<void> {
+    if (this.verbose) {
+      logger.logToStderr(`Adding a new publisher '${args.options.name}'...`);
+    }
+    try {
+      const dynamicsApiUrl = await powerPlatform.getDynamicsInstanceApiUrl(args.options.environment, args.options.asAdmin);
+
+      const requestOptions: AxiosRequestConfig = {
+        url: `${dynamicsApiUrl}/api/data/v9.0/publishers`,
+        headers: {
+          'content-type': 'application/json;odata=verbose',
+          accept: 'application/json;odata.metadata=none'
+        },
+        responseType: 'json',
+        data: {
+          uniquename: args.options.name,
+          friendlyname: args.options.displayName,
+          customizationprefix: args.options.prefix,
+          customizationoptionvalueprefix: args.options.choiceValuePrefix
+        }
+      };
+
+      await request.post(requestOptions);
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+
+
+  }
+}
+
+module.exports = new PpSolutionPublisherAddCommand();
