7279e850fc651ce959ff5a8d70e5dc9948f7ff1b
Nico De Cleyre <nico.de.cleyre@gmail.com>
2022-11-18 11:33:56 +0100

    made context hash

--- src/m365/base/ContextCommand.ts
+++ src/m365/base/ContextCommand.ts
@@ -2,9 +2,10 @@ import * as fs from 'fs';
 import AnonymousCommand from './AnonymousCommand';
 import { Logger } from '../../cli/Logger';
 import { M365RcJson } from './M365RcJson';
+import { Hash } from '../../utils/types';
 
 export default abstract class ContextCommand extends AnonymousCommand {
-  public saveContextInfo(contextInfo: any, logger: Logger): Promise<any> {
+  public saveContextInfo(context: Hash, logger: Logger): Promise<any> {
     const filePath: string = '.m365rc.json';
 
     let m365rc: M365RcJson = {};
@@ -17,12 +18,12 @@ export default abstract class ContextCommand extends AnonymousCommand {
       }
       catch (e) {
         logger.logToStderr(`Error reading ${filePath}: ${e}. Please add context info to ${filePath} manually.`);
-        return contextInfo;
+        return Promise.resolve(context);
       }
     }
 
     if (!m365rc.context) {
-      m365rc.context = contextInfo;
+      m365rc.context = context;
 
       try {
         fs.writeFileSync(filePath, JSON.stringify(m365rc, null, 2));
@@ -32,6 +33,6 @@ export default abstract class ContextCommand extends AnonymousCommand {
       }
     }
 
-    return contextInfo;
+    return Promise.resolve(context);
   }
 }
