cb8676e07e78c4c35452e024a388fa5aaeb98d46
Nanddeep Nachan <nanddeepnachan@gmail.com>
2022-11-20 09:49:08 +0000

    planner plan set

---
+++ docs/docs/cmd/planner/plan/plan-set.md
@@ -0,0 +1,144 @@
+# planner plan set
+
+Updates a Microsoft Planner plan
+
+## Usage
+
+```sh
+m365 planner plan set [options]
+```
+
+## Options
+
+`-i, --id [id]`
+: ID of the plan. Specify either `id` or `title` but not both.
+
+`-t, --title [title]`
+: Title of the plan. Specify either `id` or `title` but not both.
+
+`--ownerGroupId [ownerGroupId]`
+: ID of the group to which the plan belongs. Specify either `ownerGroupId` or `ownerGroupName` when using `title`.
+
+`--ownerGroupName [ownerGroupName]`
+: Name of the Group to which the plan belongs. Specify either `ownerGroupId` or `ownerGroupName` when using `title`.
+
+`--newTitle [newTitle]`
+: New title of the plan.
+
+`--shareWithUserIds [shareWithUserIds]`
+: The comma-separated IDs of the users with whom you want to share the plan. Specify either `shareWithUserIds` or `shareWithUserNames` but not both.
+
+`--shareWithUserNames [shareWithUserNames]`
+: The comma-separated UPNs of the users with whom you want to share the plan. Specify either `shareWithUserIds` or `shareWithUserNames` but not both.
+
+--8<-- "docs/cmd/_global.md"
+
+## Remarks
+
+This command allows using unknown options. 
+
+`--category1 [category1]`
+: New label for a category. Define the category key within your option to update the related label. Category 1 to 25 are available. E.g., `--category4`, `--category12`.
+
+## Examples
+
+Updates a Microsoft Planner plan title to New Title for the plan with id gndWOTSK60GfPQfiDDj43JgACDCb.
+
+```sh
+m365 planner plan set --id 'gndWOTSK60GfPQfiDDj43JgACDCb' --newTitle 'New Title'
+```
+
+Share a Microsoft Planner plan with title Plan Title that is owned by the group My Group, with the users user1@contoso.com and user2@contoso.com
+
+```sh
+m365 planner plan set --title 'Plan Title' --ownerGroupName 'My Group' --shareWithUserNames 'user1@contoso.com,user2@contoso.com'
+```
+
+Updates a Microsoft Planner plan category label with the key 21 to ToDo also the category label with the key 25 to Urgent. This for the plan with id gndWOTSK60GfPQfiDDj43JgACDCb.
+
+```sh
+m365 planner plan set --id 'gndWOTSK60GfPQfiDDj43JgACDCb' --category21 'ToDo' --category25 'Urgent'
+```
+
+## More information
+
+- Update plannerPlan: [https://learn.microsoft.com/en-us/graph/api/plannerplan-update?view=graph-rest-1.0&tabs=http](https://learn.microsoft.com/en-us/graph/api/plannerplan-update?view=graph-rest-1.0&tabs=http)
+- plannerPlanDetails resource type: [https://learn.microsoft.com/en-us/graph/api/resources/plannerplandetails?view=graph-rest-1.0](https://learn.microsoft.com/en-us/graph/api/resources/plannerplandetails?view=graph-rest-1.0)
+- plannerCategoryDescriptions resource type: [https://learn.microsoft.com/en-us/graph/api/resources/plannercategorydescriptions?view=graph-rest-1.0](https://learn.microsoft.com/en-us/graph/api/resources/plannercategorydescriptions?view=graph-rest-1.0)
+
+
+## Response
+
+=== "JSON"
+
+    ```json
+    {
+      "createdDateTime": "2015-03-30T18:36:49.2407981Z",
+      "owner": "ebf3b108-5234-4e22-b93d-656d7dae5874",
+      "title": "My Planner Plan",
+      "id": "xqQg5FS2LkCp935s-FIFm2QAFkHM",
+      "createdBy": {
+        "user": {
+          "displayName": null,
+          "id": "95e27074-6c4a-447a-aa24-9d718a0b86fa"
+        },
+        "application": {
+          "displayName": null,
+          "id": "ebf3b108-5234-4e22-b93d-656d7dae5874"
+        }
+      },
+      "container": {
+        "containerId": "ebf3b108-5234-4e22-b93d-656d7dae5874",
+        "type": "group",
+        "url": "https://graph.microsoft.com/v1.0/groups/ebf3b108-5234-4e22-b93d-656d7dae5874"
+      },
+      "sharedWith": {
+        "ebf3b108-5234-4e22-b93d-656d7dae5874": true,
+        "6463a5ce-2119-4198-9f2a-628761df4a62": true
+      },
+      "categoryDescriptions": {
+        "category1": null,
+        "category2": null,
+        "category3": null,
+        "category4": null,
+        "category5": null,
+        "category6": null,
+        "category7": null,
+        "category8": null,
+        "category9": null,
+        "category10": null,
+        "category11": null,
+        "category12": null,
+        "category13": null,
+        "category14": null,
+        "category15": null,
+        "category16": null,
+        "category17": null,
+        "category18": null,
+        "category19": null,
+        "category20": null,
+        "category21": null,
+        "category22": null,
+        "category23": null,
+        "category24": null,
+        "category25": null
+      }
+    }
+    ```
+
+=== "Text"
+
+    ```text
+    createdDateTime: 2015-03-30T18:36:49.2407981Z
+    id             : xqQg5FS2LkCp935s-FIFm2QAFkHM
+    owner          : ebf3b108-5234-4e22-b93d-656d7dae5874
+    title          : My Planner Plan
+    ```
+
+=== "CSV"
+
+    ```csv
+    id,title,createdDateTime,owner
+    xqQg5FS2LkCp935s-FIFm2QAFkHM,My Planner Plan,2015-03-30T18:36:49.2407981Z,ebf3b108-5234-4e22-b93d-656d7dae5874
+    ```
+
---
+++ src/m365/planner/commands/plan/plan-set.ts
@@ -0,0 +1,361 @@
+import { PlannerPlan, PlannerPlanDetails, User } from '@microsoft/microsoft-graph-types';
+import { AxiosRequestConfig } from 'axios';
+import auth from '../../../../Auth';
+import { Logger } from '../../../../cli/Logger';
+import GlobalOptions from '../../../../GlobalOptions';
+import request from '../../../../request';
+import { accessToken } from '../../../../utils/accessToken';
+import { formatting } from '../../../../utils/formatting';
+import { validation } from '../../../../utils/validation';
+import { aadGroup } from '../../../../utils/aadGroup';
+import { planner } from '../../../../utils/planner';
+import GraphCommand from '../../../base/GraphCommand';
+import commands from '../../commands';
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  id?: string;
+  title?: string;
+  ownerGroupId?: string;
+  ownerGroupName?: string;
+  newTitle?: string;
+  shareWithUserIds?: string;
+  shareWithUserNames?: string;
+}
+
+class PlannerPlanSetCommand extends GraphCommand {
+  public get name(): string {
+    return commands.PLAN_SET;
+  }
+
+  public get description(): string {
+    return 'Updates a Microsoft Planner plan';
+  }
+
+  public defaultProperties(): string[] | undefined {
+    return ['id', 'title', 'createdDateTime', 'owner'];
+  }
+
+  constructor() {
+    super();
+
+    this.#initTelemetry();
+    this.#initOptions();
+    this.#initValidators();
+    this.#initOptionSets();
+  }
+
+  #initTelemetry(): void {
+    this.telemetry.push((args: CommandArgs) => {
+      Object.assign(this.telemetryProperties, {
+        id: typeof args.options.id !== 'undefined',
+        title: typeof args.options.title !== 'undefined',
+        ownerGroupId: typeof args.options.ownerGroupId !== 'undefined',
+        ownerGroupName: typeof args.options.ownerGroupName !== 'undefined',
+        newTitle: typeof args.options.newTitle !== 'undefined',
+        shareWithUserIds: typeof args.options.shareWithUserIds !== 'undefined',
+        shareWithUserNames: typeof args.options.shareWithUserNames !== 'undefined'
+      });
+    });
+  }
+
+  #initOptions(): void {
+    this.options.unshift(
+      {
+        option: '-i, --id [id]'
+      },
+      {
+        option: '-t, --title [title]'
+      },
+      {
+        option: '--ownerGroupId [ownerGroupId]'
+      },
+      {
+        option: '--ownerGroupName [ownerGroupName]'
+      },
+      {
+        option: '--newTitle [newTitle]'
+      },
+      {
+        option: '--shareWithUserIds [shareWithUserIds]'
+      },
+      {
+        option: '--shareWithUserNames [shareWithUserNames]'
+      }
+    );
+  }
+
+  #initValidators(): void {
+    this.validators.push(
+      async (args: CommandArgs) => {
+        if (args.options.title) {
+          if (args.options.ownerGroupId && !validation.isValidGuid(args.options.ownerGroupId as string)) {
+            return `${args.options.ownerGroupId} is not a valid GUID`;
+          }
+
+          if (!args.options.ownerGroupId && !args.options.ownerGroupName) {
+            return 'Specify either ownerGroupId or ownerGroupName when using title';
+          }
+
+          if (args.options.ownerGroupId && args.options.ownerGroupName) {
+            return 'Specify either ownerGroupId or ownerGroupName when using title but not both';
+          }
+        }
+
+        if (args.options.shareWithUserIds && args.options.shareWithUserNames) {
+          return 'Specify either shareWithUserIds or shareWithUserNames but not both';
+        }
+
+        if (args.options.shareWithUserIds && !validation.isValidGuidArray(args.options.shareWithUserIds.split(','))) {
+          return 'shareWithUserIds contains invalid GUID';
+        }
+
+        const allowedCategories: string[] = [
+          'category1',
+          'category2',
+          'category3',
+          'category4',
+          'category5',
+          'category6',
+          'category7',
+          'category8',
+          'category9',
+          'category10',
+          'category11',
+          'category12',
+          'category13',
+          'category14',
+          'category15',
+          'category16',
+          'category17',
+          'category18',
+          'category19',
+          'category20',
+          'category21',
+          'category22',
+          'category23',
+          'category24',
+          'category25'
+        ];
+
+        let invalidCategoryOptions: boolean = false;
+        Object.keys(args.options).forEach(key => {
+          if (key.indexOf('category') !== -1 && allowedCategories.indexOf(key) === -1) {
+            invalidCategoryOptions = true;
+          }
+        });
+
+        if (invalidCategoryOptions) {
+          return 'Specify category values between category1 to category25';
+        }
+
+        return true;
+      }
+    );
+  }
+
+  #initOptionSets(): void {
+    this.optionSets.push(
+      ['id', 'title']
+    );
+  }
+
+  public allowUnknownOptions(): boolean | undefined {
+    return true;
+  }
+
+  private async getGroupId(args: CommandArgs): Promise<string> {
+    const { ownerGroupId, ownerGroupName } = args.options;
+
+    if (ownerGroupId) {
+      return ownerGroupId;
+    }
+
+    const group = await aadGroup.getGroupByDisplayName(ownerGroupName!);
+    return group.id!;
+  }
+
+  private async getPlanId(args: CommandArgs): Promise<string> {
+    const { id, title } = args.options;
+
+    if (id) {
+      return id;
+    }
+
+    const groupId: string = await this.getGroupId(args);
+    const plan: PlannerPlan = await planner.getPlanByTitle(title!, groupId);
+    return plan.id!;
+  }
+
+  private getUserIds(options: Options): Promise<string[]> {
+    if (options.shareWithUserIds) {
+      return Promise.resolve(options.shareWithUserIds.split(','));
+    }
+
+    // Hitting this section means assignedToUserNames won't be undefined
+    const userNames = options.shareWithUserNames as string;
+    const userArr: string[] = userNames.split(',').map(o => o.trim());
+
+    const promises: Promise<{ value: User[] }>[] = userArr.map(user => {
+      const requestOptions: any = {
+        url: `${this.resource}/v1.0/users?$filter=userPrincipalName eq '${formatting.encodeQueryParameter(user)}'&$select=id,userPrincipalName`,
+        headers: {
+          'content-type': 'application/json'
+        },
+        responseType: 'json'
+      };
+
+      return request.get(requestOptions);
+    });
+
+    return Promise
+      .all(promises)
+      .then((usersRes: { value: User[] }[]): Promise<string[]> => {
+        const userUpns = usersRes.map(res => res.value[0]?.userPrincipalName as string);
+        const userIds = usersRes.map(res => res.value[0]?.id as string);
+
+        // Find the members where no graph response was found
+        const invalidUsers = userArr.filter(user => !userUpns.some((upn) => upn?.toLowerCase() === user.toLowerCase()));
+
+        if (invalidUsers && invalidUsers.length > 0) {
+          return Promise.reject(`Cannot proceed with planner plan creation. The following users provided are invalid : ${invalidUsers.join(',')}`);
+        }
+
+        return Promise.resolve(userIds);
+      });
+  }
+
+  private async generateSharedWith(options: Options): Promise<{ [userId: string]: boolean }> {
+    const sharedWith: { [userId: string]: boolean } = {};
+
+    const userIds = await this.getUserIds(options);
+    userIds.map(x => sharedWith[x] = true);
+    return sharedWith;
+  }
+
+  private async getPlanEtag(planId: string): Promise<string> {
+    const requestOptions: any = {
+      url: `${this.resource}/v1.0/planner/plans/${planId}`,
+      headers: {
+        accept: 'application/json'
+      },
+      responseType: 'json'
+    };
+
+    const response: any = await request.get(requestOptions);
+    return response['@odata.etag'];
+  }
+
+  private async getPlanDetailsEtag(planId: string): Promise<string> {
+    const requestOptions: any = {
+      url: `${this.resource}/v1.0/planner/plans/${planId}/details`,
+      headers: {
+        accept: 'application/json'
+      },
+      responseType: 'json'
+    };
+
+    const response: any = await request.get(requestOptions);
+    return response['@odata.etag'];
+  }
+
+  private async getPlanDetails(plan: PlannerPlan): Promise<PlannerPlan & PlannerPlanDetails> {
+    const requestOptionsTaskDetails: any = {
+      url: `${this.resource}/v1.0/planner/plans/${plan.id}/details`,
+      headers: {
+        'accept': 'application/json;odata.metadata=none',
+        'Prefer': 'return=representation'
+      },
+      responseType: 'json'
+    };
+
+    const planDetails: any = await request.get(requestOptionsTaskDetails);
+    return { ...plan, ...planDetails as PlannerPlanDetails };
+  }
+
+  private async updatePlanDetails(options: Options, planId: string): Promise<PlannerPlan & PlannerPlanDetails> {
+    const plan = await planner.getPlanById(planId);
+
+    const categories: any = {};
+    let categoriesCount: number = 0;
+
+    Object.keys(options).forEach(key => {
+      if (key.indexOf('category') !== -1) {
+        categories[key] = options[key];
+        categoriesCount++;
+      }
+    });
+
+    if (!options.shareWithUserIds && !options.shareWithUserNames && categoriesCount === 0) {
+      return this.getPlanDetails(plan);
+    }
+
+    const requestBody: any = {};
+
+    if (options.shareWithUserIds || options.shareWithUserNames) {
+      const sharedWith = await this.generateSharedWith(options);
+      requestBody['sharedWith'] = sharedWith;
+    }
+
+    if (categoriesCount > 0) {
+      requestBody['categoryDescriptions'] = categories;
+    }
+
+    const etag = await this.getPlanDetailsEtag(planId);
+
+    const requestOptionsPlanDetails: any = {
+      url: `${this.resource}/v1.0/planner/plans/${planId}/details`,
+      headers: {
+        'accept': 'application/json;odata.metadata=none',
+        'If-Match': etag,
+        'Prefer': 'return=representation'
+      },
+      responseType: 'json',
+      data: requestBody
+    };
+
+    const planDetails = await request.patch(requestOptionsPlanDetails);
+    return { ...plan, ...planDetails as PlannerPlanDetails };
+  }
+
+  public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
+    if (accessToken.isAppOnlyAccessToken(auth.service.accessTokens[this.resource].accessToken)) {
+      this.handleError('This command does not support application permissions.');
+      return;
+    }
+
+    try {
+      const planId: string = await this.getPlanId(args);
+
+      if (args.options.newTitle) {
+        const etag = await this.getPlanEtag(planId);
+
+        const requestOptions: AxiosRequestConfig = {
+          url: `${this.resource}/v1.0/planner/plans/${planId}`,
+          headers: {
+            accept: 'application/json;odata.metadata=none',
+            'If-Match': etag,
+            'Prefer': 'return=representation'
+          },
+          responseType: 'json',
+          data: {
+            "title": args.options.newTitle
+          }
+        };
+
+        await request.patch(requestOptions);
+      }
+
+      const result = await this.updatePlanDetails(args.options, planId);
+      logger.log(result);
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+}
+
+module.exports = new PlannerPlanSetCommand();
