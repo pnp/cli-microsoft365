dd0cfdaaede951a864d0fe0d0541d7bb6feff61b
Martin Lingstuyl <mlingstuyl@live.com>
2022-12-16 11:31:09 +0000

    Adds new command spo listitem retentionlabel ensure. Closes #4158

---
+++ src/m365/spo/commands/listitem/listitem-retentionlabel-ensure.spec.ts
@@ -0,0 +1,296 @@
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import appInsights from '../../../../appInsights';
+import auth from '../../../../Auth';
+import { Cli } from '../../../../cli/Cli';
+import { CommandInfo } from '../../../../cli/CommandInfo';
+import { Logger } from '../../../../cli/Logger';
+import Command, { CommandError } from '../../../../Command';
+import request from '../../../../request';
+import { formatting } from '../../../../utils/formatting';
+import { pid } from '../../../../utils/pid';
+import { sinonUtil } from '../../../../utils/sinonUtil';
+import commands from '../../commands';
+import * as SpoWebRetentionLabelListCommand from '../web/web-retentionlabel-list';
+const command: Command = require('./listitem-retentionlabel-ensure');
+
+describe(commands.LISTITEM_RETENTIONLABEL_ENSURE, () => {
+
+  //#region Mock Responses
+  const mockSpoWebRetentionLabelListResponseArray = [
+    {
+      "AcceptMessagesOnlyFromSendersOrMembers": false,
+      "AccessType": null,
+      "AllowAccessFromUnmanagedDevice": null,
+      "AutoDelete": true,
+      "BlockDelete": true,
+      "BlockEdit": false,
+      "ComplianceFlags": 1,
+      "ContainsSiteLabel": false,
+      "DisplayName": "",
+      "EncryptionRMSTemplateId": null,
+      "HasRetentionAction": true,
+      "IsEventTag": false,
+      "MultiStageReviewerEmail": null,
+      "NextStageComplianceTag": null,
+      "Notes": null,
+      "RequireSenderAuthenticationEnabled": false,
+      "ReviewerEmail": null,
+      "SharingCapabilities": null,
+      "SuperLock": false,
+      "TagDuration": 2555,
+      "TagId": "def61080-111c-4aea-b72f-5b60e516e36c",
+      "TagName": "Some label",
+      "TagRetentionBasedOn": "CreationAgeInDays",
+      "UnlockedAsDefault": false
+    }
+  ];
+  //#endregion
+
+  const webUrl = 'https://contoso.sharepoint.com';
+  const listUrl = '/sites/project-x/list';
+  const listTitle = 'test';
+  const listId = 'b2307a39-e878-458b-bc90-03bc578531d6';
+  const labelName = 'Some label';
+  const labelId = 'def61080-111c-4aea-b72f-5b60e516e36c';
+
+  let log: any[];
+  let logger: Logger;
+  let commandInfo: CommandInfo;
+  let loggerLogStderrSpy: sinon.SinonSpy;
+
+  before(() => {
+    sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
+    sinon.stub(appInsights, 'trackEvent').callsFake(() => { });
+    sinon.stub(pid, 'getProcessName').callsFake(() => '');
+    auth.service.connected = true;
+    commandInfo = Cli.getCommandInfo(command);
+  });
+
+  beforeEach(() => {
+    log = [];
+    logger = {
+      log: (msg: string) => {
+        log.push(msg);
+      },
+      logRaw: (msg: string) => {
+        log.push(msg);
+      },
+      logToStderr: (msg: string) => {
+        log.push(msg);
+      }
+    };
+    loggerLogStderrSpy = sinon.spy(logger, 'logToStderr');
+    sinon.stub(Cli, 'executeCommandWithOutput').callsFake(async (command): Promise<any> => {
+      if (command === SpoWebRetentionLabelListCommand) {
+        return { stdout: JSON.stringify(mockSpoWebRetentionLabelListResponseArray) };
+      }
+
+      throw 'Unknown case';
+    });
+  });
+
+  afterEach(() => {
+    sinonUtil.restore([
+      request.get,
+      request.post,
+      Cli.executeCommandWithOutput
+    ]);
+  });
+
+  after(() => {
+    sinonUtil.restore([
+      auth.restoreAuth,
+      appInsights.trackEvent,
+      pid.getProcessName
+    ]);
+    auth.service.connected = false;
+  });
+
+  it('has correct name', () => {
+    assert.strictEqual(command.name.startsWith(commands.LISTITEM_RETENTIONLABEL_ENSURE), true);
+  });
+
+  it('has a description', () => {
+    assert.notStrictEqual(command.description, null);
+  });
+
+  it('fails validation if listId, listTitle or listUrl option is not passed', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listItemId: 1, name: labelName } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if the url option is not a valid SharePoint site URL', async () => {
+    const actual = await command.validate({ options: { webUrl: 'foo', listItemId: 1, listTitle: listTitle, name: labelName } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if the listId option is not a valid GUID', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listId: '12345', listItemId: 1, name: labelName } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if both listId and title options are passed', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listId: listId, listTitle: listTitle, listItemId: 1 } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if listItemId is not passed', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if listItemId is not a number', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listItemId: 'abc', listTitle: listTitle, name: labelName } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if name or id option is not passed', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listItemId: 1, listId: listId } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if the id option is not a valid GUID', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, id: '12345', listItemId: 1, listId: listId } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('passes validation if the id option is a valid GUID', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, id: labelId, listItemId: 1, listId: listId } }, commandInfo);
+    assert(actual);
+  });
+
+  it('passes validation if the url option is a valid SharePoint site URL', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listId: listId, listItemId: 1, name: labelName } }, commandInfo);
+    assert(actual);
+  });
+
+  it('passes validation if the listId option is a valid GUID', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listId: listId, listItemId: 1, name: labelName } }, commandInfo);
+    assert(actual);
+  });
+
+  it('applies a retentionlabel based on listId and name', async () => {
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `https://contoso.sharepoint.com/_api/web/lists(guid'${formatting.encodeQueryParameter(listId)}')/items(1)/SetComplianceTag()`
+        && JSON.stringify(opts.data) === '{"complianceTag":"Some label","isTagPolicyHold":true,"isTagPolicyRecord":false,"isEventBasedTag":false,"isTagSuperLock":false,"isUnlockedAsDefault":false}') {
+        return;
+      }
+
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        debug: false,
+        listId: listId,
+        webUrl: webUrl,
+        listItemId: 1,
+        name: labelName
+      }
+    });
+    assert(loggerLogStderrSpy.notCalled);
+  });
+
+  it('applies a retentionlabel based on listId and id (debug)', async () => {
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `https://contoso.sharepoint.com/_api/web/lists(guid'${formatting.encodeQueryParameter(listId)}')/items(1)/SetComplianceTag()`
+        && JSON.stringify(opts.data) === '{"complianceTag":"Some label","isTagPolicyHold":true,"isTagPolicyRecord":false,"isEventBasedTag":false,"isTagSuperLock":false,"isUnlockedAsDefault":false}') {
+        return;
+      }
+
+      throw 'Invalid request';
+    });
+
+    await assert.doesNotReject(command.action(logger, {
+      options: {
+        debug: true,
+        listId: listId,
+        webUrl: webUrl,
+        listItemId: 1,
+        id: labelId
+      }
+    }));
+  });
+
+  it('applies a retentionlabel based on listTitle and id (debug)', async () => {
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `https://contoso.sharepoint.com/_api/web/lists/getByTitle('${formatting.encodeQueryParameter(listTitle)}')/items(1)/SetComplianceTag()`
+        && JSON.stringify(opts.data) === '{"complianceTag":"Some label","isTagPolicyHold":true,"isTagPolicyRecord":false,"isEventBasedTag":false,"isTagSuperLock":false,"isUnlockedAsDefault":false}') {
+        return;
+      }
+
+      throw 'Invalid request';
+    });
+
+    await assert.doesNotReject(command.action(logger, {
+      options: {
+        debug: true,
+        listTitle: listTitle,
+        webUrl: webUrl,
+        listItemId: 1,
+        id: labelId
+      }
+    }));
+  });
+
+  it('applies a retentionlabel based on listUrl and id (debug)', async () => {
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `https://contoso.sharepoint.com/_api/web/GetList(@a1)/items(1)/SetComplianceTag()?@a1='${formatting.encodeQueryParameter(listUrl)}'`
+        && JSON.stringify(opts.data) === '{"complianceTag":"Some label","isTagPolicyHold":true,"isTagPolicyRecord":false,"isEventBasedTag":false,"isTagSuperLock":false,"isUnlockedAsDefault":false}') {
+        return;
+      }
+
+      throw 'Invalid request';
+    });
+
+    await assert.doesNotReject(command.action(logger, {
+      options: {
+        debug: true,
+        listUrl: listUrl,
+        webUrl: webUrl,
+        listItemId: 1,
+        id: labelId
+      }
+    }));
+  });
+
+  it('throws an error when a retentionlabel cannot be found on the site', async () => {
+    await assert.rejects(command.action(logger, {
+      options: {
+        debug: false,
+        listId: listId,
+        webUrl: webUrl,
+        listItemId: 1,
+        name: 'Some non-existing label'
+      }
+    }), new CommandError('The specified retention label does not exist'));
+  });
+
+  it('correctly handles API OData error', async () => {
+    const errorMessage = 'Something went wrong';
+
+    sinon.stub(request, 'post').callsFake(async () => { throw { error: { error: { message: errorMessage } } }; });
+
+    await assert.rejects(command.action(logger, {
+      options: {
+        debug: false,
+        listUrl: listUrl,
+        webUrl: 'https://contoso.sharepoint.com',
+        listItemId: 1,
+        name: labelName
+      }
+    }), new CommandError(errorMessage));
+  });
+
+  it('supports specifying URL', () => {
+    const options = command.options;
+    let containsTypeOption = false;
+    options.forEach(o => {
+      if (o.option.indexOf('<webUrl>') > -1) {
+        containsTypeOption = true;
+      }
+    });
+    assert(containsTypeOption);
+  });
+});
---
+++ src/m365/spo/commands/listitem/listitem-retentionlabel-ensure.ts
@@ -0,0 +1,198 @@
+import { AxiosRequestConfig } from 'axios';
+import { Logger } from '../../../../cli/Logger';
+import GlobalOptions from '../../../../GlobalOptions';
+import request from '../../../../request';
+import { formatting } from '../../../../utils/formatting';
+import { urlUtil } from '../../../../utils/urlUtil';
+import { validation } from '../../../../utils/validation';
+import SpoCommand from '../../../base/SpoCommand';
+import commands from '../../commands';
+import { SiteRetentionLabel } from './SiteRetentionLabel';
+import * as SpoWebRetentionLabelListCommand from '../web/web-retentionlabel-list';
+import { Options as SpoWebRetentionLabelListCommandOptions } from '../web/web-retentionlabel-list';
+import Command from '../../../../Command';
+import { Cli } from '../../../../cli/Cli';
+
+interface CommandArgs {
+  options: Options;
+}
+
+export interface Options extends GlobalOptions {
+  webUrl: string;
+  listId?: string;
+  listTitle?: string;
+  listUrl?: string;
+  listItemId: string;
+  name?: string;
+  id?: string;
+}
+
+class SpoListItemRetentionLabelEnsureCommand extends SpoCommand {
+  public get name(): string {
+    return commands.LISTITEM_RETENTIONLABEL_ENSURE;
+  }
+
+  public get description(): string {
+    return 'Clear the retention label from a list item';
+  }
+
+  constructor() {
+    super();
+
+    this.#initTelemetry();
+    this.#initOptions();
+    this.#initValidators();
+    this.#initOptionSets();
+  }
+
+  #initTelemetry(): void {
+    this.telemetry.push((args: CommandArgs) => {
+      Object.assign(this.telemetryProperties, {
+        listId: typeof args.options.listId !== 'undefined',
+        listTitle: typeof args.options.listTitle !== 'undefined',
+        listUrl: typeof args.options.listUrl !== 'undefined',
+        name: typeof args.options.name !== 'undefined',
+        id: typeof args.options.id !== 'undefined'
+      });
+    });
+  }
+
+  #initOptions(): void {
+    this.options.unshift(
+      {
+        option: '-u, --webUrl <webUrl>'
+      },
+      {
+        option: '--listItemId <listItemId>'
+      },
+      {
+        option: '--listId [listId]'
+      },
+      {
+        option: '--listTitle [listTitle]'
+      },
+      {
+        option: '--listUrl [listUrl]'
+      },
+      {
+        option: '-n, --name [name]'
+      },
+      {
+        option: '-i, --id [id]'
+      }
+    );
+  }
+
+  #initValidators(): void {
+    this.validators.push(
+      async (args: CommandArgs) => {
+        const id: number = parseInt(args.options.listItemId);
+        if (isNaN(id)) {
+          return `${args.options.listItemId} is not a valid list item ID`;
+        }
+
+        const isValidSharePointUrl: boolean | string = validation.isValidSharePointUrl(args.options.webUrl);
+        if (isValidSharePointUrl !== true) {
+          return isValidSharePointUrl;
+        }
+
+        if (args.options.listId &&
+          !validation.isValidGuid(args.options.listId as string)) {
+          return `${args.options.listId} is not a valid GUID`;
+        }
+
+        if (args.options.id &&
+          !validation.isValidGuid(args.options.id as string)) {
+          return `${args.options.id} is not a valid GUID`;
+        }
+
+        return true;
+      }
+    );
+  }
+
+  #initOptionSets(): void {
+    this.optionSets.push({ options: ['listId', 'listTitle', 'listUrl'] });
+    this.optionSets.push({ options: ['name', 'id'] });
+  }
+
+  public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
+    try {
+      const labelInformation = await this.getLabelInformation(args.options, logger);
+      await this.applyLabel(args.options, labelInformation, logger);
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+
+  private async getLabelInformation(options: Options, logger: Logger): Promise<any> {
+    const cmdOptions: SpoWebRetentionLabelListCommandOptions = {
+      webUrl: options.webUrl,
+      output: 'json',
+      debug: options.debug,
+      verbose: options.verbose
+    };
+
+    const output = await Cli.executeCommandWithOutput(SpoWebRetentionLabelListCommand as Command, { options: { ...cmdOptions, _: [] } });
+
+    if (this.verbose) {
+      logger.logToStderr(output.stderr);
+    }
+
+    const labels = JSON.parse(output.stdout) as SiteRetentionLabel[];
+    const label = labels.find(l => l.TagName === options.name || l.TagId === options.id);
+
+    if (this.verbose && label !== undefined) {
+      logger.logToStderr(`Retention label found in the list of available labels: '${label.TagName}' / '${label.TagId}'...`);
+    }
+
+    if (label === undefined) {
+      throw new Error(`The specified retention label does not exist`);
+    }
+
+    const requestBody = {
+      "complianceTag": label.TagName,
+      "isTagPolicyHold": label.BlockDelete,
+      "isTagPolicyRecord": label.BlockEdit,
+      "isEventBasedTag": label.IsEventTag,
+      "isTagSuperLock": label.SuperLock,
+      "isUnlockedAsDefault": label.UnlockedAsDefault
+    };
+
+    return requestBody;
+  }
+
+  private async applyLabel(options: Options, labelInformation: any, logger: Logger): Promise<void> {
+    if (this.verbose) {
+      logger.logToStderr(`Applying retention label to item in list '${options.listId || options.listTitle || options.listUrl}' in site at ${options.webUrl}...`);
+    }
+
+    let requestUrl = `${options.webUrl}/_api/web`;
+
+    if (options.listId) {
+      requestUrl += `/lists(guid'${formatting.encodeQueryParameter(options.listId)}')/items(${options.listItemId})/SetComplianceTag()`;
+    }
+    else if (options.listTitle) {
+      requestUrl += `/lists/getByTitle('${formatting.encodeQueryParameter(options.listTitle)}')/items(${options.listItemId})/SetComplianceTag()`;
+    }
+    else if (options.listUrl) {
+      const listServerRelativeUrl: string = urlUtil.getServerRelativePath(options.webUrl, options.listUrl);
+      requestUrl += `/GetList(@a1)/items(${options.listItemId})/SetComplianceTag()?@a1='${formatting.encodeQueryParameter(listServerRelativeUrl)}'`;
+    }
+
+    const requestOptions: AxiosRequestConfig = {
+      url: requestUrl,
+      method: 'POST',
+      headers: {
+        'accept': 'application/json;odata=nometadata'
+      },
+      data: labelInformation,
+      responseType: 'json'
+    };
+
+    await request.post(requestOptions);
+  }
+}
+
+module.exports = new SpoListItemRetentionLabelEnsureCommand();
