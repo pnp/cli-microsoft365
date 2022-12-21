b92abd962f88fd08e6bf205015e2028a1f228daa
Nanddeep Nachan <nanddeepnachan@gmail.com>
2022-12-21 14:04:15 +0000

    Adds 'purview retentionlabel add' command. Closes #4147

--- src/m365/purview/commands.ts
+++ src/m365/purview/commands.ts
@@ -1,6 +1,7 @@
 const prefix: string = 'purview';
 
 export default {
+  RETENTIONLABEL_ADD: `${prefix} retentionlabel add`,
   RETENTIONLABEL_GET: `${prefix} retentionlabel get`,
   RETENTIONLABEL_LIST: `${prefix} retentionlabel list`,
   RETENTIONLABEL_REMOVE: `${prefix} retentionlabel remove`,
