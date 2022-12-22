35c5b873f736db0a3bf97d6670844ebf22c7643b
Nico De Cleyre <nico.de.cleyre@gmail.com>
2022-12-22 12:50:15 +0100

    Enhances `spo list retentionlabel ensure`. Closes #4247

--- docs/docs/cmd/spo/list/list-retentionlabel-ensure.md
+++ docs/docs/cmd/spo/list/list-retentionlabel-ensure.md
@@ -19,8 +19,11 @@ m365 spo list label set [options]
 `-u, --webUrl <webUrl>`
 : The URL of the site where the list is located.
 
+`--name <name>`
+: The label name to set on the list.
+
 `--label <label>`
-: The label to set on the list.
+: (deprecated. Use `name` instead) The label name to set on the list.
 
 `-t, --listTitle [listTitle]`
 : The title of the list on which to set the label. Specify either `listTitle`, `listId`, or `listUrl` but not multiple.
@@ -48,16 +51,16 @@ A list retention label is a default label that will be applied to all new items 
 
 ## Examples
 
-Sets retention label on the list with specified site-relative URL located in the specified site.
+Sets a retention label by name on a given list
 
 ```sh
-m365 spo list retentionlabel ensure --webUrl https://contoso.sharepoint.com/sites/project-x --listUrl 'Shared Documents' --label 'Some label'
+m365 spo list retentionlabel ensure --webUrl https://contoso.sharepoint.com/sites/project-x --listUrl 'Shared Documents' --name 'Some label'
 ```
 
-Sets retention label and disables editing and deleting items on the list and all existing items for the list with specified title located in the specified site.
+Sets a retention label by name and disables editing and deleting items on the list and all existing items for a given list
 
 ```sh
-m365 spo list retentionlabel ensure --webUrl https://contoso.sharepoint.com/sites/project-x --listTitle 'Documents' --label 'Some label' --blockEdit --blockDelete --syncToItems
+m365 spo list retentionlabel ensure --webUrl https://contoso.sharepoint.com/sites/project-x --listTitle 'Documents' --name 'Some label' --blockEdit --blockDelete --syncToItems
 ```
 
 ## Response
--- src/m365/spo/commands/list/list-retentionlabel-ensure.spec.ts
+++ src/m365/spo/commands/list/list-retentionlabel-ensure.spec.ts
@@ -117,12 +117,12 @@ describe(commands.LIST_RETENTIONLABEL_ENSURE, () => {
         debug: true,
         webUrl: 'https://contoso.sharepoint.com/sites/team1',
         listTitle: 'MyLibrary',
-        label: 'abc'
+        name: 'abc'
       }
     } as any), new CommandError('404 - "404 FILE NOT FOUND"'));
   });
 
-  it('should set label for list (debug)', async () => {
+  it('should set label for list with deprecated label', async () => {
     const postStub = sinon.stub(request, 'post').callsFake((opts) => {
       if ((opts.url as string).indexOf(`https://contoso.sharepoint.com/sites/team1/_api/SP_CompliancePolicy_SPPolicyStoreProxy_SetListComplianceTag`) > -1) {
         return Promise.resolve();
@@ -156,6 +156,40 @@ describe(commands.LIST_RETENTIONLABEL_ENSURE, () => {
     assert.strictEqual(lastCall.data.syncToItems, false);
   });
 
+  it('should set label for list (debug)', async () => {
+    const postStub = sinon.stub(request, 'post').callsFake((opts) => {
+      if ((opts.url as string).indexOf(`https://contoso.sharepoint.com/sites/team1/_api/SP_CompliancePolicy_SPPolicyStoreProxy_SetListComplianceTag`) > -1) {
+        return Promise.resolve();
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    sinon.stub(request, 'get').callsFake((opts) => {
+      if (opts.url === `https://contoso.sharepoint.com/sites/team1/_api/web/lists/getByTitle('MyLibrary')/?$expand=RootFolder&$select=RootFolder`) {
+        return Promise.resolve({ "RootFolder": { "Exists": true, "IsWOPIEnabled": false, "ItemCount": 0, "Name": "MyLibrary", "ProgID": null, "ServerRelativeUrl": "/sites/team1/MyLibrary", "TimeCreated": "2019-01-11T10:03:19Z", "TimeLastModified": "2019-01-11T10:03:20Z", "UniqueId": "faaa6af2-0157-4e9a-a352-6165195923c8", "WelcomePage": "" } }
+        );
+      }
+
+      return Promise.reject('Invalid request');
+    });
+
+    await command.action(logger, {
+      options: {
+        debug: true,
+        webUrl: 'https://contoso.sharepoint.com/sites/team1',
+        listTitle: 'MyLibrary',
+        name: 'abc'
+      }
+    });
+    const lastCall = postStub.lastCall.args[0];
+    assert.strictEqual(lastCall.data.listUrl, 'https://contoso.sharepoint.com/sites/team1/MyLibrary');
+    assert.strictEqual(lastCall.data.complianceTagValue, 'abc');
+    assert.strictEqual(lastCall.data.blockDelete, false);
+    assert.strictEqual(lastCall.data.blockEdit, false);
+    assert.strictEqual(lastCall.data.syncToItems, false);
+  });
+
   it('should set label for list using listId (debug)', async () => {
     const postStub = sinon.stub(request, 'post').callsFake((opts) => {
       if ((opts.url as string).indexOf(`https://contoso.sharepoint.com/sites/team1/_api/SP_CompliancePolicy_SPPolicyStoreProxy_SetListComplianceTag`) > -1) {
@@ -178,7 +212,7 @@ describe(commands.LIST_RETENTIONLABEL_ENSURE, () => {
       options: {
         webUrl: 'https://contoso.sharepoint.com/sites/team1',
         listId: '4d535433-2a7b-40b0-9dad-8f0f8f3b3841',
-        label: 'abc'
+        name: 'abc'
       }
     });
     const lastCall = postStub.lastCall.args[0];
@@ -202,7 +236,7 @@ describe(commands.LIST_RETENTIONLABEL_ENSURE, () => {
       options: {
         webUrl: 'https://contoso.sharepoint.com/sites/team1',
         listUrl: 'MyLibrary',
-        label: 'abc',
+        name: 'abc',
         blockDelete: true,
         blockEdit: true,
         syncToItems: true
@@ -222,33 +256,22 @@ describe(commands.LIST_RETENTIONLABEL_ENSURE, () => {
   });
 
   it('passes validation if the url option is a valid SharePoint site URL', async () => {
-    const actual = await command.validate({ options: { webUrl: 'https://contoso.sharepoint.com', label: 'abc', listId: '0CD891EF-AFCE-4E55-B836-FCE03286CCCF' } }, commandInfo);
+    const actual = await command.validate({ options: { webUrl: 'https://contoso.sharepoint.com', name: 'abc', listId: '0CD891EF-AFCE-4E55-B836-FCE03286CCCF' } }, commandInfo);
     assert(actual);
   });
 
   it('fails validation if the listid option is not a valid GUID', async () => {
-    const actual = await command.validate({ options: { webUrl: 'https://contoso.sharepoint.com', label: 'abc', listId: 'XXXXX' } }, commandInfo);
+    const actual = await command.validate({ options: { webUrl: 'https://contoso.sharepoint.com', name: 'abc', listId: 'XXXXX' } }, commandInfo);
     assert.notStrictEqual(actual, true);
   });
 
   it('passes validation if the listid option is a valid GUID', async () => {
-    const actual = await command.validate({ options: { webUrl: 'https://contoso.sharepoint.com', label: 'abc', listId: 'cc27a922-8224-4296-90a5-ebbc54da2e85' } }, commandInfo);
+    const actual = await command.validate({ options: { webUrl: 'https://contoso.sharepoint.com', name: 'abc', listId: 'cc27a922-8224-4296-90a5-ebbc54da2e85' } }, commandInfo);
     assert(actual);
   });
 
   it('fails validation if listId, listUrl and listTitle options are not passed', async () => {
-    const actual = await command.validate({ options: { webUrl: 'https://contoso.sharepoint.com', label: 'abc' } }, commandInfo);
+    const actual = await command.validate({ options: { webUrl: 'https://contoso.sharepoint.com', name: 'abc' } }, commandInfo);
     assert.notStrictEqual(actual, true);
-  });
-
-  it('supports debug mode', () => {
-    const options = command.options;
-    let containsDebugOption = false;
-    options.forEach(o => {
-      if (o.option === '--debug') {
-        containsDebugOption = true;
-      }
-    });
-    assert(containsDebugOption);
   });
 });
