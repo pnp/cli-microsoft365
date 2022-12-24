7a68844e049ba660fb06f946dc057bac51324dad
Nico De Cleyre <nico.de.cleyre@gmail.com>
2022-12-24 15:41:13 +0100

    Adds command spo listitem retentionlabel record lock

---
+++ src/m365/spo/commands/listitem/listitem-retentionlabel-record-lock.spec.ts
@@ -0,0 +1,210 @@
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import appInsights from '../../../../appInsights';
+import auth from '../../../../Auth';
+import { Cli } from '../../../../cli/Cli';
+import { CommandInfo } from '../../../../cli/CommandInfo';
+import { Logger } from '../../../../cli/Logger';
+import Command, { CommandError } from '../../../../Command';
+import request from '../../../../request';
+import { pid } from '../../../../utils/pid';
+import { sinonUtil } from '../../../../utils/sinonUtil';
+import commands from '../../commands';
+const command: Command = require('./listitem-retentionlabel-record-lock');
+
+describe(commands.LISTITEM_RETENTIONLABEL_RECORD_LOCK, () => {
+  let log: any[];
+  let logger: Logger;
+  let commandInfo: CommandInfo;
+  const listUrl = "/MyLibrary";
+  const listTitle = "MyLibrary";
+  const listId = "cc27a922-8224-4296-90a5-ebbc54da2e85";
+  const webUrl = "https://contoso.sharepoint.com";
+  const listResponse = {
+    "RootFolder": {
+      "Exists": true,
+      "IsWOPIEnabled": false,
+      "ItemCount": 0,
+      "Name": listTitle,
+      "ProgID": null,
+      "ServerRelativeUrl": listUrl,
+      "TimeCreated": "2019-01-11T10:03:19Z",
+      "TimeLastModified": "2019-01-11T10:03:20Z",
+      "UniqueId": listId,
+      "WelcomePage": ""
+    }
+  };
+
+  before(() => {
+    sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
+    sinon.stub(appInsights, 'trackEvent').callsFake(() => { });
+    auth.service.connected = true;
+    commandInfo = Cli.getCommandInfo(command);
+  });
+
+  beforeEach(() => {
+    log = [];
+    logger = {
+      log: (msg: string) => {
+        log.push(msg);
+      },
+      logRaw: (msg: string) => {
+        log.push(msg);
+      },
+      logToStderr: (msg: string) => {
+        log.push(msg);
+      }
+    };
+  });
+
+  afterEach(() => {
+    sinonUtil.restore([
+      request.get,
+      request.post
+    ]);
+  });
+
+  after(() => {
+    sinonUtil.restore([
+      auth.restoreAuth,
+      appInsights.trackEvent,
+      pid.getProcessName
+    ]);
+    auth.service.connected = false;
+  });
+
+  it('has correct name', () => {
+    assert.strictEqual(command.name, commands.LISTITEM_RETENTIONLABEL_RECORD_LOCK);
+  });
+
+  it('has a description', () => {
+    assert.notStrictEqual(command.description, null);
+  });
+
+  it('locks a list item based on listUrl (debug)', async () => {
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `https://contoso.sharepoint.com/_api/SP.CompliancePolicy.SPPolicyStoreProxy.LockRecordItem()`) {
+        return;
+      }
+
+      throw 'Invalid request';
+    });
+
+    await assert.doesNotReject(command.action(logger, {
+      options: {
+        debug: true,
+        listUrl: listUrl,
+        webUrl: webUrl,
+        listItemId: 1
+      }
+    }));
+  });
+
+  it('locks a list item based on listTitle', async () => {
+    sinon.stub(request, 'get').callsFake(async (opts) => {
+      if (opts.url === `https://contoso.sharepoint.com/_api/web/lists/getByTitle('${listTitle}')/?$expand=RootFolder&$select=RootFolder`) {
+        return listResponse;
+      }
+
+      throw 'Invalid request';
+    });
+
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `https://contoso.sharepoint.com/_api/SP.CompliancePolicy.SPPolicyStoreProxy.LockRecordItem()`) {
+        return;
+      }
+
+      throw 'Invalid request';
+    });
+
+    await assert.doesNotReject(command.action(logger, {
+      options: {
+        debug: true,
+        listTitle: listTitle,
+        webUrl: webUrl,
+        listItemId: 1
+      }
+    }));
+  });
+
+  it('locks a list item based on listId', async () => {
+    sinon.stub(request, 'get').callsFake(async (opts) => {
+      if (opts.url === `https://contoso.sharepoint.com/_api/web/lists(guid'${listId}')/?$expand=RootFolder&$select=RootFolder`) {
+        return listResponse;
+      }
+
+      throw 'Invalid request';
+    });
+
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `https://contoso.sharepoint.com/_api/SP.CompliancePolicy.SPPolicyStoreProxy.LockRecordItem()`) {
+        return;
+      }
+
+      throw 'Invalid request';
+    });
+
+    await assert.doesNotReject(command.action(logger, {
+      options: {
+        debug: true,
+        listId: listId,
+        webUrl: webUrl,
+        listItemId: 1
+      }
+    }));
+  });
+
+  it('correctly handles API OData error', async () => {
+    const errorMessage = 'Something went wrong';
+
+    sinon.stub(request, 'post').callsFake(async () => { throw { error: { error: { message: errorMessage } } }; });
+
+    await assert.rejects(command.action(logger, {
+      options: {
+        listUrl: listUrl,
+        webUrl: webUrl,
+        listItemId: 1
+      }
+    }), new CommandError(errorMessage));
+  });
+
+  it('fails validation if both id and title options are not passed', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listItemId: 1 } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if the url option is not a valid SharePoint site URL', async () => {
+    const actual = await command.validate({ options: { webUrl: 'foo', listItemId: 1, listTitle: listTitle } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('passes validation if the url option is a valid SharePoint site URL', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listId: listId, listItemId: 1 } }, commandInfo);
+    assert(actual);
+  });
+
+  it('fails validation if the id option is not a valid GUID', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listId: '12345', listItemId: 1 } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('passes validation if the id option is a valid GUID', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listId: listId, listItemId: 1 } }, commandInfo);
+    assert(actual);
+  });
+
+  it('fails validation if both id and title options are passed', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listId: listId, listTitle: listTitle, listItemId: 1 } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if id is not passed', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if id is not a number', async () => {
+    const actual = await command.validate({ options: { webUrl: webUrl, listItemId: 'abc', listTitle: listTitle } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+});
