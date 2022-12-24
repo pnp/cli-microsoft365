123839c87991f8a6e00589254f926b4752170244
Milan Holemans <11723921+milanholemans@users.noreply.github.com>
2022-12-24 19:06:04 +0100

    Adds 'purview auditlog list' command. Closes #4232

--- src/m365/purview/commands.ts
+++ src/m365/purview/commands.ts
@@ -1,6 +1,7 @@
 const prefix: string = 'purview';
 
 export default {
+  AUDITLOG_LIST: `${prefix} auditlog list`,
   RETENTIONLABEL_GET: `${prefix} retentionlabel get`,
   RETENTIONLABEL_LIST: `${prefix} retentionlabel list`,
   RETENTIONLABEL_REMOVE: `${prefix} retentionlabel remove`,
