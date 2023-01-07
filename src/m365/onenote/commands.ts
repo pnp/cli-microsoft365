3f75bb6331d0dbe43853897cc1f22f856816bd6e
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2023-01-07 16:48:36 +0100

    Adds `onenote page list` command. Closes #4001

--- src/m365/onenote/commands.ts
+++ src/m365/onenote/commands.ts
@@ -1,5 +1,6 @@
 const prefix: string = 'onenote';
 
 export default {
-  NOTEBOOK_LIST: `${prefix} notebook list`
+  NOTEBOOK_LIST: `${prefix} notebook list`,
+  PAGE_LIST: `${prefix} page list`
 };
