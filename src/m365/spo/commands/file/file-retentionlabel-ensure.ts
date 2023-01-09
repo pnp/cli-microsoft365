a22d0f2529c65801a73cf2c29525c4d17666d527
Nico De Cleyre <nico.de.cleyre@gmail.com>
2023-01-09 13:13:51 +0100

    first commit

---
+++ src/m365/spo/commands/file/file-retentionlabel-ensure.ts
@@ -0,0 +1,139 @@
+import { AxiosRequestConfig } from 'axios';
+import { Cli } from '../../../../cli/Cli';
+import { Logger } from '../../../../cli/Logger';
+import Command from '../../../../Command';
+import GlobalOptions from '../../../../GlobalOptions';
+import request from '../../../../request';
+import { validation } from '../../../../utils/validation';
+import SpoCommand from '../../../base/SpoCommand';
+import commands from '../../commands';
+import { FileProperties } from './FileProperties';
+import { Options as SpoListItemRetentionLabelEnsureCommandOptions } from '../listitem/listitem-retentionlabel-ensure';
+import * as SpoListItemRetentionLabelEnsureCommand from '../listitem/listitem-retentionlabel-ensure';
+import { formatting } from '../../../../utils/formatting';
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  webUrl: string;
+  name: string;
+  fileUrl?: string;
+  fileId?: string;
+}
+
+class SpoFileRetentionLabelEnsureCommand extends SpoCommand {
+  public get name(): string {
+    return commands.FILE_RETENTIONLABEL_ENSURE;
+  }
+
+  public get description(): string {
+    return 'Clear the retention label from a file';
+  }
+
+  constructor() {
+    super();
+
+    this.#initTelemetry();
+    this.#initOptions();
+    this.#initValidators();
+    this.#initOptionSets();
+  }
+
+  #initTelemetry(): void {
+    this.telemetry.push((args: CommandArgs) => {
+      Object.assign(this.telemetryProperties, {
+        fileUrl: typeof args.options.fileUrl !== 'undefined',
+        fileId: typeof args.options.fileId !== 'undefined'
+      });
+    });
+  }
+
+  #initOptions(): void {
+    this.options.unshift(
+      {
+        option: '-u, --webUrl <webUrl>'
+      },
+      {
+        option: '--name <name>'
+      },
+      {
+        option: '--fileUrl [fileUrl]'
+      },
+      {
+        option: '-i, --fileId [fileId]'
+      }
+    );
+  }
+
+  #initValidators(): void {
+    this.validators.push(
+      async (args: CommandArgs) => {
+        const isValidSharePointUrl: boolean | string = validation.isValidSharePointUrl(args.options.webUrl);
+        if (isValidSharePointUrl !== true) {
+          return isValidSharePointUrl;
+        }
+
+        if (args.options.fileId &&
+          !validation.isValidGuid(args.options.fileId as string)) {
+          return `${args.options.fileId} is not a valid GUID`;
+        }
+
+        return true;
+      }
+    );
+  }
+
+  #initOptionSets(): void {
+    this.optionSets.push({ options: ['fileUrl', 'fileId'] });
+  }
+
+  public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
+    if (this.verbose) {
+      logger.logToStderr(`Removing retention label from file ${args.options.fileId || args.options.fileUrl} in site at ${args.options.webUrl}...`);
+    }
+    try {
+      const fileProperties = await this.getFileProperties(args);
+      const options: SpoListItemRetentionLabelEnsureCommandOptions = {
+        webUrl: args.options.webUrl,
+        listUrl: fileProperties.listServerRelativeUrl,
+        listItemId: fileProperties.id,
+        name: args.options.name,
+        output: 'json',
+        debug: this.debug,
+        verbose: this.verbose
+      };
+
+      await Cli.executeCommandWithOutput(SpoListItemRetentionLabelEnsureCommand as Command, { options: { ...options, _: [] } });
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+
+  private async getFileProperties(args: CommandArgs): Promise<{ id: string, listServerRelativeUrl: string }> {
+    const requestOptions: AxiosRequestConfig = {
+      headers: {
+        'accept': 'application/json;odata=nometadata'
+      },
+      responseType: 'json'
+    };
+
+    if (args.options.fileId) {
+      requestOptions.url = `${args.options.webUrl}/_api/web/GetFileById('${args.options.fileId}')?$expand=ListItemAllFields`;
+    }
+    else {
+      requestOptions.url = `${args.options.webUrl}/_api/web/GetFileByServerRelativeUrl('${formatting.encodeQueryParameter(args.options.fileUrl!)}')?$expand=ListItemAllFields`;
+    }
+
+    const response = await request.get<FileProperties>(requestOptions);
+    return { id: response.ListItemAllFields.Id, listServerRelativeUrl: this.getListServerRelativeUrl(response.ServerRelativeUrl) };
+  }
+
+  private getListServerRelativeUrl(fileUrl: string): string {
+    return fileUrl.replace(/\/[^\/]+$/, '');
+  }
+}
+
+module.exports = new SpoFileRetentionLabelEnsureCommand();
