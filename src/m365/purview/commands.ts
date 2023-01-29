fbe036feaa6e2dd8f623b677bc13bb28c71bf2c3
Nanddeep Nachan <nanddeepnachan@gmail.com>
2023-01-29 09:58:27 +0000

    purview retentioneventtype get

--- src/m365/purview/commands.ts
+++ src/m365/purview/commands.ts
@@ -1,6 +1,7 @@
 const prefix: string = 'purview';
 
 export default {
+  RETENTIONEVENTTYPE_GET: `${prefix} retentioneventtype get`,
   RETENTIONLABEL_ADD: `${prefix} retentionlabel add`,
   RETENTIONLABEL_GET: `${prefix} retentionlabel get`,
   RETENTIONLABEL_LIST: `${prefix} retentionlabel list`,
---
+++ src/m365/purview/commands/retentioneventtype/retentioneventtype-get.ts
@@ -0,0 +1,75 @@
+import { Logger } from '../../../../cli/Logger';
+import GraphCommand from '../../../base/GraphCommand';
+import GlobalOptions from '../../../../GlobalOptions';
+import commands from '../../commands';
+import request, { CliRequestOptions } from '../../../../request';
+import { validation } from '../../../../utils/validation';
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  id: string;
+}
+
+class PurviewRetentionEventTypeGetCommand extends GraphCommand {
+  public get name(): string {
+    return commands.RETENTIONEVENTTYPE_GET;
+  }
+
+  public get description(): string {
+    return 'Retrieve the specified retention event type';
+  }
+
+  constructor() {
+    super();
+
+    this.#initOptions();
+    this.#initValidators();
+  }
+
+  #initOptions(): void {
+    this.options.unshift(
+      {
+        option: '-i, --id <id>'
+      }
+    );
+  }
+
+  #initValidators(): void {
+    this.validators.push(
+      async (args: CommandArgs) => {
+        if (!validation.isValidGuid(args.options.id)) {
+          return `'${args.options.id}' is not a valid GUID.`;
+        }
+
+        return true;
+      }
+    );
+  }
+
+  public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
+    try {
+      if (this.verbose) {
+        logger.logToStderr(`Retrieving retention event type with id ${args.options.id}`);
+      }
+
+      const requestOptions: CliRequestOptions = {
+        url: `${this.resource}/beta/security/triggerTypes/retentionEventTypes/${args.options.id}`,
+        headers: {
+          accept: 'application/json;odata.metadata=none'
+        },
+        responseType: 'json'
+      };
+
+      const res: any = await request.get<any>(requestOptions);
+      logger.log(res);
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+}
+
+module.exports = new PurviewRetentionEventTypeGetCommand();
