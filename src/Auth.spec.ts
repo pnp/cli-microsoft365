c6550ccc9c1cc5a3ba91deef36a14491385069ee
alanc-msft <alanc@microsoft.com>
2023-02-07 11:46:38 -0800

    Adds coverage for other new case

--- src/Auth.spec.ts
+++ src/Auth.spec.ts
@@ -2057,6 +2057,10 @@ describe('Auth', () => {
     assert.strictEqual(Auth.getResourceFromUrl('https://contoso.sharepoint.com/sites/team-a'), 'https://contoso.sharepoint.com');
   });
 
+  it('correctly retrieves resource for https://api.powerapps.com', () => {
+    assert.strictEqual(Auth.getResourceFromUrl('https://api.powerapps.com'), 'https://service.powerapps.com/');
+  });
+
   it('correctly retrieves resource for https://api.bap.microsoft.com', () => {
     assert.strictEqual(Auth.getResourceFromUrl('https://api.bap.microsoft.com'), 'https://api.bap.microsoft.com/');
   });
