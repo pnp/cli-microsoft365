ba13642054c897d0121076e19d59fa543199e6b3
Nico De Cleyre <nico.de.cleyre@gmail.com>
2023-02-07 21:01:09 +0100

    Adds command context option list

---
+++ src/m365/context/commands/option/option-list.spec.ts
@@ -0,0 +1,103 @@
+import * as assert from 'assert';
+import * as fs from 'fs';
+import * as sinon from 'sinon';
+import { Cli } from '../../../../cli/Cli';
+import { Logger } from '../../../../cli/Logger';
+import Command, { CommandError } from '../../../../Command';
+import { telemetry } from '../../../../telemetry';
+import { sinonUtil } from '../../../../utils/sinonUtil';
+import commands from '../../commands';
+const command: Command = require('./option-list');
+
+describe(commands.OPTION_LIST, () => {
+  let log: any[];
+  let logger: Logger;
+  let loggerLogSpy: sinon.SinonSpy;
+
+  before(() => {
+    sinon.stub(telemetry, 'trackEvent').callsFake(() => { });
+  });
+
+  beforeEach(() => {
+    log = [];
+    logger = {
+      log: (msg: string) => {
+        log.push(msg);
+      },
+      logRaw: (msg: string) => {
+        log.push(msg);
+      },
+      logToStderr: (msg: string) => {
+        log.push(msg);
+      }
+    };
+    loggerLogSpy = sinon.spy(logger, 'log');
+  });
+
+  afterEach(() => {
+    sinonUtil.restore([
+      fs.existsSync,
+      fs.readFileSync
+    ]);
+  });
+
+  after(() => {
+    sinonUtil.restore([
+      telemetry.trackEvent
+    ]);
+  });
+
+  it('has correct name', () => {
+    assert.strictEqual(command.name, commands.OPTION_LIST);
+  });
+
+  it('has a description', () => {
+    assert.notStrictEqual(command.description, null);
+  });
+
+  it('handles an error when reading file contents fails', async () => {
+    sinon.stub(fs, 'existsSync').callsFake(_ => true);
+    sinon.stub(fs, 'readFileSync').callsFake(_ => { throw new Error('An error has occurred'); });
+
+    await assert.rejects(command.action(logger, { options: { debug: true } }), new CommandError(`Error reading .m365rc.json: Error: An error has occurred. Please retrieve context options from .m365rc.json manually.`));
+  });
+
+  it(`retrieves context info options from the existing .m365rc.json file`, async () => {
+    sinonUtil.restore(Cli.prompt);
+    sinon.stub(Cli, 'prompt').callsFake(async () => (
+      { continue: true }
+    ));
+
+    sinon.stub(fs, 'existsSync').callsFake(_ => true);
+    sinon.stub(fs, 'readFileSync').callsFake(_ => JSON.stringify({
+      "apps": [
+        {
+          "appId": "e23d235c-fcdf-45d1-ac5f-24ab2ee0695d",
+          "name": "CLI app"
+        }
+      ],
+      "context": {
+        "listName": "listNameValue"
+      }
+    }));
+
+    await command.action(logger, { options: { verbose: true } });
+    assert(loggerLogSpy.calledWith({ "listName": "listNameValue" }));
+    await assert.doesNotReject(command.action(logger, { options: { debug: true, name: 'listName' } }));
+  });
+
+  it('handles an error when context is not present in the .m365rc.json file', async () => {
+    sinon.stub(fs, 'existsSync').callsFake(_ => true);
+    sinon.stub(fs, 'readFileSync').callsFake(_ => JSON.stringify({
+      "apps": [
+        {
+          "appId": "e23d235c-fcdf-45d1-ac5f-24ab2ee0695d",
+          "name": "CLI app"
+        }
+      ]
+    }));
+
+    await assert.rejects(command.action(logger, { options: { debug: true, name: 'listName', confirm: true } }), new CommandError(`No context present`));
+  });
+
+});
