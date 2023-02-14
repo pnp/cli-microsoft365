7ccea08e767219900de3502587457a3d8b556dd0
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2023-02-14 22:50:30 +0100

    Adds command 'aad user recyclebinitem list'. Closes #4498

--- src/m365/aad/commands.ts
+++ src/m365/aad/commands.ts
@@ -60,6 +60,7 @@ export default {
   USER_LIST: `${prefix} user list`,
   USER_PASSWORD_VALIDATE: `${prefix} user password validate`,
   USER_RECYCLEBINITEM_CLEAR: `${prefix} user recyclebinitem clear`,
+  USER_RECYCLEBINITEM_LIST: `${prefix} user recyclebinitem list`,
   USER_RECYCLEBINITEM_REMOVE: `${prefix} user recyclebinitem remove`,
   USER_REMOVE: `${prefix} user remove`,
   USER_SET: `${prefix} user set`,
