8cebdeec961c1118565da163a81acc1337d6eb71
Nanddeep Nachan <nanddeepnachan@gmail.com>
2023-02-19 10:45:49 +0000

    Adds command 'aad user license list'. Closes #4502

--- src/m365/aad/commands.ts
+++ src/m365/aad/commands.ts
@@ -57,6 +57,7 @@ export default {
   USER_GET: `${prefix} user get`,
   USER_GUEST_ADD: `${prefix} user guest add`,
   USER_HIBP: `${prefix} user hibp`,
+  USER_LICENSE_LIST: `${prefix} user license list`,
   USER_LIST: `${prefix} user list`,
   USER_PASSWORD_VALIDATE: `${prefix} user password validate`,
   USER_RECYCLEBINITEM_CLEAR: `${prefix} user recyclebinitem clear`,
