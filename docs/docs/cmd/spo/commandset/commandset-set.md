ee962d1b3a9c40d5c6b1c1cd22bc80fa84b62586
Nico De Cleyre <nico.de.cleyre@gmail.com>
2023-02-22 15:44:26 +0100

    Adds command spo commandset set

---
+++ docs/docs/cmd/spo/commandset/commandset-set.md
@@ -0,0 +1,71 @@
+# spo commandset set
+
+Updates a ListView Command Set on a site.
+
+## Usage
+
+```sh
+m365 spo commandset set [options]
+```
+
+## Options
+
+`-u, --webUrl <webUrl>`
+: The site to update the ListView Command Set on.
+
+`-t, --title [title]`
+: The title of the ListView Command Set to update. Specify either `title`, `id`or `clientSideComponentId`.
+
+`-i, --id [id]`
+: The id of the ListView Command Set to update. Specify either `title`, `id`or `clientSideComponentId`.
+
+`-c, --clientSideComponentId  [clientSideComponentId]`
+: The Client Side Component Id (GUID) of the ListView Command Set to update. Specify either `title`, `id`or `clientSideComponentId`.
+
+`--newTitle [newTitle]`
+: The new title of the ListView Command Set.
+
+`-l, --listType [listType]`
+: The list or library type to register the Command Set on. Allowed values `List` or `Library`.
+
+`--clientSideComponentProperties  [clientSideComponentProperties]`
+: The Client Side Component properties of the ListView Command Set.
+
+`-s, --scope [scope]`
+: The scope where to lookup the ListView Command Set: at site level or web level. Allowed values: `Site`, `Web`, `All`. Defaults to `All`.
+
+`--location [location]`
+: The location of the ListView Command Set. Allowed values `ContextMenu`, `CommandBar` or `Both`.
+
+--8<-- "docs/cmd/_global.md"
+
+## Remarks
+
+Running this command from the Windows Command Shell (cmd.exe) or PowerShell for Windows OS XP, 7, 8, 8.1 without bash installed might require additional formatting for command options that have JSON, XML or JavaScript values because the command shell treat quotes differently. For example, this is how ApplicationCustomizer user custom action can be created from the Windows cmd.exe:
+
+```sh
+m365 spo commandset set --webUrl https://contoso.sharepoint.com/sites/test --title "CLI Commandset" --location "ClientSideExtension.ApplicationCustomizer" --listType "List" --clientSideComponentProperties '{\"testMessage\":\"Test message\"}'
+```
+
+Note, how the clientSideComponentProperties option has escaped double quotes `'{\"testMessage\":\"Test message\"}'` compared to execution from bash `'{"testMessage":"Test message"}'`.
+
+!!! warning "Escaping JSON in PowerShell"
+    When using the `--clientSideComponentProperties` option it's possible to enter a JSON string. In PowerShell 5 to 7.2 [specific escaping rules](./../../../user-guide/using-cli.md#escaping-double-quotes-in-powershell) apply due to an issue. Remember that you can also use [file tokens](./../../../user-guide/using-cli.md#passing-complex-content-into-cli-options) instead.
+
+## Examples
+
+Updates the title and location of a ListView Command Set on the sales site.
+
+```sh
+m365 spo commandset  set --clientSideComponentId  799883f5-7962-4384-a10a-105adaec6ffc --newTitle "Some new title" --location Both --webUrl https://contoso.sharepoint.com/sites/sales --scope Site
+```
+
+Updates a ListView Command Set on the sales site with some properties.
+
+```sh
+m365 spo commandset set --title "Some customizer" --clientSideComponentProperties '{ "someProperty": "Some value" }' --webUrl https://contoso.sharepoint.com/sites/sales --scope Site
+```
+
+## Response
+
+The command won't return a response on success.
---
+++ src/m365/spo/commands/commandset/commandset-set.ts
@@ -0,0 +1,221 @@
+import { Logger } from '../../../../cli/Logger';
+import GlobalOptions from '../../../../GlobalOptions';
+import commands from '../../commands';
+import { validation } from '../../../../utils/validation';
+import SpoCommand from '../../../base/SpoCommand';
+import request from '../../../../request';
+import { CustomAction } from '../customaction/customaction';
+import { formatting } from '../../../../utils/formatting';
+import { spo } from '../../../../utils/spo';
+
+interface CommandArgs {
+  options: Options;
+}
+
+export interface Options extends GlobalOptions {
+  webUrl: string;
+  title?: string;
+  id?: string;
+  clientSideComponentId?: string;
+  newTitle: string;
+  listType?: string;
+  clientSideComponentProperties?: string;
+  scope?: string;
+  location?: string;
+}
+
+class SpoCommandsetSetCommand extends SpoCommand {
+  private static readonly listTypes: string[] = ['List', 'Library'];
+  private static readonly scopes: string[] = ['All', 'Site', 'Web'];
+  private static readonly locations: string[] = ['ContextMenu', 'CommandBar', 'Both'];
+
+  public get name(): string {
+    return commands.COMMANDSET_SET;
+  }
+
+  public get description(): string {
+    return 'Updates a ListView Command Set on a site.';
+  }
+
+  constructor() {
+    super();
+
+    this.#initTelemetry();
+    this.#initOptions();
+    this.#initValidators();
+    this.#initOptionSets();
+  }
+
+  #initTelemetry(): void {
+    this.telemetry.push((args: CommandArgs) => {
+      Object.assign(this.telemetryProperties, {
+        clientSideComponentProperties: typeof args.options.clientSideComponentProperties !== 'undefined',
+        scope: typeof args.options.scope !== 'undefined',
+        location: typeof args.options.location !== 'undefined'
+      });
+    });
+  }
+
+  #initOptions(): void {
+    this.options.unshift(
+      {
+        option: '-u, --webUrl <webUrl>'
+      },
+      {
+        option: '-t, --title [title]'
+      },
+      {
+        option: '-i, --id [id]'
+      },
+      {
+        option: '-c, --clientSideComponentId  [clientSideComponentId]'
+      },
+      {
+        option: '--newTitle [newTitle]'
+      },
+      {
+        option: '-l, --listType [listType]', autocomplete: SpoCommandsetSetCommand.listTypes
+      },
+      {
+        option: '--clientSideComponentProperties  [clientSideComponentProperties]'
+      },
+      {
+        option: '-s, --scope [scope]', autocomplete: SpoCommandsetSetCommand.scopes
+      },
+      {
+        option: '--location [location]', autocomplete: SpoCommandsetSetCommand.locations
+      }
+    );
+  }
+
+  #initValidators(): void {
+    this.validators.push(
+      async (args: CommandArgs) => {
+        if (args.options.id && !validation.isValidGuid(args.options.id as string)) {
+          return `${args.options.id} is not a valid GUID`;
+        }
+
+        if (args.options.clientSideComponentId && !validation.isValidGuid(args.options.clientSideComponentId as string)) {
+          return `${args.options.clientSideComponentId} is not a valid GUID`;
+        }
+
+        if (args.options.listType && SpoCommandsetSetCommand.listTypes.indexOf(args.options.listType) < 0) {
+          return `${args.options.listType} is not a valid list type. Allowed values are ${SpoCommandsetSetCommand.listTypes.join(', ')}`;
+        }
+
+        if (args.options.scope && SpoCommandsetSetCommand.scopes.indexOf(args.options.scope) < 0) {
+          return `${args.options.scope} is not a valid scope. Allowed values are ${SpoCommandsetSetCommand.scopes.join(', ')}`;
+        }
+
+        if (args.options.location && SpoCommandsetSetCommand.locations.indexOf(args.options.location) < 0) {
+          return `${args.options.location} is not a valid location. Allowed values are ${SpoCommandsetSetCommand.locations.join(', ')}`;
+        }
+
+        if (!args.options.newTitle && !args.options.listType && !args.options.clientSideComponentProperties && !args.options.location) {
+          return `Please specify option to be updated`;
+        }
+
+        return validation.isValidSharePointUrl(args.options.webUrl);
+      }
+    );
+  }
+
+  #initOptionSets(): void {
+    this.optionSets.push(
+      { options: ['id', 'title', 'clientSideComponentId'] }
+    );
+  }
+
+  public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
+    if (this.verbose) {
+      logger.logToStderr(`Updating ListView Command Set ${args.options.clientSideComponentId} to site '${args.options.webUrl}'...`);
+    }
+
+    if (!args.options.scope) {
+      args.options.scope = 'Site';
+    }
+
+    const location: string = this.getLocation(args.options.location ? args.options.location : '');
+
+    try {
+      const requestBody: any = {
+      };
+
+      if (args.options.newTitle) {
+        requestBody.Title = args.options.newTitle;
+      }
+
+      if (args.options.location) {
+        requestBody.Location = location;
+      }
+
+      if (args.options.listType) {
+        requestBody.RegistrationId = args.options.listType === 'List' ? "100" : "101";
+      }
+
+      if (args.options.clientSideComponentProperties) {
+        requestBody.ClientSideComponentProperties = args.options.clientSideComponentProperties;
+      }
+
+      const id = await this.getCommandSetId(args.options);
+
+      const requestOptions: any = {
+        url: `${args.options.webUrl}/_api/${args.options.scope}/UserCustomActions('${formatting.encodeQueryParameter(id)}')`,
+        headers: {
+          accept: 'application/json;odata=nometadata',
+          'X-HTTP-Method': 'MERGE'
+        },
+        data: requestBody,
+        responseType: 'json'
+      };
+
+      await request.post<CustomAction>(requestOptions);
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+
+  private async getCommandSetId(options: Options): Promise<string> {
+    if (options.id) {
+      return options.id;
+    }
+    else if (options.title) {
+      const commandSets: CustomAction[] = await spo.getCustomActions(options.webUrl, options.scope, `(Title eq '${formatting.encodeQueryParameter(options.title as string)}') and (startswith(Location,'ClientSideExtension.ListViewCommandSet'))`);
+
+      if (commandSets.length === 0) {
+        throw `No user commandsets with title '${options.title}' found`;
+      }
+
+      if (commandSets.length > 1) {
+        throw `Multiple user commandsets with title '${options.title}' found. Please disambiguate using IDs: ${commandSets.map(a => a.Id).join(', ')}`;
+      }
+
+      return commandSets[0].Id;
+    }
+    else {
+      const commandSets: CustomAction[] = await spo.getCustomActions(options.webUrl, options.scope, `(ClientSideComponentId eq guid'${options.clientSideComponentId}') and (startswith(Location,'ClientSideExtension.ListViewCommandSet'))`);
+
+      if (commandSets.length === 0) {
+        throw `No user commandsets with ClientSideComponentId '${options.clientSideComponentId}' found`;
+      }
+      if (commandSets.length > 1) {
+        throw `Multiple user commandsets with ClientSideComponentId '${options.clientSideComponentId}' found. Please disambiguate using IDs: ${commandSets.map((commandSet: CustomAction) => commandSet.Id).join(', ')}`;
+      }
+      return commandSets[0].Id;
+    }
+  }
+
+  private getLocation(location: string): string {
+    switch (location) {
+      case 'Both':
+        return 'ClientSideExtension.ListViewCommandSet';
+      case 'ContextMenu':
+        return 'ClientSideExtension.ListViewCommandSet.ContextMenu';
+      default:
+        return 'ClientSideExtension.ListViewCommandSet.CommandBar';
+    }
+  }
+}
+
+module.exports = new SpoCommandsetSetCommand();
