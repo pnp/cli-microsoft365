bd484797f5b5b3996f6a68028ed81dc59cf696c1
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2023-02-25 15:53:51 +0100

    Temp check in

---
+++ src/m365/flow/commands/owner/owner-list.ts
@@ -0,0 +1,85 @@
+import { Logger } from '../../../../cli/Logger';
+import GlobalOptions from '../../../../GlobalOptions';
+import { formatting } from '../../../../utils/formatting';
+import { odata } from '../../../../utils/odata';
+import { validation } from '../../../../utils/validation';
+import AzmgmtCommand from '../../../base/AzmgmtCommand';
+import commands from '../../commands';
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  name: string;
+  environmentName: string;
+  asAdmin?: boolean;
+}
+
+class FlowOwnerListCommand extends AzmgmtCommand {
+  public get name(): string {
+    return commands.OWNER_LIST;
+  }
+
+  public get description(): string {
+    return 'Lists all owners of a Power Automate flow';
+  }
+
+  constructor() {
+    super();
+
+    this.#initTelemetry();
+    this.#initOptions();
+    this.#initValidators();
+  }
+
+  #initTelemetry(): void {
+    this.telemetry.push((args: CommandArgs) => {
+      Object.assign(this.telemetryProperties, {
+        asAdmin: !!args.options.asAdmin
+      });
+    });
+  }
+
+  #initOptions(): void {
+    this.options.unshift(
+      {
+        option: '-e, --environmentName <environmentName>'
+      },
+      {
+        option: '-n, --name <name>'
+      },
+      {
+        option: '--asAdmin'
+      }
+    );
+  }
+
+  #initValidators(): void {
+    this.validators.push(
+      async (args: CommandArgs) => {
+        if (!validation.isValidGuid(args.options.name)) {
+          return `${args.options.name} is not a valid GUID.`;
+        }
+
+        return true;
+      }
+    );
+  }
+
+  public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
+    try {
+      if (this.verbose) {
+        logger.logToStderr(`Listing owners for flow ${args.options.name} in environment ${args.options.environmentName}`);
+      }
+
+      const response = await odata.getAllItems(`${this.resource}providers/Microsoft.ProcessSimple/${args.options.asAdmin ? 'scopes/admin/' : ''}environments/${formatting.encodeQueryParameter(args.options.environmentName)}/flows/${formatting.encodeQueryParameter(args.options.name)}?api-version=2016-11-01`);
+      logger.log(response);
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+}
+
+module.exports = new FlowOwnerListCommand();
