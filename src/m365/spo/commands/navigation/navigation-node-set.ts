4a3ed73afed94ee0e7a7adf1e933c9eeac16719b
Nico De Cleyre <nico.de.cleyre@gmail.com>
2023-02-27 21:26:51 +0100

    Enhances spo navigation commands with linkless labels. Closes #4586

--- src/m365/spo/commands/navigation/navigation-node-set.ts
+++ src/m365/spo/commands/navigation/navigation-node-set.ts
@@ -107,10 +107,16 @@ class SpoNavigationNodeSetCommand extends SpoCommand {
       if (this.verbose) {
         logger.logToStderr(`Setting navigation node...`);
       }
+
+      let url = args.options.url;
+      if (url === '') {
+        url = 'http://linkless.header/';
+      }
+
       const requestBody: any = {
         Title: args.options.title,
         IsExternal: args.options.isExternal,
-        Url: args.options.url
+        Url: url
       };
 
       if (args.options.audienceIds !== undefined) {
