2c738f0e330b4619b1bbe5daf3c37974abad3f24
Nico De Cleyre <nico.de.cleyre@gmail.com>
2023-02-28 17:16:41 +0100

    Adds new command 'spo applicationcustomizer list'. Closes #4251

--- src/m365/spo/commands.ts
+++ src/m365/spo/commands.ts
@@ -13,6 +13,7 @@ export default {
   APP_UNINSTALL: `${prefix} app uninstall`,
   APP_UPGRADE: `${prefix} app upgrade`,
   APPLICATIONCUSTOMIZER_ADD: `${prefix} applicationcustomizer add`,
+  APPLICATIONCUSTOMIZER_LIST: `${prefix} applicationcustomizer list`,
   APPPAGE_ADD: `${prefix} apppage add`,
   APPPAGE_SET: `${prefix} apppage set`,
   CDN_GET: `${prefix} cdn get`,
