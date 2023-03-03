4899b12a5ae6713311ebe5e8b0dc508fb2a257dc
Nico De Cleyre <nico.de.cleyre@gmail.com>
2023-03-03 16:54:49 +0100

    Adds new command pa app export

---
+++ docs/docs/cmd/pa/app/app-export.md
@@ -0,0 +1,68 @@
+# app export
+
+Exports the specified Power App
+
+## Usage
+
+```sh
+m365 app export [options]
+```
+
+## Options
+
+`-i, --id <id>`
+: The id of the Power App to export
+
+`-e, --environment <environment>`
+: The name of the environment for which to export the app
+
+`-n, --packageDisplayName [packageDisplayName]`
+: The display name to use in the exported package
+
+`-d, --packageDescription [packageDescription]`
+: The description to use in the exported package
+
+`-c, --packageCreatedBy [packageCreatedBy]`
+: The name of the person to be used as the creator of the exported package
+
+`-s, --packageSourceEnvironment [packageSourceEnvironment]`
+: The name of the source environment from which the exported package was taken
+
+`-p, --path [path]`
+: The path to save the exported package to
+
+--8<-- "docs/cmd/_global.md"
+
+## Examples
+
+Export the specified Power App as a ZIP file
+
+```sh
+m365 app export --environment Default-d87a7535-dd31-4437-bfe1-95340acd55c5 --id 3989cb59-ce1a-4a5c-bb78-257c5c39381d --packageDisplayName "PowerApp" --packageDescription "Power App Description" --packageCreatedBy "John Doe" --packageSourceEnvironment "Contoso" --path "C:/Users/John/Documents"
+```
+
+## Response
+
+=== "JSON"
+
+    ```json
+    ./PowerApp.zip
+    ```
+
+=== "Text"
+
+    ```text
+    ./PowerApp.zip
+    ```
+
+=== "CSV"
+
+    ```csv
+    ./PowerApp.zip
+    ```
+
+=== "Markdown"
+
+    ```md
+    ./PowerApp.zip
+    ```
---
+++ src/m365/pa/commands/app/app-export.ts
@@ -0,0 +1,238 @@
+import { Logger } from '../../../../cli/Logger';
+import GlobalOptions from '../../../../GlobalOptions';
+import { validation } from '../../../../utils/validation';
+import commands from '../../commands';
+import * as fs from 'fs';
+import * as path from 'path';
+import { formatting } from '../../../../utils/formatting';
+import request, { CliRequestOptions } from '../../../../request';
+import PowerPlatformCommand from '../../../base/PowerPlatformCommand';
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  id: string;
+  environment: string;
+  packageDisplayName: string;
+  packageDescription?: string;
+  packageCreatedBy?: string;
+  packageSourceEnvironment?: string;
+  path?: string;
+}
+
+class PaAppExportCommand extends PowerPlatformCommand {
+  private static readonly sleepInterval: number = 5000;
+
+  public get name(): string {
+    return commands.APP_EXPORT;
+  }
+
+  public get description(): string {
+    return 'Exports the specified Power App';
+  }
+
+  constructor() {
+    super();
+
+    this.#initTelemetry();
+    this.#initOptions();
+    this.#initValidators();
+  }
+
+  #initTelemetry(): void {
+    this.telemetry.push((args: CommandArgs) => {
+      Object.assign(this.telemetryProperties, {
+        packageDescription: typeof args.options.packageDescription !== 'undefined',
+        packageCreatedBy: typeof args.options.packageCreatedBy !== 'undefined',
+        packageSourceEnvironment: typeof args.options.packageSourceEnvironment !== 'undefined',
+        path: typeof args.options.path !== 'undefined'
+      });
+    });
+  }
+
+  #initOptions(): void {
+    this.options.unshift(
+      {
+        option: '-i, --id <id>'
+      },
+      {
+        option: '-e, --environment <environment>'
+      },
+      {
+        option: '-n, --packageDisplayName <packageDisplayName>'
+      },
+      {
+        option: '-d, --packageDescription [packageDescription]'
+      },
+      {
+        option: '-c, --packageCreatedBy [packageCreatedBy]'
+      },
+      {
+        option: '-s, --packageSourceEnvironment [packageSourceEnvironment]'
+      },
+      {
+        option: '-p, --path [path]'
+      }
+    );
+  }
+
+  #initValidators(): void {
+    this.validators.push(
+      async (args: CommandArgs) => {
+        if (args.options.id && !validation.isValidGuid(args.options.id)) {
+          return `${args.options.id} is not a valid GUID`;
+        }
+
+        if (args.options.path && !fs.existsSync(path.dirname(args.options.path))) {
+          return 'Specified path where to save the file does not exist';
+        }
+
+        return true;
+      }
+    );
+  }
+
+  public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
+    try {
+      const packageLink = await this.getPackageLink(args, logger);
+      //Replace all illegal characters from the file name
+      const illegalCharsRegEx = /[\\\/:*?"<>|]/g;
+      const filename = args.options.packageDisplayName.replace(illegalCharsRegEx, '_');
+
+      if (this.verbose) {
+        logger.logToStderr(`Name of the Power Apps: ${filename}`);
+      }
+
+      const requestOptions: CliRequestOptions = {
+        url: packageLink,
+        // Set responseType to arraybuffer, otherwise binary data will be encoded
+        // to utf8 and binary data is corrupt
+        responseType: 'arraybuffer',
+        headers: {
+          'x-anonymous': true
+        }
+      };
+
+      const file = await request.get<string>(requestOptions);
+
+      const path = `${args.options.path ? args.options.path : './'}${args.options.packageDisplayName}.zip`;
+
+      fs.writeFileSync(path, file, 'binary');
+      if (!args.options.path || this.verbose) {
+        if (this.verbose) {
+          logger.logToStderr(`File saved to path '${path}'`);
+        }
+        else {
+          logger.log(path);
+        }
+      }
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+
+  private async getPackageResources(args: CommandArgs, logger: Logger): Promise<any> {
+    if (this.verbose) {
+      logger.logToStderr('Getting the Microsoft Power App resources...');
+    }
+    const requestOptions: CliRequestOptions = {
+      url: `${this.resource}/providers/Microsoft.BusinessAppPlatform/environments/${formatting.encodeQueryParameter(args.options.environment)}/listPackageResources?api-version=2016-11-01`,
+      headers: {
+        accept: 'application/json'
+      },
+      data: {
+        baseResourceIds: [
+          `/providers/Microsoft.PowerApps/apps/${args.options.id}`
+        ]
+      },
+      responseType: 'json'
+    };
+
+    const response = await request.post<any>(requestOptions);
+    Object.keys(response.resources).forEach((key) => {
+      response.resources[key].suggestedCreationType = 'Update';
+    });
+    return response.resources;
+  }
+
+  private async exportPackage(args: CommandArgs, logger: Logger): Promise<string> {
+    if (this.verbose) {
+      logger.logToStderr(`Initiating package export for Microsoft Power App ${args.options.id}...`);
+    }
+
+    const resources = await this.getPackageResources(args, logger);
+
+    const requestOptions: CliRequestOptions = {
+      url: `${this.resource}/providers/Microsoft.BusinessAppPlatform/environments/${formatting.encodeQueryParameter(args.options.environment)}/exportPackage?api-version=2016-11-01`,
+      headers: {
+        accept: 'application/json'
+      },
+      responseType: 'json',
+      data: {
+        includedResourceIds: [
+          `/providers/Microsoft.PowerApps/apps/${args.options.id}`
+        ],
+        details: {
+          creator: args.options.packageCreatedBy,
+          description: args.options.packageDescription,
+          displayName: args.options.packageDisplayName,
+          sourceEnvironment: args.options.packageSourceEnvironment
+        },
+        resources: resources
+      },
+      fullResponse: true
+    };
+
+    const response: any = await request.post<any>(requestOptions);
+
+    const location = response.headers.location;
+
+    if (this.verbose) {
+      logger.logToStderr(`Location of the Power Apps export: ${location}`);
+    }
+
+    return response.headers.location;
+  }
+
+  private async getPackageLink(args: CommandArgs, logger: Logger): Promise<string> {
+    let status;
+    let link;
+
+    const location = await this.exportPackage(args, logger);
+
+    const requestOptions: CliRequestOptions = {
+      url: location,
+      headers: {
+        accept: 'application/json'
+      },
+      responseType: 'json'
+    };
+
+    do {
+      const response = await request.get<any>(requestOptions);
+      status = response.properties.status;
+      if (status === "Succeeded") {
+        link = response.properties.packageLink.value;
+      }
+      else {
+        await this.sleep(PaAppExportCommand.sleepInterval);
+      }
+
+      if (this.verbose) {
+        logger.logToStderr(`Current status of the get package link: ${status}`);
+      }
+
+    } while (status === 'Running');
+
+    return link;
+  }
+
+  protected sleep(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+}
+
+module.exports = new PaAppExportCommand();
