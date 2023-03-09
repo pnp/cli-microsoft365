91fe9957f29494e94b5decefeb887fda950ae47e
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2023-03-09 23:39:06 +0100

    Enhance `navigation node add` and `navigation node set`

--- src/m365/spo/commands/navigation/navigation-node-add.spec.ts
+++ src/m365/spo/commands/navigation/navigation-node-add.spec.ts
@@ -11,6 +11,7 @@ import { pid } from '../../../../utils/pid';
 import { session } from '../../../../utils/session';
 import { sinonUtil } from '../../../../utils/sinonUtil';
 import commands from '../../commands';
+import { spo } from '../../../../utils/spo';
 const command: Command = require('./navigation-node-add');
 
 describe(commands.NAVIGATION_NODE_ADD, () => {
@@ -18,6 +19,8 @@ describe(commands.NAVIGATION_NODE_ADD, () => {
   const nodeUrl = '/sites/team-a/sitepages/about.aspx';
   const title = 'About';
   const audienceIds = '7aa4a1ca-4035-4f2f-bac7-7beada59b5ba,4bbf236f-a131-4019-b4a2-315902fcfa3a';
+  const topNavigationResponse = { 'AudienceIds': [], 'FriendlyUrlPrefix': '', 'IsAudienceTargetEnabledForGlobalNav': false, 'Nodes': [{ 'AudienceIds': [], 'CurrentLCID': 1033, 'CustomProperties': [], 'FriendlyUrlSegment': '', 'IsDeleted': false, 'IsHidden': false, 'IsTitleForExistingLanguage': false, 'Key': '2039', 'Nodes': [{ 'AudienceIds': [], 'CurrentLCID': 1033, 'CustomProperties': [], 'FriendlyUrlSegment': '', 'IsDeleted': false, 'IsHidden': false, 'IsTitleForExistingLanguage': false, 'Key': '2041', 'Nodes': [], 'NodeType': 0, 'OpenInNewWindow': true, 'SimpleUrl': '/sites/PnPCoreSDKTestGroup', 'Title': 'Sub level 1', 'Translations': [] }], 'NodeType': 0, 'OpenInNewWindow': null, 'SimpleUrl': '/sites/PnPCoreSDKTestGroup', 'Title': 'Site A', 'Translations': [] }, { 'AudienceIds': [], 'CurrentLCID': 1033, 'CustomProperties': [], 'FriendlyUrlSegment': '', 'IsDeleted': false, 'IsHidden': false, 'IsTitleForExistingLanguage': false, 'Key': '2040', 'Nodes': [], 'NodeType': 0, 'OpenInNewWindow': true, 'SimpleUrl': '/sites/PnPCoreSDKTestGroup', 'Title': 'Site B', 'Translations': [] }, { 'AudienceIds': [], 'CurrentLCID': 1033, 'CustomProperties': [], 'FriendlyUrlSegment': '', 'IsDeleted': false, 'IsHidden': false, 'IsTitleForExistingLanguage': false, 'Key': '2001', 'Nodes': [], 'NodeType': 0, 'OpenInNewWindow': true, 'SimpleUrl': '/sites/team-a/sitepages/about.aspx', 'Title': 'About', 'Translations': [] }], 'SimpleUrl': '', 'SPSitePrefix': '/sites/SharePointDemoSite', 'SPWebPrefix': '/sites/SharePointDemoSite', 'StartingNodeKey': '1025', 'StartingNodeTitle': 'Quick launch', 'Version': '2023-03-09T18:33:53.5468097Z' };
+  const quickLaunchResponse = { 'AudienceIds': [], 'FriendlyUrlPrefix': '', 'IsAudienceTargetEnabledForGlobalNav': false, 'Nodes': [{ 'AudienceIds': [], 'CurrentLCID': 1033, 'CustomProperties': [], 'FriendlyUrlSegment': '', 'IsDeleted': false, 'IsHidden': false, 'IsTitleForExistingLanguage': false, 'Key': '2003', 'Nodes': [{ 'AudienceIds': [], 'CurrentLCID': 1033, 'CustomProperties': [], 'FriendlyUrlSegment': '', 'IsDeleted': false, 'IsHidden': false, 'IsTitleForExistingLanguage': false, 'Key': '2006', 'Nodes': [], 'NodeType': 0, 'OpenInNewWindow': null, 'SimpleUrl': '/sites/SharePointDemoSite#/', 'Title': 'Sub Item', 'Translations': [] }], 'NodeType': 0, 'OpenInNewWindow': true, 'SimpleUrl': 'http://google.be', 'Title': 'Site A', 'Translations': [] }, { 'AudienceIds': [], 'CurrentLCID': 1033, 'CustomProperties': [], 'FriendlyUrlSegment': '', 'IsDeleted': false, 'IsHidden': false, 'IsTitleForExistingLanguage': false, 'Key': '2018', 'Nodes': [], 'NodeType': 0, 'OpenInNewWindow': null, 'SimpleUrl': 'https://google.be', 'Title': 'Site B', 'Translations': [] }], 'SimpleUrl': '', 'SPSitePrefix': '/sites/SharePointDemoSite', 'SPWebPrefix': '/sites/SharePointDemoSite', 'StartingNodeKey': '1002', 'StartingNodeTitle': 'SharePoint Top Navigation Bar', 'Version': '2023-03-09T18:34:53.650545Z' };
 
   let log: string[];
   let logger: Logger;
@@ -51,7 +54,10 @@ describe(commands.NAVIGATION_NODE_ADD, () => {
 
   afterEach(() => {
     sinonUtil.restore([
-      request.post
+      request.post,
+      spo.getTopNavigationMenuState,
+      spo.getQuickLaunchMenuState,
+      spo.saveMenuState
     ]);
   });
 
@@ -187,6 +193,120 @@ describe(commands.NAVIGATION_NODE_ADD, () => {
 
     await command.action(logger, { options: { webUrl: webUrl, location: 'TopNavigationBar', title: title, url: nodeUrl, audienceIds: audienceIds, verbose: true } });
     assert(loggerLogSpy.calledWith(nodeAddResponse));
+  });
+
+  it('adds new navigation node to the top navigation with audience targetting and opens it in new window', async () => {
+    let saveCalled = false;
+    const nodeAddResponse = {
+      "AudienceIds": audienceIds.split(','),
+      "CurrentLCID": 1033,
+      "Id": 2001,
+      "IsDocLib": true,
+      "IsExternal": false,
+      "IsVisible": true,
+      "ListTemplateType": 0,
+      "Title": "About",
+      "Url": "/sites/team-a/sitepages/about.aspx"
+    };
+
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `${webUrl}/_api/web/navigation/topnavigationbar`) {
+        return nodeAddResponse;
+      }
+
+      throw 'Invalid request';
+    });
+
+    sinon.stub(spo, 'getTopNavigationMenuState').callsFake(async () => {
+      return topNavigationResponse;
+    });
+
+    sinon.stub(spo, 'saveMenuState').callsFake(async () => {
+      saveCalled = true;
+      return;
+    });
+
+
+    await command.action(logger, { options: { webUrl: webUrl, location: 'TopNavigationBar', title: title, url: nodeUrl, audienceIds: audienceIds, openInNewWindow: true, verbose: true } });
+    assert(loggerLogSpy.calledWith(nodeAddResponse));
+    assert.strictEqual(saveCalled, true);
+  });
+
+  it('adds new navigation node to the quick launch navigation and opens it in new window', async () => {
+    let saveCalled = false;
+    const nodeAddResponse = {
+      "AudienceIds": [],
+      "CurrentLCID": 1033,
+      "Id": 2003,
+      "IsDocLib": true,
+      "IsExternal": false,
+      "IsVisible": true,
+      "ListTemplateType": 0,
+      "Title": "About",
+      "Url": "/sites/team-a/sitepages/about.aspx"
+    };
+
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `${webUrl}/_api/web/navigation/quicklaunch`) {
+        return nodeAddResponse;
+      }
+
+      throw 'Invalid request';
+    });
+
+    sinon.stub(spo, 'getQuickLaunchMenuState').callsFake(async () => {
+      return quickLaunchResponse;
+    });
+
+    sinon.stub(spo, 'saveMenuState').callsFake(async () => {
+      saveCalled = true;
+      return;
+    });
+
+    await command.action(logger, { options: { webUrl: webUrl, location: 'QuickLaunch', title: title, url: nodeUrl, openInNewWindow: true, verbose: true } });
+    assert(loggerLogSpy.calledWith(nodeAddResponse));
+    assert.strictEqual(saveCalled, true);
+  });
+
+  it('adds new navigation node with a parent id and opens it in new window', async () => {
+    const parentNodeId = 2039;
+    let saveCalled = false;
+    const nodeAddResponse = {
+      "AudienceIds": audienceIds.split(','),
+      "CurrentLCID": 1033,
+      "Id": 2041,
+      "IsDocLib": true,
+      "IsExternal": false,
+      "IsVisible": true,
+      "ListTemplateType": 0,
+      "Title": "About",
+      "Url": "/sites/team-a/sitepages/about.aspx"
+    };
+
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `${webUrl}/_api/web/navigation/GetNodeById(${parentNodeId})/Children`) {
+        return nodeAddResponse;
+      }
+
+      throw 'Invalid request';
+    });
+
+    sinon.stub(spo, 'getQuickLaunchMenuState').callsFake(async () => {
+      return quickLaunchResponse;
+    });
+
+    sinon.stub(spo, 'getTopNavigationMenuState').callsFake(async () => {
+      return topNavigationResponse;
+    });
+
+    sinon.stub(spo, 'saveMenuState').callsFake(async () => {
+      saveCalled = true;
+      return;
+    });
+
+    await command.action(logger, { options: { webUrl: webUrl, parentNodeId: parentNodeId, title: title, url: nodeUrl, audienceIds: audienceIds, openInNewWindow: true, verbose: true } });
+    assert(loggerLogSpy.calledWith(nodeAddResponse));
+    assert.strictEqual(saveCalled, true);
   });
 
   it('correctly handles random API error', async () => {
