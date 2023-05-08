0e9f83e9e87babc4c8d9071eae0b54183b830a38
Michael Milton <michael.r.milton@gmail.com>
2023-05-08 13:00:43 +1000

    Enhances 'spo page set' command Closes #4840

--- src/m365/spo/commands/page/page-set.spec.ts
+++ src/m365/spo/commands/page/page-set.spec.ts
@@ -484,6 +484,53 @@ describe(commands.PAGE_SET, () => {
     await command.action(logger, { options: { debug: true, name: 'page.aspx', webUrl: 'https://contoso.sharepoint.com/sites/team-a', title: newPageTitle } });
   });
 
+  it('updates page content', async () => {
+    sinonUtil.restore([request.post]);
+
+    const newContent = [{
+      "controlType": 4,
+      "position": {
+        "layoutIndex": 1,
+        "zoneIndex": 1,
+        "sectionIndex": 1,
+        "sectionFactor": 12,
+        "controlIndex": 1
+      },
+      "addedFromPersistedData": true,
+      "innerHTML": "<p>Text content</p>"
+    }];
+
+    const initialPage = {
+      Title: "article",
+      Id: 1,
+      TopicHeader: "TopicHeader",
+      AuthorByline: "AuthorByline",
+      Description: "Description",
+      BannerImageUrl: {
+        Description: '/_layouts/15/images/sitepagethumbnail.png',
+        Url: `https://contoso.sharepoint.com/_layouts/15/images/sitepagethumbnail.png`
+      },
+      CanvasContent1: "{}",
+      LayoutWebpartsContent: "{}"
+    };
+
+    let data: string = '';
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      if ((opts.url as string).includes(`/_api/sitepages/pages/GetByUrl('sitepages/page.aspx')/checkoutpage`)) {
+        return initialPage;
+      }
+      if ((opts.url as string).includes(`/_api/SitePages/Pages(1)/SavePage`) ||
+        (opts.url as string).includes(`/_api/SitePages/Pages(1)/SavePageAsDraft`)) {
+        data = opts.data.CanvasContent1;
+        return {};
+      }
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, { options: { debug: true, name: 'page.aspx', webUrl: 'https://contoso.sharepoint.com/sites/team-a', content: JSON.stringify(newContent) } });
+    assert.deepStrictEqual(JSON.parse(data), newContent);
+  });
+
   it('publishes page', async () => {
     sinonUtil.restore([request.post]);
 
@@ -778,6 +825,16 @@ describe(commands.PAGE_SET, () => {
 
   it('passes validation if commentsEnabled is false', async () => {
     const actual = await command.validate({ options: { name: 'page.aspx', webUrl: 'https://contoso.sharepoint.com', commentsEnabled: false } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('fails validation if content is not valid JSON', async () => {
+    const actual = await command.validate({ options: { name: 'page.aspx', webUrl: 'https://contoso.sharepoint.com', content: "foo" } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('passes validation if content is valid JSON', async () => {
+    const actual = await command.validate({ options: { name: 'page.aspx', webUrl: 'https://contoso.sharepoint.com', content: "[]" } }, commandInfo);
     assert.strictEqual(actual, true);
   });
 });
