d36dda33c7883fac3bb3fd91b51f0eeda5d3b243
Nanddeep Nachan <nanddeepnachan@gmail.com>
2023-05-08 12:58:49 +0000

    Allow listing terms

--- src/m365/spo/commands/term/term-list.ts
+++ src/m365/spo/commands/term/term-list.ts
@@ -17,6 +17,7 @@ interface CommandArgs {
 }
 
 interface Options extends GlobalOptions {
+  webUrl?: string;
   termGroupId?: string;
   termGroupName?: string;
   termSetId?: string;
@@ -49,6 +50,7 @@ class SpoTermListCommand extends SpoCommand {
   #initTelemetry(): void {
     this.telemetry.push((args: CommandArgs) => {
       Object.assign(this.telemetryProperties, {
+        webUrl: typeof args.options.webUrl !== 'undefined',
         termGroupId: typeof args.options.termGroupId !== 'undefined',
         termGroupName: typeof args.options.termGroupName !== 'undefined',
         termSetId: typeof args.options.termSetId !== 'undefined',
@@ -60,6 +62,9 @@ class SpoTermListCommand extends SpoCommand {
 
   #initOptions(): void {
     this.options.unshift(
+      {
+        option: '-u, --webUrl [webUrl]'
+      },
       {
         option: '--termGroupId [termGroupId]'
       },
@@ -81,6 +86,13 @@ class SpoTermListCommand extends SpoCommand {
   #initValidators(): void {
     this.validators.push(
       async (args: CommandArgs) => {
+        if (args.options.webUrl) {
+          const isValidSharePointUrl: boolean | string = validation.isValidSharePointUrl(args.options.webUrl);
+          if (isValidSharePointUrl !== true) {
+            return isValidSharePointUrl;
+          }
+        }
+
         if (args.options.termGroupId && !validation.isValidGuid(args.options.termGroupId)) {
           return `${args.options.termGroupId} is not a valid GUID`;
         }
@@ -103,8 +115,8 @@ class SpoTermListCommand extends SpoCommand {
 
   public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
     try {
-      const spoAdminUrl: string = await spo.getSpoAdminUrl(logger, this.debug);
-      const res: ContextInfo = await spo.getRequestDigest(spoAdminUrl);
+      const spoWebUrl: string = args.options.webUrl ? args.options.webUrl : await spo.getSpoAdminUrl(logger, this.debug);
+      const res: ContextInfo = await spo.getRequestDigest(spoWebUrl);
 
       if (this.verbose) {
         logger.logToStderr(`Retrieving taxonomy term sets...`);
@@ -114,14 +126,14 @@ class SpoTermListCommand extends SpoCommand {
       const termSetQuery: string = args.options.termSetId ? `<Method Id="82" ParentId="80" Name="GetById"><Parameters><Parameter Type="Guid">{${args.options.termSetId}}</Parameter></Parameters></Method>` : `<Method Id="82" ParentId="80" Name="GetByName"><Parameters><Parameter Type="String">${formatting.escapeXml(args.options.termSetName)}</Parameter></Parameters></Method>`;
       const data = `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="70" ObjectPathId="69" /><ObjectIdentityQuery Id="71" ObjectPathId="69" /><ObjectPath Id="73" ObjectPathId="72" /><ObjectIdentityQuery Id="74" ObjectPathId="72" /><ObjectPath Id="76" ObjectPathId="75" /><ObjectPath Id="78" ObjectPathId="77" /><ObjectIdentityQuery Id="79" ObjectPathId="77" /><ObjectPath Id="81" ObjectPathId="80" /><ObjectPath Id="83" ObjectPathId="82" /><ObjectIdentityQuery Id="84" ObjectPathId="82" /><ObjectPath Id="86" ObjectPathId="85" /><Query Id="87" ObjectPathId="85"><Query SelectAllProperties="false"><Properties /></Query><ChildItemQuery SelectAllProperties="true"><Properties><Property Name="Name" ScalarProperty="true" /><Property Name="Id" ScalarProperty="true" /></Properties></ChildItemQuery></Query></Actions><ObjectPaths><StaticMethod Id="69" Name="GetTaxonomySession" TypeId="{981cbc68-9edc-4f8d-872f-71146fcbb84f}" /><Method Id="72" ParentId="69" Name="GetDefaultSiteCollectionTermStore" /><Property Id="75" ParentId="72" Name="Groups" />${termGroupQuery}<Property Id="80" ParentId="77" Name="TermSets" />${termSetQuery}<Property Id="85" ParentId="82" Name="Terms" /></ObjectPaths></Request>`;
 
-      const result = await this.executeCsomCall(data, spoAdminUrl, res);
+      const result = await this.executeCsomCall(data, spoWebUrl, res);
       const terms: Term[] = [];
       if (result._Child_Items_ && result._Child_Items_.length > 0) {
         for (const term of result._Child_Items_) {
           this.setTermDetails(term);
           terms.push(term);
           if (args.options.includeChildTerms && term.TermsCount > 0) {
-            await this.getChildTerms(spoAdminUrl, res, term);
+            await this.getChildTerms(spoWebUrl, res, term);
           }
         }
       }
@@ -172,16 +184,16 @@ class SpoTermListCommand extends SpoCommand {
     });
   }
 
-  private async getChildTerms(spoAdminUrl: string, res: ContextInfo, parentTerm: Term): Promise<void> {
+  private async getChildTerms(spoWebUrl: string, res: ContextInfo, parentTerm: Term): Promise<void> {
     parentTerm.Children = [];
     const data = `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="20" ObjectPathId="19" /><Query Id="21" ObjectPathId="19"><Query SelectAllProperties="false"><Properties /></Query><ChildItemQuery SelectAllProperties="true"><Properties><Property Name="CustomSortOrder" ScalarProperty="true" /><Property Name="CustomProperties" ScalarProperty="true" /><Property Name="LocalCustomProperties" ScalarProperty="true" /></Properties></ChildItemQuery></Query></Actions><ObjectPaths><Property Id="19" ParentId="16" Name="Terms" /><Identity Id="16" Name="${parentTerm._ObjectIdentity_}" /></ObjectPaths></Request>`;
-    const result = await this.executeCsomCall(data, spoAdminUrl, res);
+    const result = await this.executeCsomCall(data, spoWebUrl, res);
     if (result._Child_Items_ && result._Child_Items_.length > 0) {
       for (const term of result._Child_Items_) {
         this.setTermDetails(term);
         parentTerm.Children.push(term);
         if (term.TermsCount > 0) {
-          await this.getChildTerms(spoAdminUrl, res, term);
+          await this.getChildTerms(spoWebUrl, res, term);
         }
       }
     }
@@ -197,9 +209,9 @@ class SpoTermListCommand extends SpoCommand {
     return new Date(Number(dateAsString.replace('/Date(', '').replace(')/', ''))).toISOString();
   }
 
-  private async executeCsomCall(data: string, spoAdminUrl: string, res: ContextInfo): Promise<TermCollection> {
+  private async executeCsomCall(data: string, spoWebUrl: string, res: ContextInfo): Promise<TermCollection> {
     const requestOptions: AxiosRequestConfig = {
-      url: `${spoAdminUrl}/_vti_bin/client.svc/ProcessQuery`,
+      url: `${spoWebUrl}/_vti_bin/client.svc/ProcessQuery`,
       headers: {
         'X-RequestDigest': res.FormDigestValue
       },
