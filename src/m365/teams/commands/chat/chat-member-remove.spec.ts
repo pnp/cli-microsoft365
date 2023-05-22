ccc6659da7cc51ee2fec4702ff84fedd91439c02
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2023-05-23 01:01:22 +0200

    Added tests

--- src/m365/teams/commands/chat/chat-member-remove.spec.ts
+++ src/m365/teams/commands/chat/chat-member-remove.spec.ts
@@ -11,13 +11,39 @@ import { pid } from '../../../../utils/pid';
 import { session } from '../../../../utils/session';
 import { sinonUtil } from '../../../../utils/sinonUtil';
 import commands from '../../commands';
-const command: Command = require('./chat-member-list');
+import { odata } from '../../../../utils/odata';
+const command: Command = require('./chat-member-remove');
 
-describe(commands.CHAT_MEMBER_LIST, () => {
+describe(commands.CHAT_MEMBER_REMOVE, () => {
+  const chatId = '19:09fd7575940146d383a4a83fc9598546@thread.v2';
+  const userPrincipalName = 'john@contoso.com';
+  const userId = 'a857e888-b602-4790-86d9-3dca2109449e';
+  const chatMemberId = 'MCMjMCMjZTFkZDQwMjMtYTY1Ni00ODBhLThhMGUtYzFiMWVlYzUxZTFkIyMxOTowOWZkNzU3NTk0MDE0NmQzODNhNGE4M2ZjOTU5ODU0NkB0aHJlYWQudjIjI2E4NTdlODg4LWI2MDItNDc5MC04NmQ5LTNkY2EyMTA5NDQ5ZQ=="';
+  const chatMembers = [
+    {
+      id: chatMemberId,
+      roles: ['owner'],
+      displayName: 'Adele Vance',
+      visibleHistoryStartDateTime: '2022-04-08T09:15:53.423Z',
+      userId: userId,
+      email: userPrincipalName,
+      tenantId: 'e1dd4023-a656-480a-8a0e-c1b1eec51e1d'
+    },
+    {
+      id: 'MCMjMCMjZTFkZDQwMjMtYTY1Ni00ODBhLThhMGUtYzFiMWVlYzUxZTFkIyMxOTowOWZkNzU3NTk0MDE0NmQzODNhNGE4M2ZjOTU5ODU0NkB0aHJlYWQudjIjI2ZlMzZmNzVlLWMxMDMtNDEwYi1hMThhLTJiZjZkZjA2YWMzYQ==',
+      roles: ['owner'],
+      displayName: 'John Doe',
+      visibleHistoryStartDateTime: '2022-04-08T09:15:53.423Z',
+      userId: 'fe36f75e-c103-410b-a18a-2bf6df06ac3a',
+      email: 'mathijs@mathijsdev2.onmicrosoft.com',
+      tenantId: 'e1dd4023-a656-480a-8a0e-c1b1eec51e1d'
+    }
+  ];
+
   let log: string[];
   let logger: Logger;
-  let loggerLogSpy: sinon.SinonSpy;
   let commandInfo: CommandInfo;
+  let promptOptions: any;
 
   before(() => {
     sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
@@ -41,12 +67,18 @@ describe(commands.CHAT_MEMBER_LIST, () => {
         log.push(msg);
       }
     };
-    loggerLogSpy = sinon.spy(logger, 'log');
+    sinon.stub(Cli, 'prompt').callsFake(async (options) => {
+      promptOptions = options;
+      return { continue: false };
+    });
+    promptOptions = undefined;
   });
 
   afterEach(() => {
     sinonUtil.restore([
-      request.get
+      odata.getAllItems,
+      request.delete,
+      Cli.prompt
     ]);
   });
 
@@ -56,203 +88,142 @@ describe(commands.CHAT_MEMBER_LIST, () => {
   });
 
   it('has correct name', () => {
-    assert.strictEqual(command.name.startsWith(commands.CHAT_MEMBER_LIST), true);
+    assert.strictEqual(command.name, commands.CHAT_MEMBER_REMOVE);
   });
 
   it('has a description', () => {
     assert.notStrictEqual(command.description, null);
   });
 
-  it('defines correct properties for the default output', () => {
-    assert.deepStrictEqual(command.defaultProperties(), ['userId', 'displayName', 'email']);
+  it('removes the member by specifying the userId', async () => {
+    sinon.stub(odata, 'getAllItems').callsFake(async url => {
+      if (url === `https://graph.microsoft.com/v1.0/chats/${chatId}/members`) {
+        return chatMembers;
+      }
+
+      throw 'Invalid request';
+    });
+    const deleteStub = sinon.stub(request, 'delete').callsFake(async (opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/chats/${chatId}/members/${chatMemberId}`) {
+        return;
+      }
+
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, { options: { chatId: chatId, userId: userId, confirm: true, verbose: true } });
+    assert(deleteStub.called);
   });
 
-  it('fails validation if chatId is not specified', async () => {
-    const actual = await command.validate({
-      options: {
+  it('removes the member from a chat by specifying the member id', async () => {
+    const deleteStub = sinon.stub(request, 'delete').callsFake(async (opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/chats/${chatId}/members/${chatMemberId}`) {
+        return;
       }
-    }, commandInfo);
+
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, { options: { chatId: chatId, id: chatMemberId, confirm: true, verbose: true } });
+    assert(deleteStub.called);
+  });
+
+
+  it('removes the specified member retrieved by user principal name when prompt confirmed', async () => {
+    sinon.stub(odata, 'getAllItems').callsFake(async url => {
+      if (url === `https://graph.microsoft.com/v1.0/chats/${chatId}/members`) {
+        return chatMembers;
+      }
+
+      throw 'Invalid request';
+    });
+    const deleteStub = sinon.stub(request, 'delete').callsFake(async (opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/chats/${chatId}/members/${chatMemberId}`) {
+        return;
+      }
+
+      throw 'Invalid request';
+    });
+
+    sinonUtil.restore(Cli.prompt);
+    sinon.stub(Cli, 'prompt').callsFake(async () => (
+      { continue: true }
+    ));
+
+    await command.action(logger, { options: { chatId: chatId, userName: userPrincipalName, verbose: true } });
+    assert(deleteStub.called);
+  });
+
+  it('throws error when member by specifying userName is not found in the chat', async () => {
+    sinon.stub(odata, 'getAllItems').callsFake(async url => {
+      if (url === `https://graph.microsoft.com/v1.0/chats/${chatId}/members`) {
+        return [...chatMembers.slice(1)];
+      }
+
+      throw 'Invalid request';
+    });
+
+    await assert.rejects(command.action(logger, { options: { chatId: chatId, userName: userPrincipalName, confirm: true, verbose: true } }), new CommandError(`Member with userName ${userPrincipalName} could not be found in the chat`));
+  });
+
+  it('throws error when member by specifying userId is not found in the chat', async () => {
+    sinon.stub(odata, 'getAllItems').callsFake(async url => {
+      if (url === `https://graph.microsoft.com/v1.0/chats/${chatId}/members`) {
+        return [...chatMembers.slice(1)];
+      }
+
+      throw 'Invalid request';
+    });
+
+    await assert.rejects(command.action(logger, { options: { chatId: chatId, userId: userId, confirm: true, verbose: true } }), new CommandError(`Member with userId ${userId} could not be found in the chat`));
+  });
+
+  it('prompts before removing the specified message when confirm option not passed', async () => {
+    await command.action(logger, { options: { chatId: chatId, id: chatMemberId } });
+
+    let promptIssued = false;
+
+    if (promptOptions && promptOptions.type === 'confirm') {
+      promptIssued = true;
+    }
+
+    assert(promptIssued);
+  });
+
+  it('aborts removing the specified chat member when confirm option not passed and prompt not confirmed', async () => {
+    const deleteSpy = sinon.spy(request, 'delete');
+
+    await command.action(logger, { options: { chatId: chatId, userId: userId } });
+    assert(deleteSpy.notCalled);
+  });
+
+  it('fails validation if the chatId is not valid chatId', async () => {
+    const actual = await command.validate({ options: { chatId: 'invalid', userId: userId } }, commandInfo);
     assert.notStrictEqual(actual, true);
   });
 
-  it('fails validation if the chatId is not valid', async () => {
-    const actual = await command.validate({
-      options: {
-        chatId: "8b081ef6"
-      }
-    }, commandInfo);
+  it('fails validation if the userId is not valid guid', async () => {
+    const actual = await command.validate({ options: { chatId: chatId, userId: 'invalid' } }, commandInfo);
     assert.notStrictEqual(actual, true);
   });
 
-  it('fails validation for an incorrect chatId missing leading 19:.', async () => {
-    const actual = await command.validate({
-      options: {
-        chatId: '8b081ef6-4792-4def-b2c9-c363a1bf41d5_5031bb31-22c0-4f6f-9f73-91d34ab2b32d@unq.gbl.spaces'
-      }
-    }, commandInfo);
+  it('fails validation if the userName is not valid UPN', async () => {
+    const actual = await command.validate({ options: { chatId: chatId, userName: 'invalid' } }, commandInfo);
     assert.notStrictEqual(actual, true);
   });
 
-  it('fails validation for an incorrect chatId missing trailing @thread.v2 or @unq.gbl.spaces', async () => {
-    const actual = await command.validate({
-      options: {
-        chatId: '19:8b081ef6-4792-4def-b2c9-c363a1bf41d5_5031bb31-22c0-4f6f-9f73-91d34ab2b32d'
-      }
-    }, commandInfo);
-    assert.notStrictEqual(actual, true);
-  });
-
-  it('validates for a correct input', async () => {
-    const actual = await command.validate({
-      options: {
-        chatId: "19:8b081ef6-4792-4def-b2c9-c363a1bf41d5_5031bb31-22c0-4f6f-9f73-91d34ab2b32d@unq.gbl.spaces"
-      }
-    }, commandInfo);
+  it('passes validation if ID of a chat member is passed', async () => {
+    const actual = await command.validate({ options: { chatId: chatId, id: chatMemberId } }, commandInfo);
     assert.strictEqual(actual, true);
   });
 
-  it('lists chat members (debug)', async () => {
-    sinon.stub(request, 'get').callsFake((opts) => {
-      if (opts.url === `https://graph.microsoft.com/v1.0/chats/19:8b081ef6-4792-4def-b2c9-c363a1bf41d5_5031bb31-22c0-4f6f-9f73-91d34ab2b32d@unq.gbl.spaces/members`) {
-        return Promise.resolve({
-          "value": [{ "@odata.type": "#microsoft.graph.aadUserConversationMember", "id": "8b081ef6-4792-4def-b2c9-c363a1bf41d5", "roles": ["owner"], "displayName": "John Doe", "userId": "8b081ef6-4792-4def-b2c9-c363a1bf41d5", "email": null, "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb", "visibleHistoryStartDateTime": "2019-04-18T23:51:43.255Z" }, { "@odata.type": "#microsoft.graph.aadUserConversationMember", "id": "2de87aaf-844d-4def-9dee-2c317f0be1b3", "roles": ["owner"], "displayName": "Bart Hogan", "userId": "2de87aaf-844d-4def-9dee-2c317f0be1b3", "email": null, "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb", "visibleHistoryStartDateTime": "0001-01-01T00:00:00Z" }, { "@odata.type": "#microsoft.graph.aadUserConversationMember", "id": "07ad17ad-ada5-4f1f-a650-7a963886a8a7", "roles": ["owner"], "displayName": "Minna Pham", "userId": "07ad17ad-ada5-4f1f-a650-7a963886a8a7", "email": null, "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb", "visibleHistoryStartDateTime": "2019-04-18T23:51:43.255Z" }]
-        });
-      }
-
-      return Promise.reject('Invalid Request');
-    });
-
-    await command.action(logger, {
-      options: {
-        debug: true,
-        chatId: "19:8b081ef6-4792-4def-b2c9-c363a1bf41d5_5031bb31-22c0-4f6f-9f73-91d34ab2b32d@unq.gbl.spaces"
-      }
-    });
-    assert(loggerLogSpy.calledWith([
-      {
-        "@odata.type": "#microsoft.graph.aadUserConversationMember",
-        "id": "8b081ef6-4792-4def-b2c9-c363a1bf41d5",
-        "roles": [
-          "owner"
-        ],
-        "displayName": "John Doe",
-        "userId": "8b081ef6-4792-4def-b2c9-c363a1bf41d5",
-        "email": null,
-        "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb",
-        "visibleHistoryStartDateTime": "2019-04-18T23:51:43.255Z"
-      },
-      {
-        "@odata.type": "#microsoft.graph.aadUserConversationMember",
-        "id": "2de87aaf-844d-4def-9dee-2c317f0be1b3",
-        "roles": [
-          "owner"
-        ],
-        "displayName": "Bart Hogan",
-        "userId": "2de87aaf-844d-4def-9dee-2c317f0be1b3",
-        "email": null,
-        "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb",
-        "visibleHistoryStartDateTime": "0001-01-01T00:00:00Z"
-      },
-      {
-        "@odata.type": "#microsoft.graph.aadUserConversationMember",
-        "id": "07ad17ad-ada5-4f1f-a650-7a963886a8a7",
-        "roles": [
-          "owner"
-        ],
-        "displayName": "Minna Pham",
-        "userId": "07ad17ad-ada5-4f1f-a650-7a963886a8a7",
-        "email": null,
-        "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb",
-        "visibleHistoryStartDateTime": "2019-04-18T23:51:43.255Z"
-      }
-    ]));
+  it('passes validation if the userId is a valid GUID', async () => {
+    const actual = await command.validate({ options: { chatId: chatId, userId: userId } }, commandInfo);
+    assert.strictEqual(actual, true);
   });
 
-  it('lists chat members', async () => {
-    sinon.stub(request, 'get').callsFake((opts) => {
-      if (opts.url === `https://graph.microsoft.com/v1.0/chats/19:8b081ef6-4792-4def-b2c9-c363a1bf41d5_5031bb31-22c0-4f6f-9f73-91d34ab2b32d@unq.gbl.spaces/members`) {
-        return Promise.resolve({
-          "value": [{ "@odata.type": "#microsoft.graph.aadUserConversationMember", "id": "8b081ef6-4792-4def-b2c9-c363a1bf41d5", "roles": ["owner"], "displayName": "John Doe", "userId": "8b081ef6-4792-4def-b2c9-c363a1bf41d5", "email": null, "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb", "visibleHistoryStartDateTime": "2019-04-18T23:51:43.255Z" }, { "@odata.type": "#microsoft.graph.aadUserConversationMember", "id": "2de87aaf-844d-4def-9dee-2c317f0be1b3", "roles": ["owner"], "displayName": "Bart Hogan", "userId": "2de87aaf-844d-4def-9dee-2c317f0be1b3", "email": null, "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb", "visibleHistoryStartDateTime": "0001-01-01T00:00:00Z" }, { "@odata.type": "#microsoft.graph.aadUserConversationMember", "id": "07ad17ad-ada5-4f1f-a650-7a963886a8a7", "roles": ["owner"], "displayName": "Minna Pham", "userId": "07ad17ad-ada5-4f1f-a650-7a963886a8a7", "email": null, "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb", "visibleHistoryStartDateTime": "2019-04-18T23:51:43.255Z" }]
-        });
-      }
-
-      return Promise.reject('Invalid Request');
-    });
-
-    await command.action(logger, {
-      options: {
-        chatId: "19:8b081ef6-4792-4def-b2c9-c363a1bf41d5_5031bb31-22c0-4f6f-9f73-91d34ab2b32d@unq.gbl.spaces"
-      }
-    });
-    assert(loggerLogSpy.calledWith([
-      {
-        "@odata.type": "#microsoft.graph.aadUserConversationMember",
-        "id": "8b081ef6-4792-4def-b2c9-c363a1bf41d5",
-        "roles": [
-          "owner"
-        ],
-        "displayName": "John Doe",
-        "userId": "8b081ef6-4792-4def-b2c9-c363a1bf41d5",
-        "email": null,
-        "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb",
-        "visibleHistoryStartDateTime": "2019-04-18T23:51:43.255Z"
-      },
-      {
-        "@odata.type": "#microsoft.graph.aadUserConversationMember",
-        "id": "2de87aaf-844d-4def-9dee-2c317f0be1b3",
-        "roles": [
-          "owner"
-        ],
-        "displayName": "Bart Hogan",
-        "userId": "2de87aaf-844d-4def-9dee-2c317f0be1b3",
-        "email": null,
-        "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb",
-        "visibleHistoryStartDateTime": "0001-01-01T00:00:00Z"
-      },
-      {
-        "@odata.type": "#microsoft.graph.aadUserConversationMember",
-        "id": "07ad17ad-ada5-4f1f-a650-7a963886a8a7",
-        "roles": [
-          "owner"
-        ],
-        "displayName": "Minna Pham",
-        "userId": "07ad17ad-ada5-4f1f-a650-7a963886a8a7",
-        "email": null,
-        "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb",
-        "visibleHistoryStartDateTime": "2019-04-18T23:51:43.255Z"
-      }
-    ]));
-  });
-
-  it('outputs all data in json output mode', async () => {
-    sinon.stub(request, 'get').callsFake((opts) => {
-      if (opts.url === `https://graph.microsoft.com/v1.0/chats/19:8b081ef6-4792-4def-b2c9-c363a1bf41d5_5031bb31-22c0-4f6f-9f73-91d34ab2b32d@unq.gbl.spaces/members`) {
-        return Promise.resolve({
-          "value": [{ "@odata.type": "#microsoft.graph.aadUserConversationMember", "id": "8b081ef6-4792-4def-b2c9-c363a1bf41d5", "roles": ["owner"], "displayName": "John Doe", "userId": "8b081ef6-4792-4def-b2c9-c363a1bf41d5", "email": null, "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb", "visibleHistoryStartDateTime": "2019-04-18T23:51:43.255Z" }, { "@odata.type": "#microsoft.graph.aadUserConversationMember", "id": "2de87aaf-844d-4def-9dee-2c317f0be1b3", "roles": ["owner"], "displayName": "Bart Hogan", "userId": "2de87aaf-844d-4def-9dee-2c317f0be1b3", "email": null, "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb", "visibleHistoryStartDateTime": "0001-01-01T00:00:00Z" }, { "@odata.type": "#microsoft.graph.aadUserConversationMember", "id": "07ad17ad-ada5-4f1f-a650-7a963886a8a7", "roles": ["owner"], "displayName": "Minna Pham", "userId": "07ad17ad-ada5-4f1f-a650-7a963886a8a7", "email": null, "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb", "visibleHistoryStartDateTime": "2019-04-18T23:51:43.255Z" }]
-        });
-      }
-
-      return Promise.reject('Invalid Request');
-    });
-
-    await command.action(logger, {
-      options: {
-        output: 'json',
-        chatId: "19:8b081ef6-4792-4def-b2c9-c363a1bf41d5_5031bb31-22c0-4f6f-9f73-91d34ab2b32d@unq.gbl.spaces"
-      }
-    });
-    assert(loggerLogSpy.calledWith([{ "@odata.type": "#microsoft.graph.aadUserConversationMember", "id": "8b081ef6-4792-4def-b2c9-c363a1bf41d5", "roles": ["owner"], "displayName": "John Doe", "userId": "8b081ef6-4792-4def-b2c9-c363a1bf41d5", "email": null, "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb", "visibleHistoryStartDateTime": "2019-04-18T23:51:43.255Z" }, { "@odata.type": "#microsoft.graph.aadUserConversationMember", "id": "2de87aaf-844d-4def-9dee-2c317f0be1b3", "roles": ["owner"], "displayName": "Bart Hogan", "userId": "2de87aaf-844d-4def-9dee-2c317f0be1b3", "email": null, "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb", "visibleHistoryStartDateTime": "0001-01-01T00:00:00Z" }, { "@odata.type": "#microsoft.graph.aadUserConversationMember", "id": "07ad17ad-ada5-4f1f-a650-7a963886a8a7", "roles": ["owner"], "displayName": "Minna Pham", "userId": "07ad17ad-ada5-4f1f-a650-7a963886a8a7", "email": null, "tenantId": "6e5147da-6a35-4275-b3f3-fc069456b6eb", "visibleHistoryStartDateTime": "2019-04-18T23:51:43.255Z" }]));
-  });
-
-  it('correctly handles error when listing members', async () => {
-    sinon.stub(request, 'get').callsFake(() => {
-      return Promise.reject('An error has occurred');
-    });
-
-    await assert.rejects(command.action(logger, {
-      options: {
-        chatId: "19:8b081ef6-4792-4def-b2c9-c363a1bf41d5_5031bb31-22c0-4f6f-9f73-91d34ab2b32d@unq.gbl.spaces"
-      }
-    } as any), new CommandError('An error has occurred'));
+  it('passes validation if the userName is a valid UPN', async () => {
+    const actual = await command.validate({ options: { chatId: chatId, userName: userPrincipalName } }, commandInfo);
+    assert.strictEqual(actual, true);
   });
 });
--- src/m365/teams/commands/chat/chat-member-remove.ts
+++ src/m365/teams/commands/chat/chat-member-remove.ts
@@ -1,9 +1,12 @@
+import { ConversationMember } from '@microsoft/microsoft-graph-types';
 import { Logger } from '../../../../cli/Logger';
 import GlobalOptions from '../../../../GlobalOptions';
 import { odata } from '../../../../utils/odata';
 import { validation } from '../../../../utils/validation';
 import GraphCommand from '../../../base/GraphCommand';
 import commands from '../../commands';
+import { Cli } from '../../../../cli/Cli';
+import request, { CliRequestOptions } from '../../../../request';
 
 interface CommandArgs {
   options: Options;
@@ -79,19 +82,61 @@ class TeamsChatMemberRemoveCommand extends GraphCommand {
   }
 
   public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
+    const removeUserFromChat: () => Promise<void> = async (): Promise<void> => {
+      try {
+        if (this.verbose) {
+          logger.logToStderr(`Removing member ${args.options.id || args.options.userId || args.options.userName} from chat with id ${args.options.chatId}...`);
+        }
 
+        const memberId = await this.getIdToRemove(args);
+        const chatMemberRemoveOptions: CliRequestOptions = {
+          url: `${this.resource}/v1.0/chats/${args.options.chatId}/members/${memberId}`,
+          headers: {}
+        };
+        await request.delete(chatMemberRemoveOptions);
+      }
+      catch (err: any) {
+        this.handleRejectedODataJsonPromise(err);
+      }
+    };
+
+    if (args.options.confirm) {
+      await removeUserFromChat();
+    }
+    else {
+      const result = await Cli.prompt<{ continue: boolean }>({
+        type: 'confirm',
+        name: 'continue',
+        default: false,
+        message: `Are you sure you want to remove the member ${args.options.id || args.options.userId || args.options.userName} from chat with id ${args.options.chatId}?`
+      });
+
+      if (result.continue) {
+        await removeUserFromChat();
+      }
+    }
+  }
+
+  private async getIdToRemove(args: CommandArgs): Promise<string> {
+    if (args.options.id) {
+      return args.options.id;
+    }
+
+    const memberRequestUrl: string = `${this.resource}/v1.0/chats/${args.options.chatId}/members`;
+    const members = await odata.getAllItems<ConversationMember>(memberRequestUrl);
     if (args.options.userName) {
-      const endpoint: string = `${this.resource}/v1.0/chats/${args.options.chatId}/members`;
+      const matchingMember: any = members.find((memb: any) => memb.email.toLowerCase() === args.options.userName);
+      if (!matchingMember) {
+        throw `Member with userName ${args.options.userName} could not be found in the chat`;
+      }
+      return matchingMember.id;
     }
-
-    const endpoint: string = `${this.resource}/v1.0/chats/${args.options.chatId}/members`;
-
-    try {
-      const items = await odata.getAllItems(endpoint);
-      logger.log(items);
-    }
-    catch (err: any) {
-      this.handleRejectedODataJsonPromise(err);
+    else {
+      const matchingMember: any = members.find((memb: any) => memb.userId === args.options.userId);
+      if (!matchingMember) {
+        throw `Member with userId ${args.options.userId} could not be found in the chat`;
+      }
+      return matchingMember.id;
     }
   }
 }
