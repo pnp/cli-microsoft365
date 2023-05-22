9eacce55076eaf99d3eda196c5f7040ab5502a66
Milan Holemans <11723921+milanholemans@users.noreply.github.com>
2023-05-22 23:17:41 +0200

    Adds theme awareness for Mendable Button in docs

--- docs/src/theme/SearchBar.tsx
+++ docs/src/theme/SearchBar.tsx
@@ -1,25 +1,24 @@
-import React from "react";
-import { MendableFloatingButton } from "@mendable/search";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import React from 'react';
+import { MendableFloatingButton } from '@mendable/search';
+import { useColorMode } from '@docusaurus/theme-common';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export default function SearchBarWrapper(): JSX.Element {
   const {
     siteConfig: { customFields }
   } = useDocusaurusContext();
 
-  const style = { darkMode: false, accentColor: "#ef5552" };
-
-  const floatingButtonStyle = {
-    color: "#fff",
-    backgroundColor: "#ef5552"
-  };
-
   return (
-    <div className="mendable-search">
-      <MendableFloatingButton
-        anon_key={customFields.mendableAnonKey}
-        style={style} 
-        floatingButtonStyle={floatingButtonStyle} />
-    </div>
+    <MendableFloatingButton
+      anon_key={customFields.mendableAnonKey as string}
+      style={{
+        darkMode: useColorMode().isDarkTheme,
+        accentColor: '#ef5552'
+      }} 
+      floatingButtonStyle={{
+        color: '#fff',
+        backgroundColor: '#ef5552'
+      }}
+    />
   );
 }
