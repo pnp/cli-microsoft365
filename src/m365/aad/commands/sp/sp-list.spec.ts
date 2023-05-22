d3671f33d7a47cc6405592d8d8cbd0b1f0c8a94c
Nico De Cleyre <nico.de.cleyre@gmail.com>
2023-05-22 11:12:09 +0200

    sinon.restore

--- src/m365/aad/commands/sp/sp-list.spec.ts
+++ src/m365/aad/commands/sp/sp-list.spec.ts
@@ -123,12 +123,7 @@ describe(commands.SP_LIST, () => {
   });
 
   after(() => {
-    sinonUtil.restore([
-      auth.restoreAuth,
-      telemetry.trackEvent,
-      pid.getProcessName,
-      session.getId
-    ]);
+    sinon.restore();
     auth.service.connected = false;
   });
 
