cfb847020f4dc0d0e071f51c0d13fa52a39bc58c
Saurabh <saurabh.k.a.tripathi@avanade.com>
2023-06-23 12:44:26 +0200

    Address code review feedback

--- src/m365/spfx/commands/project/project-upgrade/upgrade-1.17.3.ts
+++ src/m365/spfx/commands/project/project-upgrade/upgrade-1.17.3.ts
@@ -21,24 +21,22 @@ import { FN001034_DEP_microsoft_sp_adaptive_card_extension_base } from './rules/
 import { FN001035_DEP_fluentui_react } from './rules/FN001035_DEP_fluentui_react';
 import { FN002001_DEVDEP_microsoft_sp_build_web } from './rules/FN002001_DEVDEP_microsoft_sp_build_web';
 import { FN002002_DEVDEP_microsoft_sp_module_interfaces } from './rules/FN002002_DEVDEP_microsoft_sp_module_interfaces';
-import { FN002020_DEVDEP_microsoft_rush_stack_compiler_4_5 } from './rules/FN002020_DEVDEP_microsoft_rush_stack_compiler_4_5';
 import { FN002022_DEVDEP_microsoft_eslint_plugin_spfx } from './rules/FN002022_DEVDEP_microsoft_eslint_plugin_spfx';
 import { FN002023_DEVDEP_microsoft_eslint_config_spfx } from './rules/FN002023_DEVDEP_microsoft_eslint_config_spfx';
 import { FN002027_DEVDEP_fluentui_react } from './rules/FN002027_DEVDEP_fluentui_react';
 import { FN010001_YORC_version } from './rules/FN010001_YORC_version';
-import { FN014008_CODE_launch_hostedWorkbench_type } from './rules/FN014008_CODE_launch_hostedWorkbench_type';
 
 module.exports = [
-  new FN002027_DEVDEP_fluentui_react('', false),
   new FN001001_DEP_microsoft_sp_core_library('1.17.3'),
-  new FN001023_DEP_microsoft_sp_component_base('1.17.3'),
-  new FN001021_DEP_microsoft_sp_property_pane('1.17.3'),
-  new FN001004_DEP_microsoft_sp_webpart_base('1.17.3'),
   new FN001002_DEP_microsoft_sp_lodash_subset('1.17.3'),
   new FN001003_DEP_microsoft_sp_office_ui_fabric_core('1.17.3'),
+  new FN001004_DEP_microsoft_sp_webpart_base('1.17.3'),
   new FN001011_DEP_microsoft_sp_dialog('1.17.3'),
   new FN001012_DEP_microsoft_sp_application_base('1.17.3'),
+  new FN001013_DEP_microsoft_decorators('1.17.3'),
   new FN001014_DEP_microsoft_sp_listview_extensibility('1.17.3'),
+  new FN001021_DEP_microsoft_sp_property_pane('1.17.3'),
+  new FN001023_DEP_microsoft_sp_component_base('1.17.3'),
   new FN001024_DEP_microsoft_sp_diagnostics('1.17.3'),
   new FN001025_DEP_microsoft_sp_dynamic_data('1.17.3'),
   new FN001026_DEP_microsoft_sp_extension_base('1.17.3'),
@@ -48,14 +46,12 @@ module.exports = [
   new FN001030_DEP_microsoft_sp_module_interfaces('1.17.3'),
   new FN001031_DEP_microsoft_sp_odata_types('1.17.3'),
   new FN001032_DEP_microsoft_sp_page_context('1.17.3'),
-  new FN001013_DEP_microsoft_decorators('1.17.3'),
   new FN001034_DEP_microsoft_sp_adaptive_card_extension_base('1.17.3'),
   new FN001035_DEP_fluentui_react('7.199.1'),
-  new FN002020_DEVDEP_microsoft_rush_stack_compiler_4_5('0.5.0'),
-  new FN002022_DEVDEP_microsoft_eslint_plugin_spfx('1.17.3'),
-  new FN002023_DEVDEP_microsoft_eslint_config_spfx('1.17.3'),
   new FN002001_DEVDEP_microsoft_sp_build_web('1.17.3'),
   new FN002002_DEVDEP_microsoft_sp_module_interfaces('1.17.3'),
-  new FN010001_YORC_version('1.17.3'),
-  new FN014008_CODE_launch_hostedWorkbench_type('msedge')
+  new FN002022_DEVDEP_microsoft_eslint_plugin_spfx('1.17.3'),
+  new FN002023_DEVDEP_microsoft_eslint_config_spfx('1.17.3'),
+  new FN002027_DEVDEP_fluentui_react('', false),
+  new FN010001_YORC_version('1.17.3')
 ];
