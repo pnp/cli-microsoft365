d30556a6e38e33b0479943343e1cf303cd9c4fea
Nanddeep Nachan <nanddeepnachan@gmail.com>
2023-07-10 08:55:44 +0000

    Extends `spo tenant commandset set`. Closes #4961

--- src/m365/spo/commands/tenant/tenant-commandset-set.ts
+++ src/m365/spo/commands/tenant/tenant-commandset-set.ts
@@ -3,20 +3,30 @@ import GlobalOptions from '../../../../GlobalOptions';
 import { validation } from '../../../../utils/validation';
 import SpoCommand from '../../../base/SpoCommand';
 import commands from '../../commands';
+import * as os from 'os';
 import { urlUtil } from '../../../../utils/urlUtil';
+import Command from '../../../../Command';
+import { ListItemInstance } from '../listitem/ListItemInstance';
+import { Cli } from '../../../../cli/Cli';
+import { Options as spoListItemListCommandOptions } from '../listitem/listitem-list';
+import * as spoListItemListCommand from '../listitem/listitem-list';
 import request, { CliRequestOptions } from '../../../../request';
 import { formatting } from '../../../../utils/formatting';
 import { spo } from '../../../../utils/spo';
+import { odata } from '../../../../utils/odata';
+import { Solution } from './Solution';
 
 interface CommandArgs {
   options: Options;
 }
 
 interface Options extends GlobalOptions {
-  id: string;
+  id?: string;
+  title?: string;
+  clientSideComponentId?: string;
   newTitle?: string;
   listType?: string;
-  clientSideComponentId?: string;
+  newClientSideComponentId?: string;
   clientSideComponentProperties?: string;
   webTemplate?: string;
   location?: string;
@@ -40,14 +50,18 @@ class SpoTenantCommandSetSetCommand extends SpoCommand {
     this.#initTelemetry();
     this.#initOptions();
     this.#initValidators();
+    this.#initOptionSets();
   }
 
   #initTelemetry(): void {
     this.telemetry.push((args: CommandArgs) => {
       Object.assign(this.telemetryProperties, {
+        title: typeof args.options.title !== 'undefined',
+        id: typeof args.options.id !== 'undefined',
+        clientSideComponentId: typeof args.options.clientSideComponentId !== 'undefined',
         newTitle: typeof args.options.newTitle !== 'undefined',
         listType: args.options.listType,
-        clientSideComponentId: typeof args.options.clientSideComponentId !== 'undefined',
+        newClientSideComponentId: typeof args.options.newClientSideComponentId !== 'undefined',
         clientSideComponentProperties: typeof args.options.clientSideComponentProperties !== 'undefined',
         webTemplate: typeof args.options.webTemplate !== 'undefined',
         location: args.options.location
@@ -58,17 +72,23 @@ class SpoTenantCommandSetSetCommand extends SpoCommand {
   #initOptions(): void {
     this.options.unshift(
       {
-        option: '-i, --id <id>'
+        option: '-i, --id [id]'
       },
       {
-        option: '-t, --newTitle [newTitle]'
+        option: '-t, --title [title]'
+      },
+      {
+        option: '-c, --clientSideComponentId [clientSideComponentId]'
+      },
+      {
+        option: '--newTitle [newTitle]'
       },
       {
         option: '-l, --listType [listType]',
         autocomplete: SpoTenantCommandSetSetCommand.listTypes
       },
       {
-        option: '-c, --clientSideComponentId [clientSideComponentId]'
+        option: '--newClientSideComponentId [newClientSideComponentId]'
       },
       {
         option: '-p, --clientSideComponentProperties [clientSideComponentProperties]'
@@ -88,15 +108,23 @@ class SpoTenantCommandSetSetCommand extends SpoCommand {
       async (args: CommandArgs) => {
         if (!args.options.newTitle &&
           !args.options.listType &&
-          !args.options.clientSideComponentId &&
+          !args.options.newClientSideComponentId &&
           !args.options.clientSideComponentProperties &&
           !args.options.webTemplate &&
           !args.options.location) {
           return 'Specify at least one property to update';
         }
 
+        if (args.options.id && isNaN(parseInt(args.options.id))) {
+          return `${args.options.id} is not a number`;
+        }
+
         if (args.options.clientSideComponentId && !validation.isValidGuid(args.options.clientSideComponentId)) {
           return `${args.options.clientSideComponentId} is not a valid GUID`;
+        }
+
+        if (args.options.newClientSideComponentId && !validation.isValidGuid(args.options.newClientSideComponentId)) {
+          return `${args.options.newClientSideComponentId} is not a valid GUID`;
         }
 
         if (args.options.listType && SpoTenantCommandSetSetCommand.listTypes.indexOf(args.options.listType) < 0) {
@@ -112,6 +140,10 @@ class SpoTenantCommandSetSetCommand extends SpoCommand {
     );
   }
 
+  #initOptionSets(): void {
+    this.optionSets.push({ options: ['title', 'id', 'clientSideComponentId'] });
+  }
+
   public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
     try {
       const appCatalogUrl = await spo.getTenantAppCatalogUrl(logger, this.debug);
@@ -120,36 +152,116 @@ class SpoTenantCommandSetSetCommand extends SpoCommand {
         throw 'No app catalog URL found';
       }
 
-      const listServerRelativeUrl: string = urlUtil.getServerRelativePath(appCatalogUrl, '/lists/TenantWideExtensions');
-      const listItem = await this.getListItemById(logger, appCatalogUrl, listServerRelativeUrl, args.options.id);
+      if (args.options.newClientSideComponentId !== undefined) {
+        const componentManifest = await this.getComponentManifest(appCatalogUrl, args.options.newClientSideComponentId, logger);
+        const clientComponentManifest = JSON.parse(componentManifest.ClientComponentManifest);
 
-      if (listItem.TenantWideExtensionLocation.indexOf("ClientSideExtension.ListViewCommandSet") === -1) {
-        throw 'The item is not a ListViewCommandSet';
+        if (clientComponentManifest.extensionType !== "ListViewCommandSet") {
+          throw `The extension type of this component is not of type 'ListViewCommandSet' but of type '${clientComponentManifest.extensionType}'`;
+        }
+
+        const solution = await this.getSolutionFromAppCatalog(appCatalogUrl, componentManifest.SolutionId, logger);
+
+        if (!solution.ContainsTenantWideExtension) {
+          throw `The solution does not contain an extension that can be deployed to all sites. Make sure that you've entered the correct component Id.`;
+        }
+        else if (!solution.SkipFeatureDeployment) {
+          throw 'The solution has not been deployed to all sites. Make sure to deploy this solution to all sites.';
+        }
       }
 
-      await this.updateTenantWideExtension(appCatalogUrl, args.options, listServerRelativeUrl, logger);
+      const listServerRelativeUrl: string = urlUtil.getServerRelativePath(appCatalogUrl, '/lists/TenantWideExtensions');
+      const listItemId: number = await this.getListItemId(appCatalogUrl, args.options, listServerRelativeUrl, logger);
+
+      await this.updateTenantWideExtension(logger, appCatalogUrl, args.options, listServerRelativeUrl, listItemId);
     }
     catch (err: any) {
       this.handleRejectedODataJsonPromise(err);
     }
   }
 
-  private async getListItemById(logger: Logger, webUrl: string, listServerRelativeUrl: string, id: string): Promise<any> {
+  private async getComponentManifest(appCatalogUrl: string, clientSideComponentId: string, logger: Logger): Promise<any> {
     if (this.verbose) {
-      logger.logToStderr(`Getting the list item by id ${id}`);
+      logger.logToStderr('Retrieving component manifest item from the ComponentManifests list on the app catalog site so that we get the solution id');
     }
-    const reqOptions: CliRequestOptions = {
-      url: `${webUrl}/_api/web/GetList('${formatting.encodeQueryParameter(listServerRelativeUrl)}')/Items(${id})`,
-      headers: {
-        'accept': 'application/json;odata=nometadata'
-      },
-      responseType: 'json'
+
+    const camlQuery = `<View><ViewFields><FieldRef Name='ClientComponentId'></FieldRef><FieldRef Name='SolutionId'></FieldRef><FieldRef Name='ClientComponentManifest'></FieldRef></ViewFields><Query><Where><Eq><FieldRef Name='ClientComponentId' /><Value Type='Guid'>${clientSideComponentId}</Value></Eq></Where></Query></View>`;
+    const commandOptions: spoListItemListCommandOptions = {
+      webUrl: appCatalogUrl,
+      listUrl: `${urlUtil.getServerRelativeSiteUrl(appCatalogUrl)}/Lists/ComponentManifests`,
+      camlQuery: camlQuery,
+      verbose: this.verbose,
+      debug: this.debug,
+      output: 'json'
     };
 
-    return await request.get<any>(reqOptions);
+    const output = await Cli.executeCommandWithOutput(spoListItemListCommand as Command, { options: { ...commandOptions, _: [] } });
+
+    if (this.verbose) {
+      logger.logToStderr(output.stderr);
+    }
+
+    const outputParsed = JSON.parse(output.stdout);
+
+    if (outputParsed.length === 0) {
+      throw 'No component found with the specified clientSideComponentId found in the component manifest list. Make sure that the application is added to the application catalog';
+    }
+
+    return outputParsed[0];
   }
 
-  private async updateTenantWideExtension(appCatalogUrl: string, options: Options, listServerRelativeUrl: string, logger: Logger): Promise<void> {
+  private async getSolutionFromAppCatalog(appCatalogUrl: string, solutionId: string, logger: Logger): Promise<Solution> {
+    if (this.verbose) {
+      logger.logToStderr(`Retrieving solution with id ${solutionId} from the application catalog`);
+    }
+
+    const camlQuery = `<View><ViewFields><FieldRef Name='SkipFeatureDeployment'></FieldRef><FieldRef Name='ContainsTenantWideExtension'></FieldRef></ViewFields><Query><Where><Eq><FieldRef Name='AppProductID' /><Value Type='Guid'>${solutionId}</Value></Eq></Where></Query></View>`;
+    const commandOptions: spoListItemListCommandOptions = {
+      webUrl: appCatalogUrl,
+      listUrl: `${urlUtil.getServerRelativeSiteUrl(appCatalogUrl)}/AppCatalog`,
+      camlQuery: camlQuery,
+      verbose: this.verbose,
+      debug: this.debug,
+      output: 'json'
+    };
+
+    const output = await Cli.executeCommandWithOutput(spoListItemListCommand as Command, { options: { ...commandOptions, _: [] } });
+
+    if (this.verbose) {
+      logger.logToStderr(output.stderr);
+    }
+
+    const outputParsed = JSON.parse(output.stdout);
+
+    if (outputParsed.length === 0) {
+      throw `No component found with the solution id ${solutionId}. Make sure that the solution is available in the app catalog`;
+    }
+
+    return outputParsed[0];
+  }
+
+  private async getListItemId(appCatalogUrl: string, options: Options, listServerRelativeUrl: string, logger: Logger): Promise<number> {
+    const { title, id, clientSideComponentId } = options;
+    const filter = title ? `Title eq '${title}'` : id ? `Id eq '${id}'` : `TenantWideExtensionComponentId eq '${clientSideComponentId}'`;
+
+    if (this.verbose) {
+      logger.logToStderr(`Getting tenant-wide listview commandset: "${title || id || clientSideComponentId}"...`);
+    }
+
+    const listItemInstances = await odata.getAllItems<ListItemInstance>(`${appCatalogUrl}/_api/web/GetList('${formatting.encodeQueryParameter(listServerRelativeUrl)}')/Items?$filter=startswith(TenantWideExtensionLocation, 'ClientSideExtension.ListViewCommandSet') and ${filter}`);
+
+    if (!listItemInstances || listItemInstances.length === 0) {
+      throw 'The specified listview commandset was not found';
+    }
+
+    if (listItemInstances.length > 1) {
+      throw `Multiple listview commandsets with ${title ? `title '${title}'` : `ClientSideComponentId '${clientSideComponentId}'`} found. Please disambiguate using IDs: ${os.EOL}${listItemInstances.map(item => `- ${(item as any).Id}`).join(os.EOL)}`;
+    }
+
+    return listItemInstances[0].Id;
+  }
+
+  private async updateTenantWideExtension(logger: Logger, appCatalogUrl: string, options: Options, listServerRelativeUrl: string, listItemId: number): Promise<void> {
     if (this.verbose) {
       logger.logToStderr('Updating tenant wide extension to the TenantWideExtensions list');
     }
@@ -162,10 +274,10 @@ class SpoTenantCommandSetSetCommand extends SpoCommand {
       });
     }
 
-    if (options.clientSideComponentId !== undefined) {
+    if (options.newClientSideComponentId !== undefined) {
       formValues.push({
         FieldName: 'TenantWideExtensionComponentId',
-        FieldValue: options.clientSideComponentId
+        FieldValue: options.newClientSideComponentId
       });
     }
 
@@ -199,7 +311,7 @@ class SpoTenantCommandSetSetCommand extends SpoCommand {
 
     const requestOptions: CliRequestOptions =
     {
-      url: `${appCatalogUrl}/_api/web/GetList('${formatting.encodeQueryParameter(listServerRelativeUrl)}')/Items(${options.id})/ValidateUpdateListItem()`,
+      url: `${appCatalogUrl}/_api/web/GetList('${formatting.encodeQueryParameter(listServerRelativeUrl)}')/Items(${listItemId})/ValidateUpdateListItem()`,
       headers: {
         'accept': 'application/json;odata=nometadata'
       },
