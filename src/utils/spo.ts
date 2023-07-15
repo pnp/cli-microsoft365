053261ddc82ecb6eafdc4da7811ebff84032a084
Nico De Cleyre <nico.de.cleyre@gmail.com>
2023-07-15 22:02:20 +0200

    Added logging

--- src/utils/spo.ts
+++ src/utils/spo.ts
@@ -17,7 +17,6 @@ import { SiteProperties } from '../m365/spo/commands/site/SiteProperties.js';
 import { aadGroup } from './aadGroup.js';
 import { SharingCapabilities } from '../m365/spo/commands/site/SharingCapabilities.js';
 import { WebProperties } from '../m365/spo/commands/web/WebProperties.js';
-import { FileProperties } from '../m365/spo/commands/file/FileProperties.js';
 
 export interface ContextInfo {
   FormDigestTimeoutSeconds: number;
