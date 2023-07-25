98bcc913969f22b723d8a7054b8680e2be71050f
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2023-07-25 16:58:25 +0200

    Adds command `spo listitem attachment set´

--- docs/src/config/sidebars.js
+++ docs/src/config/sidebars.js
@@ -2636,6 +2636,11 @@ const sidebars = {
             },
             {
               type: 'doc',
+              label: 'listitem attachment set',
+              id: 'cmd/spo/listitem/listitem-attachment-set'
+            },
+            {
+              type: 'doc',
               label: 'listitem batch add',
               id: 'cmd/spo/listitem/listitem-batch-add'
             },
--- src/m365/spo/commands.ts
+++ src/m365/spo/commands.ts
@@ -158,6 +158,7 @@ export default {
   LIST_WEBHOOK_SET: `${prefix} list webhook set`,
   LISTITEM_ADD: `${prefix} listitem add`,
   LISTITEM_ATTACHMENT_LIST: `${prefix} listitem attachment list`,
+  LISTITEM_ATTACHMENT_SET: `${prefix} listitem attachment set`,
   LISTITEM_BATCH_ADD: `${prefix} listitem batch add`,
   LISTITEM_BATCH_SET: `${prefix} listitem batch set`,
   LISTITEM_GET: `${prefix} listitem get`,
