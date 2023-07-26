2fd89d7e96ceeec26e482993568915cc677b55f4
Milan Holemans <11723921+milanholemans@users.noreply.github.com>
2023-07-27 01:32:21 +0200

    Updates 'teams meeting list' command response. Closes #4782

--- src/m365/teams/commands/meeting/meeting-list.ts
+++ src/m365/teams/commands/meeting/meeting-list.ts
@@ -1,7 +1,6 @@
 import { Event } from '@microsoft/microsoft-graph-types';
 import auth from '../../../../Auth.js';
 import GlobalOptions from '../../../../GlobalOptions.js';
-import { Cli } from '../../../../cli/Cli.js';
 import { Logger } from '../../../../cli/Logger.js';
 import { aadUser } from '../../../../utils/aadUser.js';
 import { accessToken } from '../../../../utils/accessToken.js';
@@ -9,6 +8,8 @@ import { odata } from '../../../../utils/odata.js';
 import { validation } from '../../../../utils/validation.js';
 import GraphCommand from "../../../base/GraphCommand.js";
 import commands from '../../commands.js';
+import request, { CliRequestOptions } from '../../../../request.js';
+import { formatting } from '../../../../utils/formatting.js';
 
 interface CommandArgs {
   options: Options;
@@ -33,7 +34,7 @@ class TeamsMeetingListCommand extends GraphCommand {
   }
 
   public defaultProperties(): string[] | undefined {
-    return ['subject', 'start', 'end'];
+    return ['subject', 'startDateTime', 'endDateTime'];
   }
 
   constructor() {
@@ -83,15 +84,27 @@ class TeamsMeetingListCommand extends GraphCommand {
     this.validators.push(
       async (args: CommandArgs) => {
         if (!validation.isValidISODateTime(args.options.startDateTime)) {
-          return `'${args.options.startDateTime}' is not a valid ISO date string`;
-        }
-
-        if (args.options.userId && !validation.isValidGuid(args.options.userId)) {
-          return `${args.options.userId} is not a valid Guid`;
+          return `'${args.options.startDateTime}' is not a valid ISO date string for startDateTime.`;
         }
 
         if (args.options.endDateTime && !validation.isValidISODateTime(args.options.endDateTime)) {
-          return `'${args.options.startDateTime}' is not a valid ISO date string`;
+          return `'${args.options.startDateTime}' is not a valid ISO date string for endDateTime.`;
+        }
+
+        if (args.options.startDateTime && args.options.endDateTime && args.options.startDateTime > args.options.endDateTime) {
+          return 'startDateTime value must be before endDateTime.';
+        }
+
+        if (args.options.userId && !validation.isValidGuid(args.options.userId)) {
+          return `${args.options.userId} is not a valid GUID for userId.`;
+        }
+
+        if (args.options.userName && !validation.isValidUserPrincipalName(args.options.userName)) {
+          return `'${args.options.userName}' is not a valid UPN for userName.`;
+        }
+
+        if (args.options.email && !validation.isValidUserPrincipalName(args.options.email)) {
+          return `'${args.options.email}' is not a valid UPN for email.`;
         }
 
         return true;
@@ -101,66 +114,113 @@ class TeamsMeetingListCommand extends GraphCommand {
 
   public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
     try {
-      const isAppOnlyAccessToken: boolean | undefined = accessToken.isAppOnlyAccessToken(auth.service.accessTokens[this.resource].accessToken);
+      const isAppOnlyAccessToken = accessToken.isAppOnlyAccessToken(auth.service.accessTokens[this.resource].accessToken)!;
+      if (isAppOnlyAccessToken && !args.options.userId && !args.options.userName && !args.options.email) {
+        throw `The option 'userId', 'userName' or 'email' is required when retrieving meetings using app only permissions`;
+      }
+      else if (!isAppOnlyAccessToken && (args.options.userId || args.options.userName || args.options.email)) {
+        throw `The options 'userId', 'userName' and 'email' cannot be used when retrieving meetings using delegated permissions`;
+      }
       if (this.verbose) {
-        await logger.logToStderr(`Retrieving meetings for ${isAppOnlyAccessToken ? 'specific user' : 'currently logged in user'}`);
+        await logger.logToStderr(`Retrieving meetings for user: ${args.options.userId || args.options.userName || args.options.email || accessToken.getUserNameFromAccessToken(auth.service.accessTokens[this.resource].accessToken)}...`);
       }
 
-      let requestUrl = `${this.resource}/v1.0/`;
-      if (isAppOnlyAccessToken) {
-        if (!args.options.userId && !args.options.userName && !args.options.email) {
-          throw `The option 'userId', 'userName' or 'email' is required when retrieving meetings using app only permissions`;
-        }
+      const graphBaseUrl = await this.getGraphBaseUrl(args.options);
+      const meetingUrls = await this.getMeetingJoinUrls(graphBaseUrl, args.options);
+      const meetings = await this.getTeamsMeetings(logger, graphBaseUrl, meetingUrls);
 
-        requestUrl += 'users/';
-        if (args.options.userId) {
-          requestUrl += args.options.userId;
-        }
-        else if (args.options.userName) {
-          requestUrl += args.options.userName;
-        }
-        else if (args.options.email) {
-          const userId = await aadUser.getUserIdByEmail(args.options.email);
-          requestUrl += userId;
-        }
-      }
-      else {
-        if (args.options.userId || args.options.userName || args.options.email) {
-          throw `The options 'userId', 'userName' and 'email' cannot be used when retrieving meetings using delegated permissions`;
-        }
-
-        requestUrl += `me`;
-      }
-
-      requestUrl += `/events?$filter=start/dateTime ge '${args.options.startDateTime}'`;
-
-      if (args.options.endDateTime) {
-        requestUrl += ` and end/dateTime le '${args.options.endDateTime}'`;
-      }
-
-      if (args.options.isOrganizer) {
-        requestUrl += ' and isOrganizer eq true';
-      }
-
-      const res = await odata.getAllItems<Event>(requestUrl);
-      const resFiltered = res.filter(y => y.isOnlineMeeting);
-      if (!args.options.output || !Cli.shouldTrimOutput(args.options.output)) {
-        await logger.log(resFiltered);
-      }
-      else {
-        //converted to text friendly output
-        await logger.log(resFiltered.map(i => {
-          return {
-            subject: i.subject,
-            start: i.start!.dateTime,
-            end: i.end!.dateTime
-          };
-        }));
-      }
+      await logger.log(meetings);
     }
     catch (err: any) {
       this.handleRejectedODataJsonPromise(err);
     }
+  }
+
+  /**
+   * Get the first part of the Graph API URL that contains the user information.
+   */
+  private async getGraphBaseUrl(options: Options): Promise<string> {
+    let requestUrl = `${this.resource}/v1.0/`;
+
+    if (options.userId || options.userName) {
+      requestUrl += `users/${options.userId || options.userName}`;
+    }
+    else if (options.email) {
+      const userId = await aadUser.getUserIdByEmail(options.email);
+      requestUrl += `users/${userId}`;
+    }
+    else {
+      requestUrl += 'me';
+    }
+
+    return requestUrl;
+  }
+
+  /**
+   * Gets the meeting join urls for the specified user using calendar events.
+   */
+  private async getMeetingJoinUrls(graphBaseUrl: string, options: Options): Promise<string[]> {
+    let requestUrl = graphBaseUrl;
+
+    requestUrl += `/events?$filter=start/dateTime ge '${options.startDateTime}'`;
+    if (options.endDateTime) {
+      requestUrl += ` and end/dateTime lt '${options.endDateTime}'`;
+    }
+    if (options.isOrganizer) {
+      requestUrl += ' and isOrganizer eq true';
+    }
+    requestUrl += '&$select=onlineMeeting';
+
+    const items = await odata.getAllItems<Event>(requestUrl);
+    const result = items.filter(i => i.onlineMeeting).map(i => i.onlineMeeting!.joinUrl!);
+
+    return result;
+  }
+
+  private async getTeamsMeetings(logger: Logger, graphBaseUrl: string, meetingUrls: string[]): Promise<any[]> {
+    const graphRelativeUrl = graphBaseUrl.replace(`${this.resource}/v1.0/`, '');
+    let result: any[] = [];
+
+    for (let i = 0; i < meetingUrls.length; i += 20) {
+      if (this.verbose) {
+        logger.logToStderr(`Retrieving meetings ${i + 1}-${Math.min(i + 20, meetingUrls.length)}...`);
+      }
+      const batch = meetingUrls.slice(i, i + 20);
+      const requestOptions: CliRequestOptions = {
+        url: `${this.resource}/v1.0/$batch`,
+        headers: {
+          accept: 'application/json',
+          'content-type': 'application/json'
+        },
+        responseType: 'json',
+        data: {
+          requests: batch.map((url, index) => ({
+            id: i + index,
+            method: 'GET',
+            url: `${graphRelativeUrl}/onlineMeetings?$filter=joinWebUrl eq '${formatting.encodeQueryParameter(url)}'`
+          }))
+        }
+      };
+
+      const requestResponse = await request.post<{ responses: { id: string; status: number; headers: any; body: any; }[] }>(requestOptions);
+
+      for (const response of requestResponse.responses) {
+        if (response.status === 200) {
+          result.push(response.body.value[0]);
+        }
+        else {
+          // Encountered errors where message was empty resulting in [object Object] error messages
+          if (!response.body.error.message) {
+            throw response.body.error.code;
+          }
+          throw response.body;
+        }
+      }
+    }
+
+    // Sort all meetings by start date
+    result = result.sort((a, b) => a.startDateTime < b.startDateTime ? -1 : a.startDateTime > b.startDateTime ? 1 : 0);
+    return result;
   }
 }
 
