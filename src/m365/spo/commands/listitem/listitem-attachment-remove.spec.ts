b9c1ef2567314c4affdd9922bf7a25b01e592926
Nanddeep Nachan <nanddeepnachan@gmail.com>
2023-09-04 13:40:05 +0000

    command name

--- src/m365/spo/commands/listitem/listitem-attachment-remove.spec.ts
+++ src/m365/spo/commands/listitem/listitem-attachment-remove.spec.ts
@@ -13,7 +13,7 @@ import { sinonUtil } from '../../../../utils/sinonUtil.js';
 import commands from '../../commands.js';
 import command from './listitem-attachment-remove.js';
 
-describe(commands.LISTITEM_ATTACHMENT_LIST, () => {
+describe(commands.LISTITEM_ATTACHMENT_REMOVE, () => {
   const webUrl = 'https://contoso.sharepoint.com/sites/project-x';
   const listId = '4fc5ba1e-18b7-49e0-81fe-54515cc2eede';
   const listTitle = 'Demo List';
