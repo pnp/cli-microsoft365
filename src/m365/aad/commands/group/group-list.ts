753bc1ae1cbba91f947762dc69eb3f55dc41e2a8
Smita Nachan <smita.nachan@gmail.com>
2023-10-12 13:21:40 +0000

    Extends 'aad group list' command with 'type' option. Closes #5424

--- src/m365/aad/commands/group/group-list.ts
+++ src/m365/aad/commands/group/group-list.ts
@@ -2,12 +2,17 @@ import { Group } from '@microsoft/microsoft-graph-types';
 import { Cli } from '../../../../cli/Cli.js';
 import { Logger } from '../../../../cli/Logger.js';
 import GlobalOptions from '../../../../GlobalOptions.js';
+import { CliRequestOptions } from '../../../../request.js';
 import { odata } from '../../../../utils/odata.js';
 import GraphCommand from '../../../base/GraphCommand.js';
 import commands from '../../commands.js';
 
 interface CommandArgs {
-  options: GlobalOptions;
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  type?: string;
 }
 
 interface ExtendedGroup extends Group {
@@ -27,9 +32,82 @@ class AadGroupListCommand extends GraphCommand {
     return ['id', 'displayName', 'groupType'];
   }
 
+  constructor() {
+    super();
+
+    this.#initTelemetry();
+    this.#initOptions();
+    this.#initValidators();
+  }
+
+  #initTelemetry(): void {
+    this.telemetry.push((args: CommandArgs) => {
+      Object.assign(this.telemetryProperties, {
+        type: typeof args.options.type !== 'undefined'
+      });
+    });
+  }
+
+  #initOptions(): void {
+    this.options.unshift(
+      {
+        option: '--type [type]',
+        autocomplete: ['microsoft365', 'security', 'distribution', 'mailEnabledSecurity']
+      }
+    );
+  }
+
+  #initValidators(): void {
+    this.validators.push(
+      async (args: CommandArgs) => {
+        if (args.options.type && ['microsoft365', 'security', 'distribution', 'mailEnabledSecurity'].indexOf(args.options.type) === -1) {
+          return `${args.options.type} is not a valid type value. Allowed values microsoft365|security|distribution|mailEnabledSecurity.`;
+        }
+
+        return true;
+      }
+    );
+  }
+
   public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
     try {
-      const groups = await odata.getAllItems<Group>(`${this.resource}/v1.0/groups`);
+      let requestUrl: string = `${this.resource}/v1.0/groups`;
+
+      if (args.options.type) {
+        switch (args.options.type) {
+          case 'microsoft365':
+            requestUrl += `?$filter=groupTypes/any(c:c+eq+'Unified')`;
+            break;
+          case 'security':
+            requestUrl += '?$filter=securityEnabled eq true and mailEnabled eq false';
+            break;
+          case 'distribution':
+            requestUrl += '?$filter=securityEnabled eq false and mailEnabled eq true';
+            break;
+          case 'mailEnabledSecurity':
+            requestUrl += `?$filter=securityEnabled eq true and mailEnabled eq true and not(groupTypes/any(t:t eq 'Unified'))&$count=true`;
+            break;
+        }
+      }
+
+      let groups: Group[] = [];
+
+      if (args.options.type === 'mailEnabledSecurity') {
+        // While using not() function in the filter, we need to specify the ConsistencyLevel header.
+        const requestOptions: CliRequestOptions = {
+          url: requestUrl,
+          headers: {
+            accept: 'application/json;odata.metadata=none',
+            ConsistencyLevel: 'eventual'
+          },
+          responseType: 'json'
+        };
+
+        groups = await odata.getAllItems<Group>(requestOptions);
+      }
+      else {
+        groups = await odata.getAllItems<Group>(requestUrl);
+      }
 
       if (Cli.shouldTrimOutput(args.options.output)) {
         groups.forEach((group: ExtendedGroup) => {
