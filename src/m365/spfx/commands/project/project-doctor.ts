47513dca821d2cb1ff143c10786afcd9cb1acd1a
Waldek Mastykarz <waldek@mastykarz.nl>
2023-10-26 20:08:57 +0200

    Adds support for SPFx v1.18.1.-rc.0. Closes #5593

--- src/m365/spfx/commands/project/project-doctor.ts
+++ src/m365/spfx/commands/project/project-doctor.ts
@@ -71,7 +71,8 @@ class SpfxProjectDoctorCommand extends BaseProjectCommand {
     '1.17.2',
     '1.17.3',
     '1.17.4',
-    '1.18.0'
+    '1.18.0',
+    '1.18.1-rc.0'
   ];
 
   protected get allowedOutputs(): string[] {
