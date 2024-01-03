705df827c3f8f7cd93d219f669ae386882204194
Waldek Mastykarz <waldek@mastykarz.nl>
2024-01-03 15:03:41 +0100

    Switches to Node@20. Closes #5743

--- scripts/check-version.js
+++ scripts/check-version.js
@@ -1,6 +1,6 @@
 const nodeVersion = process.versions.node.split('.')[0];
 
-if (nodeVersion !== "18") {
-  console.error("Node version must be 18");
+if (nodeVersion !== "20") {
+  console.error("Node version must be 20");
   process.exitCode = 1;
 }
