8feea8bbcfca47d1371c885ce6423babf28e1886
reshmee011 <reshmee011@gmail.com>
2024-01-30 09:47:32 +0000

    more options to spo user remove

--- src/m365/spo/commands/user/user-remove.spec.ts
+++ src/m365/spo/commands/user/user-remove.spec.ts
@@ -4,6 +4,7 @@ import auth from '../../../../Auth.js';
 import { cli } from '../../../../cli/cli.js';
 import { CommandInfo } from '../../../../cli/CommandInfo.js';
 import { Logger } from '../../../../cli/Logger.js';
+import { formatting } from '../../../../utils/formatting.js';
 import { CommandError } from '../../../../Command.js';
 import request from '../../../../request.js';
 import { telemetry } from '../../../../telemetry.js';
@@ -15,6 +16,78 @@ import command from './user-remove.js';
 import { settingsNames } from '../../../../settingsNames.js';
 
 describe(commands.USER_REMOVE, () => {
+  const validUserName = 'john.deo_hotmail.com#ext#@contoso.onmicrosoft.com';
+  const validEmail = 'john.deo@contoso.onmicrosoft.com';
+  const validEntraGroupId = '2056d2f6-3257-4253-8cfc-b73393e414e5';
+  const validEntraM365GroupName = 'Finance';
+  const validEntraSecurityGroupName = 'EntraGroupTest';
+  const validLoginName = `i:0#.f|membership|${validUserName}`;
+  const validWebUrl = 'https://contoso.sharepoint.com/subsite';
+  const groupM365Response = {
+    value: [{
+      "id": "2056d2f6-3257-4253-8cfc-b73393e414e5",
+      "deletedDateTime": null,
+      "classification": null,
+      "createdDateTime": "2017-11-29T03:27:05Z",
+      "description": "This is the Contoso Finance Group. Please come here and check out the latest news, posts, files, and more.",
+      "displayName": "Finance",
+      "groupTypes": [
+        "Unified"
+      ],
+      "mail": "finance@contoso.onmicrosoft.com",
+      "mailEnabled": true,
+      "mailNickname": "finance",
+      "onPremisesLastSyncDateTime": null,
+      "onPremisesProvisioningErrors": [],
+      "onPremisesSecurityIdentifier": null,
+      "onPremisesSyncEnabled": null,
+      "preferredDataLocation": null,
+      "proxyAddresses": [
+        "SMTP:finance@contoso.onmicrosoft.com"
+      ],
+      "renewedDateTime": "2017-11-29T03:27:05Z",
+      "securityEnabled": false,
+      "visibility": "Public"
+    }]
+  };
+
+  const groupSecurityResponse = {
+    value: [{
+      "id": "2056d2f6-3257-4253-8cfc-b73393e414e5",
+      "deletedDateTime": null,
+      "classification": null,
+      "createdDateTime": "2024-01-27T16:02:56Z",
+      "creationOptions": [],
+      "description": "Entra Group Test",
+      "displayName": "EntraGroupTest",
+      "expirationDateTime": null,
+      "groupTypes": [],
+      "isAssignableToRole": true,
+      "mail": null,
+      "mailEnabled": false,
+      "mailNickname": "f45205a2-d",
+      "membershipRule": null,
+      "membershipRuleProcessingState": null,
+      "onPremisesDomainName": null,
+      "onPremisesLastSyncDateTime": null,
+      "onPremisesNetBiosName": null,
+      "onPremisesSamAccountName": null,
+      "onPremisesSecurityIdentifier": null,
+      "onPremisesSyncEnabled": null,
+      "preferredDataLocation": null,
+      "preferredLanguage": null,
+      "proxyAddresses": [],
+      "renewedDateTime": "2024-01-27T16:02:56Z",
+      "resourceBehaviorOptions": [],
+      "resourceProvisioningOptions": [],
+      "securityEnabled": true,
+      "securityIdentifier": "S-1-12-1-1968173404-1154184881-1694549896-3083850660",
+      "theme": null,
+      "visibility": "Private",
+      "onPremisesProvisioningErrors": [],
+      "serviceProvisioningErrors": []
+    }]
+  };
   let log: any[];
   let requests: any[];
   let logger: Logger;
@@ -54,6 +127,7 @@ describe(commands.USER_REMOVE, () => {
   afterEach(() => {
     sinonUtil.restore([
       request.post,
+      request.get,
       cli.promptForConfirmation,
       cli.getSettingWithDefaultValue
     ]);
@@ -72,24 +146,23 @@ describe(commands.USER_REMOVE, () => {
     assert.notStrictEqual(command.description, null);
   });
 
-  it('fails validation if id or loginName options are not passed', async () => {
+  it('fails validation if id or loginName or userName or email or entraGroupName or entraGroupId options are not passed', async () => {
     sinon.stub(cli, 'getSettingWithDefaultValue').callsFake((settingName, defaultValue) => {
       if (settingName === settingsNames.prompt) {
         return false;
       }
-
       return defaultValue;
     });
 
     const actual = await command.validate({
       options: {
-        webUrl: 'https://contoso.sharepoint.com'
+        webUrl: validWebUrl
       }
     }, commandInfo);
     assert.notStrictEqual(actual, true);
   });
 
-  it('fails validation if id or loginname options are passed', async () => {
+  it('fails validation if more than one of the options userName or email or entraGroupName or entraGroupId are passed', async () => {
     sinon.stub(cli, 'getSettingWithDefaultValue').callsFake((settingName, defaultValue) => {
       if (settingName === settingsNames.prompt) {
         return false;
@@ -100,9 +173,9 @@ describe(commands.USER_REMOVE, () => {
 
     const actual = await command.validate({
       options: {
-        webUrl: 'https://contoso.sharepoint.com',
+        webUrl: validWebUrl,
         id: 10,
-        loginName: "i:0#.f|membership|john.doe@mytenant.onmicrosoft.com"
+        loginName: validLoginName
       }
     }, commandInfo);
     assert.notStrictEqual(actual, true);
@@ -118,6 +191,57 @@ describe(commands.USER_REMOVE, () => {
     }, commandInfo);
     assert.notStrictEqual(actual, true);
   });
+
+
+  it('fails validation if entraGroupId is not a valid id', async () => {
+    const actual = await command.validate({ options: { webUrl: validWebUrl, entraGroupId: 'invalid' } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if id is not a valid id', async () => {
+    const actual = await command.validate({ options: { webUrl: validWebUrl, id: 'invalid' } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if userName is not a valid user principal name', async () => {
+    const actual = await command.validate({ options: { webUrl: validWebUrl, userName: 'invalid' } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if email is not a valid user principal name', async () => {
+    const actual = await command.validate({ options: { webUrl: validWebUrl, email: 'invalid' } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+  it('passes validation url is valid and id is passed', async () => {
+    const actual = await command.validate({ options: { webUrl: validWebUrl, id: 1 } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('passes validation if the url is valid and email is passed', async () => {
+    const actual = await command.validate({ options: { webUrl: validWebUrl, email: validEmail } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('passes validation if the url is valid and loginName is passed', async () => {
+    const actual = await command.validate({ options: { webUrl: validWebUrl, loginName: validLoginName } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('passes validation if the url is valid and userName is passed', async () => {
+    const actual = await command.validate({ options: { webUrl: validWebUrl, userName: validUserName } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('passes validation if the url is valid and entraGroupName is passed', async () => {
+    const actual = await command.validate({ options: { webUrl: validWebUrl, entraGroupName: validEntraM365GroupName } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('passes validation if the url is valid and entraGroupId is passed', async () => {
+    const actual = await command.validate({ options: { webUrl: validWebUrl, entraGroupId: validEntraGroupId } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
 
   it('should prompt before removing user using id from web when confirmation argument not passed ', async () => {
     await command.action(logger, {
@@ -235,8 +359,8 @@ describe(commands.USER_REMOVE, () => {
     sinon.stub(cli, 'promptForConfirmation').resolves(true);
     await command.action(logger, {
       options: {
-        webUrl: "https://contoso.sharepoint.com/subsite",
-        loginName: "i:0#.f|membership|john.doe@mytenant.onmicrosoft.com"
+        webUrl: validWebUrl,
+        loginName: validLoginName
       }
     });
     let correctRequestIssued = false;
@@ -303,7 +427,174 @@ describe(commands.USER_REMOVE, () => {
     assert(correctRequestIssued);
   });
 
-  it('handles error when removing using from web', async () => {
+  it('removes email by username successfully without prompting with confirmation argument', async () => {
+    let removeRequestIssued = false;
+
+    sinon.stub(request, 'get').callsFake(async (opts) => {
+      if ((opts.url as string).indexOf(`_api/web/siteusers/GetByEmail('${formatting.encodeQueryParameter(validEmail)}')`) > -1) {
+        return {
+          "Id": 10,
+          "IsHiddenInUI": false,
+          "LoginName": validLoginName,
+          "Title": "John Doe",
+          "PrincipalType": 1,
+          "Email": validEmail,
+          "Expiration": "",
+          "IsEmailAuthenticationGuestUser": false,
+          "IsShareByEmailGuestUser": false,
+          "IsSiteAdmin": false,
+          "UserId": { "NameId": "10010001b0c19a2", "NameIdIssuer": "urn:federation:microsoftonline" },
+          "UserPrincipalName": validUserName
+        };
+      }
+      throw 'Invalid request';
+    });
+
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      requests.push(opts);
+      if ((opts.url as string).indexOf('_api/web/siteusers/removebyid(10)') > -1) {
+        removeRequestIssued = true;
+        return Promise.resolve();
+      }
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        debug: true,
+        webUrl: validWebUrl,
+        email: validEmail,
+        force: true
+      }
+    });
+    assert(removeRequestIssued);
+  });
+
+  it('removes user by username successfully without prompting with confirmation argument', async () => {
+    sinon.stub(request, 'get').callsFake(async (opts) => {
+      requests.push(opts);
+      if ((opts.url as string).indexOf(`_api/web/siteusers?$filter=UserPrincipalName eq ('${formatting.encodeQueryParameter(validUserName)}')`) > -1) {
+        return {
+          "value": [{
+            "Id": 10,
+            "IsHiddenInUI": false,
+            "LoginName": validLoginName,
+            "Title": "John Doe",
+            "PrincipalType": 1,
+            "Email": validEmail,
+            "Expiration": "",
+            "IsEmailAuthenticationGuestUser": false,
+            "IsShareByEmailGuestUser": false,
+            "IsSiteAdmin": false,
+            "UserId": { "NameId": "10010001b0c19a2", "NameIdIssuer": "urn:federation:microsoftonline" },
+            "UserPrincipalName": validUserName
+          }]
+        };
+      }
+      throw 'Invalid request';
+    });
+
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      requests.push(opts);
+      if ((opts.url as string).indexOf('_api/web/siteusers/removebyid(10)') > -1) {
+        return true;
+      }
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        debug: true,
+        webUrl: validWebUrl,
+        userName: validUserName,
+        force: true
+      }
+    });
+    assert(true);
+  });
+
+  it('removes user by entraGroupId successfully without prompting with confirmation argument', async () => {
+    sinon.stub(request, 'get').callsFake(async (opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/groups/${validEntraGroupId}`) {
+        return groupM365Response.value[0];
+      }
+
+      throw 'Invalid request';
+    });
+
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      requests.push(opts);
+      if ((opts.url as string).indexOf(`${validWebUrl}/_api/web/siteusers/removeByLoginName('c:0o.c|federateddirectoryclaimprovider|${validEntraGroupId}')`) > -1) {
+        return true;
+      }
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        webUrl: validWebUrl,
+        entraGroupId: validEntraGroupId,
+        force: true
+      }
+    });
+    assert(true);
+  });
+
+  it('removes m365 group by entraGroupName successfully without prompting with confirmation argument', async () => {
+    sinon.stub(request, 'get').callsFake(async (opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/groups?$filter=displayName eq '${validEntraM365GroupName}'`) {
+        return groupM365Response;
+      }
+
+      throw 'Invalid request';
+    });
+
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      requests.push(opts);
+      if (opts.url === `${validWebUrl}/_api/web/siteusers/removeByLoginName('c:0o.c|federateddirectoryclaimprovider|${validEntraGroupId}')`) {
+        return true;
+      }
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        debug: true,
+        webUrl: validWebUrl,
+        entraGroupName: validEntraM365GroupName,
+        force: true
+      }
+    });
+    assert(true);
+  });
+
+  it('removes security group by entraGroupName successfully without prompting with confirmation argument', async () => {
+    sinon.stub(request, 'get').callsFake(async (opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/groups?$filter=displayName eq '${validEntraSecurityGroupName}'`) {
+        return groupSecurityResponse;
+      }
+
+      throw 'Invalid request';
+    });
+
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      requests.push(opts);
+      if (opts.url === `${validWebUrl}/_api/web/siteusers/removeByLoginName('c:0t.c|tenant|${validEntraGroupId}')`) {
+        return true;
+      }
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        webUrl: validWebUrl,
+        entraGroupName: validEntraSecurityGroupName,
+        force: true
+      }
+    } as any);
+  });
+
+  it('handles error when removing user using from web', async () => {
     sinon.stub(request, 'post').callsFake(async (opts) => {
       requests.push(opts);
       if ((opts.url as string).indexOf('_api/web/siteusers/removebyid(10)') > -1) {
@@ -320,4 +611,34 @@ describe(commands.USER_REMOVE, () => {
       }
     } as any), new CommandError('An error has occurred'));
   });
+
+
+  it('handles generic error when user not found when username is passed without prompting with confirmation argument', async () => {
+    sinon.stub(request, 'get').callsFake(async (opts) => {
+      requests.push(opts);
+      if ((opts.url as string).indexOf(`_api/web/siteusers?$filter=UserPrincipalName eq ('${formatting.encodeQueryParameter(validUserName)}')`) > -1) {
+        return { "value": [] };
+      }
+      throw 'Invalid request';
+    });
+
+    sinon.stub(request, 'post').callsFake(async (opts) => {
+      requests.push(opts);
+      if ((opts.url as string).indexOf('_api/web/siteusers/removebyid(10)') > -1) {
+        return Promise.resolve();
+      }
+      throw 'Invalid request';
+    });
+
+    await assert.rejects(command.action(logger, {
+      options: {
+        debug: true,
+        webUrl: validWebUrl,
+        userName: validUserName,
+        force: true
+      }
+    }), new CommandError(`User not found: ${validUserName}`));;
+  });
+
 });
+
--- src/m365/spo/commands/user/user-remove.ts
+++ src/m365/spo/commands/user/user-remove.ts
@@ -1,8 +1,10 @@
+import { Group } from '@microsoft/microsoft-graph-types';
 import { cli } from '../../../../cli/cli.js';
 import { Logger } from '../../../../cli/Logger.js';
 import GlobalOptions from '../../../../GlobalOptions.js';
 import request, { CliRequestOptions } from '../../../../request.js';
 import { formatting } from '../../../../utils/formatting.js';
+import { aadGroup } from '../../../../utils/aadGroup.js';
 import { validation } from '../../../../utils/validation.js';
 import SpoCommand from '../../../base/SpoCommand.js';
 import commands from '../../commands.js';
@@ -15,6 +17,10 @@ interface Options extends GlobalOptions {
   webUrl: string;
   id?: string;
   loginName?: string;
+  email?: string;
+  userName?: string;
+  entraGroupId?: string;
+  entraGroupName?: string;
   force: boolean;
 }
 
@@ -41,6 +47,10 @@ class SpoUserRemoveCommand extends SpoCommand {
       Object.assign(this.telemetryProperties, {
         id: (!(!args.options.id)).toString(),
         loginName: (!(!args.options.loginName)).toString(),
+        email: (!(!args.options.email)).toString(),
+        userName: (!(!args.options.userName)).toString(),
+        entraGroupId: (!(!args.options.entraGroupId)).toString(),
+        entraGroupName: (!(!args.options.entraGroupName)).toString(),
         force: (!(!args.options.force)).toString()
       });
     });
@@ -58,6 +68,18 @@ class SpoUserRemoveCommand extends SpoCommand {
         option: '--loginName [loginName]'
       },
       {
+        option: '--email [email]'
+      },
+      {
+        option: '--userName [userName]'
+      },
+      {
+        option: '--entraGroupId [entraGroupId]'
+      },
+      {
+        option: '--entraGroupName [entraGroupName]'
+      },
+      {
         option: '-f, --force'
       }
     );
@@ -66,13 +88,36 @@ class SpoUserRemoveCommand extends SpoCommand {
   #initValidators(): void {
     this.validators.push(
       async (args: CommandArgs) => {
-        return validation.isValidSharePointUrl(args.options.webUrl);
+        const isValidSharePointUrl: boolean | string = validation.isValidSharePointUrl(args.options.webUrl);
+        if (isValidSharePointUrl !== true) {
+          return isValidSharePointUrl;
+        }
+
+        if (args.options.id &&
+          typeof args.options.id !== 'number') {
+          return `Specified id ${args.options.id} is not a number`;
+        }
+
+        if (args.options.entraGroupId && !validation.isValidGuid(args.options.entraGroupId)) {
+          return `${args.options.entraId} is not a valid GUID.`;
+        }
+
+        if (args.options.userName && !validation.isValidUserPrincipalName(args.options.userName)) {
+          return `${args.options.userName} is not a valid userName.`;
+        }
+
+        if (args.options.email && !validation.isValidUserPrincipalName(args.options.email)) {
+          return `${args.options.email} is not a valid email.`;
+        }
+        return true;
       }
     );
   }
 
   #initOptionSets(): void {
-    this.optionSets.push({ options: ['id', 'loginName'] });
+    this.optionSets.push({
+      options: ['id', 'loginName', 'email', 'userName', 'entraGroupId', 'entraGroupName']
+    });
   }
 
   public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
@@ -92,13 +137,64 @@ class SpoUserRemoveCommand extends SpoCommand {
     if (this.verbose) {
       await logger.logToStderr(`Removing user from  subsite ${options.webUrl} ...`);
     }
+    try {
+      let requestUrl: string = `${encodeURI(options.webUrl)}/_api/web/siteusers/`;
+      if (options.id) {
+        requestUrl += `removebyid(${options.id})`;
+      }
+      else if (options.loginName) {
+        requestUrl += `removeByLoginName('${formatting.encodeQueryParameter(options.loginName as string)}')`;
+      }
+      else if (options.email || options.userName) {
+        const user = await this.getUser(options.webUrl, options);
 
-    let requestUrl: string = `${encodeURI(options.webUrl)}/_api/web/siteusers/`;
-    if (options.id) {
-      requestUrl += `removebyid(${options.id})`;
+        if (user?.Title) {
+          if (this.verbose) {
+            await logger.logToStderr(`Removing user ${user.Title} ...`);
+          }
+          requestUrl += `removebyid(${user.Id})`;
+        }
+        else {
+          throw new Error(`User not found: ${options.userName}`);
+        }
+      }
+      else if (options.entraGroupId || options.entraGroupName) {
+        const entraGroup = await this.getEntraGroup(options.webUrl, options);
+        if (this.verbose) {
+          await logger.logToStderr(`Removing entra group ${entraGroup.displayName} ...`);
+        }
+        //for entra groups, M365 groups have an associated email and security groups don't
+        if (entraGroup.mail) {
+          //M365 group is prefixed with c:0o.c|federateddirectoryclaimprovider
+          requestUrl += `removeByLoginName('c:0o.c|federateddirectoryclaimprovider|${entraGroup.id}')`;
+        }
+        else {
+          //security group is prefixed with c:0t.c|tenant
+          requestUrl += `removeByLoginName('c:0t.c|tenant|${entraGroup.id}')`;
+        }
+      }
+
+      const requestOptions: CliRequestOptions = {
+        url: requestUrl,
+        headers: {
+          accept: 'application/json;odata=nometadata'
+        },
+        responseType: 'json'
+      };
+      await request.post(requestOptions);
     }
-    else if (options.loginName) {
-      requestUrl += `removeByLoginName('${formatting.encodeQueryParameter(options.loginName as string)}')`;
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+
+  private async getUser(webUrl: string, options: GlobalOptions): Promise<any> {
+    let requestUrl: string = '';
+    if (options.email) {
+      requestUrl = `${options.webUrl}/_api/web/siteusers/GetByEmail('${formatting.encodeQueryParameter(options.email)}')`;
+    }
+    else if (options.userName) {
+      requestUrl = `${options.webUrl}/_api/web/siteusers?$filter=UserPrincipalName eq ('${formatting.encodeQueryParameter(options.userName)}')`;
     }
 
     const requestOptions: CliRequestOptions = {
@@ -109,12 +205,26 @@ class SpoUserRemoveCommand extends SpoCommand {
       responseType: 'json'
     };
 
-    try {
-      await request.post(requestOptions);
+    const userInstance = await request.get(requestOptions);
+    if (options.email) {
+      return userInstance;
     }
-    catch (err: any) {
-      this.handleRejectedODataJsonPromise(err);
+    else if (options.userName) {
+      return (userInstance as { value: any[] }).value[0];
     }
+  }
+
+  private async getEntraGroup(webUrl: string, options: GlobalOptions): Promise<any> {
+    let group: Group;
+    if (options.entraGroupId) {
+      group = await aadGroup.getGroupById(options.entraGroupId);
+      return group;
+    }
+    else if (options.entraGroupName) {
+      group = await aadGroup.getGroupByDisplayName(options.entraGroupName);
+      return group;
+    }
+
   }
 }
 
