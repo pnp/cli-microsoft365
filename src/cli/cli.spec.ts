b80fddcc48df6aadfc0a134aadedee48b4a45a14
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2024-02-14 23:27:06 +0100

    Enhances cli warning when non-existing command is called

--- src/cli/cli.spec.ts
+++ src/cli/cli.spec.ts
@@ -343,6 +343,18 @@ describe('cli', () => {
       }, e => done(e));
   });
 
+  it('shows message that command cannot be found when an unknown command is entered', async () => {
+    const commandName = 'unknown';
+    await cli.execute([commandName]);
+    assert(cliErrorStub.calledWith(`Command '${commandName}' not found. Please find a list of available commands below.`));
+  });
+
+  it('shows message that command cannot be found when a uncompleted command is entered', async () => {
+    const commandName = 'spo site';
+    await cli.execute([commandName]);
+    assert(cliErrorStub.calledWith(`Command '${commandName}' not found. Please find a list of available commands below.`));
+  });
+
   it('exits with 0 code when no command specified', (done) => {
     cli
       .execute([])
