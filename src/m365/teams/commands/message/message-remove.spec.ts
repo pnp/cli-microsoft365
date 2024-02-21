f80cd948679d50e9c2536ed7542862c28890a59f
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2024-02-21 12:02:37 +0100

    Adds command 'teams message remove'. Closes #5859

---
+++ src/m365/teams/commands/message/message-remove.spec.ts
@@ -0,0 +1,184 @@
+import assert from 'assert';
+import sinon from 'sinon';
+import auth from '../../../../Auth.js';
+import { cli } from '../../../../cli/cli.js';
+import { CommandInfo } from '../../../../cli/CommandInfo.js';
+import { Logger } from '../../../../cli/Logger.js';
+import { CommandError } from '../../../../Command.js';
+import request from '../../../../request.js';
+import { telemetry } from '../../../../telemetry.js';
+import { pid } from '../../../../utils/pid.js';
+import { session } from '../../../../utils/session.js';
+import { sinonUtil } from '../../../../utils/sinonUtil.js';
+import commands from '../../commands.js';
+import command from './message-remove.js';
+import { accessToken } from '../../../../utils/accessToken.js';
+import { formatting } from '../../../../utils/formatting.js';
+import { teams } from '../../../../utils/teams.js';
+
+describe(commands.MESSAGE_REMOVE, () => {
+  const channelId = '19:f3dcbb1674574677abcae89cb626f1e6@thread.skype';
+  const channelName = 'channelName';
+  const teamId = 'd66b8110-fcad-49e8-8159-0d488ddb7656';
+  const teamName = 'Team Name';
+  const messageId = '157836366';
+
+  let log: string[];
+  let logger: Logger;
+  let promptIssued: boolean = false;
+  let commandInfo: CommandInfo;
+
+  before(() => {
+    sinon.stub(auth, 'restoreAuth').resolves();
+    sinon.stub(telemetry, 'trackEvent').returns();
+    sinon.stub(pid, 'getProcessName').returns('');
+    sinon.stub(session, 'getId').returns('');
+    sinon.stub(accessToken, 'assertDelegatedAccessToken').resolves();
+    sinon.stub(teams, 'getTeamIdByDisplayName').resolves(teamId);
+    sinon.stub(teams, 'getChannelIdByDisplayName').resolves(channelId);
+    auth.connection.active = true;
+    commandInfo = cli.getCommandInfo(command);
+  });
+
+  beforeEach(() => {
+    log = [];
+    logger = {
+      log: async (msg: string) => {
+        log.push(msg);
+      },
+      logRaw: async (msg: string) => {
+        log.push(msg);
+      },
+      logToStderr: async (msg: string) => {
+        log.push(msg);
+      }
+    };
+    sinon.stub(cli, 'promptForConfirmation').callsFake(async () => {
+      promptIssued = true;
+      return false;
+    });
+
+    promptIssued = false;
+  });
+
+  afterEach(() => {
+    sinonUtil.restore([
+      request.post,
+      cli.promptForConfirmation
+    ]);
+  });
+
+  after(() => {
+    sinon.restore();
+    auth.connection.active = false;
+  });
+
+  it('has correct name', () => {
+    assert.strictEqual(command.name, commands.MESSAGE_REMOVE);
+  });
+
+  it('has a description', () => {
+    assert.notStrictEqual(command.description, null);
+  });
+
+  it('fails validation if the teamId is not a valid guid', async () => {
+    const actual = await command.validate({ options: { teamId: 'invalid', channelId: channelId, id: messageId } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('fails validation if the channel id is not a valid channel id', async () => {
+    const actual = await command.validate({ options: { teamId: teamId, channelId: 'invalid', id: messageId } }, commandInfo);
+    assert.notStrictEqual(actual, true);
+  });
+
+  it('passes validation if the channel id and team id both are valid', async () => {
+    const actual = await command.validate({ options: { teamId: teamId, channelId: channelId, id: messageId } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('passes validation if the channel name and team name are set', async () => {
+    const actual = await command.validate({ options: { teamName: teamName, channelName: channelName, id: messageId } }, commandInfo);
+    assert.strictEqual(actual, true);
+  });
+
+  it('prompts before removing the specified message when force option not passed', async () => {
+    await command.action(logger, { options: { id: messageId, teamId: teamId, channelId: channelId } });
+    assert(promptIssued);
+  });
+
+  it('aborts removing the specified message when force option not passed and prompt not confirmed', async () => {
+    const postStub = sinon.stub(request, 'post').resolves();
+    await command.action(logger, { options: { id: messageId, teamId: teamId, channelId: channelId } });
+    assert(postStub.notCalled);
+  });
+
+  it('removes the specified message when teamId, channelId and force option passed', async () => {
+    const postStub = sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams/${teamId}/channels/${formatting.encodeQueryParameter(channelId)}/messages/${messageId}/softDelete`) {
+        return;
+      }
+
+      throw 'Invalid request';
+    });
+
+    await command.action(logger, { options: { id: messageId, teamId: teamId, channelId: channelId, force: true, verbose: true } });
+    assert(postStub.calledOnce);
+  });
+
+  it('removes the specified message when teamName, channelName is used and prompt is confirmed', async () => {
+    const postStub = sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams/${teamId}/channels/${formatting.encodeQueryParameter(channelId)}/messages/${messageId}/softDelete`) {
+        return;
+      }
+
+      throw 'Invalid request';
+    });
+
+    sinonUtil.restore(cli.promptForConfirmation);
+    sinon.stub(cli, 'promptForConfirmation').resolves(true);
+
+    await command.action(logger, { options: { id: messageId, teamName: teamName, channelName: channelName, verbose: true } });
+    assert(postStub.calledOnce);
+  });
+
+  it('throws error when the message we are trying to delete is not found', async () => {
+    const error = {
+      error: {
+        error: {
+          code: 'NotFound',
+          message: 'NotFound',
+          innerError: {
+            code: '1',
+            message: `MessageNotFound-Message does not exist in the thread: ColdStoreNotSupportedForMessageException:ColdStoreMessageOperations is not supported if cutOffColdStoreEpoch is not provided. (msgVersion:${messageId}, cutoff:1707944123071)`,
+            date: '2024-02-21T20:55:23',
+            'request-id': 'fe227b45-0b96-47c2-bac4-3d5e17dfc70d',
+            'client-request-id': 'fe227b45-0b96-47c2-bac4-3d5e17dfc70d'
+          }
+        }
+      }
+    };
+    sinon.stub(request, 'post').rejects(error);
+
+    await assert.rejects(command.action(logger, { options: { id: messageId, teamName: teamName, channelName: channelName, force: true, verbose: true } }),
+      new CommandError('The message was not found in the Teams channel.'));
+  });
+
+  it('correctly handles generic error when removing message', async () => {
+    const error = {
+      "error": {
+        "code": "UnknownError",
+        "message": "An error has occurred",
+        "innerError": {
+          "date": "2022-02-14T13:27:37",
+          "request-id": "77e0ed26-8b57-48d6-a502-aca6211d6e7c",
+          "client-request-id": "77e0ed26-8b57-48d6-a502-aca6211d6e7c"
+        }
+      }
+    };
+
+    sinon.stub(request, 'post').rejects(error);
+
+    await assert.rejects(command.action(logger, { options: { id: messageId, channelId: channelId, teamName: teamName, force: true, verbose: true } } as any),
+      new CommandError('An error has occurred'));
+  });
+});
---
+++ src/utils/teams.spec.ts
@@ -0,0 +1,116 @@
+import sinon from 'sinon';
+import request from '../request.js';
+import { sinonUtil } from './sinonUtil.js';
+import { formatting } from './formatting.js';
+import { teams } from './teams.js';
+import assert from 'assert';
+import { cli } from '../cli/cli.js';
+import { settingsNames } from '../settingsNames.js';
+
+const teamName = 'HR Team';
+const teamId = '0b0b204f-7ca0-4c7f-baf2-53caa381828b';
+const teamResponse = {
+  id: teamId,
+  displayName: teamName
+};
+const channelName = 'General';
+const channelId = '19:7a3a82caa8f8436889fbb017acdb11b6@thread.tacv2';
+const channelResponse = { id: channelId };
+
+describe('utils/teams', () => {
+  before(() => {
+    sinon.stub(cli, 'getSettingWithDefaultValue').callsFake((settingName, defaultValue) => settingName === settingsNames.prompt ? false : defaultValue);
+  });
+
+  afterEach(() => {
+    sinonUtil.restore([
+      request.get,
+      teams.getTeamIdByDisplayName,
+      teams.getChannelIdByDisplayName
+    ]);
+  });
+
+  after(() => {
+    sinon.restore();
+  });
+
+  it('correctly gets team id by display name', async () => {
+    sinon.stub(request, 'get').callsFake(async opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams?$filter=displayName eq '${formatting.encodeQueryParameter(teamName)}'&$select=id`) {
+        return { value: [teamResponse] };
+      }
+
+      throw 'Invalid Request';
+    });
+
+    const actual = await teams.getTeamIdByDisplayName(teamName);
+    assert.strictEqual(actual, teamId);
+  });
+
+  it('throws error if no team is found when retrieving teamId by display name', async () => {
+    sinon.stub(request, 'get').callsFake(async opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams?$filter=displayName eq '${formatting.encodeQueryParameter(teamName)}'&$select=id`) {
+        return { value: [] };
+      }
+
+      throw 'Invalid Request';
+    });
+
+    await assert.rejects(teams.getTeamIdByDisplayName(teamName),
+      new Error(`The specified team '${teamName}' does not exist.`));
+  });
+
+  it('throws error message when multiple teams were found using getTeamIdByDisplayName', async () => {
+    sinon.stub(request, 'get').callsFake(async opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams?$filter=displayName eq '${formatting.encodeQueryParameter(teamName)}'&$select=id`) {
+        return { value: [teamResponse, { id: 'df20c966-aa55-4810-a086-7e20001e0788', displayName: teamName }] };
+      }
+
+      throw 'Invalid Request';
+    });
+
+    await assert.rejects(teams.getTeamIdByDisplayName(teamName),
+      new Error(`Multiple teams with name '${teamName}' found. Found: ${teamId}, df20c966-aa55-4810-a086-7e20001e0788.`));
+  });
+
+  it('handles selecting single result when multiple teams with the specified name found using getTeamIdByDisplayName and cli is set to prompt', async () => {
+    sinon.stub(request, 'get').callsFake(async opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams?$filter=displayName eq '${formatting.encodeQueryParameter(teamName)}'&$select=id`) {
+        return { value: [teamResponse, { id: 'df20c966-aa55-4810-a086-7e20001e0788', displayName: teamName }] };
+      }
+
+      throw 'Invalid Request';
+    });
+
+    sinon.stub(cli, 'handleMultipleResultsFound').resolves({ id: teamId });
+
+    const actual = await teams.getTeamIdByDisplayName(teamName);
+    assert.deepStrictEqual(actual, teamId);
+  });
+
+  it('correctly retrieves channel id by displayName', async () => {
+    sinon.stub(request, 'get').callsFake(async opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams/${teamId}/channels?$filter=displayName eq '${formatting.encodeQueryParameter(channelName)}'&$select=id`) {
+        return { value: [channelResponse] };
+      }
+
+      throw 'Invalid Request';
+    });
+
+    const actual = await teams.getChannelIdByDisplayName(teamId, channelName);
+    assert.strictEqual(actual, channelId);
+  });
+
+  it('throws error if no channel with the specified name is found', async () => {
+    sinon.stub(request, 'get').callsFake(async opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams/${teamId}/channels?$filter=displayName eq '${formatting.encodeQueryParameter(channelName)}'&$select=id`) {
+        return { value: [] };
+      }
+
+      throw 'Invalid Request';
+    });
+
+    await assert.rejects(teams.getChannelIdByDisplayName(teamId, channelName),
+      new Error(`The channel '${channelName}' does not exist in the Microsoft Teams team with ID '${teamId}'.`));
+  });
+}); 
