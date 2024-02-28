fbd1751b17f32afcd37209955a549ac175f1b4a7
Martin Lingstuyl <mlingstuyl@live.com>
2024-02-28 16:50:16 +0100

    Fix rebase issue

--- src/m365/purview/commands/threatassessment/threatassessment-add.spec.ts
+++ src/m365/purview/commands/threatassessment/threatassessment-add.spec.ts
@@ -27,8 +27,8 @@ describe(commands.THREATASSESSMENT_ADD, () => {
     sinon.stub(telemetry, 'trackEvent').returns();
     sinon.stub(pid, 'getProcessName').returns('');
     sinon.stub(session, 'getId').returns('');
-    auth.service.connected = true;
-    auth.service.accessTokens[(command as any).resource] = {
+    auth.connection.active = true;
+    auth.connection.accessTokens[(command as any).resource] = {
       accessToken: 'abc',
       expiresOn: new Date()
     };
@@ -66,8 +66,8 @@ describe(commands.THREATASSESSMENT_ADD, () => {
 
   after(() => {
     sinon.restore();
-    auth.service.connected = false;
-    auth.service.accessTokens = {};
+    auth.connection.active = false;
+    auth.connection.accessTokens = {};
   });
 
   it('has correct name', () => {
