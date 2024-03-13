0e16f2aff015b43becca64192537a6d4b208046b
mkm17 <kornetmichal@gmail.com>
2024-03-13 22:48:20 +0100

    Adds command 'spo site admin list'. Closes #5882

--- docs/src/config/sidebars.ts
+++ docs/src/config/sidebars.ts
@@ -3288,6 +3288,11 @@ const sidebars: SidebarsConfig = {
             },
             {
               type: 'doc',
+              label: 'site admin list',
+              id: 'cmd/spo/site/site-admin-list'
+            },
+            {
+              type: 'doc',
               label: 'site ensure',
               id: 'cmd/spo/site/site-ensure'
             },
