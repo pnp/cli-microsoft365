6918d9846064776481ce9ae3f1e71f2de776b62f
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2024-03-14 13:48:43 +0100

    Renames environment variables to 'entra'. Closes #5917

--- src/config.spec.ts
+++ src/config.spec.ts
@@ -12,7 +12,14 @@ describe('Config', () => {
     process.env.CLIMICROSOFT365_AADAPPID = 'appId123';
 
     const config = await import(`./config.js#${Math.random()}`);
-    assert.strictEqual(config.default.cliAadAppId, 'appId123');
+    assert.strictEqual(config.default.cliEntraAppId, 'appId123');
+  });
+
+  it('returns process.env CLIMICROSOFT365_ENTRAAPPID value', async () => {
+    process.env.CLIMICROSOFT365_ENTRAAPPID = 'appId123';
+
+    const config = await import(`./config.js#${Math.random()}`);
+    assert.strictEqual(config.default.cliEntraAppId, 'appId123');
   });
 
   it('returns default value since env CLIMICROSOFT365_TENANT not present', async () => {
@@ -22,10 +29,11 @@ describe('Config', () => {
     assert.strictEqual(config.default.tenant, 'common');
   });
 
-  it('returns default value since env CLIMICROSOFT365_AADAPPID not present', async () => {
+  it('returns default value since env CLIMICROSOFT365_AADAPPID or CLIMICROSOFT365_ENTRAAPPID not present', async () => {
     delete process.env.CLIMICROSOFT365_AADAPPID;
+    delete process.env.CLIMICROSOFT365_ENTRAAPPID;
 
     const config = await import(`./config.js#${Math.random()}`);
-    assert.strictEqual(config.default.cliAadAppId, '31359c7f-bd7e-475c-86db-fdb8c937548e');
+    assert.strictEqual(config.default.cliEntraAppId, '31359c7f-bd7e-475c-86db-fdb8c937548e');
   });
 });
