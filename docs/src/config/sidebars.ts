f749d47e1a93d7e12f2c07df47b61f8d7eedb9bd
Saurabh <saurabh.tripathi@epicalgroup.com>
2024-03-14 12:20:21 +0100

    New command: viva engage community get. Closes #5754
    Add 'community' to command name word breaking list

--- docs/src/config/sidebars.ts
+++ docs/src/config/sidebars.ts
@@ -4235,6 +4235,11 @@ const sidebars: SidebarsConfig = {
             },
             {
               type: 'doc',
+              label: 'engage community get',
+              id: 'cmd/viva/engage/engage-community-get'
+            },
+            {
+              type: 'doc',
               label: 'engage group list',
               id: 'cmd/viva/engage/engage-group-list'
             },
