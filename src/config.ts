4c7e2fa797628f64edcc3f94351a9b55df7f0a4e
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2024-03-16 21:27:31 +0100

    Change requested by Milan

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ const cliEntraAppId: string = '31359c7f-bd7e-475c-86db-fdb8c937548e';
 export default {
   applicationName: `CLI for Microsoft 365 v${app.packageJson().version}`,
   delimiter: 'm365\$',
-  cliEntraAppId: process.env.CLIMICROSOFT365_AADAPPID || process.env.CLIMICROSOFT365_ENTRAAPPID || cliEntraAppId,
+  cliEntraAppId: process.env.CLIMICROSOFT365_ENTRAAPPID || process.env.CLIMICROSOFT365_AADAPPID || cliEntraAppId,
   tenant: process.env.CLIMICROSOFT365_TENANT || 'common',
   configstoreName: 'cli-m365-config'
 };
