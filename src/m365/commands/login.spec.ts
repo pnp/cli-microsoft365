3c25c895da62995381fc661139fc427981ec6f02
Nico De Cleyre <nico.de.cleyre@gmail.com>
2024-03-24 16:16:06 +0100

    change auth.service to auth.connection

--- src/m365/commands/login.spec.ts
+++ src/m365/commands/login.spec.ts
@@ -17,6 +17,7 @@ describe(commands.LOGIN, () => {
   let log: string[];
   let logger: Logger;
   let commandInfo: CommandInfo;
+  let deactivateStub: any;
 
   before(() => {
     sinon.stub(auth, 'restoreAuth').callsFake(() => Promise.resolve());
@@ -45,7 +46,7 @@ describe(commands.LOGIN, () => {
         log.push(msg);
       }
     };
-    sinon.stub(auth.connection, 'deactivate').callsFake(() => { });
+    deactivateStub = sinon.stub(auth.connection, 'deactivate').callsFake(() => { });
     sinon.stub(auth, 'ensureAccessToken').callsFake(() => {
       auth.connection.name = '028de82d-7fd9-476e-a9fd-be9714280ff3';
       auth.connection.identityName = 'alexw@contoso.com';
@@ -299,32 +300,33 @@ describe(commands.LOGIN, () => {
   });
 
   it(`Don't start login flow when the CLI is signed in`, async () => {
-    sinon.stub(auth, 'ensureAccessToken').callsFake(() => Promise.resolve(''));
-
     await command.action(logger, { options: { ensure: true } });
-    assert.strictEqual(auth.service.connected, true);
+    await command.action(logger, { options: { ensure: true } });
+
+    assert(deactivateStub.callCount <= 1);
   });
 
   it(`Don't start login flow if the CLI is signed in as a user`, async () => {
-    sinon.stub(auth, 'ensureAccessToken').callsFake(() => Promise.resolve(''));
-
     await command.action(logger, { options: { ensure: true, authType: 'password', userName: 'john.doe@contoso.com', password: 'password' } });
-    assert.strictEqual(auth.service.connected, true);
+    await command.action(logger, { options: { ensure: true, authType: 'password', userName: 'john.doe@contoso.com', password: 'password' } });
+
+    assert(deactivateStub.callCount <= 1);
   });
 
   it(`Don't start login flow, if the CLI is signed in using a certificate`, async () => {
-    sinon.stub(auth, 'ensureAccessToken').callsFake(() => Promise.resolve(''));
     sinon.stub(fs, 'readFileSync').callsFake(() => 'certificate');
 
     await command.action(logger, { options: { ensure: true, authType: 'certificate ', certificateFile: 'certificate' } });
-    assert.strictEqual(auth.service.connected, true);
+    await command.action(logger, { options: { ensure: true, authType: 'certificate ', certificateFile: 'certificate' } });
+
+    assert(deactivateStub.callCount <= 1);
   });
 
   it(`Don't start login flow if the CLI is signed in using the specified app and to the specified tenant`, async () => {
-    sinon.stub(auth, 'ensureAccessToken').callsFake(() => Promise.resolve(''));
-
     await command.action(logger, { options: { ensure: true, appId: '1cf21ca6-c8f0-4a21-839d-68a09d3a0f55', tenant: '973fce64-6409-4843-9328-c2cef0427f4e' } });
-    assert.strictEqual(auth.service.connected, true);
+    await command.action(logger, { options: { ensure: true, appId: '1cf21ca6-c8f0-4a21-839d-68a09d3a0f55', tenant: '973fce64-6409-4843-9328-c2cef0427f4e' } });
+
+    assert(deactivateStub.callCount <= 1);
   });
 
   it('correctly handles error when clearing persisted auth information', async () => {
