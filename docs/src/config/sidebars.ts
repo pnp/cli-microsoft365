83c25ff95af4a6906ee1eb82928a9502f090dc40
Nanddeep Nachan <nanddeepnachan@gmail.com>
2024-04-09 11:07:10 +0000

    Adds command 'teams meeting transcript get'. Closes #3908

--- docs/src/config/sidebars.ts
+++ docs/src/config/sidebars.ts
@@ -4144,6 +4144,11 @@ const sidebars: SidebarsConfig = {
             },
             {
               type: 'doc',
+              label: 'meeting transcript get',
+              id: 'cmd/teams/meeting/meeting-transcript-get'
+            },
+            {
+              type: 'doc',
               label: 'meeting transcript list',
               id: 'cmd/teams/meeting/meeting-transcript-list'
             }
--- src/m365/teams/commands.ts
+++ src/m365/teams/commands.ts
@@ -33,6 +33,7 @@ export default {
   MEETING_LIST: `${prefix} meeting list`,
   MEETING_ATTENDANCEREPORT_GET: `${prefix} meeting attendancereport get`,
   MEETING_ATTENDANCEREPORT_LIST: `${prefix} meeting attendancereport list`,
+  MEETING_TRANSCRIPT_GET: `${prefix} meeting transcript get`,
   MEETING_TRANSCRIPT_LIST: `${prefix} meeting transcript list`,
   MEMBERSETTINGS_LIST: `${prefix} membersettings list`,
   MEMBERSETTINGS_SET: `${prefix} membersettings set`,
