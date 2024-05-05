57511ec5f08e2820abfd03d15f1298c5ac41e174
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2024-05-05 21:39:55 +0200

    Fixes spinner issue on update check

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,21 @@
 import { cli } from './cli/cli.js';
 import { app } from './utils/app.js';
 
-// required to make console.log() in combination with piped output synchronous
-// on Windows/in PowerShell so that the output is not trimmed by calling
-// process.exit() after executing the command, while the output is still
-// being processed; https://github.com/pnp/cli-microsoft365/issues/1266
-if ((process.stdout as any)._handle) {
-  (process.stdout as any)._handle.setBlocking(true);
+async function main(): Promise<void> {
+  // required to make console.log() in combination with piped output synchronous
+  // on Windows/in PowerShell so that the output is not trimmed by calling
+  // process.exit() after executing the command, while the output is still
+  // being processed; https://github.com/pnp/cli-microsoft365/issues/1266
+  if ((process.stdout as any)._handle) {
+    (process.stdout as any)._handle.setBlocking(true);
+  }
+
+  if (!process.env.CLIMICROSOFT365_NOUPDATE) {
+    const updateNotifier = await import('update-notifier');
+    updateNotifier.default({ pkg: app.packageJson() as any, updateCheckInterval: 1 }).notify({ defer: false });
+  }
+
+  await cli.execute(process.argv.slice(2));
 }
 
-if (!process.env.CLIMICROSOFT365_NOUPDATE) {
-  import('update-notifier').then(updateNotifier => {
-    updateNotifier.default({ pkg: app.packageJson() as any }).notify({ defer: false });
-  });
-}
-
-cli.execute(process.argv.slice(2));
+main();
