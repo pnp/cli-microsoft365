653edda880fa9e9c4d3d569babdd330b9f2f37b7
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2024-05-17 15:00:18 +0200

    Enhances 'entra user list' output. Closes #4667

--- src/m365/entra/commands/user/user-list.ts
+++ src/m365/entra/commands/user/user-list.ts
@@ -17,6 +17,8 @@ interface Options extends GlobalOptions {
 }
 
 class EntraUserListCommand extends GraphCommand {
+  private static readonly allowedTypes: string[] = ['Member', 'Guest'];
+
   public get name(): string {
     return commands.USER_LIST;
   }
@@ -33,11 +35,17 @@ class EntraUserListCommand extends GraphCommand {
     return true;
   }
 
+  public defaultProperties(): string[] | undefined {
+    return ['id', 'displayName', 'mail', 'userPrincipalName'];
+  }
+
   constructor() {
     super();
 
     this.#initTelemetry();
     this.#initOptions();
+    this.#initValidators();
+    this.#initTypes();
   }
 
   #initTelemetry(): void {
@@ -52,37 +60,57 @@ class EntraUserListCommand extends GraphCommand {
   #initOptions(): void {
     this.options.unshift(
       {
-        option: "--type [type]",
-        autocomplete: ["Member", "Guest"]
+        option: '--type [type]',
+        autocomplete: EntraUserListCommand.allowedTypes
       },
-      { option: '-p, --properties [properties]' }
+      {
+        option: '-p, --properties [properties]'
+      }
     );
+  }
+
+  #initValidators(): void {
+    this.validators.push(
+      async (args: CommandArgs) => {
+        if (args.options.type && !EntraUserListCommand.allowedTypes.some(t => t.toLowerCase() === args.options.type!.toLowerCase())) {
+          return `'${args.options.type}' is not a valid value for option 'type'. Allowed values are: ${EntraUserListCommand.allowedTypes.join(', ')}.`;
+        }
+
+        return true;
+      }
+    );
+  }
+
+  #initTypes(): void {
+    this.types.string.push('type', 'properties');
   }
 
   public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
     await this.showDeprecationWarning(logger, aadCommands.USER_LIST, commands.USER_LIST);
 
     try {
-      const selectProperties = args.options.properties ? args.options.properties : 'id,displayName,mail,userPrincipalName';
-      const allSelectProperties = selectProperties.split(',');
-      const propertiesWithSlash = allSelectProperties.filter(item => item.includes('/'));
+      let url = `${this.resource}/v1.0/users`;
 
-      const fieldExpand = propertiesWithSlash
-        .map(p => `${p.split('/')[0]}($select=${p.split('/')[1]})`)
-        .join(',');
+      if (args.options.properties) {
+        const selectProperties = args.options.properties;
+        const allSelectProperties = selectProperties.split(',');
+        const propertiesWithSlash = allSelectProperties.filter(item => item.includes('/'));
 
-      const expandParam = fieldExpand.length > 0 ? `&$expand=${fieldExpand}` : '';
-      const selectParam = allSelectProperties.filter(item => !item.includes('/'));
+        const fieldExpand = propertiesWithSlash
+          .map(p => `${p.split('/')[0]}($select=${p.split('/')[1]})`)
+          .join(',');
 
-      let filter: string = '';
-      try {
-        filter = this.getFilter(args.options);
-      }
-      catch (ex: any) {
-        throw ex;
+        const expandParam = fieldExpand.length > 0 ? `&$expand=${fieldExpand}` : '';
+        const selectParam = allSelectProperties.filter(item => !item.includes('/'));
+
+        url += `?$select=${selectParam}${expandParam}`;
       }
 
-      const url = `${this.resource}/v1.0/users?$select=${selectParam}${expandParam}${(filter.length > 0 ? '&' + filter : '')}&$top=100`;
+      const filter = this.getFilter(args.options);
+      if (filter) {
+        url += `${args.options.properties ? '&' : '?'}${filter}`;
+      }
+
       const users = await odata.getAllItems<User>(url);
       await logger.log(users);
     }
@@ -91,41 +119,27 @@ class EntraUserListCommand extends GraphCommand {
     }
   }
 
-  private getFilter(options: Options): string {
-    const filters: any = {};
-    const excludeOptions: string[] = [
-      'type',
-      'properties',
-      'p',
-      'd',
-      'debug',
-      'verbose',
-      'output',
-      'o',
-      'query',
-      '_'
-    ];
+  private getFilter(options: Options): string | null {
+    const filters: string[] = [];
 
-    Object.keys(options).forEach(key => {
-      if (excludeOptions.indexOf(key) === -1) {
-        if (typeof options[key] === 'boolean') {
-          throw `Specify value for the ${key} property`;
-        }
-
-        filters[key] = formatting.encodeQueryParameter(options[key].toString());
+    const unknownOptions = this.getUnknownOptions(options);
+    Object.keys(unknownOptions).forEach(key => {
+      if (typeof options[key] === 'boolean') {
+        throw `Specify value for the ${key} property`;
       }
+
+      filters.push(`startsWith(${key}, '${formatting.encodeQueryParameter(options[key].toString())}')`);
     });
 
-    let filter: string = Object.keys(filters).map(key => `startsWith(${key}, '${filters[key]}')`).join(' and ');
-    if (filter.length > 0) {
-      filter = `$filter=${filter}`;
-    }
-
     if (options.type) {
-      filter += filter.length > 0 ? ` and userType eq '${options.type}'` : `$filter=userType eq '${options.type}'`;
+      filters.push(`userType eq '${options.type}'`);
     }
 
-    return filter;
+    if (filters.length > 0) {
+      return `$filter=${filters.join(' and ')}`;
+    }
+
+    return null;
   }
 }
 
