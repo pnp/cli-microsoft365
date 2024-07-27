21a6dc8e694a072483e93552fde1e937857c07f6
mkm17 <kornetmichal@gmail.com>
2024-07-27 13:03:29 +0200

    Adds new command `spp contentcenter list` Closes #6101

--- docs/src/config/sidebars.ts
+++ docs/src/config/sidebars.ts
@@ -3852,6 +3852,19 @@ const sidebars: SidebarsConfig = {
       ]
     },
     {
+      'SharePoint Premium (spp)': [
+        {
+          contentcenter: [
+            {
+              type: 'doc',
+              label: 'contentcenter list',
+              id: 'cmd/spp/contentcenter/contentcenter-list'
+            }
+          ]
+        }
+      ]
+    },
+    {
       'Teams (teams)': [
         {
           app: [
