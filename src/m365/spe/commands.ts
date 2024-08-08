52f2eaa73e6f48e83bba6054406cbab60ba6710a
reshmee011 <reshmee011@gmail.com>
2024-08-08 11:25:04 +0000

    Adds command 'spe containertype get'. Closes #5991

--- src/m365/spe/commands.ts
+++ src/m365/spe/commands.ts
@@ -4,5 +4,6 @@ export default {
   CONTAINER_GET: `${prefix} container get`,
   CONTAINER_LIST: `${prefix} container list`,
   CONTAINERTYPE_ADD: `${prefix} containertype add`,
+  CONTAINERTYPE_GET: `${prefix} containertype get`,
   CONTAINERTYPE_LIST: `${prefix} containertype list`
 };
