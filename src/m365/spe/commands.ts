7512dea490b9276d722d3bb8a3edf2a10ecfa89c
reshmee011 <reshmee011@gmail.com>
2024-08-08 11:25:04 +0000

    new cmdlet to get container type

--- src/m365/spe/commands.ts
+++ src/m365/spe/commands.ts
@@ -2,5 +2,6 @@ const prefix: string = 'spe';
 
 export default {
   CONTAINERTYPE_ADD: `${prefix} containertype add`,
-  CONTAINERTYPE_LIST: `${prefix} containertype list`
+  CONTAINERTYPE_LIST: `${prefix} containertype list`,
+  CONTAINERTYPE_GET: `${prefix} containertype get`
 };
