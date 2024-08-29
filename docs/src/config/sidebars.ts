a51d625d8f91ffb03565bcf1783739d6f85595bf
Martin Machacek <machacek@edhouse.cz>
2024-08-29 15:51:29 +0200

    Adds command 'viva engage community set'. Closes #6279

--- docs/src/config/sidebars.ts
+++ docs/src/config/sidebars.ts
@@ -4529,6 +4529,11 @@ const sidebars: SidebarsConfig = {
             },
             {
               type: 'doc',
+              label: 'engage community set',
+              id: 'cmd/viva/engage/engage-community-set'
+            },
+            {
+              type: 'doc',
               label: 'engage community user list',
               id: 'cmd/viva/engage/engage-community-user-list'
             },
