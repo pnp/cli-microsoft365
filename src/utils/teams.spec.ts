19762756eb5d682e5914605e1126780b5902bc4f
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2024-09-21 00:13:29 +0200

    Enhances teams util

--- src/utils/teams.spec.ts
+++ src/utils/teams.spec.ts
@@ -24,14 +24,71 @@ describe('utils/teams', () => {
 
   afterEach(() => {
     sinonUtil.restore([
+      cli.handleMultipleResultsFound,
       request.get,
+      teams.getTeamByDisplayName,
       teams.getTeamIdByDisplayName,
+      teams.getChannelByDisplayName,
       teams.getChannelIdByDisplayName
     ]);
   });
 
   after(() => {
     sinon.restore();
+  });
+
+  it('correctly gets team by display name', async () => {
+    sinon.stub(request, 'get').callsFake(async opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams?$filter=displayName eq '${formatting.encodeQueryParameter(teamName)}'`) {
+        return { value: [teamResponse] };
+      }
+
+      throw 'Invalid Request';
+    });
+
+    const actual = await teams.getTeamByDisplayName(teamName);
+    assert.strictEqual(actual, teamResponse);
+  });
+
+  it('throws error if no team is found when retrieving team by display name', async () => {
+    sinon.stub(request, 'get').callsFake(async opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams?$filter=displayName eq '${formatting.encodeQueryParameter(teamName)}'`) {
+        return { value: [] };
+      }
+
+      throw 'Invalid Request';
+    });
+
+    await assert.rejects(teams.getTeamByDisplayName(teamName),
+      new Error(`The specified team '${teamName}' does not exist.`));
+  });
+
+  it('throws error message when multiple teams were found using getTeamByDisplayName', async () => {
+    sinon.stub(request, 'get').callsFake(async opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams?$filter=displayName eq '${formatting.encodeQueryParameter(teamName)}'`) {
+        return { value: [teamResponse, { id: 'df20c966-aa55-4810-a086-7e20001e0788', displayName: teamName }] };
+      }
+
+      throw 'Invalid Request';
+    });
+
+    await assert.rejects(teams.getTeamByDisplayName(teamName),
+      new Error(`Multiple teams with name '${teamName}' found. Found: ${teamId}, df20c966-aa55-4810-a086-7e20001e0788.`));
+  });
+
+  it('handles selecting single result when multiple teams with the specified name found using getTeamByDisplayName and cli is set to prompt', async () => {
+    sinon.stub(request, 'get').callsFake(async opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams?$filter=displayName eq '${formatting.encodeQueryParameter(teamName)}'`) {
+        return { value: [teamResponse, { id: 'df20c966-aa55-4810-a086-7e20001e0788', displayName: teamName }] };
+      }
+
+      throw 'Invalid Request';
+    });
+
+    sinon.stub(cli, 'handleMultipleResultsFound').resolves(teamResponse);
+
+    const actual = await teams.getTeamByDisplayName(teamName);
+    assert.deepStrictEqual(actual, teamResponse);
   });
 
   it('correctly gets team id by display name', async () => {
@@ -46,6 +103,7 @@ describe('utils/teams', () => {
     const actual = await teams.getTeamIdByDisplayName(teamName);
     assert.strictEqual(actual, teamId);
   });
+
 
   it('throws error if no team is found when retrieving teamId by display name', async () => {
     sinon.stub(request, 'get').callsFake(async opts => {
@@ -86,6 +144,32 @@ describe('utils/teams', () => {
 
     const actual = await teams.getTeamIdByDisplayName(teamName);
     assert.deepStrictEqual(actual, teamId);
+  });
+
+  it('correctly retrieves channel by displayName', async () => {
+    sinon.stub(request, 'get').callsFake(async opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams/${teamId}/channels?$filter=displayName eq '${formatting.encodeQueryParameter(channelName)}'`) {
+        return { value: [channelResponse] };
+      }
+
+      throw 'Invalid Request';
+    });
+
+    const actual = await teams.getChannelByDisplayName(teamId, channelName);
+    assert.strictEqual(actual, channelResponse);
+  });
+
+  it('throws error if no channel with the specified name is found by name', async () => {
+    sinon.stub(request, 'get').callsFake(async opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/teams/${teamId}/channels?$filter=displayName eq '${formatting.encodeQueryParameter(channelName)}'`) {
+        return { value: [] };
+      }
+
+      throw 'Invalid Request';
+    });
+
+    await assert.rejects(teams.getChannelByDisplayName(teamId, channelName),
+      new Error(`The channel '${channelName}' does not exist in the Microsoft Teams team with ID '${teamId}'.`));
   });
 
   it('correctly retrieves channel id by displayName', async () => {
--- src/utils/teams.ts
+++ src/utils/teams.ts
@@ -9,6 +9,29 @@ const graphResource = 'https://graph.microsoft.com';
 export const teams = {
 
   /**
+   * Retrieve the team by its name.  
+   * @param displayName Name of the team to retrieve.
+   * @throws Error if the team cannot be found.
+   * @throws Error when multiple teams with the same name and prompting is disabled.
+   * @returns The team.
+   */
+  async getTeamByDisplayName(displayName: string): Promise<Team> {
+    const teams = await odata.getAllItems<Team>(`${graphResource}/v1.0/teams?$filter=displayName eq '${formatting.encodeQueryParameter(displayName)}'`);
+
+    if (!teams.length) {
+      throw Error(`The specified team '${displayName}' does not exist.`);
+    }
+
+    if (teams.length > 1) {
+      const resultAsKeyValuePair = formatting.convertArrayToHashTable('id', teams);
+      const result = await cli.handleMultipleResultsFound<Team>(`Multiple teams with name '${displayName}' found.`, resultAsKeyValuePair);
+      return result;
+    }
+
+    return teams[0];
+  },
+
+  /**
    * Retrieve the id of a team by its name.  
    * @param displayName Name of the team to retrieve.
    * @throws Error if the team cannot be found.
@@ -29,6 +52,33 @@ export const teams = {
     }
 
     return teams[0].id!;
+  },
+
+  /**
+     * Retrieves the channel by its name in a Microsoft Teams team.
+     * @param teamId The ID of the team.
+     * @param name The name of the channel.
+     * @returns The channel.
+     * @throws Throws an error if the specified channel does not exist in the team.
+     */
+  async getChannelByDisplayName(teamId: string, name: string): Promise<Channel> {
+    const channelRequestOptions: CliRequestOptions = {
+      url: `${graphResource}/v1.0/teams/${teamId}/channels?$filter=displayName eq '${formatting.encodeQueryParameter(name)}'`,
+      headers: {
+        accept: 'application/json;odata.metadata=none'
+      },
+      responseType: 'json'
+    };
+
+    const response = await odata.getAllItems<Channel>(channelRequestOptions);
+    // Only one channel can have the same name in a team
+    const channelItem = response[0];
+
+    if (!channelItem) {
+      throw Error(`The channel '${name}' does not exist in the Microsoft Teams team with ID '${teamId}'.`);
+    }
+
+    return channelItem;
   },
 
   /**
