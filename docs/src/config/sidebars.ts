8fde5f409e7073da555d1bf1d1257b700552a18e
Jwaegebaert <38426621+Jwaegebaert@users.noreply.github.com>
2024-09-20 16:21:33 +0200

    Adds command 'viva engage community user list'. Closes #6295

--- docs/src/config/sidebars.ts
+++ docs/src/config/sidebars.ts
@@ -4510,6 +4510,11 @@ const sidebars: SidebarsConfig = {
             },
             {
               type: 'doc',
+              label: 'engage community user list',
+              id: 'cmd/viva/engage/engage-community-user-list'
+            },
+            {
+              type: 'doc',
               label: 'engage group list',
               id: 'cmd/viva/engage/engage-group-list'
             },
