e0cb4868e537d4dd544b63465ad2f86ee9dfd8e3
mkm17 <kornetmichal@gmail.com>
2024-10-06 13:47:06 +0200

    Adds `spp model apply` command. Closes #6119

--- docs/src/config/sidebars.ts
+++ docs/src/config/sidebars.ts
@@ -4168,6 +4168,11 @@ const sidebars: SidebarsConfig = {
           model: [
             {
               type: 'doc',
+              label: 'model apply',
+              id: 'cmd/spp/model/model-apply'
+            },
+            {
+              type: 'doc',
               label: 'model get',
               id: 'cmd/spp/model/model-get'
             },
