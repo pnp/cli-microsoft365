346b5190e7a6d7b1884de62224fd2f2910a7ced6
mkm17 <kornetmichal@gmail.com>
2024-10-30 22:07:21 +0100

    Adds spp autofillcolumn apply command. Closes #6203

--- src/m365/spp/commands.ts
+++ src/m365/spp/commands.ts
@@ -1,6 +1,7 @@
 const prefix: string = 'spp';
 
 export default {
+  AUTOFILLCOLUMN_SET: `${prefix} autofillcolumn set`,
   CONTENTCENTER_LIST: `${prefix} contentcenter list`,
   MODEL_APPLY: `${prefix} model apply`,
   MODEL_GET: `${prefix} model get`,
