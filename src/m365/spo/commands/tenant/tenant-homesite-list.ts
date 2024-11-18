1e20c786f8f8dca95bd812fcddee0320461dd497
reshmee011 <reshmee011@gmail.com>
2024-11-18 18:29:40 +0000

    Adds command 'spo tenant homesite list'. Closes #6492

---
+++ src/m365/spo/commands/tenant/tenant-homesite-list.ts
@@ -0,0 +1,43 @@
+import { Logger } from '../../../../cli/Logger.js';
+import { odata } from '../../../../utils/odata.js';
+import { spo } from '../../../../utils/spo.js';
+import SpoCommand from '../../../base/SpoCommand.js';
+import commands from '../../commands.js';
+import { CliRequestOptions } from "../../../../request.js";
+
+class SpoTenantHomeSiteListCommand extends SpoCommand {
+  public get name(): string {
+    return commands.TENANT_HOMESITE_LIST;
+  }
+
+  public get description(): string {
+    return 'Lists all home sites';
+  }
+
+  public defaultProperties(): string[] | undefined {
+    return ['Url', 'Title'];
+  }
+
+  public async commandAction(logger: Logger): Promise<void> {
+    try {
+      const spoAdminUrl: string = await spo.getSpoAdminUrl(logger, this.verbose);
+      const requestOptions: CliRequestOptions = {
+        url: `${spoAdminUrl}/_api/SPO.Tenant/GetTargetedSitesDetails`,
+        headers: {
+          accept: 'application/json;odata=nometadata'
+        },
+        responseType: 'json'
+      };
+      if (this.verbose) {
+        await logger.logToStderr(`Retrieving all home sites...`);
+      }
+      const res = await odata.getAllItems(requestOptions);
+      await logger.log(res);
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+}
+
+export default new SpoTenantHomeSiteListCommand();
