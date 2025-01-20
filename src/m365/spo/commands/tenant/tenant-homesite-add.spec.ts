4a6dd5b111bf02d7d83b0d644a8076cc3baa213d
Reshmee Auckloo <reshmee011@gmail.com>
2025-01-20 07:31:06 +0000

    Update src/m365/spo/commands/tenant/tenant-homesite-add.spec.ts
    Co-authored-by: Milan Holemans <11723921+milanholemans@users.noreply.github.com>

--- src/m365/spo/commands/tenant/tenant-homesite-add.spec.ts
+++ src/m365/spo/commands/tenant/tenant-homesite-add.spec.ts
@@ -170,6 +170,6 @@ describe(commands.TENANT_HOMESITE_ADD, () => {
   it('correctly handles OData error when adding a home site', async () => {
     sinon.stub(request, 'post').rejects({ error: { 'odata.error': { message: { value: 'An error has occurred' } } } });
 
-    await assert.rejects(command.action(logger, { options: {} } as any), new CommandError('An error has occurred'));
+    await assert.rejects(command.action(logger, { options: { url: 'https://....' } } as any), new CommandError('An error has occurred'));
   });
 });
