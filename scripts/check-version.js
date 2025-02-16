ac40ad68207a0a1871a6973a53a47fd300496377
Jwaegebaert <38426621+Jwaegebaert@users.noreply.github.com>
2025-02-16 23:14:52 +0100

    Switches to Node@22. Closes #6604

--- scripts/check-version.js
+++ scripts/check-version.js
@@ -1,6 +1,6 @@
 const nodeVersion = process.versions.node.split('.')[0];
 
-if (nodeVersion !== "20") {
-  console.error("Node version must be 20");
+if (nodeVersion !== "22") {
+  console.error("Node version must be 22");
   process.exitCode = 1;
 }
