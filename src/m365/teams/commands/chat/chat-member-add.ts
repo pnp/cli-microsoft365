5e6ef4a75e4f34015712b05cc904185dee649f72
Nanddeep Nachan <nanddeepnachan@gmail.com>
2025-03-01 06:06:26 +0000

    Aligns --with and --include options. Closes #6574

--- src/m365/teams/commands/chat/chat-member-add.ts
+++ src/m365/teams/commands/chat/chat-member-add.ts
@@ -17,6 +17,7 @@ interface Options extends GlobalOptions {
   role?: string;
   visibleHistoryStartDateTime?: string;
   includeAllHistory?: boolean;
+  withAllHistory?: boolean;
 }
 
 class TeamsChatMemberAddCommand extends GraphCommand {
@@ -46,7 +47,8 @@ class TeamsChatMemberAddCommand extends GraphCommand {
         userName: typeof args.options.userName !== 'undefined',
         role: typeof args.options.role !== 'undefined',
         visibleHistoryStartDateTime: typeof args.options.visibleHistoryStartDateTime !== 'undefined',
-        includeAllHistory: !!args.options.includeAllHistory
+        includeAllHistory: !!args.options.includeAllHistory,
+        withAllHistory: !!args.options.withAllHistory
       });
     });
   }
@@ -71,6 +73,9 @@ class TeamsChatMemberAddCommand extends GraphCommand {
       },
       {
         option: '--includeAllHistory'
+      },
+      {
+        option: '--withAllHistory'
       }
     );
   }
@@ -107,8 +112,8 @@ class TeamsChatMemberAddCommand extends GraphCommand {
     this.optionSets.push(
       { options: ['userId', 'userName'] },
       {
-        options: ['visibleHistoryStartDateTime', 'includeAllHistory'],
-        runsWhen: (args) => args.options.visibleHistoryStartDateTime || args.options.includeAllHistory
+        options: ['visibleHistoryStartDateTime', 'includeAllHistory', 'withAllHistory'],
+        runsWhen: (args) => args.options.visibleHistoryStartDateTime || args.options.includeAllHistory || args.options.withAllHistory
       });
   }
 
@@ -116,6 +121,10 @@ class TeamsChatMemberAddCommand extends GraphCommand {
     try {
       if (this.verbose) {
         await logger.logToStderr(`Adding member ${args.options.userId || args.options.userName} to chat with id ${args.options.chatId}...`);
+      }
+
+      if (args.options.includeAllHistory) {
+        await this.warn(logger, `Parameter 'includeAllHistory' is deprecated. Please use 'withAllHistory' instead`);
       }
 
       const chatMemberAddOptions: CliRequestOptions = {
@@ -127,7 +136,7 @@ class TeamsChatMemberAddCommand extends GraphCommand {
         data: {
           '@odata.type': '#microsoft.graph.aadUserConversationMember',
           'user@odata.bind': `https://graph.microsoft.com/v1.0/users/${args.options.userId || formatting.encodeQueryParameter(args.options.userName!)}`,
-          visibleHistoryStartDateTime: args.options.includeAllHistory ? '0001-01-01T00:00:00Z' : args.options.visibleHistoryStartDateTime,
+          visibleHistoryStartDateTime: (args.options.includeAllHistory || args.options.withAllHistory) ? '0001-01-01T00:00:00Z' : args.options.visibleHistoryStartDateTime,
           roles: [args.options.role || 'owner']
         }
       };
