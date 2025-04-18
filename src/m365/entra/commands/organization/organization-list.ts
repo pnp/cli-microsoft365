2b0acf2cfc2bc7c3b3f9132769a8bb429a5f4d0d
Martin Machacek <machacek@edhouse.cz>
2025-04-18 17:10:34 +0200

    New command: m365 entra organization list

---
+++ src/m365/entra/commands/organization/organization-list.ts
@@ -0,0 +1,67 @@
+import { z } from 'zod';
+import { globalOptionsZod } from '../../../../Command.js';
+import { zod } from '../../../../utils/zod.js';
+import GraphCommand from '../../../base/GraphCommand.js';
+import { Logger } from '../../../../cli/Logger.js';
+import commands from '../../commands.js';
+import { CliRequestOptions } from '../../../../request.js';
+import { Organization } from '@microsoft/microsoft-graph-types';
+import { odata } from '../../../../utils/odata.js';
+
+const options = globalOptionsZod
+  .extend({
+    properties: zod.alias('p', z.string().optional())
+  })
+  .strict();
+declare type Options = z.infer<typeof options>;
+
+interface CommandArgs {
+  options: Options;
+}
+
+class EntraOrganizationListCommand extends GraphCommand {
+  public get name(): string {
+    return commands.ORGANIZATION_LIST;
+  }
+
+  public get description(): string {
+    return 'Lists all Microsoft Entra ID organizations';
+  }
+
+  public defaultProperties(): string[] | undefined {
+    return ['id', 'displayName', 'tenantType'];
+  }
+
+  public get schema(): z.ZodTypeAny | undefined {
+    return options;
+  }
+
+  public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
+    try {
+      let url = `${this.resource}/v1.0/organization`;
+      if (args.options.properties) {
+        url += `?$select=${args.options.properties}`;
+      }
+      const requestOptions: CliRequestOptions = {
+        url: url,
+        headers: {
+          accept: 'application/json;odata.metadata=none',
+          'content-type': 'application/json'
+        },
+        responseType: 'json'
+      };
+
+      if (args.options.verbose) {
+        await logger.logToStderr(`Retrieving organizations...`);
+      }
+
+      const res = await odata.getAllItems<Organization>(requestOptions);
+      await logger.log(res);
+    }
+    catch (err: any) {
+      this.handleRejectedODataJsonPromise(err);
+    }
+  }
+}
+
+export default new EntraOrganizationListCommand();
