f437a59ad0a13ab3c0c37a1716601b5dc5fe9e7d
Waldek Mastykarz <waldek@mastykarz.nl>
2025-04-25 11:26:34 +0200

    Fixes Login fails with error: Network request failed. Closes #6692

--- src/auth/MsalNetworkClient.spec.ts
+++ src/auth/MsalNetworkClient.spec.ts
@@ -1,4 +1,5 @@
 import assert from 'assert';
+import { AxiosError } from 'axios';
 import sinon from 'sinon';
 import request from '../request.js';
 import { sinonUtil } from '../utils/sinonUtil.js';
@@ -74,5 +75,56 @@ describe('MsalNetworkClient', () => {
     assert.strictEqual(response.status, 200, 'Status code should be 200');
     assert.strictEqual((response.body as any).success, true, 'Response body should contain success: true');
     assert.strictEqual(response.headers['Content-Type'], 'application/json', 'Response should have Content-Type header');
+  });
+
+  it('returns response on error', async () => {
+    const url = 'https://example.com/api/resource';
+    const options = {
+      method: 'POST',
+      headers: {
+        'some-header': 'some-value'
+      },
+      body: JSON.stringify({ key: 'value' })
+    };
+    sinon.stub(request, 'execute').callsFake(async () => {
+      throw new AxiosError('Request failed', 'ERR_BAD_REQUEST', undefined, undefined, {
+        status: 400,
+        statusText: 'Bad Request',
+        headers: {
+          'Content-Type': 'application/json',
+          "expires": -1
+        },
+        config: {} as any,
+        data: JSON.stringify({
+          "error": "authorization_pending",
+          "error_description": "AADSTS70016: OAuth 2.0 device flow error. Authorization is pending. Continue polling. Trace ID: 5ae9c106-cbfd-4d15-9cc1-dad6de7e2d00 Correlation ID: e95d8a70-e1c2-4693-a2f0-5441d290fbd0 Timestamp: 2025-04-25 09:05:06Z",
+          "error_codes": [
+            70016
+          ],
+          "timestamp": "2025-04-25 09:05:06Z",
+          "trace_id": "5ae9c106-cbfd-4d15-9cc1-dad6de7e2d00",
+          "correlation_id": "e95d8a70-e1c2-4693-a2f0-5441d290fbd0",
+          "error_uri": "https://login.microsoftonline.com/error?code=70016"
+        })
+      });
+    });
+    const response = await msalNetworkClient.sendPostRequestAsync(url, options);
+    assert.strictEqual(response.status, 400, 'Status code should be 400');
+  });
+
+  it('returns response on error without response information', async () => {
+    const url = 'https://example.com/api/resource';
+    const options = {
+      method: 'POST',
+      headers: {
+        'some-header': 'some-value'
+      },
+      body: JSON.stringify({ key: 'value' })
+    };
+    sinon.stub(request, 'execute').callsFake(async () => {
+      throw new AxiosError('Request failed', 'ERR_BAD_REQUEST', undefined, undefined, undefined);
+    });
+    const response = await msalNetworkClient.sendPostRequestAsync(url, options);
+    assert.strictEqual(response.status, 400, 'Status code should be 400');
   });
 });
--- src/auth/MsalNetworkClient.ts
+++ src/auth/MsalNetworkClient.ts
@@ -1,5 +1,5 @@
 import type { INetworkModule, NetworkRequestOptions, NetworkResponse } from '@azure/msal-node';
-import type { AxiosResponse } from 'axios';
+import type { AxiosError, AxiosResponse } from 'axios';
 import request, { CliRequestOptions } from '../request.js';
 
 export class MsalNetworkClient implements INetworkModule {
@@ -26,16 +26,33 @@ export class MsalNetworkClient implements INetworkModule {
       fullResponse: true
     };
 
-    const res: AxiosResponse = await request.execute<AxiosResponse>(requestOptions);
-    const headersObj: Record<string, string> = {};
-    for (const [key, value] of Object.entries(res.headers)) {
-      headersObj[key] = typeof value === 'string' ? value : String(value);
-    }
+    try {
+      const res: AxiosResponse = await request.execute<AxiosResponse>(requestOptions);
+      const headersObj: Record<string, string> = {};
+      for (const [key, value] of Object.entries(res.headers)) {
+        headersObj[key] = typeof value === 'string' ? value : String(value);
+      }
 
-    return {
-      headers: headersObj,
-      body: JSON.parse(res.data),
-      status: res.status
-    };
+      return {
+        headers: headersObj,
+        body: JSON.parse(res.data),
+        status: res.status
+      };
+    }
+    catch (ex) {
+      const error = ex as AxiosError;
+      const headersObj: Record<string, string> = {};
+      if (error.response?.headers) {
+        for (const [key, value] of Object.entries(error.response.headers)) {
+          headersObj[key] = typeof value === 'string' ? value : String(value);
+        }
+      }
+
+      return {
+        headers: headersObj,
+        body: JSON.parse(error.response?.data as string ?? '{}') as any,
+        status: error.response?.status ?? 400
+      };
+    }
   }
 }
