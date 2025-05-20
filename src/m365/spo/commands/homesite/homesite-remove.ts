12ddad951f4727a8a8dbcea914baa75059674b8b
Saurabh Tripathi <saurabh7019@gmail.com>
2025-05-20 18:54:20 +0200

    fix: m365 spo homesite remove fails. Closes #6491

--- src/m365/spo/commands/homesite/homesite-remove.ts
+++ src/m365/spo/commands/homesite/homesite-remove.ts
@@ -1,18 +1,30 @@
+import { z } from 'zod';
+import { zod } from '../../../../utils/zod.js';
+import { globalOptionsZod } from '../../../../Command.js';
+import { validation } from '../../../../utils/validation.js';
 import { cli } from '../../../../cli/cli.js';
 import { Logger } from '../../../../cli/Logger.js';
+import { odata } from '../../../../utils/odata.js';
 import config from '../../../../config.js';
-import GlobalOptions from '../../../../GlobalOptions.js';
 import request, { CliRequestOptions } from '../../../../request.js';
 import { ClientSvcResponse, ClientSvcResponseContents, spo } from '../../../../utils/spo.js';
 import SpoCommand from '../../../base/SpoCommand.js';
 import commands from '../../commands.js';
 
+const options = globalOptionsZod
+  .extend({
+    url: zod.alias('u', z.string()
+      .refine((url: string) => validation.isValidSharePointUrl(url) === true, url => ({
+        message: `'${url}' is not a valid SharePoint Online site URL.`
+      }))
+    ).optional(),
+    force: zod.alias('f', z.boolean().optional())
+  })
+  .strict();
+
+declare type Options = z.infer<typeof options>;
 interface CommandArgs {
   options: Options;
-}
-
-interface Options extends GlobalOptions {
-  force?: boolean;
 }
 
 class SpoHomeSiteRemoveCommand extends SpoCommand {
@@ -21,30 +33,11 @@ class SpoHomeSiteRemoveCommand extends SpoCommand {
   }
 
   public get description(): string {
-    return 'Removes the current Home Site';
+    return 'Removes a Home Site';
   }
 
-  constructor() {
-    super();
-
-    this.#initTelemetry();
-    this.#initOptions();
-  }
-
-  #initTelemetry(): void {
-    this.telemetry.push((args: CommandArgs) => {
-      Object.assign(this.telemetryProperties, {
-        force: args.options.force || false
-      });
-    });
-  }
-
-  #initOptions(): void {
-    this.options.unshift(
-      {
-        option: '-f, --force'
-      }
-    );
+  public get schema(): z.ZodTypeAny {
+    return options;
   }
 
   public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
@@ -54,23 +47,41 @@ class SpoHomeSiteRemoveCommand extends SpoCommand {
         const spoAdminUrl = await spo.getSpoAdminUrl(logger, this.debug);
         const reqDigest = await spo.getRequestDigest(spoAdminUrl);
 
-        const requestOptions: CliRequestOptions = {
-          url: `${spoAdminUrl}/_vti_bin/client.svc/ProcessQuery`,
-          headers: {
-            'X-RequestDigest': reqDigest.FormDigestValue
-          },
-          data: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="28" ObjectPathId="27" /><Method Name="RemoveSPHSite" Id="29" ObjectPathId="27" /></Actions><ObjectPaths><Constructor Id="27" TypeId="{268004ae-ef6b-4e9b-8425-127220d84719}" /></ObjectPaths></Request>`
-        };
-
-        const res = await request.post<string>(requestOptions);
-
-        const json: ClientSvcResponse = JSON.parse(res);
-        const response: ClientSvcResponseContents = json[0];
-        if (response.ErrorInfo) {
-          throw response.ErrorInfo.ErrorMessage;
+        if (args.options.url) {
+          await this.removeHomeSiteByUrl(args.options.url, spoAdminUrl, logger);
         }
         else {
-          await logger.log(json[json.length - 1]);
+          await this.showDeprecationWarning(
+            logger,
+            commands.HOMESITE_REMOVE,
+            `${commands.HOMESITE_REMOVE} --url <url>`
+          );
+
+          const homeSites = await this.getHomeSites(spoAdminUrl);
+
+          if (homeSites.length > 1) {
+            await this.removeHomeSiteByUrl(homeSites[0].Url, spoAdminUrl, logger);
+          }
+          else {
+            const requestOptions: CliRequestOptions = {
+              url: `${spoAdminUrl}/_vti_bin/client.svc/ProcessQuery`,
+              headers: {
+                'X-RequestDigest': reqDigest.FormDigestValue
+              },
+              data: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="28" ObjectPathId="27" /><Method Name="RemoveSPHSite" Id="29" ObjectPathId="27" /></Actions><ObjectPaths><Constructor Id="27" TypeId="{268004ae-ef6b-4e9b-8425-127220d84719}" /></ObjectPaths></Request>`
+            };
+
+            const res = await request.post<string>(requestOptions);
+
+            const json: ClientSvcResponse = JSON.parse(res);
+            const response: ClientSvcResponseContents = json[0];
+            if (response.ErrorInfo) {
+              throw response.ErrorInfo.ErrorMessage;
+            }
+            else {
+              await logger.log(json[json.length - 1]);
+            }
+          }
         }
       }
       catch (err: any) {
@@ -83,12 +94,46 @@ class SpoHomeSiteRemoveCommand extends SpoCommand {
       await removeHomeSite();
     }
     else {
-      const result = await cli.promptForConfirmation({ message: `Are you sure you want to remove the Home Site?` });
+      const result = await cli.promptForConfirmation({
+        message: args.options.url
+          ? `Are you sure you want to remove the Home Site at ${args.options.url}?`
+          : `Are you sure you want to remove the primary Home Site?`
+      });
 
       if (result) {
         await removeHomeSite();
       }
     }
+  }
+
+  private async removeHomeSiteByUrl(siteUrl: string, spoAdminUrl: string, logger: Logger): Promise<void> {
+    const siteAdminProperties = await spo.getSiteAdminPropertiesByUrl(siteUrl, false, logger, this.verbose);
+
+    const requestOptions: CliRequestOptions = {
+      url: `${spoAdminUrl}/_api/SPO.Tenant/RemoveTargetedSite`,
+      headers: {
+        'Accept': 'application/json;odata=nometadata'
+      },
+      data: {
+        siteId: siteAdminProperties.SiteId
+      }
+    };
+
+    await request.post(requestOptions);
+
+    await logger.log(`${siteUrl} has been removed as a Home Site. It may take some time for the change to apply. Check aka.ms/homesites for details.`);
+  }
+
+  private async getHomeSites(spoAdminUrl: string): Promise<any[]> {
+    const requestOptions: CliRequestOptions = {
+      url: `${spoAdminUrl}/_api/SPO.Tenant/GetTargetedSitesDetails`,
+      headers: {
+        accept: 'application/json;odata=nometadata'
+      },
+      responseType: 'json'
+    };
+
+    return await odata.getAllItems(requestOptions);
   }
 }
 
