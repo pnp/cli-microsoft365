399de8521757c188080c7aeded2f3378d8ed036c
Martin Machacek <machacek@edhouse.cz>
2025-05-31 20:04:25 +0200

    New command: cli app add

---
+++ src/m365/cli/commands/app/app-add.spec.ts
@@ -0,0 +1,231 @@
+import assert from 'assert';
+import Configstore from 'configstore';
+import sinon from 'sinon';
+import { z } from 'zod';
+import auth from '../../../../Auth.js';
+import cliConfig from '../../../../config.js';
+import { CommandInfo } from '../../../../cli/CommandInfo.js';
+import { Logger } from '../../../../cli/Logger.js';
+import commands from '../../commands.js';
+import { telemetry } from '../../../../telemetry.js';
+import { pid } from '../../../../utils/pid.js';
+import { session } from '../../../../utils/session.js';
+import { cli } from '../../../../cli/cli.js';
+import command from './app-add.js';
+import { sinonUtil } from '../../../../utils/sinonUtil.js';
+import { CommandError } from '../../../../Command.js';
+import { entraApp } from '../../../../utils/entraApp.js';
+import { accessToken } from '../../../../utils/accessToken.js';
+import request from '../../../../request.js';
+
+describe(commands.APP_ADD, () => {
+  let log: any[];
+  let logger: Logger;
+  let commandInfo: CommandInfo;
+  let commandOptionsSchema: z.ZodTypeAny;
+  let config: Configstore;
+  let configSetSpy: sinon.SinonSpy;
+
+  before(() => {
+    sinon.stub(auth, 'restoreAuth').resolves();
+    sinon.stub(telemetry, 'trackEvent').resolves();
+    sinon.stub(pid, 'getProcessName').returns('');
+    sinon.stub(session, 'getId').returns('');
+    auth.connection.active = true;
+    commandInfo = cli.getCommandInfo(command);
+    commandOptionsSchema = commandInfo.command.getSchemaToParse()!;
+    config = cli.getConfig();
+    configSetSpy = sinon.stub(config, 'set').returns();
+  });
+
+  beforeEach(() => {
+    log = [];
+    logger = {
+      log: async (msg: string) => {
+        log.push(msg);
+      },
+      logRaw: async (msg: string) => {
+        log.push(msg);
+      },
+      logToStderr: async (msg: string) => {
+        log.push(msg);
+      }
+    };
+  });
+
+  afterEach(() => {
+    sinonUtil.restore([
+      accessToken.getTenantIdFromAccessToken,
+      entraApp.resolveApis,
+      entraApp.createAppRegistration,
+      entraApp.grantAdminConsent
+    ]);
+    configSetSpy.resetHistory();
+  });
+
+  after(() => {
+    sinonUtil.restore([
+      config.set
+    ]);
+    auth.connection.active = false;
+  });
+
+  it('has correct name', () => {
+    assert.strictEqual(command.name, commands.APP_ADD);
+  });
+
+  it('has a description', () => {
+    assert.notStrictEqual(command.description, null);
+  });
+
+  it('fails validation is scopes is not a valid scope option', () => {
+    const actual = commandOptionsSchema.safeParse({
+      name: 'Custom App',
+      scopes: 'foo'
+    });
+    assert.notStrictEqual(actual.success, true);
+  });
+
+  it('passes validation is scopes is minimal', () => {
+    const actual = commandOptionsSchema.safeParse({
+      name: 'Custom App',
+      scopes: 'minimal'
+    });
+    assert.strictEqual(actual.success, true);
+  });
+
+  it('passes validation is scopes is all', () => {
+    const actual = commandOptionsSchema.safeParse({
+      name: 'Custom App',
+      scopes: 'all'
+    });
+    assert.strictEqual(actual.success, true);
+  });
+
+  it('correctly creates an app registration with all scopes and custom name without saving the app registration info to the CLI config', async () => {
+    sinon.stub(accessToken, 'getTenantIdFromAccessToken').returns('00000000-0000-0000-0000-000000000003');
+    const scopes = [
+      {
+        resourceAppId: '00000000-0000-0000-0000-000000000000',
+        resourceAccess: [
+          {
+            id: '00000000-0000-0000-0000-000000000000',
+            type: 'Minimal'
+          }
+        ]
+      }
+    ];
+    sinon.stub(entraApp, 'resolveApis').resolves(scopes);
+    const createAppRegistrationSpy = sinon.stub(entraApp, 'createAppRegistration').resolves({
+      appId: '00000000-0000-0000-0000-000000000001',
+      id: '00000000-0000-0000-0000-000000000002',
+      tenantId: '00000000-0000-0000-0000-000000000003',
+      requiredResourceAccess: scopes
+    });
+    sinon.stub(entraApp, 'grantAdminConsent').resolves();
+    const parsedSchema = commandOptionsSchema.safeParse({
+      name: 'Custom App',
+      scopes: 'all',
+      verbose: true
+    });
+    await command.action(logger, { options: parsedSchema.data });
+    assert.deepEqual(createAppRegistrationSpy.getCall(0).args[0], {
+      options: {
+        allowPublicClientFlows: true,
+        apisDelegated: cliConfig.allScopes.join(','),
+        implicitFlow: false,
+        multitenant: false,
+        name: 'Custom App',
+        platform: 'publicClient',
+        redirectUris: 'http://localhost,https://localhost,https://login.microsoftonline.com/common/oauth2/nativeclient'
+      },
+      unknownOptions: {},
+      apis: scopes,
+      logger: logger,
+      verbose: true,
+      debug: false
+    });
+  });
+
+  it('correctly creates an app registration with default scopes and default name and saves the app registration info to the CLI config', async () => {
+    sinon.stub(accessToken, 'getTenantIdFromAccessToken').returns('00000000-0000-0000-0000-000000000003');
+    const scopes = [
+      {
+        resourceAppId: '00000000-0000-0000-0000-000000000000',
+        resourceAccess: [
+          {
+            id: '00000000-0000-0000-0000-000000000000',
+            type: 'Minimal'
+          }
+        ]
+      }
+    ];
+    sinon.stub(entraApp, 'resolveApis').resolves(scopes);
+    const createAppRegistrationSpy = sinon.stub(entraApp, 'createAppRegistration').resolves({
+      appId: '00000000-0000-0000-0000-000000000001',
+      id: '00000000-0000-0000-0000-000000000002',
+      tenantId: '00000000-0000-0000-0000-000000000003',
+      requiredResourceAccess: scopes
+    });
+    sinon.stub(entraApp, 'grantAdminConsent').resolves();
+    const parsedSchema = commandOptionsSchema.safeParse({
+      saveToConfig: true,
+      verbose: true
+    });
+    await command.action(logger, { options: parsedSchema.data });
+    const expected = {
+      clientId: '00000000-0000-0000-0000-000000000001',
+      tenantId: '00000000-0000-0000-0000-000000000003'
+    };
+    assert.deepEqual(createAppRegistrationSpy.getCall(0).args[0], {
+      options: {
+        allowPublicClientFlows: true,
+        apisDelegated: cliConfig.minimalScopes.join(','),
+        implicitFlow: false,
+        multitenant: false,
+        name: 'CLI for M365',
+        platform: 'publicClient',
+        redirectUris: 'http://localhost,https://localhost,https://login.microsoftonline.com/common/oauth2/nativeclient'
+      },
+      unknownOptions: {},
+      apis: scopes,
+      logger: logger,
+      verbose: true,
+      debug: false
+    });
+    Object.keys(expected).forEach(setting => {
+      assert(configSetSpy.calledWith(setting, (expected as any)[setting]), `Incorrect setting for ${setting}`);
+    });
+  });
+
+  it('correctly handles API OData error', async () => {
+    sinon.stub(accessToken, 'getTenantIdFromAccessToken').returns('00000000-0000-0000-0000-000000000003');
+    const scopes = [
+      {
+        resourceAppId: '00000000-0000-0000-0000-000000000000',
+        resourceAccess: [
+          {
+            id: '00000000-0000-0000-0000-000000000000',
+            type: 'Minimal'
+          }
+        ]
+      }
+    ];
+    sinon.stub(entraApp, 'resolveApis').resolves(scopes);
+    sinon.stub(request, 'post').rejects({
+      error: {
+        'odata.error': {
+          code: '-1, InvalidOperationException',
+          message: {
+            value: 'Invalid request'
+          }
+        }
+      }
+    });
+    const parsedSchema = commandOptionsSchema.safeParse({
+      saveToConfig: true,
+      verbose: true
+    });
+    await assert.rejects(command.action(logger, { options: parsedSchema.data }), new CommandError('Invalid request'));
+  });
+});
