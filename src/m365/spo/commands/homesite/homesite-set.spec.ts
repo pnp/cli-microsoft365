c33e5975f00313f6d520e7868113d03d986a1e14
Reshmee Auckloo <Reshmee011@gmail.com>
2025-07-27 09:46:46 +0000

    Updates 'spo homesite set' to update existing sites. Closes #6489

--- src/m365/spo/commands/homesite/homesite-set.spec.ts
+++ src/m365/spo/commands/homesite/homesite-set.spec.ts
@@ -12,35 +12,38 @@ import { session } from '../../../../utils/session.js';
 import { sinonUtil } from '../../../../utils/sinonUtil.js';
 import commands from '../../commands.js';
 import command from './homesite-set.js';
+import { z } from 'zod';
+import { entraGroup } from '../../../../utils/entraGroup.js';
 
 describe(commands.HOMESITE_SET, () => {
   let log: any[];
   let logger: Logger;
   let commandInfo: CommandInfo;
-  const siteUrl = `https:\\contoso.sharepoint.com\sites\Work`;
-  const outputDefaultResponse = `The Home site has been set to ${siteUrl}. It may take some time for the change to apply. Check aka.ms/homesites for details.`;
+  let commandOptionsSchema: z.ZodTypeAny;
+  const siteUrl = 'https://contoso.sharepoint.com/sites/Work';
+  const spoAdminUrl = 'https://contoso-admin.sharepoint.com';
+
   const defaultResponse = {
-    "value": outputDefaultResponse
-  };
-
-  const outputVivaConnectionDefaultResponse = `The Home site has been set to ${siteUrl} and the Viva Connections default experience to True. It may take some time for the change to apply. Check aka.ms/homesites for details.`;
-  const vivaConnectionDefaultResponse = {
-    "value": outputVivaConnectionDefaultResponse
-  };
-
-  const outputErrorResponse = `[Error ID: 09149788-0a26-4cee-a333-699b81f629d7] The provided site url can't be set as a Home site. Check aka.ms\homesites for cmdlet requirements.`;
-  const errorResponse = {
-    error: {
-      "odata.error": {
-        "code": "-2147213238, Microsoft.SharePoint.SPException",
-        "message": {
-          "lang": "en-US",
-          "value": outputErrorResponse
-        }
+    "value": [
+      {
+        "Audiences": [
+          {
+            "Email": "work@contoso.onmicrosoft.com",
+            "Id": "7a1eea7f-9ab0-40ff-8f2e-0083d9d63451",
+            "Title": "active Members"
+          }
+        ],
+        "IsInDraftMode": true,
+        "IsVivaBackendSite": false,
+        "SiteId": "431d7819-4aaf-49a1-b664-b2fe9e609b63",
+        "TargetedLicenseType": 2,
+        "Title": "Work",
+        "Url": "https://contoso.sharepoint.com/sites/Work",
+        "VivaConnectionsDefaultStart": true,
+        "WebId": "626c1724-8ac8-45d5-af87-c07c752fab75"
       }
-    }
+    ]
   };
-
   before(() => {
     sinon.stub(auth, 'restoreAuth').resolves();
     sinon.stub(telemetry, 'trackEvent').resolves();
@@ -49,6 +52,7 @@ describe(commands.HOMESITE_SET, () => {
     auth.connection.active = true;
     auth.connection.spoUrl = 'https://contoso.sharepoint.com';
     commandInfo = cli.getCommandInfo(command);
+    commandOptionsSchema = commandInfo.command.getSchemaToParse()!;
   });
 
   beforeEach(() => {
@@ -68,7 +72,8 @@ describe(commands.HOMESITE_SET, () => {
 
   afterEach(() => {
     sinonUtil.restore([
-      request.post
+      request.post,
+      request.get
     ]);
   });
 
@@ -86,62 +91,244 @@ describe(commands.HOMESITE_SET, () => {
     assert.notStrictEqual(command.description, null);
   });
 
-  it('sets the specified site as the Home Site', async () => {
-    const requestBody = { sphSiteUrl: siteUrl };
+  it('correctly sets the option vivaConnectionsDefaultStart', async () => {
     const postRequestStub = sinon.stub(request, 'post').callsFake(async (opts) => {
-      if (opts.url === `https://contoso-admin.sharepoint.com/_api/SPO.Tenant/SetSPHSite`) {
+      if (opts.url === `${spoAdminUrl}/_api/SPO.Tenant/UpdateTargetedSite`) {
         return defaultResponse;
       }
-
       return 'Invalid request';
     });
 
     await command.action(logger, {
       options: {
-        siteUrl: siteUrl,
-        verbose: true
-      }
-    } as any);
-    assert.deepStrictEqual(postRequestStub.lastCall.args[0].data, requestBody);
-  });
-
-  it('sets the specified site as the Home Site and sets the Viva Connections default experience to True', async () => {
-    const requestBody = { sphSiteUrl: siteUrl, configuration: { vivaConnectionsDefaultStart: true } };
-    const postRequestStub = sinon.stub(request, 'post').callsFake(async (opts) => {
-      if (opts.url === `https://contoso-admin.sharepoint.com/_api/SPO.Tenant/SetSPHSiteWithConfiguration`) {
-        return vivaConnectionDefaultResponse;
-      }
-      return 'Invalid request';
-    });
-
-    await command.action(logger, {
-      options: {
-        siteUrl: siteUrl,
+        url: siteUrl,
         vivaConnectionsDefaultStart: true
       }
-    } as any);
-    assert.deepStrictEqual(postRequestStub.lastCall.args[0].data, requestBody);
+    });
+
+    assert.deepStrictEqual(postRequestStub.lastCall.args[0].data, {
+      siteUrl: siteUrl,
+      configurationParam: { IsVivaConnectionsDefaultStartPresent: true, vivaConnectionsDefaultStart: true }
+    });
   });
 
-  it('correctly handles error when setting the Home Site', async () => {
+  it('correctly sets the option draftMode', async () => {
+    const postRequestStub = sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `${spoAdminUrl}/_api/SPO.Tenant/UpdateTargetedSite`) {
+        return defaultResponse;
+      }
+      return 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        url: siteUrl,
+        draftMode: true
+      }
+    });
+
+    assert.deepStrictEqual(postRequestStub.lastCall.args[0].data, {
+      siteUrl: siteUrl,
+      configurationParam: { IsInDraftModePresent: true, isInDraftMode: true }
+    });
+  });
+
+  it('correctly sets the option targetedLicenseType to frontLineWorkers', async () => {
+    const postRequestStub = sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `${spoAdminUrl}/_api/SPO.Tenant/UpdateTargetedSite`) {
+        return defaultResponse;
+      }
+      return 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        url: siteUrl,
+        targetedLicenseType: "frontLineWorkers"
+      }
+    });
+
+    assert.deepStrictEqual(postRequestStub.lastCall.args[0].data, {
+      siteUrl: siteUrl,
+      configurationParam: { IsTargetedLicenseTypePresent: true, TargetedLicenseType: 1 }
+    });
+  });
+
+  it('correctly sets the option targetedLicenseType to informationWorkers', async () => {
+    const postRequestStub = sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `${spoAdminUrl}/_api/SPO.Tenant/UpdateTargetedSite`) {
+        return defaultResponse;
+      }
+      return 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        url: siteUrl,
+        targetedLicenseType: "informationWorkers"
+      }
+    });
+
+    assert.deepStrictEqual(postRequestStub.lastCall.args[0].data, {
+      siteUrl: siteUrl,
+      configurationParam: { IsTargetedLicenseTypePresent: true, TargetedLicenseType: 2 }
+    });
+  });
+
+  it('correctly sets the option audienceNames with multiple audience names', async () => {
+    const entraGroupStub = sinon.stub(entraGroup, 'getGroupIdByDisplayName');
+    entraGroupStub.withArgs('Marketing Team').resolves('00000000-0000-0000-0000-000000000001');
+    entraGroupStub.withArgs('Sales Team').resolves('00000000-0000-0000-0000-000000000002');
+
+    const postRequestStub = sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `${spoAdminUrl}/_api/SPO.Tenant/UpdateTargetedSite`) {
+        return defaultResponse;
+      }
+      return 'Invalid request';
+    });
+
+    try {
+      await command.action(logger, {
+        options: {
+          url: siteUrl,
+          audienceNames: 'Marketing Team, Sales Team'
+        }
+      });
+
+      assert.deepStrictEqual(postRequestStub.lastCall.args[0].data, {
+        siteUrl: siteUrl,
+        configurationParam: { IsAudiencesPresent: true, Audiences: ['00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000002'] }
+      });
+    }
+    finally {
+      entraGroupStub.restore();
+    }
+  });
+
+  it('correctly clears audience names', async () => {
+    const postRequestStub = sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `${spoAdminUrl}/_api/SPO.Tenant/UpdateTargetedSite`) {
+        return defaultResponse;
+      }
+      return 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        url: siteUrl,
+        audienceNames: ''
+      }
+    });
+
+    assert.deepStrictEqual(postRequestStub.lastCall.args[0].data, {
+      siteUrl: siteUrl,
+      configurationParam: { IsAudiencesPresent: true, Audiences: [] }
+    });
+  });
+
+  it('correctly sets multiple configuration options', async () => {
+    const postRequestStub = sinon.stub(request, 'post').callsFake(async (opts) => {
+      if (opts.url === `${spoAdminUrl}/_api/SPO.Tenant/UpdateTargetedSite`) {
+        return defaultResponse;
+      }
+      return 'Invalid request';
+    });
+
+    await command.action(logger, {
+      options: {
+        url: siteUrl,
+        vivaConnectionsDefaultStart: true,
+        draftMode: false,
+        audienceIds: '00000000-0000-0000-0000-000000000001',
+        targetedLicenseType: "everyone",
+        order: 1,
+        verbose: true
+      }
+    });
+
+    assert.deepStrictEqual(postRequestStub.lastCall.args[0].data, {
+      siteUrl: siteUrl,
+      configurationParam: {
+        IsAudiencesPresent: true,
+        IsInDraftModePresent: true,
+        IsVivaConnectionsDefaultStartPresent: true,
+        IsOrderPresent: true,
+        IsTargetedLicenseTypePresent: true,
+        Order: 1,
+        TargetedLicenseType: 0,
+        isInDraftMode: false,
+        vivaConnectionsDefaultStart: true,
+        Audiences: ['00000000-0000-0000-0000-000000000001']
+      }
+    });
+  });
+
+  it('correctly handles errors', async () => {
     sinon.stub(request, 'post').callsFake(async () => {
-      throw errorResponse;
+      throw {
+        error: {
+          "odata.error": {
+            "code": "-2147213238, Microsoft.SharePoint.SPException",
+            "message": {
+              "lang": "en-US",
+              "value": "The provided site url can't be set as a Home site."
+            }
+          }
+        }
+      };
     });
 
     await assert.rejects(command.action(logger, {
       options: {
-        siteUrl: siteUrl
+        url: siteUrl
       }
-    } as any), new CommandError(outputErrorResponse));
+    }), new CommandError("The provided site url can't be set as a Home site."));
   });
 
-  it('fails validation if the siteUrl option is not a valid SharePoint site URL', async () => {
-    const actual = await command.validate({ options: { siteUrl: 'foo' } }, commandInfo);
-    assert.notStrictEqual(actual, true);
+  it('fails validation if the url is not a valid SharePoint url', async () => {
+    const actual = commandOptionsSchema.safeParse({ url: 'invalid', audienceIds: '00000000-0000-0000-0000-000000000001' });
+    assert.strictEqual(actual.success, false);
   });
 
   it('passes validation if the siteUrl option is a valid SharePoint site URL', async () => {
-    const actual = await command.validate({ options: { siteUrl: 'https://contoso.sharepoint.com' } }, commandInfo);
-    assert.strictEqual(actual, true);
+    const actual = commandOptionsSchema.safeParse({ url: 'https://contoso.sharepoint.com', audienceIds: '00000000-0000-0000-0000-000000000001' });
+    assert.strictEqual(actual.success, true);
+  });
+
+  it('fails validation if both audienceIds and audienceNames are specified', async () => {
+    const actual = commandOptionsSchema.safeParse({
+      options: {
+        url: 'https://contoso.sharepoint.com',
+        audienceIds: '00000000-0000-0000-0000-000000000001',
+        audienceNames: 'Marketing Team'
+      }
+    });
+    assert.strictEqual(actual.success, false);
+  });
+
+  it('correctly handles invalid GUIDs in audiences', async () => {
+    const actual = commandOptionsSchema.safeParse({ url: 'https://contoso.sharepoint.com', audienceIds: 'invalid-guid' });
+    assert.strictEqual(actual.success, false);
+  });
+
+  it('passes validation if only audienceIds is specified', async () => {
+    const actual = commandOptionsSchema.safeParse({
+      url: 'https://contoso.sharepoint.com',
+      audienceIds: '00000000-0000-0000-0000-000000000001'
+    });
+    assert.strictEqual(actual.success, true);
+  });
+
+  it('passes validation if only audienceNames is specified', async () => {
+    const actual = commandOptionsSchema.safeParse({
+      url: 'https://contoso.sharepoint.com',
+      audienceNames: 'Marketing Team'
+    });
+    assert.strictEqual(actual.success, true);
+  });
+
+  it('correctly handles non-integer order', async () => {
+    const actual = commandOptionsSchema.safeParse({ url: 'https://contoso.sharepoint.com', order: -1 });
+    assert.strictEqual(actual.success, false);
   });
 });
--- src/m365/spo/commands/homesite/homesite-set.ts
+++ src/m365/spo/commands/homesite/homesite-set.ts
@@ -1,18 +1,39 @@
+import { z } from 'zod';
+import { zod } from '../../../../utils/zod.js';
+import { globalOptionsZod } from '../../../../Command.js';
 import { Logger } from '../../../../cli/Logger.js';
-import GlobalOptions from '../../../../GlobalOptions.js';
 import request, { CliRequestOptions } from '../../../../request.js';
 import { spo } from '../../../../utils/spo.js';
 import { validation } from '../../../../utils/validation.js';
 import SpoCommand from '../../../base/SpoCommand.js';
 import commands from '../../commands.js';
+import { entraGroup } from '../../../../utils/entraGroup.js';
+
+const optionsSchema = globalOptionsZod
+  .extend({
+    url: zod.alias('u', z.string()
+      .refine((url: string) => validation.isValidSharePointUrl(url) === true, url => ({
+        message: `'${url}' is not a valid SharePoint Online site URL.`
+      }))
+    ),
+    vivaConnectionsDefaultStart: z.boolean().optional(),
+    draftMode: z.boolean().optional(),
+    audienceIds: z.string()
+      .refine(audiences => validation.isValidGuidArray(audiences) === true, audiences => ({
+        message: `The following GUIDs are invalid: ${validation.isValidGuidArray(audiences)}.`
+      })).optional(),
+    audienceNames: z.string().optional(),
+    targetedLicenseType: z.enum(['everyone', 'frontLineWorkers', 'informationWorkers']).optional(),
+    order: z.number()
+      .refine(order => validation.isValidPositiveInteger(order) === true, order => ({
+        message: `'${order}' is not a positive integer.`
+      })).optional()
+  });
+
+type Options = z.infer<typeof optionsSchema>;
 
 interface CommandArgs {
   options: Options;
-}
-
-interface Options extends GlobalOptions {
-  siteUrl: string;
-  vivaConnectionsDefaultStart?: boolean;
 }
 
 class SpoHomeSiteSetCommand extends SpoCommand {
@@ -21,82 +42,111 @@ class SpoHomeSiteSetCommand extends SpoCommand {
   }
 
   public get description(): string {
-    return 'Sets the specified site as the Home Site';
+    return 'Updates an existing SharePoint home site.';
   }
 
-  constructor() {
-    super();
-    this.#initTelemetry();
-    this.#initOptions();
-    this.#initValidators();
-    this.#initTypes();
+  public get schema(): z.ZodTypeAny {
+    return optionsSchema;
   }
 
-  #initTelemetry(): void {
-    this.telemetry.push((args: CommandArgs) => {
-      Object.assign(this.telemetryProperties, {
-        vivaConnectionsDefaultStart: typeof args.options.vivaConnectionsDefaultStart !== 'undefined'
-      });
-    });
-  }
 
-  #initOptions(): void {
-    this.options.unshift(
-      {
-        option: '-u, --siteUrl <siteUrl>'
-      },
-      {
-        option: '--vivaConnectionsDefaultStart [vivaConnectionsDefaultStart]',
-        autocomplete: ['true', 'false']
-      }
-    );
-  }
-
-  #initValidators(): void {
-    this.validators.push(
-      async (args: CommandArgs) => validation.isValidSharePointUrl(args.options.siteUrl)
-    );
-  }
-
-  #initTypes(): void {
-    this.types.boolean.push('vivaConnectionsDefaultStart');
+  public getRefinedSchema(schema: z.ZodTypeAny): z.ZodEffects<any> | undefined {
+    return schema
+      .refine(
+        (options: Options) => [options.audienceIds, options.audienceNames].filter(o => o !== undefined).length <= 1,
+        {
+          message: 'You must specify either audienceIds or audienceNames but not both.'
+        }
+      )
+      .refine(
+        (options: Options) =>
+          options.vivaConnectionsDefaultStart !== undefined ||
+          options.draftMode !== undefined ||
+          options.audienceIds !== undefined ||
+          options.audienceNames !== undefined ||
+          options.targetedLicenseType !== undefined ||
+          options.order !== undefined,
+        {
+          message: 'You must specify at least one option to configure.'
+        }
+      );
   }
 
   public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
     try {
       if (this.verbose) {
-        await logger.logToStderr(`Setting the SharePoint home site to: ${args.options.siteUrl}...`);
-        await logger.logToStderr('Attempting to retrieve the SharePoint admin URL.');
+        await logger.logToStderr(`Configuring SharePoint home site: ${args.options.url}...`);
+        await logger.logToStderr(`Attempting to retrieve the SharePoint admin URL.`);
       }
 
       const spoAdminUrl = await spo.getSpoAdminUrl(logger, this.debug);
+
+      const configuration: any = {};
+      if (args.options.vivaConnectionsDefaultStart !== undefined) {
+        configuration.IsVivaConnectionsDefaultStartPresent = true;
+        configuration.vivaConnectionsDefaultStart = args.options.vivaConnectionsDefaultStart;
+      }
+      if (args.options.draftMode !== undefined) {
+        configuration.IsInDraftModePresent = true;
+        configuration.isInDraftMode = args.options.draftMode;
+      }
+      if (args.options.audienceIds !== undefined) {
+        configuration.IsAudiencesPresent = true;
+        configuration.Audiences = args.options.audienceIds.split(',').map(id => id.trim());
+      }
+      if (args.options.audienceNames !== undefined) {
+        configuration.IsAudiencesPresent = true;
+        configuration.Audiences = args.options.audienceNames.trim() === '' ? [] : await this.transformAudienceNamesToIds(args.options.audienceNames);
+      }
+      if (args.options.targetedLicenseType !== undefined) {
+        configuration.IsTargetedLicenseTypePresent = true;
+        configuration.TargetedLicenseType = this.convertTargetedLicenseTypeToNumber(args.options.targetedLicenseType);
+      }
+      if (args.options.order !== undefined) {
+        configuration.IsOrderPresent = true;
+        configuration.Order = args.options.order;
+      }
+
       const requestOptions: CliRequestOptions = {
-        url: `${spoAdminUrl}/_api/SPO.Tenant`,
+        url: `${spoAdminUrl}/_api/SPO.Tenant/UpdateTargetedSite`,
         headers: {
           accept: 'application/json;odata=nometadata',
           'content-Type': 'application/json'
         },
         responseType: 'json',
         data: {
-          sphSiteUrl: args.options.siteUrl
+          siteUrl: args.options.url,
+          configurationParam: configuration
         }
       };
 
-      if (args.options.vivaConnectionsDefaultStart !== undefined) {
-        requestOptions.url += '/SetSPHSiteWithConfiguration';
-        requestOptions.data.configuration = { vivaConnectionsDefaultStart: args.options.vivaConnectionsDefaultStart };
-      }
-      else {
-        requestOptions.url += '/SetSPHSite';
-      }
-
-      const res = await request.post<{ value: string; }>(requestOptions);
-      await logger.log(res.value);
+      const res = await request.post(requestOptions);
+      await logger.log(res);
     }
     catch (err: any) {
       this.handleRejectedODataJsonPromise(err);
     }
   }
-}
 
+  private convertTargetedLicenseTypeToNumber(licenseType: string): number {
+    const licenseTypeMap: Record<string, number> = {
+      'everyone': 0,
+      'frontLineWorkers': 1,
+      'informationWorkers': 2
+    };
+    return licenseTypeMap[licenseType];
+  }
+
+  private async transformAudienceNamesToIds(audienceNames: string): Promise<string[]> {
+    const names = audienceNames.split(',');
+    const ids: string[] = [];
+
+    for (const name of names) {
+      const id = await entraGroup.getGroupIdByDisplayName(name.trim());
+      ids.push(id);
+    }
+
+    return ids;
+  }
+}
 export default new SpoHomeSiteSetCommand();
