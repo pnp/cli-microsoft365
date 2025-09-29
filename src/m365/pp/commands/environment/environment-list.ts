358c596d21874b4e10e1c5a99aabd828a4011834
Reshmee Auckloo <Reshmee011@gmail.com>
2025-09-29 07:55:38 +0000

    convert pp environment list to ZOD

--- src/m365/pp/commands/environment/environment-list.ts
+++ src/m365/pp/commands/environment/environment-list.ts
@@ -1,15 +1,20 @@
 import { Logger } from '../../../../cli/Logger.js';
-import GlobalOptions from '../../../../GlobalOptions.js';
 import request, { CliRequestOptions } from '../../../../request.js';
 import PowerPlatformCommand from '../../../base/PowerPlatformCommand.js';
 import commands from '../../commands.js';
+import { z } from 'zod';
+import { globalOptionsZod } from '../../../../Command.js';
+
+const options = globalOptionsZod
+  .extend({
+    asAdmin: z.boolean().optional()
+  })
+  .strict();
+
+declare type Options = z.infer<typeof options>;
 
 interface CommandArgs {
   options: Options;
-}
-
-interface Options extends GlobalOptions {
-  asAdmin?: boolean;
 }
 
 class PpEnvironmentListCommand extends PowerPlatformCommand {
@@ -21,37 +26,19 @@ class PpEnvironmentListCommand extends PowerPlatformCommand {
     return 'Lists Microsoft Power Platform environments';
   }
 
+  public get schema(): z.ZodTypeAny | undefined {
+    return options;
+  }
+
   public defaultProperties(): string[] | undefined {
     return ['name', 'displayName'];
-  }
-
-  constructor() {
-    super();
-
-    this.#initTelemetry();
-    this.#initOptions();
-  }
-
-  #initTelemetry(): void {
-    this.telemetry.push((args: CommandArgs) => {
-      Object.assign(this.telemetryProperties, {
-        asAdmin: !!args.options.asAdmin
-      });
-    });
-  }
-
-  #initOptions(): void {
-    this.options.unshift(
-      {
-        option: '--asAdmin'
-      }
-    );
   }
 
   public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
     if (this.verbose) {
       await logger.logToStderr(`Retrieving list of Microsoft Power Platform environments...`);
     }
+
     let url: string = '';
     if (args.options.asAdmin) {
       url = `${this.resource}/providers/Microsoft.BusinessAppPlatform/scopes/admin/environments`;
