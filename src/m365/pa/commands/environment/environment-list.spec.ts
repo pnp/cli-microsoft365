d0165ac6992847dba282570e1f294209376c9885
Reshmee Auckloo <reshmee011@gmail.com>
2025-10-01 14:59:04 +0100

    Update src/m365/pa/commands/environment/environment-list.spec.ts
    Co-authored-by: Martin Machacek <machacek@edhouse.cz>

--- src/m365/pa/commands/environment/environment-list.spec.ts
+++ src/m365/pa/commands/environment/environment-list.spec.ts
@@ -78,7 +78,7 @@ describe(commands.ENVIRONMENT_LIST, () => {
   });
 
   it('fails validation with unknown options', () => {
-    const actual = commandOptionsSchema.safeParse({ "option": "value" });
+    const actual = commandOptionsSchema.safeParse({ option: "value" });
     assert.strictEqual(actual.success, false);
   });
 
