3bc8dce113b5ff7bce3e7cece7705053ede888db
Tobias Maestrini <tobias.maestrini@gmail.com>
2025-10-24 01:09:47 +0200

    feat(PpTenantSettingsListCommand)!: Refactor tenant settings list command to include schema validation with Zod

--- src/m365/pp/commands/tenant/tenant-settings-list.ts
+++ src/m365/pp/commands/tenant/tenant-settings-list.ts
@@ -1,7 +1,17 @@
+import { z } from 'zod';
 import { Logger } from '../../../../cli/Logger.js';
+import { globalOptionsZod } from '../../../../Command.js';
 import request, { CliRequestOptions } from '../../../../request.js';
 import PowerPlatformCommand from '../../../base/PowerPlatformCommand.js';
 import commands from '../../commands.js';
+
+const options = globalOptionsZod.strict();
+
+declare type Options = z.infer<typeof options>;
+
+interface CommandArgs {
+  options: Options;
+}
 
 class PpTenantSettingsListCommand extends PowerPlatformCommand {
   public get name(): string {
@@ -12,11 +22,15 @@ class PpTenantSettingsListCommand extends PowerPlatformCommand {
     return 'Lists the global Power Platform tenant settings';
   }
 
+  public get schema(): z.ZodTypeAny | undefined {
+    return options;
+  }
+
   public defaultProperties(): string[] | undefined {
     return ['disableCapacityAllocationByEnvironmentAdmins', 'disableEnvironmentCreationByNonAdminUsers', 'disableNPSCommentsReachout', 'disablePortalsCreationByNonAdminUsers', 'disableSupportTicketsVisibleByAllUsers', 'disableSurveyFeedback', 'disableTrialEnvironmentCreationByNonAdminUsers', 'walkMeOptOut'];
   }
 
-  public async commandAction(logger: Logger): Promise<void> {
+  public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
     const requestOptions: CliRequestOptions = {
       url: `${this.resource}/providers/Microsoft.BusinessAppPlatform/listtenantsettings?api-version=2020-10-01`,
       headers: {
@@ -26,8 +40,12 @@ class PpTenantSettingsListCommand extends PowerPlatformCommand {
     };
 
     try {
-      const res = await request.post<any>(requestOptions);
-      await logger.log(res);
+      if (args) {
+        const res = await request.post<any>(requestOptions);
+        await logger.log(res);
+      } else {
+        throw 'Invalid arguments';
+      }
     }
     catch (err: any) {
       this.handleRejectedODataJsonPromise(err);
