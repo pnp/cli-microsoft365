5a5b2168bbb9ef17665587d7c5f4a67e785b8311
Michał Kornet <kornetmichal@gmail.com>
2026-01-26 09:00:07 +0100

    Adds 'spp autofillcolumn set' command. Closes #6203

--- docs/src/config/sidebars.ts
+++ docs/src/config/sidebars.ts
@@ -4242,6 +4242,15 @@ const sidebars: SidebarsConfig = {
     {
       'SharePoint Premium (spp)': [
         {
+          autofillcolumn: [
+            {
+              type: 'doc',
+              label: 'autofillcolumn set',
+              id: 'cmd/spp/autofillcolumn/autofillcolumn-set'
+            }
+          ]
+        },
+        {
           contentcenter: [
             {
               type: 'doc',
