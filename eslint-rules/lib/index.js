bfbdf89fbc9e9798a9a9e4a31bfffb426386a955
Milan Holemans <11723921+milanholemans@users.noreply.github.com>
2026-03-07 00:40:42 +0100

    Removes cli es-lint from npm package. Closes #7159

--- eslint-rules/lib/index.js
+++ eslint-rules/lib/index.js
@@ -1,5 +1,9 @@
-module.exports.rules = {
-  'correct-command-class-name': require('./rules/correct-command-class-name'),
-  'correct-command-name': require('./rules/correct-command-name'),
-  'no-by-server-relative-url-usage': require('./rules/no-by-server-relative-url-usage')
+import correctCommandClassName from './rules/correct-command-class-name.js';
+import correctCommandName from './rules/correct-command-name.js';
+import noByServerRelativeUrlUsage from './rules/no-by-server-relative-url-usage.js';
+
+export const rules = {
+  'correct-command-class-name': correctCommandClassName,
+  'correct-command-name': correctCommandName,
+  'no-by-server-relative-url-usage': noByServerRelativeUrlUsage
 };
