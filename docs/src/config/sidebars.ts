a4a50978b9094fe9b97574dd6fa34a242f28a04a
Mathijs Verbeeck <verbeeckmathijs@gmail.com>
2026-03-11 06:33:49 +0100

    Adds `spo tenant site get` command. Closes #6654

--- docs/src/config/sidebars.ts
+++ docs/src/config/sidebars.ts
@@ -4057,6 +4057,11 @@ const sidebars: SidebarsConfig = {
               type: 'doc',
               label: 'tenant settings set',
               id: 'cmd/spo/tenant/tenant-settings-set'
+            },
+            {
+              type: 'doc',
+              label: 'tenant site get',
+              id: 'cmd/spo/tenant/tenant-site-get'
             }
           ]
         },
