d079d12f1c2e51b11d9f320b0a91b9035a8abe9e
Martin Machacek <machacek@edhouse.cz>
2026-05-08 20:09:33 +0200

    Adds 'spec container permission set' command. Closes #6160

--- docs/src/config/sidebars.ts
+++ docs/src/config/sidebars.ts
@@ -2223,6 +2223,11 @@ const sidebars: SidebarsConfig = {
             },
             {
               type: 'doc',
+              label: 'container permission set',
+              id: 'cmd/spe/container/container-permission-set'
+            },
+            {
+              type: 'doc',
               label: 'container recyclebinitem list',
               id: 'cmd/spe/container/container-recyclebinitem-list'
             },
