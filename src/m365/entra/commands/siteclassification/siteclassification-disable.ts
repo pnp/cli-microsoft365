df6ed38c0630025517a42e4432045aeb5491fc2e
Waldek Mastykarz <waldek@mastykarz.nl>
2026-06-24 13:50:11 +0200

    Migrates entra siteclassification commands to Zod. Closes #7300

--- src/m365/entra/commands/siteclassification/siteclassification-disable.ts
+++ src/m365/entra/commands/siteclassification/siteclassification-disable.ts
@@ -1,17 +1,20 @@
 import { GroupSetting } from '@microsoft/microsoft-graph-types';
+import { z } from 'zod';
 import { cli } from '../../../../cli/cli.js';
 import { Logger } from '../../../../cli/Logger.js';
-import GlobalOptions from '../../../../GlobalOptions.js';
+import { globalOptionsZod } from '../../../../Command.js';
 import request, { CliRequestOptions } from '../../../../request.js';
 import GraphCommand from '../../../base/GraphCommand.js';
 import commands from '../../commands.js';
 
+export const options = z.strictObject({
+  ...globalOptionsZod.shape,
+  force: z.boolean().optional().alias('f')
+});
+declare type Options = z.infer<typeof options>;
+
 interface CommandArgs {
   options: Options;
-}
-
-interface Options extends GlobalOptions {
-  force?: boolean;
 }
 
 class EntraSiteClassificationDisableCommand extends GraphCommand {
@@ -23,27 +26,8 @@ class EntraSiteClassificationDisableCommand extends GraphCommand {
     return 'Disables site classification';
   }
 
-  constructor() {
-    super();
-
-    this.#initTelemetry();
-    this.#initOptions();
-  }
-
-  #initTelemetry(): void {
-    this.telemetry.push((args: CommandArgs) => {
-      Object.assign(this.telemetryProperties, {
-        force: (!(!args.options.force)).toString()
-      });
-    });
-  }
-
-  #initOptions(): void {
-    this.options.unshift(
-      {
-        option: '-f, --force'
-      }
-    );
+  public get schema(): z.ZodType | undefined {
+    return options;
   }
 
   public async commandAction(logger: Logger, args: CommandArgs): Promise<void> {
