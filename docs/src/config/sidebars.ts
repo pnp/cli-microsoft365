1fc90a19f66f6f3360d73a15dda3e72496a8d912
Antanina Druzhkina <antanina.druzhkina@gmail.com>
2026-07-12 02:19:21 +0400

    New command: m365 pp website webrole list. Closes #6281

--- docs/src/config/sidebars.ts
+++ docs/src/config/sidebars.ts
@@ -2054,6 +2054,11 @@ const sidebars: SidebarsConfig = {
               type: 'doc',
               label: 'website get',
               id: 'cmd/pp/website/website-get'
+            },
+            {
+              type: 'doc',
+              label: 'website webrole list',
+              id: 'cmd/pp/website/website-webrole-list'
             }
           ]
         }
